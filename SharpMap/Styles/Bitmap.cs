c4a8960e1d90b79b8160dc1decb10ae993ad1b8c
time@Tim.GEODAN.NL <time@Tim.GEODAN.NL>
2012-05-23 08:15:58 +0200

    SharpMap.Styles.Bitmap.Data (Stream) is now getting copied to prevent a crash when using an image multiple times

--- SharpMap/Styles/Bitmap.cs
+++ SharpMap/Styles/Bitmap.cs
@@ -1,11 +1,35 @@
-﻿using System;
-using System.Linq;
-using System.IO;
+﻿using System.IO;
 
 namespace SharpMap.Styles
 {
     public class Bitmap
     {
-        public Stream Data { get; set; }
+        private Stream _data { get; set; }
+
+        public Bitmap()
+        {
+            _data = new MemoryStream();
+        }
+
+        public Stream Data
+        {
+            get { return _data; }
+            set
+            {                
+                CopyStream(value, _data);
+                value.Close();
+                _data.Position = 0;
+            }
+        }
+
+        private static void CopyStream(Stream input, Stream output)
+        {
+            var buffer = new byte[4096];
+            int read;
+            while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                output.Write(buffer, 0, read);
+            }
+        }
     }
 }
