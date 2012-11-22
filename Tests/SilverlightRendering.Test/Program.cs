2dd52aa0c52a210a5ed7a31b0517566b8d2fe937
pauldendulk <none@none>
2012-11-23 00:10:21 +0100

    updating Mapsui.sln for file moving

--- Tests/SilverlightRendering.Test/Program.cs
+++ Tests/SilverlightRendering.Test/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
-using SharpMap.Data.Providers;
+using SharpMap.Providers;
 using SharpMap.Geometries;
 using SharpMap;
 using SharpMap.Layers;
@@ -18,19 +18,16 @@ namespace SilverlightRendering.Test
         public static void RenderLayer()
         {
             var provider = new MemoryProvider();
-            
-            var feature = provider.features.New();
+
+            var feature = new Feature();
             feature.Geometry = new Point(50, 50);
-            provider.features.Add(feature);
+            provider.Features.Add(feature);
             
-            var renderer = new SilverlightRenderer();
-            var view = new View() { Center = new Point(50, 50), Width = 100, Height = 100 };
+            var renderer = new MapRenderer();
+            var view = new View{ Center = new Point(50, 50), Width = 100, Height = 100 };
             var map = new Map();
             map.Layers.Add(new Layer("test") { DataSource = provider});
-            renderer.Render(view, map);
-
-
-            
+            renderer.Render(view, map.Layers);
         }
     }
 }
