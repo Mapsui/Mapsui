8e3d657893d6277f773113458f236b4223d8c45e
pauldendulk <none@none>
2013-01-17 10:30:32 +0100

    adding TinyTransformation

---
+++ Mapsui/Projection/TinyTransformation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Mapsui.Projection
+{
+    class TinyTransformation : ITransformation
+    {
+        private int _SRID;
+
+        public int MapSRID
+        {
+            get { return _SRID; }
+            set { _SRID = value; }
+        }
+
+        public Geometries.Geometry Transform(int fromSRID, int toSRID, Geometries.Geometry geometry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Geometries.BoundingBox Transfrom(int fromSRID, int toSRID, Geometries.BoundingBox boundingBox)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
