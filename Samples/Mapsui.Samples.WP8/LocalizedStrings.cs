a1e73ca53b9767d5e18eb53eb0001d034105514a
harmens <none@none>
2013-04-19 17:28:24 +0200

    Added new projects form Windows Phone 8.

---
+++ Samples/Mapsui.Samples.WP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Mapsui.Samples.WP8.Resources;
+
+namespace Mapsui.Samples.WP8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
