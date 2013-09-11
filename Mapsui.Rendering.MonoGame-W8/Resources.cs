bff0e520fd29adcac2e0f16ea68b1deec0a2f590
pauldendulk <none@none>
2013-09-11 23:43:36 +0200

    adding resources.cs

---
+++ Mapsui.Rendering.MonoGame-W8/Resources.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.IO;
+namespace Mapsui.Rendering.MonoGame
+{
+    public class Resources
+    {
+        private IDictionary<string, MemoryStream> Bitmaps { get; set; }
+    }
+}
