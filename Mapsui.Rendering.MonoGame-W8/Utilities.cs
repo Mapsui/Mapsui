d6a3f30bc349d66958e2db7c5c637c86a3618676
pauldendulk <none@none>
2013-11-14 12:31:21 +0100

    adding MatrixHelper and Utilities

---
+++ Mapsui.Rendering.MonoGame-W8/Utilities.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mapsui.Rendering.MonoGame
+{
+    public static class Utilities
+    {
+        public static MemoryStream ToBitmapStream(GraphicsDevice graphicsDevice, 
+            double width, double height)
+        {
+            var renderTarget = new RenderTarget2D(graphicsDevice, (int)width, (int)height);
+            graphicsDevice.SetRenderTarget(renderTarget);
+
+            graphicsDevice.SetRenderTarget(null);
+            var stream = new MemoryStream();
+            renderTarget.SaveAsPng(stream, (int)width, (int)height);
+            return stream;
+        }
+    }
+}
