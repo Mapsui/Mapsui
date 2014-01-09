e2a9c43c33628e9534d82aaf20eada503abd8bcb
time@Tim.GEODAN.NL <time@Tim.GEODAN.NL>
2014-01-09 15:55:54 +0100

    Mapsui android sample changes

--- Samples/Mapsui.Samples.Android/Activity1.cs
+++ Samples/Mapsui.Samples.Android/Activity1.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-
+using System.Net;
 using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using BruTile.Tms;
 using BruTile.Web;
 using Mapsui.Layers;
 using Mapsui.UI.Android;
@@ -19,8 +16,18 @@ namespace Mapsui.Samples.Android
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
+
             var mapControl = FindViewById<MapControl>(Resource.Id.mapcontrol);
-            mapControl.Map.Layers.Add(new TileLayer(new OsmTileSource()));
+            mapControl.Map.Layers.Add(new TileLayer(new OsmTileSource()));                        
+            //mapControl.Map.Layers.Add(LufoTest());
+        }
+
+        private TileLayer LufoTest()
+        {            
+            var client = new WebClient();
+            var stream = client.OpenRead(new Uri(@"http://geodata1.nationaalgeoregister.nl/luchtfoto/tms/1.0.0/luchtfoto/EPSG28992"));
+            var t = TileMapParser.CreateTileSource(stream);
+            return new TileLayer(t);
         }
     }
 }
