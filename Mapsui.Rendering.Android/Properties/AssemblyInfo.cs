ac23f73ac00d6f32663e2c30e7231f17366c0a2d
pauldendulk <none@none>
2014-01-17 11:48:46 +0100

    0.2.5

--- Mapsui.Rendering.Android/Properties/AssemblyInfo.cs
+++ Mapsui.Rendering.Android/Properties/AssemblyInfo.cs
@@ -25,9 +25,9 @@ using Android.App;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.2.4")]
-[assembly: AssemblyVersion("0.2.4")]
-[assembly: AssemblyFileVersion("0.2.4")]
+// [assembly: AssemblyVersion("0.2.5")]
+[assembly: AssemblyVersion("0.2.5")]
+[assembly: AssemblyFileVersion("0.2.5")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
