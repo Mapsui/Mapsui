80e2a61fa3d0f1bb29be1eb505801f6bd11d9c63
pauldendulk <none@none>
2014-01-20 08:24:34 +0100

    removing monogame files

--- Mapsui.Rendering.MonoGame-W8/Resources.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using System.IO;
-namespace Mapsui.Rendering.MonoGame
-{
-    public class Resources
-    {
-        private IDictionary<string, MemoryStream> Bitmaps { get; set; }
-    }
-}
