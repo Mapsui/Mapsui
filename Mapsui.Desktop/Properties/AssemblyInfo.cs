401fcf778b084fe7f5c5be8d17c887850f2f6d78
pauldendulk <none@none>
2014-01-25 11:01:14 +0100

    Renaming Mapsui.Providers to Mapsui.Desktop
    --HG--
    rename : Mapsui.Providers/Mapsui.Providers.csproj => Mapsui.Desktop/Mapsui.Desktop.csproj
    rename : Mapsui.Providers/Properties/AssemblyInfo.cs => Mapsui.Desktop/Properties/AssemblyInfo.cs
    rename : Mapsui.Providers/Utilities.cs => Mapsui.Desktop/Utilities.cs
    rename : Mapsui.Providers/packages.config => Mapsui.Desktop/packages.config

--- Mapsui.Desktop/Properties/AssemblyInfo.cs
+++ Mapsui.Desktop/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mapsui.Providers")]
+[assembly: AssemblyTitle("Mapsui.Desktop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Geodan")]
-[assembly: AssemblyProduct("Mapsui.Providers")]
-[assembly: AssemblyCopyright("Copyright © Geodan 2012")]
+[assembly: AssemblyProduct("Mapsui.Desktop")]
+[assembly: AssemblyCopyright("Copyright � Geodan 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Mapsui.Providers/Wms/IGetFeatureInfoParser.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace Mapsui.Providers.Wms
-{
-    public interface IGetFeatureInfoParser
-    {
-        FeatureInfo ParseWMSResult(string layerName, Stream result);
-    }
-}
