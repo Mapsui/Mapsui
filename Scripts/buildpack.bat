d6835d8efba1ff1744cd233793b60698cde643eb
pauldendulk <none@none>
2014-02-11 18:45:45 +0100

    rmdir of 'release 'so that it can not be push when msbuild fails

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -3,6 +3,8 @@ SETLOCAL
 SET VERSION=%1
 SET NUGET=.\..\.nuget\nuget.exe
 
+rmdir .\..\obj /s /q
+rmdir .\..\release /s /q
 msbuild updateversionnumber.proj /p:AsmVersion=%VERSION%
 msbuild build.proj 
 %NUGET% pack Mapsui.nuspec -Version %VERSION% -outputdirectory .\..\Release
