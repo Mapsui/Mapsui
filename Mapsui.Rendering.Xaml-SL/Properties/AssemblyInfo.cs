eca04e8af70bcf56b2cab6ddcde55d7cf4436470
pauldendulk <none@none>
2014-02-21 11:48:26 +0100

    0.2.13

--- Mapsui.Rendering.Xaml-SL/Properties/AssemblyInfo.cs
+++ Mapsui.Rendering.Xaml-SL/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.2.12")]
-[assembly: AssemblyFileVersion("0.2.12")]
+[assembly: AssemblyVersion("0.2.13")]
+[assembly: AssemblyFileVersion("0.2.13")]
