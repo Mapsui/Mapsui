38a6feae6f0efa0d631019773b2c3db4cde7a53d
pauldendulk <none@none>
2014-02-28 09:12:32 +0100

    0.2.14

--- Mapsui.Rendering.Xaml-W8/Properties/AssemblyInfo.cs
+++ Mapsui.Rendering.Xaml-W8/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.2.13")]
-[assembly: AssemblyVersion("0.2.13")]
-[assembly: AssemblyFileVersion("0.2.13")]
+// [assembly: AssemblyVersion("0.2.14")]
+[assembly: AssemblyVersion("0.2.14")]
+[assembly: AssemblyFileVersion("0.2.14")]
 [assembly: ComVisible(false)]
--- Mapsui.UI.Android/Properties/AssemblyInfo.cs
+++ Mapsui.UI.Android/Properties/AssemblyInfo.cs
@@ -25,9 +25,9 @@ using Android.App;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.2.13")]
-[assembly: AssemblyVersion("0.2.13")]
-[assembly: AssemblyFileVersion("0.2.13")]
+// [assembly: AssemblyVersion("0.2.14")]
+[assembly: AssemblyVersion("0.2.14")]
+[assembly: AssemblyFileVersion("0.2.14")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
