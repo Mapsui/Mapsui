b795857df3e583cb4c5e0e57244907a983476146
pauldendulk <none@none>
2014-03-03 17:54:29 +0100

    0.2.16

--- Samples/Mapsui.Samples.Windows8/Properties/AssemblyInfo.cs
+++ Samples/Mapsui.Samples.Windows8/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.2.15")]
-[assembly: AssemblyVersion("0.2.15")]
-[assembly: AssemblyFileVersion("0.2.15")]
+// [assembly: AssemblyVersion("0.2.16")]
+[assembly: AssemblyVersion("0.2.16")]
+[assembly: AssemblyFileVersion("0.2.16")]
 [assembly: ComVisible(false)]
