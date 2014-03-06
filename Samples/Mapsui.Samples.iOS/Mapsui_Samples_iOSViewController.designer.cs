68b035097f7e0d31e6e70283b041833207045e4d
christianp <none@none>
2014-03-06 11:47:43 +0100

    Sample project iOS

---
+++ Samples/Mapsui.Samples.iOS/Mapsui_Samples_iOSViewController.designer.cs
@@ -0,0 +1,17 @@
+﻿//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+
+namespace Mapsui.Samples.iOS
+{
+	[Register ("Mapsui_Samples_iOSViewController")]
+	partial class Mapsui_Samples_iOSViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
