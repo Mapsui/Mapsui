e193de83c559e1e424d14908085a2adf4cef58d8
pauldendulk <none@none>
2014-03-16 23:44:13 +0100

    0.2.19

--- Samples/Mapsui.Samples.Wpf/Properties/AssemblyInfo.cs
+++ Samples/Mapsui.Samples.Wpf/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.2.18")]
-[assembly: AssemblyVersion("0.2.18")]
-[assembly: AssemblyFileVersion("0.2.18")]
+// [assembly: AssemblyVersion("0.2.19")]
+[assembly: AssemblyVersion("0.2.19")]
+[assembly: AssemblyFileVersion("0.2.19")]
