1acdf5ae2abd847d8ca9d6e5b2709c732af18b9c
pauldendulk <none@none>
2014-03-25 12:52:52 +0100

    0.2.24

--- Samples/Mapsui.Samples.WindowsPhone8/Properties/AssemblyInfo.cs
+++ Samples/Mapsui.Samples.WindowsPhone8/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.2.23")]
-[assembly: AssemblyFileVersion("0.2.23")]
+[assembly: AssemblyVersion("0.2.24")]
+[assembly: AssemblyFileVersion("0.2.24")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
