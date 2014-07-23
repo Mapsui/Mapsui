cd61f55f12d401ec40daf85db39f1fadcb815390
pauldendulk <pauldendulk@gmail.com>
2014-07-23 13:58:44 +0200

    adding IProjectingProvider.cs

---
+++ Mapsui/Providers/IProjectingProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace Mapsui.Providers
+{
+    public interface IProjectingProvider : IProvider
+    {
+        /// <summary>
+        /// Queries whether a provider supports projection to a certain CRS.
+        /// </summary>
+        /// <param name="crs">The crs to project to</param>
+        /// <returns>True if is does, false if it does not, null if it is unknown</returns>
+        bool? IsCrsSupported(string crs);
+    }
+}
