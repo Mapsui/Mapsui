e7df243a1345d52e1d28188882fd117095562cd5
pauldendulk <pauldendulk@gmail.com>
2014-07-29 11:56:39 +0200

    adding empty OpenTK.rendering projects

---
+++ Mapsui.Rendering.OpenTK/MapRenderer.cs
@@ -0,0 +1,20 @@
+﻿using Mapsui.Layers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Mapsui.Rendering.OpenTK
+{
+    class MapRenderer : IRenderer
+    {
+        public void Render(IViewport viewport, IEnumerable<ILayer> layers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MemoryStream RenderToBitmapStream(IViewport viewport, IEnumerable<ILayer> layers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
