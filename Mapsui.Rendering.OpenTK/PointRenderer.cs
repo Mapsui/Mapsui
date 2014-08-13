e8003cc231410efbb6ed1b02b5992a4e271d9f31
pauldendulk <pauldendulk@gmail.com>
2014-08-14 00:02:44 +0200

    OpenTK: cleaning up PointRenderer

--- Mapsui.Rendering.OpenTK/PointRenderer.cs
+++ Mapsui.Rendering.OpenTK/PointRenderer.cs
@@ -10,44 +10,40 @@ namespace Mapsui.Rendering.OpenTK
         public static void Draw(IViewport viewport, IStyle style, IFeature feature)
         {
             var point = feature.Geometry as Point;
-            var dest = viewport.WorldToScreen(point);
-            var symbolSize = (float)SymbolStyle.DefaultHeight;
-            var symbolType = SymbolType.Ellipse;
+            var destination = viewport.WorldToScreen(point);
 
-            var symbolStyle = style as SymbolStyle;
-            if (symbolStyle != null)
+            if (style is SymbolStyle) DrawPointWithSymbolStyle(feature, (SymbolStyle)style, destination);
+            else if (style is VectorStyle) DrawPointWithVectorStyle((VectorStyle)style, destination);
+        }
+
+        private static void DrawPointWithVectorStyle(VectorStyle vectorStyle, Point destination)
+        {
+            var color = vectorStyle.Fill.Color;
+            GL.Color4((byte) color.R, (byte) color.G, (byte) color.B, (byte) color.A);
+            GL.PointSize((float) SymbolStyle.DefaultWidth);
+            GL.EnableClientState(All.VertexArray);
+            var destAsArray = new[] {(float) destination.X, (float) destination.Y};
+            GL.VertexPointer(2, All.Float, 0, destAsArray);
+            GL.DrawArrays(All.Points, 0, 1);
+            GL.DisableClientState(All.VertexArray);
+        }
+
+        private static void DrawPointWithSymbolStyle(IFeature feature, SymbolStyle symbolStyle, Point destination)
+        {
+            if (symbolStyle.Symbol != null && symbolStyle.Symbol.Data != null)
             {
-                if (symbolStyle.Symbol != null && symbolStyle.Symbol.Data != null)
+                TextureInfo textureInfo;
+                if (!feature.RenderedGeometry.ContainsKey(symbolStyle))
                 {
-                    TextureInfo textureInfo;
-                    if (!feature.RenderedGeometry.ContainsKey(style))
-                    {
-                        textureInfo = TextureHelper.LoadTexture(symbolStyle.Symbol.Data);
-                        feature.RenderedGeometry[style] = textureInfo;
-                    }
-                    else
-                    {
-                        textureInfo = (TextureInfo)feature.RenderedGeometry[style];
-                    }
-
-                    TextureHelper.RenderTexture(textureInfo, (float)dest.X, (float)dest.Y, (float)symbolStyle.SymbolRotation, (float)symbolStyle.SymbolOffset.X, (float)symbolStyle.SymbolOffset.Y);
-                    return;
+                    textureInfo = TextureHelper.LoadTexture(symbolStyle.Symbol.Data);
+                    feature.RenderedGeometry[symbolStyle] = textureInfo;
                 }
-                symbolType = symbolStyle.SymbolType;
-                if (symbolStyle.SymbolScale > 0) symbolSize = (float)symbolStyle.SymbolScale * symbolSize;
-            }
-
-            var vectorStyle = style as VectorStyle;
-            if (vectorStyle != null)
-            {
-                var color = vectorStyle.Fill.Color;
-                GL.Color4((byte)color.R, (byte)color.G, (byte)color.B, (byte)color.A);
-                GL.PointSize((float)SymbolStyle.DefaultWidth);
-                GL.EnableClientState(All.VertexArray);
-                var destAsArray = new[] { (float)dest.X, (float)dest.Y };
-                GL.VertexPointer(2, All.Float, 0, destAsArray);
-                GL.DrawArrays(All.Points, 0, 1);
-                GL.DisableClientState(All.VertexArray);
+                else
+                {
+                    textureInfo = (TextureInfo)feature.RenderedGeometry[symbolStyle];
+                }
+                TextureHelper.RenderTexture(textureInfo, (float)destination.X, (float)destination.Y, 
+                    (float)symbolStyle.SymbolRotation, (float)symbolStyle.SymbolOffset.X, (float)symbolStyle.SymbolOffset.Y);
             }
         }
     }
