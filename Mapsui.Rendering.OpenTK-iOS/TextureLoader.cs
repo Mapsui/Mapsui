50ab2196151293beb8c1ec0bd0d3325eb60263f9
pauldendulk <pauldendulk@gmail.com>
2014-08-26 13:44:47 +0200

    [OpenTK] Working on MapControl for iOS

--- Mapsui.Rendering.OpenTK-iOS/TextureLoader.cs
+++ Mapsui.Rendering.OpenTK-iOS/TextureLoader.cs
@@ -1,4 +1,12 @@
+using System;
+using System.Diagnostics;
+using System.Drawing;
 using System.IO;
+using MonoTouch.CoreGraphics;
+using MonoTouch.Foundation;
+using MonoTouch.GLKit;
+using MonoTouch.UIKit;
+using OpenTK.Graphics.ES20;
 
 namespace Mapsui.Rendering.OpenTK
 {
@@ -9,8 +17,26 @@ namespace Mapsui.Rendering.OpenTK
     {
         public static void TexImage2D(Stream data, out int width, out int height)
         {
-            width = 0;  //!!!bitmap.Width;
-            height = 0; //!!!bitmap.Height;
+			data.Position = 0;		
+			var nsData = NSData.FromStream(data);
+
+			UIImage image = UIImage.LoadFromData(nsData);
+			if (image == null) new Exception ("could not load image data");
+
+			width = image.CGImage.Width;
+			height = image.CGImage.Height;
+
+			CGColorSpace colorSpace = CGColorSpace.CreateDeviceRGB();
+			byte [] imageData = new byte[height * width * 4];
+			CGContext context = new CGBitmapContext  (imageData, width, height, 8, 4 * width, colorSpace,
+				CGBitmapFlags.PremultipliedLast | CGBitmapFlags.ByteOrder32Big);
+
+			colorSpace.Dispose();
+			context.ClearRect(new RectangleF(0, 0, width, height));
+			context.DrawImage(new RectangleF(0, 0, width, height), image.CGImage);
+
+			GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, width, height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, imageData);
+			context.Dispose();
         }
     }
 }
