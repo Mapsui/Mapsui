6c736c609e772268e6b11c452469222c73609b07
pauldendulk <pauldendulk@gmail.com>
2014-09-11 14:39:01 +0200

    * Mapsui.userprefs: * Mapsui.Rendering.iOS.csproj: * PlatformLabelBitmap.cs:
    * Mapsui.UI.Xaml-W8.csproj: opentk labels on ios

--- Mapsui.Rendering.OpenTK-iOS/PlatformLabelBitmap.cs
+++ Mapsui.Rendering.OpenTK-iOS/PlatformLabelBitmap.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.IO;
 using System.Threading;
 using Mapsui.Styles;
+using MonoTouch.CoreAnimation;
 using MonoTouch.CoreGraphics;
 using MonoTouch.UIKit;
 
@@ -10,6 +11,8 @@ namespace Mapsui.Rendering.OpenTK
 {
     public class PlatformLabelBitmap
     {
+		private static readonly CGColor TransparentColor =  new CGColor(0, 0, 0, 0);
+
         public static MemoryStream Create(LabelStyle style, string text)
         {
             UIImage image = null;
@@ -19,21 +22,12 @@ namespace Mapsui.Rendering.OpenTK
             view.InvokeOnMainThread(() =>
             {
                 view.Opaque = false;
-                view.BackgroundColor = UIColor.Clear;
-                // draw 
-
+					view.BackgroundColor = UIColor.Clear;
+                
                 var bitmapSize = view.StringSize(text, UIFont.SystemFontOfSize(14), new SizeF(115, float.MaxValue), UILineBreakMode.WordWrap);
 
-                // Draw them with a 2.0 stroke width so they are a bit more visible.
-
-                CGContext context = UIGraphics.GetCurrentContext();
-                context.SetStrokeColor(255, 128, 128, 128);
-                context.SetLineWidth(2.0f);
-                context.MoveTo(0.0f, 0.0f); //start at this point
-                context.AddLineToPoint(20.0f, 20.0f); //draw to this point
-                
-                // end draw
-
+				view.Layer.AddSublayer(CreateCATextLayer(style, text));
+					view.BackgroundColor = new UIColor(ToCGColor(style.BackColor.Color));
 
                 image = ToImage(view, new RectangleF(0, 0, (float)bitmapSize.Width, (float)bitmapSize.Height));
                 handle.Set();
@@ -46,12 +40,52 @@ namespace Mapsui.Rendering.OpenTK
             }
         }
 
+		private static CATextLayer CreateCATextLayer(LabelStyle style, string text)
+		{
+			var label = new CATextLayer ();
+
+			var ctFont = new MonoTouch.CoreText.CTFont (style.Font.FontFamily, (float)style.Font.Size);
+			var aString = new MonoTouch.Foundation.NSAttributedString (text, 
+				new MonoTouch.CoreText.CTStringAttributes() { Font = ctFont });
+
+			label.SetFont(ctFont);
+			label.FontSize = (float)style.Font.Size;
+			label.ForegroundColor = ToCGColor(style.ForeColor);
+			label.BackgroundColor = TransparentColor;
+			label.ShadowOpacity = 0;
+			label.BorderWidth = 0;
+
+			label.String = text;
+
+			var size = GetSizeForText (0, aString);
+
+			label.Frame = new RectangleF (0, 0, size.Width, size.Height);
+
+			return label;
+		}
+
+		private static CGColor ToCGColor(Mapsui.Styles.Color color)
+		{
+			return new MonoTouch.CoreGraphics.CGColor(color.R / 255f, color.G / 255f, color.B / 255f, color.A / 255f);
+		}
+
+		private static SizeF GetSizeForText(int width, MonoTouch.Foundation.NSAttributedString aString)
+		{
+			var frameSetter = new MonoTouch.CoreText.CTFramesetter (aString);
+
+			MonoTouch.Foundation.NSRange range;
+			var size = frameSetter.SuggestFrameSize (new MonoTouch.Foundation.NSRange (0, 0), null,
+				new System.Drawing.Size (width, Int32.MaxValue), out range);
+
+			return size;
+		}
 
         private static UIImage ToImage(UIView view, RectangleF frame)
         {
             UIGraphics.BeginImageContext(frame.Size);
-            UIColor.Clear.SetColor();
-            UIGraphics.RectFill(view.Frame);
+			view.Layer.BackgroundColor = TransparentColor;
+			var context = UIGraphics.GetCurrentContext ();
+			UIGraphics.RectFill(view.Frame);
             view.Layer.RenderInContext(UIGraphics.GetCurrentContext());
             var image = UIGraphics.GetImageFromCurrentImageContext();
             UIGraphics.EndImageContext();
