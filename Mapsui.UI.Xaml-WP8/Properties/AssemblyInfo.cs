1a8ba60ef09930a1e10ad396618ae3a2de3ec742
pauldendulk <pauldendulk@gmail.com>
2014-11-14 16:38:13 +0100

    Version to 0.3.0

--- Mapsui.UI.Xaml-WP8/Properties/AssemblyInfo.cs
+++ Mapsui.UI.Xaml-WP8/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.2.27")]
-[assembly: AssemblyFileVersion("0.2.27")]
+[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyFileVersion("0.3.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
