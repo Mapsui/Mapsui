b830f74bf5e10e889ca1144cb67fcbdb34655f84
pauldendulk <pauldendulk@gmail.com>
2014-11-14 17:39:06 +0100

    Version to 0.3.1

--- Mapsui.UI.Xaml-WP8/Properties/AssemblyInfo.cs
+++ Mapsui.UI.Xaml-WP8/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.3.0")]
+[assembly: AssemblyVersion("0.3.1")]
+[assembly: AssemblyFileVersion("0.3.1")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
--- Tests/Mapsui.Rendering.OpenTK.Android.Tests/Properties/AssemblyInfo.cs
+++ Tests/Mapsui.Rendering.OpenTK.Android.Tests/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.3.0")]
+[assembly: AssemblyVersion("0.3.1")]
+[assembly: AssemblyFileVersion("0.3.1")]
