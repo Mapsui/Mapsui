fdc86088a4b7303d069b02384127e409144666d9
pauldendulk <pauldendulk@gmail.com>
2015-05-12 15:56:43 +0200

    Version to 0.6.1

--- Mapsui.Rendering.Android/Properties/AssemblyInfo.cs
+++ Mapsui.Rendering.Android/Properties/AssemblyInfo.cs
@@ -25,9 +25,9 @@ using Android.App;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.6.0")]
-[assembly: AssemblyVersion("0.6.0")]
-[assembly: AssemblyFileVersion("0.6.0")]
+// [assembly: AssemblyVersion("0.6.1")]
+[assembly: AssemblyVersion("0.6.1")]
+[assembly: AssemblyFileVersion("0.6.1")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
--- Mapsui.UI.Xaml/Properties/AssemblyInfo.cs
+++ Mapsui.UI.Xaml/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.6.0")]
-[assembly: AssemblyFileVersion("0.6.0")]
+[assembly: AssemblyVersion("0.6.1")]
+[assembly: AssemblyFileVersion("0.6.1")]
