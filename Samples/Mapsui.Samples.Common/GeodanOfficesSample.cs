8da25cdaaf1c0481bce719b6b9db10832b2956c9
pauldendulk <pauldendulk@gmail.com>
2016-04-07 09:11:19 +0200

    Geodan offices sample moved to its own class

---
+++ Samples/Mapsui.Samples.Common/GeodanOfficesSample.cs
@@ -0,0 +1,22 @@
+﻿using Mapsui.Layers;
+using Mapsui.Providers;
+using Mapsui.Styles;
+
+namespace Mapsui.Samples.Common
+{
+    public class GeodanOfficesSample
+    {
+        public static MemoryLayer CreateLayer()
+        {
+            var geodanAmsterdam = new Geometries.Point(122698, 483922);
+            var geodanDenBosch = new Geometries.Point(148949, 411446);
+            var layer = new MemoryLayer
+            {
+                DataSource = new MemoryProvider(new [] { geodanAmsterdam, geodanDenBosch }),
+                Style = new SymbolStyle { Fill = new Brush(Color.Red), SymbolScale = 1 },
+                CRS = "EPSG:28992"
+            };
+            return layer;
+        }
+    }
+}
