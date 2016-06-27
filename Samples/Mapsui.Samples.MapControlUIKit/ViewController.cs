777e13b780a7e0ca380ca7778d4a366ef9b5f6fb
pauldendulk <pauldendulk@gmail.com>
2016-06-27 16:39:43 +0200

    Adding sample for MapControlUIKit

---
+++ Samples/Mapsui.Samples.MapControlUIKit/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BruTile.Predefined;
+using Mapsui.Layers;
+using UIKit;
+
+namespace Mapsui.Samples.MapControlUIKit
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			var layer = new TileLayer(KnownTileSources.Create());
+			mapControlUIKit.Map.Layers.Add(layer);
+			mapControlUIKit.Map.NavigateTo(mapControlUIKit.Map.Envelope);
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
