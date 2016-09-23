83a8e00fa451c4ff679f8b219ccfa9e5b9cdceeb
pauldendulk <pauldendulk@gmail.com>
2016-09-23 13:52:42 +0200

    Adding MapControl to UWP sample using the new Xaml PCLs for UI en Rendering

--- Samples/Mapsui.Samples.Uwp/MainPage.xaml.cs
+++ Samples/Mapsui.Samples.Uwp/MainPage.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using Windows.UI.Xaml.Controls;
+using BruTile;
+using BruTile.Predefined;
+using BruTile.Web;
+using Mapsui.Layers;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -20,11 +11,21 @@ namespace Mapsui.Samples.Uwp
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    // ReSharper disable once RedundantExtendsListEntry
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            MapControl.Map.Layers.Add(new TileLayer(CreateOsmTileSource()));
+        }
+
+        private ITileSource CreateOsmTileSource()
+        {
+            return new HttpTileSource(new GlobalSphericalMercator(0, 18),
+                "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                new[] {"a", "b", "c"}, name: "OSM");
         }
     }
 }
