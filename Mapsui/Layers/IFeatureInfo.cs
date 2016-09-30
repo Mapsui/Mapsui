416f23ad45f92261f83460c0885e01b543ba55cd
pauldendulk <pauldendulk@gmail.com>
2016-09-30 20:57:51 +0200

    Just comments

--- Mapsui/Layers/IFeatureInfo.cs
+++ Mapsui/Layers/IFeatureInfo.cs
@@ -4,6 +4,10 @@ using Mapsui.Providers;
 
 namespace Mapsui.Layers
 {
+    /// <summary>
+    /// Some data source like WMS allow request for feature info. 
+    /// This is information not available in the primary response such as the WMS image.
+    /// </summary>
     public interface IFeatureInfo
     {
         void GetFeatureInfo(IViewport viewport, double x, double y, Action<IDictionary<string, IEnumerable<IFeature>>> callback);
