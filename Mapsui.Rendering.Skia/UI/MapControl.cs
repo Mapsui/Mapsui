6c0254545c7a5b3739d4d7d77652f056c3d1830c
pauldendulk <pauldendulk@gmail.com>
2016-10-24 23:01:32 +0200

    Fixing viewport initialize

--- Mapsui.Rendering.Skia/UI/MapControl.cs
+++ Mapsui.Rendering.Skia/UI/MapControl.cs
@@ -513,13 +513,20 @@ namespace Mapsui.Rendering.Skia.UI
         {
             if (ActualWidth.IsNanOrZero()) return;
             if (_map?.Envelope == null) return;
-            if (_map.Envelope.Width.IsNanOrZero()) return;
-            if (_map.Envelope.Height.IsNanOrZero()) return;
+            if (double.IsNaN(_map.Envelope.Width)) return;
+            if (double.IsNaN(_map.Envelope.Height)) return;
             if (_map.Envelope.GetCentroid() == null) return;
 
-            if (double.IsNaN(Map.Viewport.Resolution))
-                Map.Viewport.Resolution = _map.Envelope.Width / ActualWidth;
-            if (double.IsNaN(Map.Viewport.Center.X) || double.IsNaN(Map.Viewport.Center.Y))
+            if (double.IsNaN(Map.Viewport.Resolution)) // only when not set yet
+            {
+                if (Math.Abs(_map.Envelope.Width) > Constants.Epsilon)
+                    Map.Viewport.Resolution = _map.Envelope.Width / ActualWidth;
+                else
+                    // An envelope width of zero can happen when there is no data in the Maps' layers (yet).
+                    // It should be possible to start with an empty map.
+                    Map.Viewport.Resolution = Constants.DefaultResolution;
+            }
+            if (double.IsNaN(Map.Viewport.Center.X) || double.IsNaN(Map.Viewport.Center.Y)) // only when not set yet
                 Map.Viewport.Center = _map.Envelope.GetCentroid();
 
             Map.Viewport.Width = ActualWidth;
