ee35d3bf0e86ce819e9f4595e116ecf729bb6adc
pauldendulk <pauldendulk@gmail.com>
2016-11-01 07:02:28 +0100

    Just resharping

--- Mapsui/Layers/LabelLayer.cs
+++ Mapsui/Layers/LabelLayer.cs
@@ -22,146 +22,142 @@ using Mapsui.Rendering;
 namespace Mapsui.Layers
 {
     /// <summary>
-    /// Label layer class
+    ///     Label layer class
     /// </summary>
     public class LabelLayer : Layer
     {
-
         /// <summary>
-        /// Delegate method for creating advanced label texts
+        ///     Delegate method for creating advanced label texts
         /// </summary>
         /// <param name="fdr"></param>
         /// <returns></returns>
         public delegate string GetLabelMethod(IFeature fdr);
 
         /// <summary>
-        /// Delegate method for calculating the priority of label rendering
+        ///     Delegate method for calculating the priority of label rendering
         /// </summary>
         /// <param name="fdr"></param>
         /// <returns></returns>
         public delegate int GetPriorityMethod(IFeature fdr);
 
-
-
         /// <summary>
-        /// Labelling behaviour for Multipart geometry collections
+        ///     Labelling behaviour for Multipart geometry collections
         /// </summary>
         public enum MultipartGeometryBehaviourEnum
         {
             /// <summary>
-            /// Place label on all parts (default)
+            ///     Place label on all parts (default)
             /// </summary>
             All,
+
             /// <summary>
-            /// Place label on object which the greatest length or area.
+            ///     Place label on object which the greatest length or area.
             /// </summary>
             /// <remarks>
-            /// Multipoint geometries will default to <see cref="First"/>
+            ///     Multipoint geometries will default to <see cref="First" />
             /// </remarks>
             Largest,
+
             /// <summary>
-            /// The center of the combined geometries
+            ///     The center of the combined geometries
             /// </summary>
             CommonCenter,
+
             /// <summary>
-            /// Center of the first geometry in the collection (fastest method)
+            ///     Center of the first geometry in the collection (fastest method)
             /// </summary>
             First
         }
+        
+        /// <summary>
+        ///     Creates a new instance of a LabelLayer
+        /// </summary>
+        public LabelLayer(string name)
+            : base(name)
+        {
+            //PriorityColumn = "";
+            //Name = name;
+            //MultipartGeometryBehaviour = MultipartGeometryBehaviourEnum.All;
+            //LabelFilter = LabelCollisionDetection.SimpleCollisionDetection;
+        }
 
         /// <summary>
-        /// Gets or sets labelling behavior on multipart geometries
+        ///     Gets or sets labelling behavior on multipart geometries
         /// </summary>
-        /// <remarks>Default value is <see cref="MultipartGeometryBehaviourEnum.All"/></remarks>
+        /// <remarks>Default value is <see cref="MultipartGeometryBehaviourEnum.All" /></remarks>
         public MultipartGeometryBehaviourEnum MultipartGeometryBehaviour { get; set; }
 
         /// <summary>
-        /// Filtermethod delegate for performing filtering
+        ///     Filtermethod delegate for performing filtering
         /// </summary>
         /// <remarks>
-        /// Default method is <see cref="Mapsui.Rendering.LabelCollisionDetection.SimpleCollisionDetection"/>
+        ///     Default method is <see cref="Mapsui.Rendering.LabelCollisionDetection.SimpleCollisionDetection" />
         /// </remarks>
         public LabelCollisionDetection.LabelFilterMethod LabelFilter { get; set; }
 
         /// <summary>
-        /// Data column or expression where label text is extracted from.
+        ///     Data column or expression where label text is extracted from.
         /// </summary>
         /// <remarks>
-        /// This property is overriden by the <see cref="LabelStringDelegate"/>.
+        ///     This property is overriden by the <see cref="LabelStringDelegate" />.
         /// </remarks>
         public string LabelColumn { get; set; }
 
         /// <summary>
-        /// Gets or sets the method for creating a custom label string based on a feature.
+        ///     Gets or sets the method for creating a custom label string based on a feature.
         /// </summary>
         /// <remarks>
-        /// <para>If this method is not null, it will override the <see cref="LabelColumn"/> value.</para>
-        /// <para>The label delegate must take a <see cref="Mapsui.Providers.IFeature"/> and return a string.</para>
-        /// <example>
-        /// Creating a label-text by combining attributes "ROADNAME" and "STATE" into one string, using
-        /// an anonymous delegate:
-        /// <code lang="C#">
-        /// myLabelLayer.LabelStringDelegate = delegate(FeatureDataRow fdr)
-        ///				{ return fdr["ROADNAME"].ToString() + ", " + fdr["STATE"].ToString(); };
-        /// </code>
-        /// </example>
+        ///     <para>If this method is not null, it will override the <see cref="LabelColumn" /> value.</para>
+        ///     <para>The label delegate must take a <see cref="Mapsui.Providers.IFeature" /> and return a string.</para>
+        ///     <example>
+        ///         Creating a label-text by combining attributes "ROADNAME" and "STATE" into one string, using
+        ///         an anonymous delegate:
+        ///         <code lang="C#">
+        ///  myLabelLayer.LabelStringDelegate = delegate(FeatureDataRow fdr)
+        /// 				{ return fdr["ROADNAME"].ToString() + ", " + fdr["STATE"].ToString(); };
+        ///  </code>
+        ///     </example>
         /// </remarks>
         public GetLabelMethod LabelStringDelegate { get; set; }
 
         /// <summary>
-        /// Gets or sets the method for calculating the render priority of a label based on a feature.
+        ///     Gets or sets the method for calculating the render priority of a label based on a feature.
         /// </summary>
         /// <remarks>
-        /// <para>If this method is not null, it will override the <see cref="PriorityColumn"/> value.</para>
-        /// <para>The label delegate must take a <see cref="Mapsui.Providers.IFeature"/> and return an Int32.</para>
-        /// <example>
-        /// Creating a priority by combining attributes "capital" and "population" into one value, using
-        /// an anonymous delegate:
-        /// <code lang="C#">
+        ///     <para>If this method is not null, it will override the <see cref="PriorityColumn" /> value.</para>
+        ///     <para>The label delegate must take a <see cref="Mapsui.Providers.IFeature" /> and return an Int32.</para>
+        ///     <example>
+        ///         Creating a priority by combining attributes "capital" and "population" into one value, using
+        ///         an anonymous delegate:
+        ///         <code lang="C#">
         /// myLabelLayer.PriorityDelegate = delegate(FeatureDataRow fdr) 
         ///     { 
         ///         Int32 retVal = 100000000 * (Int32)( (String)fdr["capital"] == "Y" ? 1 : 0 );
         ///         return  retVal + Convert.ToInt32(fdr["population"]);
         ///     };
         /// </code>
-        /// </example>
+        ///     </example>
         /// </remarks>
         public GetPriorityMethod PriorityDelegate { get; set; }
 
         /// <summary>
-        /// Data column from where the label rotation is derived.
-        /// If this is empty, rotation will be zero, or aligned to a linestring.
-        /// Rotation are in degrees (positive = clockwise).
+        ///     Data column from where the label rotation is derived.
+        ///     If this is empty, rotation will be zero, or aligned to a linestring.
+        ///     Rotation are in degrees (positive = clockwise).
         /// </summary>
         public string RotationColumn { get; set; }
 
         /// <summary>
-        /// A value indication the priority of the label in cases of label-collision detection
+        ///     A value indication the priority of the label in cases of label-collision detection
         /// </summary>
         public int Priority { get; set; }
 
         public string PriorityColumn { get; set; }
 
-
-
-        /// <summary>
-        /// Creates a new instance of a LabelLayer
-        /// </summary>
-        public LabelLayer(string name)
-            : base(name)
-        {
-            PriorityColumn = "";
-            Name = name;
-            MultipartGeometryBehaviour = MultipartGeometryBehaviourEnum.All;
-            LabelFilter = LabelCollisionDetection.SimpleCollisionDetection;
-        }
-
         public string GetLabelText(IFeature feature)
         {
             if (LabelStringDelegate != null)
-            {
                 return LabelStringDelegate(feature);
-            }
             if (LabelColumn == null) throw new Exception("LabelColumn was not set");
             return feature[LabelColumn] == null ? "" : feature[LabelColumn].ToString();
         }
