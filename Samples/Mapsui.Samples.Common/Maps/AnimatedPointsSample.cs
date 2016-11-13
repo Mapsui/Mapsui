a8a28c59e5c6a8d099d34d424f95c8fd43a11f10
pauldendulk <pauldendulk@gmail.com>
2016-11-13 22:27:52 +0100

    Just style

--- Samples/Mapsui.Samples.Common/Maps/AnimatedPointsSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/AnimatedPointsSample.cs
@@ -6,6 +6,7 @@ using BruTile.Predefined;
 using Mapsui.Geometries;
 using Mapsui.Layers;
 using Mapsui.Providers;
+using Mapsui.Styles;
 using Mapsui.Utilities;
 
 namespace Mapsui.Samples.Common.Maps
@@ -28,12 +29,13 @@ namespace Mapsui.Samples.Common.Maps
         public AnimatedPointsWithAutoUpdateLayer()
             : base(new DynamicMemoryProvider())
         {
+            Style = new SymbolStyle {Fill = {Color = new Color(255, 215, 0, 200)}, SymbolScale = 0.9};
             _timer = new Timer(arg => UpdateData(), this, 0, 2000);
         }
 
         private class DynamicMemoryProvider : MemoryProvider
         {
-            readonly Random _random = new Random(0);
+            private readonly Random _random = new Random(0);
 
             public override IEnumerable<IFeature> GetFeaturesInView(BoundingBox box, double resolution)
             {
@@ -41,7 +43,7 @@ namespace Mapsui.Samples.Common.Maps
                 var geometries = PointsSample.GenerateRandomPoints(box, 10).ToList();
                 var count = 0;
                 var random = _random.Next(geometries.Count);
-                
+
                 foreach (var geometry in geometries)
                 {
                     if (count != random) // skip a random element to test robustness
