74a824dfeedbc741a7119813a33082a1ca7f508e
pauldendulk <pauldendulk@gmail.com>
2016-11-13 08:37:59 +0100

    Added an RGBA constructor to Color

--- Mapsui/Styles/Color.cs
+++ Mapsui/Styles/Color.cs
@@ -9,17 +9,25 @@ namespace Mapsui.Styles
 
         public Color(Color color)
         {
-            A = color.A;
             R = color.R;
             G = color.G;
             B = color.B;
+            A = color.A;
         }
 
-        public int A { get; set; }
+        public Color(int red, int green, int blue, int alpha = 255)
+        {
+            R = red;
+            G = green;
+            B = blue;
+            A = alpha;
+        }
+
         public int R { get; set; }
         public int G { get; set; }
         public int B { get; set; }
-
+        public int A { get; set; }
+        
         public static Color Transparent => new Color {A = 0, R = 255, G = 255, B = 255};
         public static Color Black => new Color {A = 255, R = 0, G = 0, B = 0};
         public static Color White => new Color {A = 255, R = 255, G = 255, B = 255};
@@ -37,8 +45,7 @@ namespace Mapsui.Styles
         {
             return new Color {A = a, R = r, G = g, B = b};
         }
-
-
+        
         public override bool Equals(object obj)
         {
             if (!(obj is Color))
@@ -48,16 +55,16 @@ namespace Mapsui.Styles
 
         public bool Equals(Color color)
         {
-            if (A != color.A) return false;
             if (R != color.R) return false;
             if (G != color.G) return false;
             if (B != color.B) return false;
+            if (A != color.A) return false;
             return true;
         }
 
         public override int GetHashCode()
         {
-            return A.GetHashCode() ^ R.GetHashCode() ^ G.GetHashCode() ^ B.GetHashCode();
+            return R.GetHashCode() ^ G.GetHashCode() ^ B.GetHashCode() ^ A.GetHashCode();
         }
 
         public static bool operator ==(Color color1, Color color2)
