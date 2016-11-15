96449d25f9529aec36292c8272e01287972c4374
pauldendulk <pauldendulk@gmail.com>
2016-11-15 10:03:07 +0100

    Removing unnecessary things from deprecated rasterizing provider

--- Mapsui.Rendering.Xaml/RasterizingProvider.cs
+++ Mapsui.Rendering.Xaml/RasterizingProvider.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Windows.Controls;
 using Mapsui.Geometries;
-using Mapsui.Layers;
 using Mapsui.Providers;
-using Mapsui.Styles;
 
 namespace Mapsui.Rendering.Xaml
 {
@@ -16,80 +12,16 @@ namespace Mapsui.Rendering.Xaml
     [Obsolete("Use RasterizingLayer as an alternative approach", true)]
     public class RasterizingProvider : IProvider
     {
-        private readonly object _syncLock = new object();
-        private readonly ILayer _layer;
-
-        public RasterizingProvider(ILayer layer)
-        {
-            _layer = layer;
-        }
-
-        public RasterizingProvider(IProvider provider, IStyle style = null)
-        {
-            _layer = new MemoryLayer {DataSource = provider, Style = style};
-        }
-
         public string CRS { get; set; }
 
         public IEnumerable<IFeature> GetFeaturesInView(BoundingBox extent, double resolution)
         {
-            lock (_syncLock)
-            {
-                foreach (var feature in _layer.GetFeaturesInView(extent, resolution))
-                {
-                    // hack: clear cache to prevent cross thread exception. 
-                    // todo: remove this caching mechanism.
-                    feature.RenderedGeometry.Clear();
-                }
-
-                IFeatures features = null;
-                var viewport = CreateViewport(extent, resolution);
-                RunMethodOnStaThread(() => RenderToRaster(viewport, _layer, out features));
-                return features;
-            }
+            throw new NotImplementedException();
         }
-
-        private static void RunMethodOnStaThread(ThreadStart operation)
-        {
-            var thread = new Thread(operation);
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Priority = ThreadPriority.Lowest;
-            thread.Start();
-            thread.Join();
-        }
-
+        
         public BoundingBox GetExtents()
         {
-            return _layer.Envelope;
-        }
-
-        private void RenderToRaster(IViewport viewport, ILayer layer, out IFeatures features)
-        {
-            var canvas = new Canvas();
-            MapRenderer.RenderLayer(canvas, viewport, layer);
-            canvas.UpdateLayout();
-            var bitmap = BitmapRendering.BitmapConverter.ToBitmapStream(canvas, viewport.Width, viewport.Height);
-            features = new Features { new Feature { Geometry = new Raster(bitmap, viewport.Extent) } };
-        }
-
-        public IProvider DataSource
-        {
-            get
-            {
-                var layer = _layer as MemoryLayer;
-                return layer != null ? layer.DataSource : null;
-            }
-        }
-
-        private static Viewport CreateViewport(BoundingBox extent, double resolution)
-        {
-            return new Viewport
-                {
-                    Resolution = resolution,
-                    Center = extent.GetCentroid(),
-                    Width = extent.Width/resolution,
-                    Height = extent.Height/resolution
-                };
+            throw new NotImplementedException();
         }
     }
 }
--- Mapsui/Providers/IProvider.cs
+++ Mapsui/Providers/IProvider.cs
@@ -15,7 +15,6 @@
 // along with Mapsui; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
 
-using System;
 using Mapsui.Geometries;
 using System.Collections.Generic;
 
