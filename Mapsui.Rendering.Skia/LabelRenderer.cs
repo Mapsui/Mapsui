0de82a099cc62b326f3a85d133c03e04326b45c9
pauldendulk <pauldendulk@gmail.com>
2016-11-15 09:20:20 +0100

    Moving plaform label renderer into proper label renderer

--- Mapsui.Rendering.Skia/LabelRenderer.cs
+++ Mapsui.Rendering.Skia/LabelRenderer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Mapsui.Providers;
 using Mapsui.Styles;
@@ -18,13 +19,54 @@ namespace Mapsui.Rendering.Skia
                       style.BackColor + "_" + style.ForeColor;
 
             if (!LabelBitmapCache.Keys.Contains(key))
-                LabelBitmapCache[key] = new SKBitmapInfo {Bitmap = PlatformLabelBitmap.Create(style, text)};
+                LabelBitmapCache[key] = new SKBitmapInfo {Bitmap = CreateLabelAsBitmap(style, text)};
 
             var info = LabelBitmapCache[key];
 
             BitmapHelper.RenderTexture(canvas, info.Bitmap, x, y,
                 offsetX: (float) style.Offset.X, offsetY: (float) -style.Offset.Y,
                 horizontalAlignment: style.HorizontalAlignment, verticalAlignment: style.VerticalAlignment);
+        }
+
+        static SKBitmap CreateLabelAsBitmap(LabelStyle style, string text)
+        {
+            using (var paint = new SKPaint())
+            {
+                paint.TextSize = (float)style.Font.Size;
+                paint.IsAntialias = true;
+                paint.Color = style.ForeColor.ToSkia();
+                paint.Typeface = SKTypeface.FromFamilyName(style.Font.FontFamily);
+                paint.IsStroke = false;
+                paint.FakeBoldText = false;
+                paint.IsEmbeddedBitmapText = true;
+
+                var rect = new SKRect();
+                paint.MeasureText(text, ref rect);
+
+                var padding = 3; // todo get this from LabelStyle
+                rect = SKRect.Inflate(rect, padding, padding);
+
+                var bitmap = new SKBitmap((int)Math.Ceiling(rect.Width), (int)Math.Ceiling(rect.Height));
+
+                using (var target = new SKCanvas(bitmap))
+                {
+                    target.Clear();
+                    if (style.BackColor != null)
+                    {
+                        var color = style.BackColor?.Color?.ToSkia();
+                        if (color.HasValue)
+                        {
+                            var rounding = 6;
+                            using (var backgroundPaint = new SKPaint { Color = color.Value })
+                            {
+                                target.DrawRoundRect(new SKRect(0, 0, rect.Width, rect.Height), rounding, rounding, backgroundPaint);
+                            }
+                        }
+                    }
+                    target.DrawText(text, -rect.Left, -rect.Top, paint);
+                    return bitmap;
+                }
+            }
         }
     }
 }
