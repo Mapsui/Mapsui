cb852a21ef1dec3f9ec495044d3de586db009ef4
pauldendulk <pauldendulk@gmail.com>
2016-11-16 21:32:31 +0100

    Rendering labels without bitmap cache

--- Mapsui.Rendering.Skia/LabelRenderer.cs
+++ Mapsui.Rendering.Skia/LabelRenderer.cs
@@ -11,62 +11,123 @@ namespace Mapsui.Rendering.Skia
         private static readonly IDictionary<string, SKBitmapInfo> LabelBitmapCache =
             new Dictionary<string, SKBitmapInfo>();
 
-        public static void Draw(SKCanvas canvas, LabelStyle style, IFeature feature, float x, float y)
+        public static void DrawAsBitmap(SKCanvas canvas, LabelStyle style, IFeature feature, float x, float y)
         {
             var text = style.GetLabelText(feature);
 
-            var key = text + "_" + style.Font.FontFamily + "_" + style.Font.Size + "_" + (float) style.Font.Size + "_" +
+            var key = text + "_" + style.Font.FontFamily + "_" + style.Font.Size + "_" + (float)style.Font.Size + "_" +
                       style.BackColor + "_" + style.ForeColor;
 
             if (!LabelBitmapCache.Keys.Contains(key))
-                LabelBitmapCache[key] = new SKBitmapInfo {Bitmap = CreateLabelAsBitmap(style, text)};
+                LabelBitmapCache[key] = new SKBitmapInfo { Bitmap = CreateLabelAsBitmap(style, text) };
 
             var info = LabelBitmapCache[key];
 
-            BitmapHelper.RenderTexture(canvas, info.Bitmap, x, y,
-                offsetX: (float) style.Offset.X, offsetY: (float) -style.Offset.Y,
+            BitmapHelper.RenderTexture(canvas, info.Bitmap, (int)Math.Round(x), (int)Math.Round(y),
+                offsetX: (float)style.Offset.X, offsetY: (float)-style.Offset.Y,
                 horizontalAlignment: style.HorizontalAlignment, verticalAlignment: style.VerticalAlignment);
         }
 
-        static SKBitmap CreateLabelAsBitmap(LabelStyle style, string text)
+        public static void Draw(SKCanvas canvas, LabelStyle style, IFeature feature, float x, float y)
         {
-            using (var paint = new SKPaint())
+            var text = style.GetLabelText(feature);
+            DrawLabel(canvas, x, y, style, text);
+        }
+
+        private static SKBitmap CreateLabelAsBitmap(LabelStyle style, string text)
+        {
+            using (var paint = CreatePaint(style))
             {
-                paint.TextSize = (float)style.Font.Size;
-                paint.IsAntialias = true;
-                paint.Color = style.ForeColor.ToSkia();
-                paint.Typeface = SKTypeface.FromFamilyName(style.Font.FontFamily);
-                paint.IsStroke = false;
-                paint.FakeBoldText = false;
-                paint.IsEmbeddedBitmapText = true;
+                return CreateLabelAsBitmap(style, text, paint);
+            }
+        }
 
-                var rect = new SKRect();
-                paint.MeasureText(text, ref rect);
+        private static SKBitmap CreateLabelAsBitmap(LabelStyle style, string text, SKPaint paint)
+        {
+            var rect = new SKRect();
+            paint.MeasureText(text, ref rect);
 
-                var padding = 3; // todo get this from LabelStyle
-                rect = SKRect.Inflate(rect, padding, padding);
+            var backRect = new SKRect(0, 0, rect.Width + 6, rect.Height + 6);
 
-                var bitmap = new SKBitmap((int)Math.Ceiling(rect.Width), (int)Math.Ceiling(rect.Height));
+            var bitmap = new SKBitmap((int)backRect.Width, (int)backRect.Height);
 
-                using (var target = new SKCanvas(bitmap))
+            using (var target = new SKCanvas(bitmap))
+            {
+                target.Clear();
+
+                DrawBackground(style, backRect, target);
+                target.DrawText(text, -rect.Left + 3, -rect.Top +3, paint);
+                return bitmap;
+            }
+        }
+
+        public static void DrawLabel(SKCanvas target, float x, float y, LabelStyle style, string text)
+        {
+            var paint = CreatePaint(style);
+
+            var rect = new SKRect();
+
+            paint.MeasureText(text, ref rect);
+
+            rect.Offset(
+                x - rect.Width*0.5f + (float) style.Offset.X,
+                y + rect.Height*0.5f + (float) style.Offset.Y);
+
+            var backRect = rect;
+            backRect.Inflate(3, 3);
+            DrawBackground(style, backRect, target);
+
+            target.DrawText(text, rect.Left, rect.Bottom, paint);
+        }
+
+        private static SKRect MeasureRect(string text, SKPaint paint)
+        {
+            var rect = new SKRect();
+            paint.MeasureText(text, ref rect);
+
+            var padding = 3; // todo get this from LabelStyle
+            rect = SKRect.Inflate(rect, padding, padding);
+            //rect = SnapToPixel(rect);
+            return rect;
+        }
+
+        private static SKRect SnapToPixel(SKRect rect)
+        {
+            return new SKRect(
+                (int) Math.Floor(rect.Left),
+                (int) Math.Floor(rect.Top),
+                (int) Math.Ceiling(rect.Right),
+                (int) Math.Ceiling(rect.Bottom));
+        }
+
+        private static void DrawBackground(LabelStyle style, SKRect rect, SKCanvas target)
+        {
+            if (style.BackColor != null)
+            {
+                var color = style.BackColor?.Color?.ToSkia();
+                if (color.HasValue)
                 {
-                    target.Clear();
-                    if (style.BackColor != null)
+                    var rounding = 6;
+                    using (var backgroundPaint = new SKPaint {Color = color.Value})
                     {
-                        var color = style.BackColor?.Color?.ToSkia();
-                        if (color.HasValue)
-                        {
-                            var rounding = 6;
-                            using (var backgroundPaint = new SKPaint { Color = color.Value })
-                            {
-                                target.DrawRoundRect(new SKRect(0, 0, rect.Width, rect.Height), rounding, rounding, backgroundPaint);
-                            }
-                        }
+                        target.DrawRoundRect(rect, rounding, rounding, backgroundPaint);
                     }
-                    target.DrawText(text, -rect.Left, -rect.Top, paint);
-                    return bitmap;
                 }
             }
+        }
+
+        private static SKPaint CreatePaint(LabelStyle style)
+        {
+            return new SKPaint
+            {
+                TextSize = (float) style.Font.Size,
+                IsAntialias = true,
+                Color = style.ForeColor.ToSkia(),
+                Typeface = SKTypeface.FromFamilyName(style.Font.FontFamily),
+                IsStroke = false,
+                FakeBoldText = false,
+                IsEmbeddedBitmapText = true
+            };
         }
     }
 }
