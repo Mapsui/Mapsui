2424601f726a3b30009ea4f11663da1b669d8752
Sergey Tregub <drwatson1@yandex.ru>
2016-11-17 18:47:58 +0300

    Create virtual top level layer while parsing WMS metadata if it is contains more than one layer on the top level

--- Mapsui.Desktop/Wms/Client.cs
+++ Mapsui.Desktop/Wms/Client.cs
@@ -398,10 +398,31 @@ namespace Mapsui.Web.Wms
             if (xnRequest == null)
                 throw (new Exception("Request parameter not specified in Service Description"));
             ParseRequest(xnRequest);
-            XmlNode xnLayer = xnCapability.SelectSingleNode("sm:Layer", nsmgr);
-            if (xnLayer == null)
-                throw (new Exception("No layer tag found in Service Description"));
-            Layer = ParseLayer(xnLayer);
+
+			// Workaround for some WMS servers that have returning more than one root layer
+			var layerNodes = xnCapability.SelectNodes("sm:Layer", nsmgr);
+			if (layerNodes.Count > 1)
+			{
+				List<WmsServerLayer> layers = new List<WmsServerLayer>();
+				foreach (XmlNode l in layerNodes)
+				{
+					layers.Add(ParseLayer(l));
+				}
+
+				var rootLayer = new WmsServerLayer();
+				rootLayer = layers[0];
+				rootLayer.Name = "__auto_generated_root_layer__";
+				rootLayer.Title = "";
+				rootLayer.ChildLayers = layers.ToArray();
+				Layer = rootLayer;
+			}
+			else
+			{
+				XmlNode xnLayer = xnCapability.SelectSingleNode("sm:Layer", nsmgr);
+				if (xnLayer == null)
+					throw (new Exception("No layer tag found in Service Description"));
+				Layer = ParseLayer(xnLayer);
+			}
 
             XmlNode xnException = xnCapability.SelectSingleNode("sm:Exception", nsmgr);
             if (xnException != null)
