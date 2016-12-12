daaa176b3714ab978d78c82dc5f55eb11a331096
pauldendulk <pauldendulk@gmail.com>
2016-12-12 22:30:07 +0100

    Resharper full cleanup

--- Mapsui.Rendering.OpenTK/MapRenderer.cs
+++ Mapsui.Rendering.OpenTK/MapRenderer.cs
@@ -1,23 +1,26 @@
-using Mapsui.Geometries;
-using Mapsui.Layers;
-using Mapsui.Providers;
-using Mapsui.Styles;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Mapsui.Geometries;
+using Mapsui.Layers;
+using Mapsui.Providers;
+using Mapsui.Styles;
 using OpenTK.Graphics.ES11;
 
 namespace Mapsui.Rendering.OpenTK
 {
     public class MapRenderer : IRenderer
     {
-        readonly IDictionary<int, TextureInfo> _symbolTextureCache = new Dictionary<int, TextureInfo>();
-        readonly IDictionary<object, TextureInfo> _tileTextureCache = new Dictionary<object, TextureInfo>(new IdentityComparer<object>());
-        private long _currentIteration;
         private const int TilesToKeepMultiplier = 3;
+        private readonly IDictionary<int, TextureInfo> _symbolTextureCache = new Dictionary<int, TextureInfo>();
 
-        public void Render(object target, IViewport viewport, IEnumerable<ILayer> layers,  Color background = null)
+        private readonly IDictionary<object, TextureInfo> _tileTextureCache =
+            new Dictionary<object, TextureInfo>(new IdentityComparer<object>());
+
+        private long _currentIteration;
+
+        public void Render(object target, IViewport viewport, IEnumerable<ILayer> layers, Color background = null)
         {
             layers = layers.ToList();
 
@@ -30,50 +33,60 @@ namespace Mapsui.Rendering.OpenTK
             _currentIteration++;
         }
 
-		public void DeleteAllBoundTextures()
-		{
-			DeleteAllTileTextures ();
-			DeleteAllSymbolTextures ();
-		}
+        public MemoryStream RenderToBitmapStream(IViewport viewport, IEnumerable<ILayer> layers,
+            Color background = null)
+        {
+            throw new NotImplementedException();
+        }
 
-		private void DeleteAllSymbolTextures()
-		{
-				foreach (var key in _symbolTextureCache.Keys) {
-					var textureInfo = _symbolTextureCache [key];
-					GL.BindTexture (All.Texture2D, 0);
-					GL.DeleteTextures (1, ref textureInfo.TextureId);
-				}
-				_symbolTextureCache.Clear ();
-		}
+        public void DeleteAllBoundTextures()
+        {
+            DeleteAllTileTextures();
+            DeleteAllSymbolTextures();
+        }
 
-		private void DeleteAllTileTextures()
-		{
-				foreach (var key in _tileTextureCache.Keys) {
-					var textureInfo = _tileTextureCache [key];
-					GL.BindTexture (All.Texture2D, 0);
-					GL.DeleteTextures (1, ref textureInfo.TextureId);
-				}
-				_tileTextureCache.Clear ();
-		}
+        private void DeleteAllSymbolTextures()
+        {
+            foreach (var key in _symbolTextureCache.Keys)
+            {
+                var textureInfo = _symbolTextureCache[key];
+                GL.BindTexture(All.Texture2D, 0);
+                GL.DeleteTextures(1, ref textureInfo.TextureId);
+            }
+            _symbolTextureCache.Clear();
+        }
+
+        private void DeleteAllTileTextures()
+        {
+            foreach (var key in _tileTextureCache.Keys)
+            {
+                var textureInfo = _tileTextureCache[key];
+                GL.BindTexture(All.Texture2D, 0);
+                GL.DeleteTextures(1, ref textureInfo.TextureId);
+            }
+            _tileTextureCache.Clear();
+        }
 
         private void RemoveUnusedTextureInfos()
         {
-			var numberOfTilesUsedInCurrentIteration = _tileTextureCache.Values.Count (i => i.IterationUsed == _currentIteration);
+            var numberOfTilesUsedInCurrentIteration =
+                _tileTextureCache.Values.Count(i => i.IterationUsed == _currentIteration);
 
-			var orderedKeys = _tileTextureCache.OrderBy (kvp => kvp.Value.IterationUsed).Select (kvp => kvp.Key).ToList ();
+            var orderedKeys = _tileTextureCache.OrderBy(kvp => kvp.Value.IterationUsed).Select(kvp => kvp.Key).ToList();
 
-			var counter = 0;
-			var tilesToKeep = orderedKeys.Count () * TilesToKeepMultiplier;
-			var numberToRemove = numberOfTilesUsedInCurrentIteration - tilesToKeep;
-			foreach (var key in orderedKeys) {
-				if (counter > numberToRemove)
-					break;
-				var textureInfo = _tileTextureCache [key];
-				_tileTextureCache.Remove (key);
-				GL.BindTexture (All.Texture2D, 0);
-				GL.DeleteTextures (1, ref textureInfo.TextureId);
-				counter++;
-			}
+            var counter = 0;
+            var tilesToKeep = orderedKeys.Count()*TilesToKeepMultiplier;
+            var numberToRemove = numberOfTilesUsedInCurrentIteration - tilesToKeep;
+            foreach (var key in orderedKeys)
+            {
+                if (counter > numberToRemove)
+                    break;
+                var textureInfo = _tileTextureCache[key];
+                _tileTextureCache.Remove(key);
+                GL.BindTexture(All.Texture2D, 0);
+                GL.DeleteTextures(1, ref textureInfo.TextureId);
+                counter++;
+            }
         }
 
         private void SetAllTextureInfosToUnused()
@@ -89,27 +102,13 @@ namespace Mapsui.Rendering.OpenTK
         private void RenderFeature(IViewport viewport, IStyle style, IFeature feature)
         {
             if (feature.Geometry is Point)
-            {
                 PointRenderer.Draw(viewport, style, feature, _symbolTextureCache);
-            }
             else if (feature.Geometry is LineString)
-            {
                 LineStringRenderer.Draw(viewport, style, feature);
-            }
             else if (feature.Geometry is Polygon)
-            {
                 PolygonRenderer.Draw(viewport, style, feature);
-            }
             else if (feature.Geometry is IRaster)
-            {
                 RasterRenderer.Draw(viewport, style, feature, _tileTextureCache, _currentIteration);
-            }
-        }
-
-        public MemoryStream RenderToBitmapStream(IViewport viewport, IEnumerable<ILayer> layers, 
-            Color background = null)
-        {
-             throw new NotImplementedException();
         }
     }
 
