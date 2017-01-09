97a34233e2495a1e0648a3dc56ba90800f903be6
pauldendulk <pauldendulk@gmail.com>
2017-01-09 20:52:34 +0100

    Moving to Mapsui.Sample.iOS again

---
+++ Samples/Mapsui.Samples.iOS/ViewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Mapsui.UI.iOS;
+using UIKit;
+
+namespace Mapsui.Samples.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var mapControl = new MapControlUIKit(View.Bounds)
+            {
+                Map = Common.AllSamples.CreateList().First(s => s.Key == "Various Layers").Value()
+            };
+            View.AddSubview(mapControl);
+        }
+    }
+}
