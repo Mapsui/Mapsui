e02fcb45a1cf392af5fe7fca22d404c7dcfc324a
pauldendulk <pauldendulk@gmail.com>
2017-01-10 22:43:54 +0100

    Just resharping Mercator

--- Mapsui/Projection/Mercator.cs
+++ Mapsui/Projection/Mercator.cs
@@ -6,15 +6,15 @@ namespace Mapsui.Projection
 	public class Mercator
 	{
 		private const double Radius = 6378137;
-		private const double e = 0.0818191908426;
+		private const double E = 0.0818191908426;
 		private const double D2R = Math.PI / 180;
 		private const double HalfPi = Math.PI / 2;
 		private const double PiDiv4 = Math.PI / 4;
 
-		private const double c1 = 0.00335655146887969;
-		private const double c2 = 0.00000657187271079536;
-		private const double c3 = 0.00000001764564338702;
-		private const double c4 = 0.00000000005328478445;
+		private const double C1 = 0.00335655146887969;
+		private const double C2 = 0.00000657187271079536;
+		private const double C3 = 0.00000001764564338702;
+		private const double C4 = 0.00000000005328478445;
 
 		public static Point FromLonLat(double lon, double lat)
 		{
@@ -23,7 +23,7 @@ namespace Mapsui.Projection
 
 			var x = Radius * lonRadians;
 			//y=a×ln[tan(π/4+φ/2)×((1-e×sinφ)/(1+e×sinφ))^(e/2)]
-			var y = Radius * Math.Log(Math.Tan(PiDiv4 + latRadians * 0.5) / Math.Pow(Math.Tan(PiDiv4 + Math.Asin(e * Math.Sin(latRadians)) / 2), e));
+			var y = Radius * Math.Log(Math.Tan(PiDiv4 + latRadians * 0.5) / Math.Pow(Math.Tan(PiDiv4 + Math.Asin(E * Math.Sin(latRadians)) / 2), E));
 
 			return new Point((float)x, (float)y);
 		}
@@ -31,7 +31,7 @@ namespace Mapsui.Projection
 		public static Point ToLonLat(double x, double y)
 		{
 			double g = HalfPi - 2 * Math.Atan(1 / Math.Exp(y / Radius));
-			double latRadians = g + c1 * Math.Sin(2 * g) + c2 * Math.Sin(4 * g) + c3 * Math.Sin(6 * g) + c4 * Math.Sin(8 * g);
+			double latRadians = g + C1 * Math.Sin(2 * g) + C2 * Math.Sin(4 * g) + C3 * Math.Sin(6 * g) + C4 * Math.Sin(8 * g);
 
 			var lonRadians = x / Radius;
 
