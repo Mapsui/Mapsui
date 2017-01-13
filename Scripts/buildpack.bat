8da0b0466565a26e6a88401470db46637eb274a3
pauldendulk <pauldendulk@gmail.com>
2017-01-13 21:15:49 +0100

    Build script started from sln folder

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 SETLOCAL
 SET VERSION=%1
-SET NUGET=.\..\tools\nuget\nuget.exe
+SET NUGET=tools\nuget\nuget.exe
 
-rmdir .\..\obj /s /q
-rmdir .\..\release /s /q
-msbuild updateversionnumber.proj /p:AsmVersion=%VERSION%
-msbuild build_with_ios.proj 
-%NUGET% pack Mapsui.nuspec -Version %VERSION% -outputdirectory .\..\Release
+rmdir obj /s /q
+rmdir release /s /q
+msbuild Scripts\updateversionnumber.proj /p:AsmVersion=%VERSION%
+msbuild Scripts\build_with_ios.proj 
+%NUGET% pack Scripts\Mapsui.nuspec -Version %VERSION% -outputdirectory Release
