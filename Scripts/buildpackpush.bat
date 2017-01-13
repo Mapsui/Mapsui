8e08b6709fbd4c6b31efe677cc6b9028e84a3928
pauldendulk <pauldendulk@gmail.com>
2017-01-13 22:19:23 +0100

    0.10.3

--- Scripts/buildpackpush.bat
+++ Scripts/buildpackpush.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 SETLOCAL
 SET VERSION=%1
-SET NUGET=.\..\tools\nuget\nuget.exe
+SET NUGET=.\tools\nuget\nuget.exe
 
-CALL buildpack %VERSION%
+CALL Scripts\buildpack %VERSION%
 ECHO buildpack done
-%NUGET% push .\..\Release\Mapsui.%VERSION%.nupkg -source nuget.org
+%NUGET% push .\Release\Mapsui.%VERSION%.nupkg -source nuget.org
 ECHO nuget push done
 git commit -m %VERSION% -a
 ECHO git commit done
