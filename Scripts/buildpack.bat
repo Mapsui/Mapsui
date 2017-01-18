2f7c518ea0f4ee0f406251bf4503e9f6aba7b700
pauldendulk <pauldendulk@gmail.com>
2017-01-18 09:13:00 +0100

    Writing nupkg to Release folder

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -4,7 +4,7 @@ SET VERSION=%1
 SET NUGET=tools\nuget\nuget.exe
 
 rmdir obj /s /q
-rmdir release /s /q
+rmdir Release /s /q
 msbuild Scripts\updateversionnumber.proj /p:AsmVersion=%VERSION%
 msbuild Scripts\build_with_ios.proj 
-%NUGET% pack Scripts\Mapsui.nuspec -Version %VERSION% -outputdirectory .
+%NUGET% pack Scripts\Mapsui.nuspec -Version %VERSION% -outputdirectory Release
