df83b93197fd2941ad282d09c76b7830a35e84ff
pauldendulk <pauldendulk@gmail.com>
2017-01-24 09:24:51 +0100

    Adding Mapsui's own CreateTileLayer that does not depend on BruTile's KnownTileSources

---
+++ Mapsui/Utilities/OpenStreetMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BruTile.Cache;
+using BruTile.Predefined;
+using BruTile.Web;
+using Mapsui.Layers;
+
+namespace Mapsui.Utilities
+{
+    public static class OpenStreetMap
+    {
+        private static readonly BruTile.Attribution OpenStreetMapAttribution = new BruTile.Attribution(
+            "© OpenStreetMap contributors", "http://www.openstreetmap.org/copyright");
+
+        public static TileLayer CreateTileLayer(
+            IPersistentCache<byte[]> persistentCache = null, Func<Uri, byte[]> tileFetcher = null)
+        {
+            return new TileLayer(new HttpTileSource(new GlobalSphericalMercator(0, 18),
+                        "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        new[] { "a", "b", "c" }, name: "OpenStreetMap",
+                        persistentCache: persistentCache, tileFetcher: tileFetcher,
+                        attribution: OpenStreetMapAttribution));
+        }
+    }
+}
