0de9cd599c87c431c419bb85821dcd9c261117a4
pauldendulk <pauldendulk@gmail.com>
2017-03-30 22:35:31 +0200

    Starting on MbTiles sample

---
+++ Samples/Mapsui.Samples.Common/Maps/MbTilesSample.cs
@@ -0,0 +1,17 @@
+﻿
+using Mapsui.Utilities;
+
+namespace Mapsui.Samples.Common.Maps
+{
+    public static class MbTilesSample
+    {
+
+        public static Map CreateMap()
+        {
+            var map = new Map();
+            map.Layers.Add(OpenStreetMap.CreateTileLayer());
+            return map;
+        }
+
+    }
+}
