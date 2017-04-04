df4979e2f1d8451b51e0e8a4e654e41480556fe2
pauldendulk <pauldendulk@gmail.com>
2017-04-04 22:30:01 +0200

    Adding unit tests for geometry equals, related to #168

---
+++ Tests/Mapsui.Geometries.Tests/TestGeometries.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Mapsui.Geometries.Tests
+{
+    [TestFixture]
+    public class TestGeometries
+    {
+        [Test]
+        public void TestPointEquals()
+        {
+            // arrange
+            var point = new Point(0, 42);
+            var pointSame = new Point(0, 42);
+            var pointDifferent = new Point(42, 1337);
+
+            // assert
+            Assert.IsTrue(point.Equals(pointSame));
+            Assert.IsFalse(point.Equals(pointDifferent));
+        }
+
+        [Test]
+        public void TestLineStringEquals()
+        {
+            // arrange
+            var lineString = new LineString(new[] {new Point(0, 42), new Point(42, 1337)});
+            var lineStringSame = new LineString(new[] { new Point(0, 42), new Point(42, 1337) });
+            var lineStringDifferent = new LineString(new[] { new Point(42, 1337), new Point(0, 42) });
+            
+            // assert
+            Assert.IsTrue(lineString.Equals(lineStringSame));
+            Assert.IsFalse(lineString.Equals(lineStringDifferent));
+        }
+
+        [Test]
+        public void TestPolygonEquals()
+        {
+            // arrange
+            var point1 = new Point(0, 42);
+            var point2 = new Point(42, 1337);
+            var point3 = new Point(1337, 0);
+
+            var polygon = new Polygon(new LinearRing(new List<Point> { point1, point2, point3 }));
+            var polygonSame = new Polygon(new LinearRing(new List<Point> { point1, point2, point3 }));
+            var polygonDifferent = new Polygon(new LinearRing(new List<Point> { point1, point3, point2 }));
+
+            // assert
+            Assert.IsFalse(polygon.Equals(point1));
+            Assert.IsTrue(polygon.Equals(polygonSame));
+            Assert.IsFalse(polygon.Equals(polygonDifferent));
+        }
+    }
+}
