99459042cfb77a4248db73adcd06a816c0237cc4
pauldendulk <pauldendulk@gmail.com>
2017-04-04 22:47:34 +0200

    Also testing the Equals(Geometry geometry) overloads, related to #168

--- Tests/Mapsui.Geometries.Tests/TestGeometries.cs
+++ Tests/Mapsui.Geometries.Tests/TestGeometries.cs
@@ -13,10 +13,12 @@ namespace Mapsui.Geometries.Tests
             var point = new Point(0, 42);
             var pointSame = new Point(0, 42);
             var pointDifferent = new Point(42, 1337);
+            Geometry geometry = pointSame;
 
             // assert
             Assert.IsTrue(point.Equals(pointSame));
             Assert.IsFalse(point.Equals(pointDifferent));
+            Assert.IsTrue(point.Equals(geometry));
         }
 
         [Test]
@@ -26,10 +28,12 @@ namespace Mapsui.Geometries.Tests
             var lineString = new LineString(new[] {new Point(0, 42), new Point(42, 1337)});
             var lineStringSame = new LineString(new[] { new Point(0, 42), new Point(42, 1337) });
             var lineStringDifferent = new LineString(new[] { new Point(42, 1337), new Point(0, 42) });
+            Geometry geometry = lineStringSame;
             
             // assert
             Assert.IsTrue(lineString.Equals(lineStringSame));
             Assert.IsFalse(lineString.Equals(lineStringDifferent));
+            Assert.IsTrue(lineString.Equals(geometry));
         }
 
         [Test]
@@ -43,11 +47,13 @@ namespace Mapsui.Geometries.Tests
             var polygon = new Polygon(new LinearRing(new List<Point> { point1, point2, point3 }));
             var polygonSame = new Polygon(new LinearRing(new List<Point> { point1, point2, point3 }));
             var polygonDifferent = new Polygon(new LinearRing(new List<Point> { point1, point3, point2 }));
+            Geometry geometry = polygonSame;
 
             // assert
             Assert.IsFalse(polygon.Equals(point1));
             Assert.IsTrue(polygon.Equals(polygonSame));
             Assert.IsFalse(polygon.Equals(polygonDifferent));
+            Assert.IsTrue(polygon.Equals(geometry));
         }
     }
 }
