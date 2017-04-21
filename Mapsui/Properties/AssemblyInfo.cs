9312b8b4e5120c4cbdaf19e0c87f5ae6a00845e3
pauldendulk <pauldendulk@gmail.com>
2017-04-21 11:33:40 +0200

    0.17.0

--- Mapsui/Properties/AssemblyInfo.cs
+++ Mapsui/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.9.0")]
-[assembly: AssemblyVersion("0.9.0")]
-[assembly: AssemblyFileVersion("0.9.0")]
+// [assembly: AssemblyVersion("0.17.0")]
+[assembly: AssemblyVersion("0.17.0")]
+[assembly: AssemblyFileVersion("0.17.0")]
