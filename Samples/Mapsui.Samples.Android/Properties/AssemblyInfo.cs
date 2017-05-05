c10ced422949e409753d48e79635914e870ad533
pauldendulk <pauldendulk@gmail.com>
2017-05-05 15:52:19 +0200

    1.0.7

--- Samples/Mapsui.Samples.Android/Properties/AssemblyInfo.cs
+++ Samples/Mapsui.Samples.Android/Properties/AssemblyInfo.cs
@@ -24,9 +24,9 @@ using Android.App;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.6")]
-[assembly: AssemblyVersion("1.0.6")]
-[assembly: AssemblyFileVersion("1.0.6")]
+// [assembly: AssemblyVersion("1.0.7")]
+[assembly: AssemblyVersion("1.0.7")]
+[assembly: AssemblyFileVersion("1.0.7")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
