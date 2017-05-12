54c4aad892a6c5d8186533c45320279e6ee02f62
pauldendulk <pauldendulk@gmail.com>
2017-05-12 10:30:17 +0200

    Adding Utilities.StreamExtensions

---
+++ Mapsui/ExtensionMethods/StreamExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Mapsui
+{
+    public static class StreamExtensions
+    {
+        public static byte[] ToBytes(this Stream input)
+        {
+            using (var ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
