a12205ec4f9a16bda8b41a568f2bea88b74cade1
pauldendulk <pauldendulk@gmail.com>
2017-05-25 17:05:16 +0200

    1.0.9

--- Mapsui.Geometries/Properties/AssemblyInfo.cs
+++ Mapsui.Geometries/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.8")]
+// [assembly: AssemblyVersion("1.0.9")]
 
-[assembly: AssemblyVersion("1.0.8")]
-[assembly: AssemblyFileVersion("1.0.8")]
+[assembly: AssemblyVersion("1.0.9")]
+[assembly: AssemblyFileVersion("1.0.9")]
--- Samples/Mapsui.Samples.Wpf/Properties/AssemblyInfo.cs
+++ Samples/Mapsui.Samples.Wpf/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.8")]
-[assembly: AssemblyVersion("1.0.8")]
-[assembly: AssemblyFileVersion("1.0.8")]
+// [assembly: AssemblyVersion("1.0.9")]
+[assembly: AssemblyVersion("1.0.9")]
+[assembly: AssemblyFileVersion("1.0.9")]
