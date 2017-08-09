88c4fd41f622322b2c47fd27f3802ca5eabfae88
pauldendulk <pauldendulk@gmail.com>
2017-08-09 19:50:29 +0200

    Fixed offset of raster in skia when using rotation

--- Mapsui.Rendering.Skia-PCL/RasterRenderer.cs
+++ Mapsui.Rendering.Skia-PCL/RasterRenderer.cs
@@ -13,66 +13,61 @@ namespace Mapsui.Rendering.Skia
 		public static void Draw (SKCanvas canvas, IViewport viewport, IStyle style, IFeature feature,
 			IDictionary<object, BitmapInfo> tileCache, long currentIteration)
 		{
-			try
-			{
-				var raster = (IRaster)feature.Geometry;
+		    try
+		    {
+		        var raster = (IRaster)feature.Geometry;
 
-				BitmapInfo bitmapInfo;
+		        BitmapInfo bitmapInfo;
 
-				if (!tileCache.Keys.Contains (raster))
-				{
-					bitmapInfo = BitmapHelper.LoadBitmap (raster.Data);
-					tileCache [raster] = bitmapInfo;
-				}
-				else
-				{
-					bitmapInfo = tileCache [raster];
-				}
+		        if (!tileCache.Keys.Contains(raster))
+		        {
+		            bitmapInfo = BitmapHelper.LoadBitmap(raster.Data);
+		            tileCache[raster] = bitmapInfo;
+		        }
+		        else
+		        {
+		            bitmapInfo = tileCache[raster];
+		        }
 
-				bitmapInfo.IterationUsed = currentIteration;
-				tileCache [raster] = bitmapInfo;
+		        bitmapInfo.IterationUsed = currentIteration;
+		        tileCache[raster] = bitmapInfo;
 
-				var boundingBox = feature.Geometry.GetBoundingBox ();
+		        var boundingBox = feature.Geometry.GetBoundingBox();
 
-				var priorMatrix = canvas.TotalMatrix;
+		        var priorMatrix = canvas.TotalMatrix;
 
-				SKMatrix matrix;
+		        SKMatrix matrix;
 
-				{
-					//The front-end sets up the canvas with a matrix based on screen scaling (e.g. retina).
-					//We need to retain that effect by combining our matrix with the incoming matrix.
+		        {
+		            //The front-end sets up the canvas with a matrix based on screen scaling (e.g. retina).
+		            //We need to retain that effect by combining our matrix with the incoming matrix.
 
-					//We'll create four matrices in addition to the incoming matrix. They perform the
-					//zoom scale, focal point offset, user rotation and finally, centering in the screen.
+		            //We'll create four matrices in addition to the incoming matrix. They perform the
+		            //zoom scale, focal point offset, user rotation and finally, centering in the screen.
 
-					var userRotation = SKMatrix.MakeRotationDegrees ((float)viewport.Rotation);
-					var focalPointOffset = SKMatrix.MakeTranslation (
-						(float)(boundingBox.Left - viewport.Center.X),
-						(float)(viewport.Center.Y - boundingBox.Top));
-					var zoomScale = SKMatrix.MakeScale ((float)(1.0 / viewport.Resolution), (float)(1.0 / viewport.Resolution));
-					var centerInScreen = SKMatrix.MakeTranslation ((float)(viewport.Width / 2.0), (float)(viewport.Height / 2.0));
+		            var userRotation = SKMatrix.MakeRotationDegrees((float)viewport.Rotation);
+		            var focalPointOffset = SKMatrix.MakeTranslation(
+		                (float)(boundingBox.Left - viewport.Center.X),
+		                (float)(viewport.Center.Y - boundingBox.Top));
+		            var zoomScale = SKMatrix.MakeScale((float)(1.0 / viewport.Resolution), (float)(1.0 / viewport.Resolution));
+		            var centerInScreen = SKMatrix.MakeTranslation((float)(viewport.Width / 2.0), (float)(viewport.Height / 2.0));
 
-					//We'll concatenate them like so: incomingMatrix * centerInScreen * userRotation * zoomScale * focalPointOffset
+		            //We'll concatenate them like so: incomingMatrix * centerInScreen * userRotation * zoomScale * focalPointOffset
 
-					var bufferOne = new SKMatrix ();
-					var bufferTwo = new SKMatrix ();
+		            SKMatrix.Concat(ref matrix, zoomScale, focalPointOffset);
+		            SKMatrix.Concat(ref matrix, userRotation, matrix);
+		            SKMatrix.Concat(ref matrix, centerInScreen, matrix);
+		            SKMatrix.Concat(ref matrix, priorMatrix, matrix);
+		        }
 
-					SKMatrix.Concat (ref bufferOne, zoomScale, focalPointOffset);
-					SKMatrix.Concat (ref bufferTwo, userRotation, bufferOne);
-					SKMatrix.Concat (ref bufferOne, centerInScreen, bufferTwo);
-					SKMatrix.Concat (ref bufferTwo, priorMatrix, bufferOne);
+		        canvas.SetMatrix(matrix);
 
-					matrix = bufferTwo;
-				}
+		        var destination = new BoundingBox(0.0, 0.0, boundingBox.Width, boundingBox.Height);
 
-				canvas.SetMatrix (matrix);
+		        BitmapHelper.RenderRaster(canvas, bitmapInfo.Bitmap, destination.ToSkia());
 
-				var destination = new BoundingBox (0.0, 0.0, boundingBox.Width, boundingBox.Height);
-
-				BitmapHelper.RenderRaster (canvas, bitmapInfo.Bitmap, RoundToPixel (destination).ToSkia ());
-
-				canvas.SetMatrix (priorMatrix);
-			}
+		        canvas.SetMatrix(priorMatrix);
+            }
 			catch (Exception ex)
 			{
 				Logger.Log (LogLevel.Error, ex.Message, ex);
