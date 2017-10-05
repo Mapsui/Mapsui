c481f5162a9a165770a6b8f9da553f4e8738b1ed
pauldendulk <pauldendulk@gmail.com>
2017-10-05 17:50:37 +0200

    Added MinimalFetchStrategy

---
+++ Mapsui/Fetcher/MinimalFetchStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BruTile;
+using Mapsui.Geometries.Utilities;
+
+namespace Mapsui.Fetcher
+{
+    public class MinimalFetchStrategy : IFetchStrategy
+    {
+        public IList<TileInfo> GetTilesWanted(ITileSchema schema, Extent extent, string levelId)
+        {
+            return schema.GetTileInfos(extent, levelId).OrderBy(
+                t => Algorithms.Distance(extent.CenterX, extent.CenterY, t.Extent.CenterX, t.Extent.CenterY)).ToList();
+        }
+    }
+}
