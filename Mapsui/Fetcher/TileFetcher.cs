b948205c508e5ac612e1e988c0c7412eaf8a7fea
pauldendulk <pauldendulk@gmail.com>
2017-10-05 17:53:07 +0200

    Tiny steps on TileFetcher

--- Mapsui/Fetcher/TileFetcher.cs
+++ Mapsui/Fetcher/TileFetcher.cs
@@ -18,6 +18,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -25,6 +26,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using BruTile;
 using BruTile.Cache;
+using ConcurrentCollections;
 using Mapsui.Annotations;
 using Mapsui.Geometries;
 using Mapsui.Providers;
@@ -38,18 +40,16 @@ namespace Mapsui.Fetcher
         private readonly ITileSource _tileSource;
         private BoundingBox _extent;
         private double _resolution;
-        private readonly IList<TileIndex> _tilesInProgress = new List<TileIndex>();
+        private readonly ConcurrentHashSet<TileIndex> _tilesInProgress = new ConcurrentHashSet<TileIndex>();
         private readonly IFetchStrategy _strategy;
         private volatile bool _viewportChanged;
         private bool _busy;
         private int _numberTilesNeeded;
 		private Task _fetchLoopTask;
         private CancellationTokenSource _fetchLoopCancellationTokenSource;
-        private readonly int _maxRequests = 32;
+        private readonly int _maxRequests = int.MaxValue;
         private int _currentRequests;
-        //private readonly EventWaitHandle _maxRequestsWaitHandle = new AutoResetEvent(true);
-   
-
+        private readonly EventWaitHandle _maxRequestsWaitHandle = new AutoResetEvent(true);
         public event DataChangedEventHandler DataChanged;
 
 		public TileFetcher (ITileSource tileSource, MemoryCache<Feature> memoryCache, int maxRetries, int maxThreads, IFetchStrategy strategy = null)
@@ -72,74 +72,24 @@ namespace Mapsui.Fetcher
 
         public int NumberTilesNeeded => _numberTilesNeeded;
 
-        public void ViewChanged(BoundingBox newExtent, double newResolution)
-        {
-            _extent = newExtent;
-            _resolution = newResolution;
-            _viewportChanged = true;
-
-			if (_fetchLoopTask == null || _fetchLoopTask.IsCompleted || _fetchLoopCancellationTokenSource.IsCancellationRequested)
-			{
-				Busy = true;
-				_fetchLoopCancellationTokenSource = new CancellationTokenSource();
-				var fetchLoopCancellationToken = _fetchLoopCancellationTokenSource.Token;
-				_fetchLoopTask = Task.Run (() => TileFetchLoop(fetchLoopCancellationToken), fetchLoopCancellationToken);
-			}
-        }
-
-		public void AbortFetch()
+        public void AbortFetch()
 		{
 			_fetchLoopCancellationTokenSource?.Cancel();
         }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        void TileFetchLoop(CancellationToken globalCancellationToken)
-        {
-            List<TileInfo> tilesMissing = null;
-
-            if (_tileSource.Schema == null) Busy = false;
-
-		    while (Busy && !globalCancellationToken.IsCancellationRequested)
-		    {
-		        // 1) If the viewport is changed you need to calculate the needed tiles again.
-		        if (_viewportChanged)
-		        {
-		            var tilesNeeded = GetTilesNeeded();
-                    // assign needed tiles to missing tiles and over every iteration
-                    // missing tiles will be smaller. This has a big impact on performance.
-		            tilesMissing = tilesNeeded;
-                    _viewportChanged = false;
-		        }
-
-		        // 2) From the needed tiles get those that are still missing
-		        tilesMissing = GetMissingTiles(tilesMissing);
-
-		        // 3) Check if we are done
-		        if (!tilesMissing.Any()) { Busy = false; }
-                
-                // 4) Actually fetch the tiles missing.
-                if (_currentRequests <  _maxRequests)
-		            FetchMissingTiles(tilesMissing, globalCancellationToken);
-		    }
-		}
-
+        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void FetchMissingTiles(List<TileInfo> missingTiles, CancellationToken globalCancellationToken)
         {
             foreach (var info in missingTiles)
             {
+                if (!_tilesInProgress.Add(info.Index)) continue;
+   
+                _currentRequests++;
                 if (_currentRequests >= _maxRequests)
                 {
-                    //_maxRequestsWaitHandle.Stop();
-                    //_maxRequestsWaitHandle.WaitOne();
+                    _maxRequestsWaitHandle.Stop();
                 }
-                lock (_tilesInProgress)
-                {
-                    if (_tilesInProgress.Contains(info.Index)) return;
-                    _tilesInProgress.Add(info.Index);
-                }
-
-                _currentRequests++;
+                
                 // Not passing the cancellation token to Task.Run. 
                 // Cancelling because of a viewport change can be ineffective because most of the tiles
                 // will still be useful, even if they are outside of the viewport at that moment.
@@ -150,7 +100,7 @@ namespace Mapsui.Fetcher
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private List<TileInfo> GetMissingTiles(List<TileInfo> tilesNeeded)
+        private List<TileInfo> GetTilesNotInCache(List<TileInfo> tilesNeeded)
         {
             var missingTiles = new List<TileInfo>();
 
@@ -187,15 +137,12 @@ namespace Mapsui.Fetcher
             }
             finally
             {
-                lock (_tilesInProgress)
-                {
-                    if (_tilesInProgress.Contains(tileInfo.Index))
-                        _tilesInProgress.Remove(tileInfo.Index);
-                }
-                _currentRequests--;
-                //if (_currentRequests < _maxRequests) _maxRequestsWaitHandle.Go();
-            }
+                _tilesInProgress.TryRemove(tileInfo.Index);
 
+
+                _currentRequests--;
+                if (_currentRequests < _maxRequests) _maxRequestsWaitHandle.Go();
+            }
         }
 
         private static Raster CreateTileGeometry(TileInfo tileInfo, byte[] tileData)
@@ -220,6 +167,72 @@ namespace Mapsui.Fetcher
         {
             var handler = PropertyChanged;
             handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public long NumberOfTimesLoopStarted;
+
+        public void ViewChanged(BoundingBox newExtent, double newResolution)
+        {
+            _extent = newExtent;
+            _resolution = newResolution;
+            _viewportChanged = true;
+
+            if (_fetchLoopTask == null || _fetchLoopTask.IsCompleted || _fetchLoopCancellationTokenSource.IsCancellationRequested)
+            {
+                Busy = true;
+                _fetchLoopCancellationTokenSource = new CancellationTokenSource();
+                var fetchLoopCancellationToken = _fetchLoopCancellationTokenSource.Token;
+
+                NumberOfTimesLoopStarted++;
+                Debug.WriteLine($"New loop {NumberOfTimesLoopStarted}");
+
+                _fetchLoopTask = Task.Run(() => TileFetchLoop(fetchLoopCancellationToken, _fetchLoopCancellationTokenSource), fetchLoopCancellationToken);
+            }
+        }
+
+        public long IterationsInLoop;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void TileFetchLoop(CancellationToken globalCancellationToken, CancellationTokenSource globalCancellationTokenSource)
+        {
+            List<TileInfo> tilesMissing = null;
+
+            if (_tileSource.Schema == null)
+            {
+                Busy = false;
+                return;
+            }
+
+            while (!globalCancellationToken.IsCancellationRequested)
+            {
+                // 1) If the viewport is changed you need to calculate the needed tiles again.
+                if (_viewportChanged)
+                {
+                    var tilesNeeded = GetTilesNeeded();
+                    // assign needed tiles to missing tiles and work with the missing tiles.
+                    // This set will become smaller and smaller over every iteration as long
+                    // as the viewport does not change  This has a big impact on performance.
+                    tilesMissing = tilesNeeded;
+                    _viewportChanged = false;
+                }
+                
+                // 2) From the needed tiles get those that are still missing
+                tilesMissing = GetTilesNotInCache(tilesMissing);
+           
+                // 3) Check if we are done
+                if (!tilesMissing.Any())
+                {
+                    Busy = false;
+                    globalCancellationTokenSource.Cancel();
+                }
+
+                // 4) Actually fetch the tiles missing.
+                if (_currentRequests < _maxRequests)
+                    FetchMissingTiles(tilesMissing, globalCancellationToken);
+
+                IterationsInLoop++;
+                Debug.WriteLine($"New loop {IterationsInLoop}");
+            }
         }
     }
 }
