a410adbe7c581473d12c8cfdd7269e27355fd18c
pauldendulk <pauldendulk@gmail.com>
2017-10-13 16:23:51 +0200

    1.1.1

--- Mapsui.UI.Android/Properties/AssemblyInfo.cs
+++ Mapsui.UI.Android/Properties/AssemblyInfo.cs
@@ -24,9 +24,9 @@ using Android.App;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+// [assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
