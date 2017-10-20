4e42063b0654fed7e1a239d3b57654d6047f04f1
pauldendulk <pauldendulk@gmail.com>
2017-10-20 16:58:52 +0200

    1.1.2

--- Samples/Mapsui.Samples.Wpf/Properties/AssemblyInfo.cs
+++ Samples/Mapsui.Samples.Wpf/Properties/AssemblyInfo.cs
@@ -48,6 +48,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
+// [assembly: AssemblyVersion("1.1.2")]
+[assembly: AssemblyVersion("1.1.2")]
+[assembly: AssemblyFileVersion("1.1.2")]
