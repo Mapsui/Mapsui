b8852e3565a0b23fbbe49b9bad89fbb6f17c7353
pauldendulk <pauldendulk@gmail.com>
2017-11-01 23:22:53 +0100

    1.1.3-pre1

--- Mapsui.UI.Android/Properties/AssemblyInfo.cs
+++ Mapsui.UI.Android/Properties/AssemblyInfo.cs
@@ -24,9 +24,9 @@ using Android.App;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.2")]
-[assembly: AssemblyVersion("1.1.2")]
-[assembly: AssemblyFileVersion("1.1.2")]
+// [assembly: AssemblyVersion("1.1.3")]
+[assembly: AssemblyVersion("1.1.3")]
+[assembly: AssemblyFileVersion("1.1.3-pre1")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
--- Mapsui/Properties/AssemblyInfo.cs
+++ Mapsui/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.2")]
-[assembly: AssemblyVersion("1.1.2")]
-[assembly: AssemblyFileVersion("1.1.2")]
+// [assembly: AssemblyVersion("1.1.3")]
+[assembly: AssemblyVersion("1.1.3")]
+[assembly: AssemblyFileVersion("1.1.3-pre1")]
