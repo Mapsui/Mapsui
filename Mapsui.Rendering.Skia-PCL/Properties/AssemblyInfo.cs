85617c91f7a61b438060c1f05892de19d971135f
pauldendulk <pauldendulk@gmail.com>
2017-12-15 16:45:55 +0100

    1.2.1

--- Mapsui.Rendering.Skia-PCL/Properties/AssemblyInfo.cs
+++ Mapsui.Rendering.Skia-PCL/Properties/AssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.1.3")]
 [assembly: AssemblyVersion("1.1.3")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.1")]
--- Mapsui/Properties/AssemblyInfo.cs
+++ Mapsui/Properties/AssemblyInfo.cs
@@ -29,4 +29,4 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.1.3")]
 [assembly: AssemblyVersion("1.1.3")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.1")]
