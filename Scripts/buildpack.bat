f904d45766abc5ed029ecea0428d7b769d6a4907
pauldendulk <pauldendulk@gmail.com>
2017-12-22 11:25:49 +0100

    Adding bumpy

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -4,6 +4,7 @@ SET VERSION=%1
 
 rmdir obj /s /q
 rmdir Release /s /q
+choco install bumpy.portable
 msbuild Scripts\updateversionnumber.proj /p:AsmVersion=%VERSION%  
 msbuild Scripts\build.proj
 nuget pack Scripts\Mapsui.nuspec -Version %VERSION% -outputdirectory Release
