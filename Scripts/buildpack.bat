f69d5888de6ed08271de0a94f0f307d2ced9a1c0
pauldendulk <pauldendulk@gmail.com>
2017-12-24 09:44:08 +0100

    Building VersionUpdater from script

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -4,7 +4,7 @@ SET VERSION=%1
 
 rmdir obj /s /q
 rmdir Release /s /q
+msbuild tools\versionupdater\versionupdater.csproj /p:Configuration=Release /p:OutputPath=..
 tools\versionupdater /version:%VERSION%
-REM msbuild Scripts\updateversionnumber.proj /p:AsmVersion=%VERSION%  
 msbuild Scripts\build.proj
 nuget pack NuSpec\Mapsui.nuspec -Version %VERSION% -outputdirectory Release
