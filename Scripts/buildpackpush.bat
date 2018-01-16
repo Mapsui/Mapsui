6f2905fbeb807abd301aa2f357defef9b89916ca
pauldendulk <pauldendulk@gmail.com>
2018-01-16 20:21:15 +0100

    Cleaning up buildpackpush.bat

--- Scripts/buildpackpush.bat
+++ Scripts/buildpackpush.bat
@@ -1,14 +1,8 @@
 SETLOCAL
 SET VERSION=%1
-
 CALL Scripts\buildpack %VERSION%
-ECHO buildpack done
 nuget push .\Release\Mapsui.%VERSION%.nupkg -source nuget.org
-ECHO nuget push done
 git commit -m %VERSION% -a
-ECHO git commit done
 git tag %VERSION%
 git push origin %VERSION%
-ECHO git tag done
 git push
-ECHO git push done
