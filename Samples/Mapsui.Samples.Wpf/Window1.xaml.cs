1fed8f86be1a33edfe6a0ffdbbf0b4e7abc231b6
pauldendulk <pauldendulk@gmail.com>
2018-02-02 21:42:14 +0100

    In sample rearranging log display

--- Samples/Mapsui.Samples.Wpf/Window1.xaml.cs
+++ Samples/Mapsui.Samples.Wpf/Window1.xaml.cs
@@ -133,7 +133,7 @@ namespace Mapsui.Samples.Wpf
             return radioButton;
         }
 
-        readonly LimitedQueue<LogModel> _logMessage = new LimitedQueue<LogModel>(10); 
+        readonly LimitedQueue<LogModel> _logMessage = new LimitedQueue<LogModel>(6); 
 
         private void LogMethod(LogLevel logLevel, string message, Exception exception)
         {
