d22473fe721ac21f1bd6b86861040ec5e12853e6
pauldendulk <pauldendulk@gmail.com>
2018-02-22 09:00:46 +0100

    1.3.2

--- Mapsui/Properties/AssemblyInfo.cs
+++ Mapsui/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.CompilerServices;
  
 
 
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.2")]
+[assembly: AssemblyFileVersion("1.3.2")]
+[assembly: AssemblyInformationalVersion("1.3.2")]
--- Samples/Mapsui.Samples.Uwp/Properties/AssemblyInfo.cs
+++ Samples/Mapsui.Samples.Uwp/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
  
 
 
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.2")]
+[assembly: AssemblyFileVersion("1.3.2")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyInformationalVersion("1.3.0")]
+[assembly: AssemblyInformationalVersion("1.3.2")]
