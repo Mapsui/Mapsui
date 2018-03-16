3f2b65d77b44273183c18f302d67f534ab69a7c8
Charlenni <info@wfplayer.com>
2018-03-16 13:19:35 +0100

    Moved MapControl to shared project Moved all platform dependent MapControls to a shared project basis, so that same code could be reused on all platforms. No changes for namespaces.
    Perhaps it could be moved more code to shared project. This wouldn't tested.

---
+++ Mapsui.UI.Shared/EventArgs/TouchedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mapsui.UI
+{
+    public class TouchedEventArgs : EventArgs
+    {
+        public List<Geometries.Point> ScreenPoints { get; }
+        public bool Handled { get; set; } = false;
+
+        public TouchedEventArgs(List<Geometries.Point> screenPoints)
+        {
+            ScreenPoints = screenPoints;
+        }
+    }
+}
