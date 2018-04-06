f3d9830c552e10d3ea31f41203bb630928a0a910
Paul den Dulk <pauldendulk@gmail.com>
2018-04-06 16:41:48 +0200

    Adding rotate to IViewport.ToSKMatrix

--- Mapsui.Rendering.Skia-PCL/ExtensionMethods/ViewportExtensions.cs
+++ Mapsui.Rendering.Skia-PCL/ExtensionMethods/ViewportExtensions.cs
@@ -13,6 +13,12 @@ namespace Mapsui.Rendering.Skia
             var matrix = SKMatrix.MakeIdentity();
             SKMatrix.Concat(ref matrix, matrix, SKMatrix.MakeScale(invertedResolution, invertedResolution, mapCenterX, mapCenterY));
             SKMatrix.Concat(ref matrix, matrix, SKMatrix.MakeScale(1, -1, 0, -mapCenterY)); // As a consequence images will be up side down :(
+
+            if (viewport.IsRotated)
+            {
+                SKMatrix.Concat(ref matrix, matrix, SKMatrix.MakeRotationDegrees((float)-viewport.Rotation));
+            }
+
             SKMatrix.Concat(ref matrix, matrix, SKMatrix.MakeTranslation((float)-viewport.Center.X, (float)-viewport.Center.Y));
 
             return matrix;
