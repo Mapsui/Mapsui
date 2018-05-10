d3485f45242aae8793eaf1b152b09c26b916dba2
Paul den Dulk <pauldendulk@gmail.com>
2018-05-10 12:42:31 +0200

    Fighting git

--- Samples/Mapsui.Samples.Android/Resources/Resource.Designer.cs
+++ Samples/Mapsui.Samples.Android/Resources/Resource.Designer.cs
@@ -63,11 +63,8 @@ namespace Mapsui.Samples.Android
 			// aapt resource value: 0x7f050000
 			public const int mainLayout = 2131034112;
 			
-			// aapt resource value: 0x7f050002
-			public const int mapcontrol = 2131034114;
-			
 			// aapt resource value: 0x7f050001
-			public const int textView1 = 2131034113;
+			public const int mapcontrol = 2131034113;
 			
 			static Id()
 			{
