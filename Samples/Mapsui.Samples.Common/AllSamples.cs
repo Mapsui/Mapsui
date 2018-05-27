dd0806363a06530c52cd06ea222a6720e69d15e7
Paul den Dulk <pauldendulk@gmail.com>
2018-05-27 16:19:07 +0200

    Added empty itinero routing to sample list

--- Samples/Mapsui.Samples.Common/AllSamples.cs
+++ Samples/Mapsui.Samples.Common/AllSamples.cs
@@ -38,6 +38,7 @@ namespace Mapsui.Samples.Common
                 ["OpacityStyle"] = OpacityStyleSample.CreateMap,
                 ["Svg"] = SvgSample.CreateMap,
                 ["Atlas"] = AtlasSample.CreateMap,
+                ["Itinero routing"] = ItineroRoutingSample.CreateMap
             };
         }
     }
