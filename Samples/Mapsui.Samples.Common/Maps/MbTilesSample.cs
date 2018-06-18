15018ad9bbbb8527533ca657412064824261bb3a
charlenni <web@weltz-online.de>
2018-06-18 12:33:52 +0200

    Added Mac sample for Xamarin.Forms MapControl

--- Samples/Mapsui.Samples.Common/Maps/MbTilesSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/MbTilesSample.cs
@@ -8,7 +8,9 @@ namespace Mapsui.Samples.Common.Maps
     public static class MbTilesSample
     {
         // This is a hack used for iOS/Android deployment
-        public static string MbTilesLocation { get; set; } = @".\MbTiles\";
+        // For Mac it should be @"." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "MbTiles";
+        //public static string MbTilesLocation { get; set; } = @"." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "MbTiles";
+        public static string MbTilesLocation { get; set; } = @"." + Path.DirectorySeparatorChar + "MbTiles";
 
         public static Map CreateMap()
         {
---
+++ Samples/Mapsui.Samples.Forms/Mapsui.Samples.Forms.Mac/AppDelegate.cs
@@ -0,0 +1,39 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace Mapsui.Samples.Forms.Mac
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow window;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Titled | NSWindowStyle.Resizable;
+            var rect = new CoreGraphics.CGRect(0, 0, 800, 600);
+
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+
+            window.Title = "Mapsui Samples for Mac";
+            window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override NSWindow MainWindow => window;
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
