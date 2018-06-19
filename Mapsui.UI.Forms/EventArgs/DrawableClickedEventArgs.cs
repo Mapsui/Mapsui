c2e9872637fe39a0bda5f809ba4163eb9401a9d9
charlenni <web@weltz-online.de>
2018-06-19 10:38:01 +0200

    Added comments for DrawableClickEvent

--- Mapsui.UI.Forms/EventArgs/DrawableClickedEventArgs.cs
+++ Mapsui.UI.Forms/EventArgs/DrawableClickedEventArgs.cs
@@ -5,9 +5,28 @@ namespace Mapsui.UI.Forms
 {
     public sealed class DrawableClickedEventArgs : EventArgs
     {
+        /// <summary>
+        /// Point of click in EPSG:4326 coordinates
+        /// </summary>
+        /// <value>The point.</value>
         public Position Point { get; }
+
+        /// <summary>
+        /// Point of click in screen coordinates
+        /// </summary>
+        /// <value>The screen point.</value>
         public Point ScreenPoint { get; }
+
+        /// <summary>
+        /// Number of taps
+        /// </summary>
+        /// <value>The number of taps.</value>
         public int NumOfTaps { get; }
+
+        /// <summary>
+        /// Flag, if this event was handled
+        /// </summary>
+        /// <value><c>true</c> if handled; otherwise, <c>false</c>.</value>
         public bool Handled { get; set; } = false;
 
         internal DrawableClickedEventArgs(Position point, Point screenPoint, int numOfTaps)
