ed1491388da1896f8c3fa1e4c991c6baca2b0255
Charlenni <info@wfplayer.com>
2018-06-20 13:14:15 +0200

    Added seed for random numbers at AnimatedPointsSample

--- Samples/Mapsui.Samples.Common/Maps/AnimatedPointsSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/AnimatedPointsSample.cs
@@ -42,7 +42,7 @@ namespace Mapsui.Samples.Common.Maps
             public override IEnumerable<IFeature> GetFeaturesInView(BoundingBox box, double resolution)
             {
                 var features = new List<IFeature>();
-                var geometries = RandomPointHelper.GenerateRandomPoints(box, 10).ToList();
+                var geometries = RandomPointHelper.GenerateRandomPoints(box, 10, _random.Next()).ToList();
                 var count = 0;
                 var random = _random.Next(geometries.Count);
 
