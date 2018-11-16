c3c87cca0e91032533a27c045442717e6ce7df4d
Charlenni <geocaching@weltz-online.de>
2018-11-16 14:52:06 +0100

    Removed changes to project files

--- Mapsui/Providers/ArcGIS/CapabilitiesHelper.cs
+++ Mapsui/Providers/ArcGIS/CapabilitiesHelper.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using BruTile;
-using System.Runtime.Serialization.Json;
 using Mapsui.Logging;
 using Mapsui.Providers.ArcGIS.Dynamic;
 using Mapsui.Providers.ArcGIS.Image;
