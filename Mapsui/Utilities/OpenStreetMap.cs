8d3ac3398c84e835c31052899a7bd74459b10963
Charlenni <geocaching@weltz-online.de>
2018-12-07 12:08:43 +0100

    Changed OpenStreetMap sample so that it use https instead http because of NSUrlSession allows only secure websites.

--- Mapsui/Utilities/OpenStreetMap.cs
+++ Mapsui/Utilities/OpenStreetMap.cs
@@ -7,7 +7,7 @@ namespace Mapsui.Utilities
     public static class OpenStreetMap
     {
         private static readonly BruTile.Attribution OpenStreetMapAttribution = new BruTile.Attribution(
-            "© OpenStreetMap contributors", "http://www.openstreetmap.org/copyright");
+            "© OpenStreetMap contributors", "https://www.openstreetmap.org/copyright");
 
         public static TileLayer CreateTileLayer()
         {
@@ -17,7 +17,7 @@ namespace Mapsui.Utilities
         private static HttpTileSource CreateTileSource()
         {
             return new HttpTileSource(new GlobalSphericalMercator(),
-                "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
                 new[] { "a", "b", "c" }, name: "OpenStreetMap",
                 attribution: OpenStreetMapAttribution);
         }
