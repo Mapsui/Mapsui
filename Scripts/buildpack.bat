3315a5cf64fdc0bf04dc70b2a5dbd53fc27f64dc
Paul den Dulk <pauldendulk@gmail.com>
2018-12-27 22:07:03 +0100

    Working Mapsui.Forms package

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -9,3 +9,4 @@ tools\versionupdater /version:%VERSION%
 msbuild.exe mapsui.sln /p:Configuration=Release /t:restore
 msbuild.exe mapsui.sln /p:Configuration=Release
 nuget pack NuSpec\Mapsui.nuspec -Version %VERSION% -outputdirectory Artifacts
+nuget pack NuSpec\Mapsui.Forms.nuspec -Version %VERSION% -outputdirectory Artifacts
