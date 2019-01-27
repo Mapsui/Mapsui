04403f40e09fc893f90752150ce652f6a22bce39
Paul den Dulk <pauldendulk@gmail.com>
2019-01-27 11:35:45 +0100

    Fixed copy command

--- docfx/build-site.cmd
+++ docfx/build-site.cmd
@@ -7,5 +7,5 @@ REM settings page. Apparently github uses this file to determine the custom
 REM domain. The line above deletes the whole docs folder so we now we copy it 
 REM from the docfx folder. If we ever want to change the custom domain settings
 REM we need to alter the docfx\CNAME file and not use the github settings page.
-copy docfx\CNAME doc
+copy docfx\CNAME doc /Y
 xcopy docfx\_site docs /E /Y
