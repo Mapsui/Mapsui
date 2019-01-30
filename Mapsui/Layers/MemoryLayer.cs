ff23c80a8c29d8b5b6dfcb3e226ff483b40c1094
Charlenni <geocaching@weltz-online.de>
2019-01-30 11:33:00 +0100

    Added ctors for MemoryLayer

--- Mapsui/Layers/MemoryLayer.cs
+++ Mapsui/Layers/MemoryLayer.cs
@@ -6,8 +6,22 @@ using Mapsui.Styles;
 
 namespace Mapsui.Layers
 {
+    /// <summary>
+    /// A layer to use, when DataSource doesn't fetch anything because it is already in memory
+    /// </summary>
     public  class MemoryLayer : BaseLayer
     {
+        /// <summary>
+        /// Create a new layer
+        /// </summary>
+        public MemoryLayer() : this("MemoryLayer") { }
+
+        /// <summary>
+        /// Create layer with name
+        /// </summary>
+        /// <param name="layername">Name to use for layer</param>
+        public MemoryLayer(string layername) : base(layername) { }
+
         public IProvider DataSource { get; set; }
 
         public override IEnumerable<IFeature> GetFeaturesInView(BoundingBox box, double resolution)
