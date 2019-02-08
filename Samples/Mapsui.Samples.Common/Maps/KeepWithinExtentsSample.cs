2493aec284b5664bdcf0ef9ae160b0bb47d1b44c
Paul den Dulk <pauldendulk@gmail.com>
2019-02-08 19:32:03 +0100

    Keep within sample now limited to Madagaskar

--- Samples/Mapsui.Samples.Common/Maps/KeepWithinExtentsSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/KeepWithinExtentsSample.cs
@@ -1,4 +1,6 @@
-﻿using Mapsui.UI;
+﻿using Mapsui.Geometries;
+using Mapsui.Projection;
+using Mapsui.UI;
 using Mapsui.Utilities;
 
 namespace Mapsui.Samples.Common.Maps
@@ -15,12 +17,20 @@ namespace Mapsui.Samples.Common.Maps
 
         public static Map CreateMap()
         {
-            var map = new Map
-            {
-                Limiter = new ViewportLimiterKeepWithin()
-            };
+            var map = new Map();
             map.Layers.Add(OpenStreetMap.CreateTileLayer());
+            map.Limiter = new ViewportLimiterKeepWithin
+            {
+                PanLimits = GetLimitsOfMadagaskar()
+            };
             return map;
+        }
+
+        private static BoundingBox GetLimitsOfMadagaskar()
+        {
+            return new BoundingBox(
+                SphericalMercator.FromLonLat(41.8, -27.2),
+                SphericalMercator.FromLonLat(52.5, -11.6));
         }
     }
 }
