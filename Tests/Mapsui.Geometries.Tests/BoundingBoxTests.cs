25bf09fa6399875dcb6f47bda1df3442cf0624f9
Paul den Dulk <pauldendulk@gmail.com>
2019-02-26 18:20:20 +0100

    Adding failing unit test for #613

---
+++ Tests/Mapsui.Geometries.Tests/BoundingBoxTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Mapsui.Geometries.Tests
+{
+    [TestFixture]
+    public class BoundingBoxTests
+    {
+        [Test]
+        public void BoundingBoxTouches()
+        {
+            // Arrange 
+            BoundingBox b1 = new BoundingBox(0, 0, 1, 1);
+            BoundingBox b2 = new BoundingBox(0, 1, 1, 2);
+
+            // Act
+            bool touch = b1.Touches(b2);
+
+            // Assert
+            Assert.AreEqual(true, touch);
+        }
+    }
+}
