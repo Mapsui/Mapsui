2a3d219a2596591113dad582c8b4c21b12147da7
Paul den Dulk <pauldendulk@gmail.com>
2019-03-08 17:54:22 +0100

    Disabling itinero

--- Samples/Mapsui.Samples.Common/Maps/ItineroRoutingSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/ItineroRoutingSample.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using Itinero;
-using Itinero.IO.Osm;
-using Itinero.LocalGeo;
-using Itinero.Osm.Vehicles;
+//using Itinero;
+//using Itinero.IO.Osm;
+//using Itinero.LocalGeo;
+//using Itinero.Osm.Vehicles;
 using Mapsui.Geometries;
 using Mapsui.Layers;
 using Mapsui.Projection;
@@ -36,55 +36,53 @@ namespace Mapsui.Samples.Common.Maps
             map.Widgets.Add(new ScaleBarWidget(map) { TextAlignment = Widgets.Alignment.Center, HorizontalAlignment = Widgets.HorizontalAlignment.Center, VerticalAlignment = Widgets.VerticalAlignment.Top });
             map.Widgets.Add(new Widgets.Zoom.ZoomInOutWidget { MarginX = 20, MarginY = 40 });
 
-            DoStuff();
-
             return map;
         }
 
-        private static void DoStuff()
-        {
-            var routerDb = new RouterDb();
-            using (var stream = new FileInfo(@"/Resources/luxembourg-latest.osm.pbf").OpenRead())
-            {
-                routerDb.LoadOsmData(stream, Vehicle.Car); // create the network for cars only.
-            }
+        //private static void DoStuff()
+        //{
+        //    var routerDb = new RouterDb();
+        //    using (var stream = new FileInfo(@"/Resources/luxembourg-latest.osm.pbf").OpenRead())
+        //    {
+        //        routerDb.LoadOsmData(stream, Vehicle.Car); // create the network for cars only.
+        //    }
 
-            // create a router.
-            var router = new Router(routerDb);
+        //    // create a router.
+        //    var router = new Router(routerDb);
 
-            // get a profile.
-            var profile = Vehicle.Car.Fastest(); // the default OSM car profile.
+        //    // get a profile.
+        //    var profile = Vehicle.Car.Fastest(); // the default OSM car profile.
 
-            // create a routerpoint from a location.
-            // snaps the given location to the nearest routable edge.
-            var start = router.Resolve(profile, 51.26797020271655f, 4.801905155181885f);
-            var end = router.Resolve(profile, 51.26797020271655f, 4.801905155181885f);
+        //    // create a routerpoint from a location.
+        //    // snaps the given location to the nearest routable edge.
+        //    var start = router.Resolve(profile, 51.26797020271655f, 4.801905155181885f);
+        //    var end = router.Resolve(profile, 51.26797020271655f, 4.801905155181885f);
 
-            // calculate a route.
-            var route = router.Calculate(profile, start, end);
-        }
+        //    // calculate a route.
+        //    var route = router.Calculate(profile, start, end);
+        //}
 
-        private ILayer LayerRouteW(Route route)
-        {
-            List<Point> p = new List<Point>();
-            foreach (Coordinate coordinate in route.Shape)
-            {
-                var spherical = SphericalMercator.FromLonLat(coordinate.Longitude, coordinate.Latitude);
-                p.Add(new Point(spherical.X, spherical.Y));
-            }
-            LineString ls = new LineString(p);
-            Feature f = new Feature
-            {
-                Geometry = ls,
-                ["Name"] = "Line 1",
-                Styles = new List<IStyle> { new VectorStyle { Line = new Pen(Color.Blue, 6) } }
-            };
-            return new MemoryLayer
-            {
-                Name = "Route",
-                DataSource = new MemoryProvider(f),
-                Style = null
-            };
-        }
+        //private ILayer LayerRouteW(Route route)
+        //{
+        //    List<Point> p = new List<Point>();
+        //    foreach (Coordinate coordinate in route.Shape)
+        //    {
+        //        var spherical = SphericalMercator.FromLonLat(coordinate.Longitude, coordinate.Latitude);
+        //        p.Add(new Point(spherical.X, spherical.Y));
+        //    }
+        //    LineString ls = new LineString(p);
+        //    Feature f = new Feature
+        //    {
+        //        Geometry = ls,
+        //        ["Name"] = "Line 1",
+        //        Styles = new List<IStyle> { new VectorStyle { Line = new Pen(Color.Blue, 6) } }
+        //    };
+        //    return new MemoryLayer
+        //    {
+        //        Name = "Route",
+        //        DataSource = new MemoryProvider(f),
+        //        Style = null
+        //    };
+        //}
     }
 }
