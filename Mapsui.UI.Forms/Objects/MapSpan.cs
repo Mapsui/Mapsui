15fefe2200042630826f95fffc6a0a9ec48c35f6
HNO Mobile <heinis.mark@gmail.com>
2019-10-10 16:30:23 +0200

    Update MapSpan.cs

--- Mapsui.UI.Forms/Objects/MapSpan.cs
+++ Mapsui.UI.Forms/Objects/MapSpan.cs
@@ -108,6 +108,29 @@ namespace Mapsui.UI.Forms
             return new MapSpan(center, 2 * DistanceToLatitudeDegrees(radius), 2 * DistanceToLongitudeDegrees(center, radius));
         }
 
+        /// <summary>
+        /// Create a new MapSpan from multiple positions
+        /// </summary>
+        /// <returns>New MapSpan</returns>
+        /// <param name="center">Center for new MapSpan</param>
+        /// <param name="radius">Radius for new MapSpan</param>
+        public static MapSpan FromPositions(IEnumerable<Position> positions)
+        {
+            double minLat = double.MaxValue;
+            double minLon = double.MaxValue;
+            double maxLat = double.MinValue;
+            double maxLon = double.MinValue;
+
+            foreach (var p in positions)
+            {
+                minLat = Math.Min(minLat, p.Latitude);
+                minLon = Math.Min(minLon, p.Longitude);
+                maxLat = Math.Max(maxLat, p.Latitude);
+                maxLon = Math.Max(maxLon, p.Longitude);
+            }
+            return new MapSpan(new Position((minLat + maxLat) / 2d, (minLon + maxLon) / 2d), maxLat - minLat, maxLon - minLon);
+        }
+        
         public override int GetHashCode()
         {
             unchecked
