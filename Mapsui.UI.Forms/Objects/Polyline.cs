b0e8a81b3d92922626d0809663ec49e960295a4c
Mark Heinis <m.heinis@jfhillebrand.com>
2019-10-11 10:46:02 +0200

    Create polyline object from encoded polyline string

--- Mapsui.UI.Forms/Objects/Polyline.cs
+++ Mapsui.UI.Forms/Objects/Polyline.cs
@@ -2,6 +2,7 @@
 using Mapsui.Providers;
 using Mapsui.Styles;
 using Mapsui.UI.Forms.Extensions;
+using Mapsui.UI.Forms.Utils;
 using Mapsui.UI.Objects;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -23,6 +24,19 @@ namespace Mapsui.UI.Forms
             _positions.CollectionChanged += OnCollectionChanged;
 
             CreateFeature();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Mapsui.UI.Forms.Polyline"/> class from encoded string as described here
+        /// https://developers.google.com/maps/documentation/utilities/polylinealgorithm
+        /// </summary>
+        /// <param name="encodedPolyline">Encoded polyline</param>
+        public Polyline(string encodedPolyline)
+        {
+            _positions.CollectionChanged += OnCollectionChanged;
+
+            CreateFeature();
+            DecodePolyline(encodedPolyline);
         }
 
         /// <summary>
@@ -52,6 +66,9 @@ namespace Mapsui.UI.Forms
 
         private object sync = new object();
 
+        /// <summary>
+        /// Create feature
+        /// </summary>
         private void CreateFeature()
         {
             lock (sync)
@@ -67,11 +84,22 @@ namespace Mapsui.UI.Forms
                     Feature.Styles.Clear();
                     Feature.Styles.Add(new VectorStyle
                     {
-                        Line = new Pen { Width = StrokeWidth, Color = StrokeColor.ToMapsui()},
-                        
+                        Line = new Pen { Width = StrokeWidth, Color = StrokeColor.ToMapsui() },
+
                     });
                 }
             }
+        }
+
+        /// <summary>
+        /// Decode polyline
+        /// </summary>
+        /// <param name="encodedPolyline">Encoded polyline</param>
+        private void DecodePolyline(string encodedPolyline)
+        {
+            var positions = PolylineConverter.DecodePolyline(encodedPolyline);
+            if (positions != null)
+                positions.ForEach(o => Positions.Add(o));
         }
     }
 }
---
+++ Mapsui.UI.Forms/Utils/PolylineConverter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mapsui.UI.Forms.Utils
+{
+    /// <summary>
+    /// Polyline helper class
+    /// </summary>
+    public static class PolylineConverter
+    {
+        /// <summary>
+        /// Decode
+        /// </summary>
+        /// <param name="encodedPolyline"></param>
+        /// <returns></returns>
+        public static List<Position> DecodePolyline(string encodedPolyline)
+        {
+            if (string.IsNullOrWhiteSpace(encodedPolyline))
+                return null;
+
+            int index = 0;
+            var polylineChars = encodedPolyline.ToCharArray();
+            var poly = new List<Position>();
+            int currentLat = 0;
+            int currentLng = 0;
+            int next5Bits;
+
+            while (index < polylineChars.Length)
+            {
+                int sum = 0;
+                int shifter = 0;
+
+                do
+                {
+                    next5Bits = polylineChars[index++] - 63;
+                    sum |= (next5Bits & 31) << shifter;
+                    shifter += 5;
+                }
+                while (next5Bits >= 32 && index < polylineChars.Length);
+
+                if (index >= polylineChars.Length)
+                    break;
+
+                currentLat += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+                sum = 0;
+                shifter = 0;
+
+                do
+                {
+                    next5Bits = polylineChars[index++] - 63;
+                    sum |= (next5Bits & 31) << shifter;
+                    shifter += 5;
+                }
+                while (next5Bits >= 32 && index < polylineChars.Length);
+
+                if (index >= polylineChars.Length && next5Bits >= 32)
+                {
+                    break;
+                }
+
+                currentLng += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+                var mLatLng = new Position(Convert.ToDouble(currentLat) / 100000.0, Convert.ToDouble(currentLng) / 100000.0);
+                poly.Add(mLatLng);
+            }
+
+            return poly;
+        }
+    }
+}
