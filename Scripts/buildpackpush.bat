84f538237ebba934c7d9fe99f8aed5b418add4ec
Paul den Dulk <pauldendulk@gmail.com>
2019-10-12 17:41:50 +0200

    Revert test code in buidpackpush.bat

--- Scripts/buildpackpush.bat
+++ Scripts/buildpackpush.bat
@@ -1,8 +1,6 @@
 SETLOCAL
 SET VERSION=%1
 CALL Scripts\buildpack %VERSION% || exit /B 1
-ECHO this should not happen
-exit /B 1
 nuget push .\Artifacts\Mapsui.%VERSION%.nupkg -source nuget.org || exit /B 1
 nuget push .\Artifacts\Mapsui.Forms.%VERSION%.nupkg -source nuget.org || exit /B 1
 git commit -m %VERSION% -a || exit /B 1
