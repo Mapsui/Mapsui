50d3c14871c57196300ab7e60c73aa36e6769b07
Paul den Dulk <pauldendulk@gmail.com>
2019-10-12 17:08:10 +0200

    Quit publish script if an error occurs

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -4,9 +4,9 @@ SET VERSION=%1
 
 rmdir obj /s /q
 rmdir Release /s /q
-msbuild tools\versionupdater\versionupdater.csproj /p:Configuration=Release /p:OutputPath=..
-tools\versionupdater /version:%VERSION%
-msbuild.exe mapsui.sln /p:Configuration=Release /t:restore
-msbuild.exe mapsui.sln /p:Configuration=Release
-nuget pack NuSpec\Mapsui.nuspec -Version %VERSION% -outputdirectory Artifacts
-nuget pack NuSpec\Mapsui.Forms.nuspec -Version %VERSION% -outputdirectory Artifacts
+msbuild tools\versionupdater\versionupdater.csproj /p:Configuration=Release /p:OutputPath=..  || exit /B 1
+tools\versionupdater /version:%VERSION%  || exit /B 1
+msbuild.exe mapsui.sln /p:Configuration=Release /t:restore  || exit /B 1
+msbuild.exe mapsui.sln /p:Configuration=Release  || exit /B 1
+nuget pack NuSpec\Mapsui.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
+nuget pack NuSpec\Mapsui.Forms.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
