806e87160bee4fb4185b970fd34f328b194c2781
pauldendulk <pauldendulk@gmail.com>
2019-12-08 21:21:49 +0100

    Add exit after fail in build-site.cmd

--- docfx/build-site.cmd
+++ docfx/build-site.cmd
@@ -1,11 +1,17 @@
-choco install docfx -y
-choco upgrade docfx -y
-docfx docfx\docfx.json
-del docs /s /q 
+ECHO CHOCO INSTALL
+choco install docfx -y || exit /b 
+ECHO CHOCO UPGRADE
+choco upgrade docfx -y || exit /b 
+ECHO DOCFX
+docfx docfx\docfx.json || exit /b 
+ECHO DELETE docs 
+del docs /s /q || exit /b 
 REM the CNAME file is generated in the doc folder when added through the github 
 REM settings page. Apparently github uses this file to determine the custom 
 REM domain. The line above deletes the whole docs folder so we need to copy it 
 REM from the docfx folder. If we ever want to change the custom domain settings
 REM we need to alter the docfx\CNAME file and not use the github settings page.
-copy docfx\CNAME docs /Y
-xcopy docfx\_site docs /E /Y
+ECHO COPY CNAME
+copy docfx\CNAME docs /Y || exit /b 
+ECHO COPY _site TO docs
+xcopy docfx\_site docs /E /Y || exit /b 
