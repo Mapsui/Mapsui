a99be14515bda464b51551c0689d3cdf903a4f46
Dirk Weltz <web@weltz-online.de>
2019-12-16 18:02:24 +0100

    Added a snapshot function for MapControl and MapView to get a snapshout of all or a list of layers in png image format

---
+++ Samples/Mapsui.Samples.Forms/Mapsui.Samples.Forms.Shared/SnapshotSample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Mapsui.Samples.Common.Maps;
+using Mapsui.UI;
+using Mapsui.UI.Forms;
+using Xamarin.Forms;
+
+namespace Mapsui.Samples.Forms
+{
+    public class SnapshotSample : IFormsSample
+    {
+        public string Name => "Snapshot Sample";
+
+        public string Category => "Forms";
+
+        public bool OnClick(object sender, EventArgs args)
+        {
+            var mapView = sender as MapView;
+            var e = args as MapClickedEventArgs;
+
+            var snapshot = mapView.GetSnapshot();
+            
+            var test = ImageSource.FromStream(() => new MemoryStream(snapshot));
+
+            return true;
+        }
+
+        public void Setup(IMapControl mapControl)
+        {
+            mapControl.Map = OsmSample.CreateMap();
+        }
+    }
+}
