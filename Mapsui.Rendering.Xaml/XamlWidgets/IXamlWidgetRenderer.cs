b4a0de14c9d9c0c863e925edd2dca69bc45ef9f9
pauldendulk <pauldendulk@gmail.com>
2020-01-15 18:58:25 +0100

    Add comment to IXamlWidgetRenderer interface for #722

--- Mapsui.Rendering.Xaml/XamlWidgets/IXamlWidgetRenderer.cs
+++ Mapsui.Rendering.Xaml/XamlWidgets/IXamlWidgetRenderer.cs
@@ -3,6 +3,11 @@ using Mapsui.Widgets;
 
 namespace Mapsui.Rendering.Xaml.XamlWidgets
 {
+    /// <summary>
+    /// The IXamlWidgetRenderer interface is not public because xaml rendering will
+    /// probably be removed on some point and we do not want users to go further
+    /// in that direction. 
+    /// </summary>
     interface IXamlWidgetRenderer : IWidgetRenderer
     {
         void Draw(Canvas canvas, IReadOnlyViewport viewport, IWidget widget);
