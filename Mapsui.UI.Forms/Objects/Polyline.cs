0d11c441f37bec19d126e86969f194045adc5411
Dirk Weltz <web@weltz-online.de>
2020-02-03 17:29:45 +0100

    Add a ObservableRangeCollection, so that many new positions could be added to the positions for Polyline and Polygon of MapView. See #797.

--- Mapsui.UI.Forms/Objects/Polyline.cs
+++ Mapsui.UI.Forms/Objects/Polyline.cs
@@ -13,7 +13,7 @@ namespace Mapsui.UI.Forms
 {
     public class Polyline : Drawable
     {
-        private readonly ObservableCollection<Position> _positions = new ObservableCollection<Position>();
+        private readonly ObservableRangeCollection<Position> _positions = new ObservableRangeCollection<Position>();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="T:Mapsui.UI.Forms.Polyline"/> class.
