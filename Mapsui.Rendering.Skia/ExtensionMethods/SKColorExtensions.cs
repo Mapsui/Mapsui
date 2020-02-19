360a87a0e8b2668ae1d89e1d317e96f58ebac727
pauldendulk <pauldendulk@gmail.com>
2020-02-19 18:14:00 +0100

    Add SKColorExtensions

---
+++ Mapsui.Rendering.Skia/ExtensionMethods/SKColorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using SkiaSharp;
+
+namespace Mapsui.Rendering.Skia
+{
+    public static class SKColorExtensions
+    {
+        public static Styles.Color ToMapsui(this SKColor color)
+        {
+            return new Styles.Color(color.Red, color.Green, color.Blue, color.Alpha);
+        }
+    }
+}
