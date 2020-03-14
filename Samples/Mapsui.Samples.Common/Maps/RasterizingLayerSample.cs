090c0d9eecb926b0f55b00d90ddccb1d288ed7c8
pauldendulk <pauldendulk@gmail.com>
2020-03-14 17:49:14 +0100

    Add zoom into rasterlayer data in the sample

--- Samples/Mapsui.Samples.Common/Maps/RasterizingLayerSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/RasterizingLayerSample.cs
@@ -22,12 +22,13 @@ namespace Mapsui.Samples.Common.Maps
             var map = new Map();
             map.Layers.Add(OpenStreetMap.CreateTileLayer());
             map.Layers.Add(new RasterizingLayer(CreateRandomPointLayer()));
+            map.Home = n => n.NavigateTo(map.Layers[1].Envelope.Grow(map.Layers[1].Envelope.Width * 0.1));
             return map;
         }
 
         private static MemoryLayer CreateRandomPointLayer()
         {
-            var rnd = new Random();
+            var rnd = new Random(3462); // Fix the random seed so the features don't move after a refresh
             var features = new Features();
             for (var i = 0; i < 100; i++)
             {
