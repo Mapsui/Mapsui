50a61d12b21f3e362f699cab3b5695cd13aaec6d
pauldendulk <pauldendulk@gmail.com>
2020-03-14 17:48:19 +0100

    Add pixelDensity parameter to RenderToBitmapStream

--- Mapsui/Rendering/IRenderer.cs
+++ Mapsui/Rendering/IRenderer.cs
@@ -10,7 +10,7 @@ namespace Mapsui.Rendering
     public interface IRenderer : IRenderInfo
     {
         void Render(object target, IReadOnlyViewport viewport, IEnumerable<ILayer> layers, IEnumerable<IWidget> widgets, Color background = null);
-        MemoryStream RenderToBitmapStream(IReadOnlyViewport viewport, IEnumerable<ILayer> layers, Color background = null);
+        MemoryStream RenderToBitmapStream(IReadOnlyViewport viewport, IEnumerable<ILayer> layers, Color background = null, float pixelDensity = 1);
         ISymbolCache SymbolCache { get; }
         IDictionary<Type, IWidgetRenderer> WidgetRenders { get; }
         IDictionary<Type, IStyleRenderer> StyleRenderers { get; }
