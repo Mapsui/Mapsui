ae98dbe1ec1a7e44e3ba4b14c415c266ddea16a0
pauldendulk <pauldendulk@gmail.com>
2020-03-25 06:49:49 +0100

    Some automatic changes after build with new vs

--- Mapsui.UI.Android/Resources/Resource.Designer.cs
+++ Mapsui.UI.Android/Resources/Resource.Designer.cs
@@ -15,7 +15,7 @@ namespace Mapsui.UI.Android
 {
 	
 	
-	[System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
 	public partial class Resource
 	{
 		
