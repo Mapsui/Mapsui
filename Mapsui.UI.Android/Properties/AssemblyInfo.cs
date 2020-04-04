f65b60f7b6d471c57a60912891dd391014d6aead
pauldendulk <pauldendulk@gmail.com>
2020-04-04 12:11:20 +0200

    2.0.0-rc.2

--- Mapsui.UI.Android/Properties/AssemblyInfo.cs
+++ Mapsui.UI.Android/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
-[assembly: AssemblyInformationalVersion("2.0.0-rc.1")]
+[assembly: AssemblyInformationalVersion("2.0.0-rc.2")]
