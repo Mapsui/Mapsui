db3a758164f6c74b1294233437b7c5136efbf5ee
pauldendulk <pauldendulk@gmail.com>
2020-05-04 09:01:04 +0200

    2.0.0

--- Samples/Mapsui.Samples.Wpf/Properties/AssemblyInfo.cs
+++ Samples/Mapsui.Samples.Wpf/Properties/AssemblyInfo.cs
@@ -51,4 +51,4 @@ using System.Windows;
 
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-rc.5")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
