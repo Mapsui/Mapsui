cfd56eb7091e01d0483790ba66c8a7ff4e8d02d1
Dirk Weltz <web@weltz-online.de>
2020-05-26 18:25:45 +0200

    Made a sample for testing animations

---
+++ Samples/Mapsui.Samples.Common/Maps/AnimationsSample.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using Mapsui.Geometries;
+using Mapsui.Layers;
+using Mapsui.Providers;
+using Mapsui.Styles;
+using Mapsui.UI;
+using Mapsui.Utilities;
+
+namespace Mapsui.Samples.Common.Maps
+{
+    public class AnimationsSample : ISample
+    {
+        public string Name => "Animations";
+        public string Category => "Special";
+
+        public void Setup(IMapControl mapControl)
+        {
+            mapControl.Map = CreateMap();
+        }
+
+        public static Map CreateMap()
+        {
+            var map = new Map();
+            map.Layers.Add(OpenStreetMap.CreateTileLayer());
+            map.Layers.Add(CreateLayer());
+            return map;
+        }
+
+        public static ILayer CreateLayer()
+        {
+            return new Layer("Points")
+            {
+                DataSource = new MemoryProvider(CreatePoints()),
+            };
+        }
+
+        private static List<Feature> CreatePoints()
+        {
+            var result = new List<Feature>();
+
+            result.Add(CreatePoint(1000000, 1000000));
+            result.Add(CreatePoint(9000000, 1000000));
+            result.Add(CreatePoint(9000000, 9000000));
+            result.Add(CreatePoint(1000000, 9000000));
+
+            return result;
+        }
+
+        private static Feature CreatePoint(double x, double y)
+        {
+            var result = new Feature();
+            var style = new SymbolStyle() { Fill = new Brush(Color.Red), SymbolScale = 1, };
+
+            result.Geometry = new Point(x, y);
+            result.Styles.Add(style);
+
+            var animations = new List<AnimationEntry>();
+
+            var entry1 = new AnimationEntry(
+                start: style.SymbolScale,
+                end: style.SymbolScale * 2,
+                animationStart: 0,
+                animationEnd: .5,
+                easing: Easing.SinInOut,
+                tick: (entry, value) => { style.SymbolScale = (double)((double)entry.Start + ((double)entry.End - (double)entry.Start) * entry.Easing.Ease(value)); },
+                final: (entry) => { style.SymbolScale = (double)entry.End; }
+            );
+            animations.Add(entry1);
+
+            var entry2 = new AnimationEntry(
+                start: style.SymbolScale * 2,
+                end: style.SymbolScale,
+                animationStart: .5,
+                animationEnd: 1,
+                easing: Easing.SinInOut,
+                tick: (entry, value) => { style.SymbolScale = (double)((double)entry.Start + ((double)entry.End - (double)entry.Start) * entry.Easing.Ease(value)); },
+                final: (entry) => { style.SymbolScale = (double)entry.End; }
+            );
+            animations.Add(entry2);
+
+            var entry3 = new AnimationEntry(
+                start: style.Outline.Color,
+                end: style.Outline.Color,
+                animationStart: 0,
+                animationEnd: 1,
+                easing: Easing.Linear,
+                tick: (entry, value) => {
+                    var color = (Color)entry.Start;
+                    style.Fill.Color = new Color(color.R, color.G, (int)(value < 0.5 ? (1.0 - 2.0 * value) * 255 : ((value - 0.5) * 2.0) * 255));
+                },
+                final: (entry) => { style.Fill.Color = (Color)entry.End; }
+            );
+            animations.Add(entry3);
+
+            var animation = new Animation(1000);
+            animation.Loop = true;
+            animation.Entries.AddRange(animations);
+            animation.Start();
+
+            return result;
+        }
+    }
+}
