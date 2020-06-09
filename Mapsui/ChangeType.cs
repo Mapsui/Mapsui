d6eb5eee2533f4117a8b0943999b848b378d9cda
pauldendulk <pauldendulk@gmail.com>
2020-06-09 09:10:13 +0200

    Add ChangeType

---
+++ Mapsui/ChangeType.cs
@@ -0,0 +1,24 @@
+﻿namespace Mapsui
+{
+    public enum ChangeType
+    {
+        /// <summary>
+        /// Discrete changes in Viewport state.
+        /// Examples: 
+        /// - Plus and minus buttons. 
+        /// - Map Initialization.
+        /// - Final change in an animation
+        /// - Touch-up after dragging.
+        /// - Final mouse wheel change
+        /// </summary>
+        Discrete,
+        /// <summary>
+        /// Continuous changes in Viewport state.
+        /// Examples:
+        /// - Dragging the map
+        /// - During animations
+        /// - Mouse wheel changes
+        /// </summary>
+        Continuous
+    }
+}
