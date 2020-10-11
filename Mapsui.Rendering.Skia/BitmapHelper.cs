7bf68918a4763b1c12f773d0ef5be7493c4b64eb
inforithmics <thomas.stocker@gmail.com>
2020-10-11 13:16:42 +0200

    Improve svg detection

--- Mapsui.Rendering.Skia/BitmapHelper.cs
+++ Mapsui.Rendering.Skia/BitmapHelper.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text;
 using Mapsui.Styles;
 using SkiaSharp;
 
@@ -13,13 +14,7 @@ namespace Mapsui.Rendering.Skia
             // SymbolImage. Which holds the type, data and other parameters.
             if (bitmapStream is Stream stream)
             {
-                byte[] buffer = new byte[4];
-
-                stream.Position = 0;
-                stream.Read(buffer, 0, 4);
-                stream.Position = 0;
-
-                if (System.Text.Encoding.UTF8.GetString(buffer, 0, 4).ToLower().Equals("<svg"))
+                if (IsSvg(stream))
                 {
                     var svg = new SkiaSharp.Extended.Svg.SKSvg();
                     svg.Load(stream);
@@ -37,6 +32,71 @@ namespace Mapsui.Rendering.Skia
             }
 
             return null;
-        }     
+        }
+
+        /// <summary>
+        /// Detects if stream is svg stream
+        /// </summary>
+        /// <param name="stream">stream</param>
+        /// <returns>true if is svg stream</returns>
+        private static bool IsSvg(Stream stream)
+        {
+            byte[] buffer = new byte[5];
+
+            stream.Position = 0;
+            stream.Read(buffer, 0, 5);
+            stream.Position = 0;
+
+            if (Encoding.UTF8.GetString(buffer, 0, 4).ToLowerInvariant().Equals("<svg"))
+            {
+                return true;
+            }
+
+            if (Encoding.UTF8.GetString(buffer, 0, 5).ToLowerInvariant().Equals("<?xml"))
+            {
+                var svg = Encoding.UTF8.GetBytes("<svg");
+                if (ReadOneSearch(stream, svg) >= 0)
+                {
+                    stream.Position = 0;
+                    return true;
+                };
+
+                stream.Position = 0;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/1471975/best-way-to-find-position-in-the-stream-where-given-byte-sequence-starts
+        /// </summary>
+        /// <param name="haystack">stream to search</param>
+        /// <param name="needle">pattern to find</param>
+        /// <returns>position</returns>
+        private static long ReadOneSearch(Stream haystack, byte[] needle)
+        {
+            int b;
+            long i = 0;
+            while ((b = haystack.ReadByte()) != -1)
+            {
+                if (b == needle[i++])
+                {
+                    if (i == needle.Length)
+                    {
+                        return haystack.Position - needle.Length;
+                    }
+                }
+                else if (b == needle[0])
+                {
+                    i = 1;
+                }
+                else
+                {
+                    i = 0;
+                }
+            }
+
+            return -1;
+}
     }
 }
---
+++ Tests/Mapsui.Rendering.Xaml.Tests/BitmapHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Mapsui.Rendering.Xaml.Tests
+{
+    [NUnit.Framework.TestFixtureAttribute, NUnit.Framework.ApartmentAttribute(System.Threading.ApartmentState.STA)]
+    public class BitmapHelperTests
+    {
+        [Test]
+        public void LoadSvgTest()
+        {
+            // arrange
+            const string fileName = "Pin.svg";
+
+            // act
+            var bitmap = BitmapHelper
+
+            // assert
+            Assert.IsTrue(CompareBitmaps(File.ReadFromOriginalFolder(fileName), bitmap, 1, 0.99));
+        }
+
+        [Test]
+        public void LoadXmlSvgTest()
+        {
+
+        }
+    }
+}
