40a14bdf9484fa5e506a9208f1761813a953a5fc
pauldendulk <pauldendulk@gmail.com>
2020-11-10 10:41:43 +0100

    2.0.0-alpha.3

--- Tests/Mapsui.Tests/Properties/AssemblyInfo.cs
+++ Tests/Mapsui.Tests/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
  
 
 
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0-alpha.2")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha.3")]
