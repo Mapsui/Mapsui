938570cc3245cfd09d9068174ea07f123e2cef7a
pauldendulk <pauldendulk@gmail.com>
2020-11-10 10:45:25 +0100

    3.0.0-alpha.3

--- Mapsui.UI.Uwp/Properties/AssemblyInfo.cs
+++ Mapsui.UI.Uwp/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
  
 
 
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyInformationalVersion("2.0.0-alpha.3")]
+[assembly: AssemblyInformationalVersion("3.0.0-alpha.3")]
--- Tools/VersionUpdater/Properties/AssemblyInfo.cs
+++ Tools/VersionUpdater/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 
 
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-alpha.3")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-alpha.3")]
