81007a478dea800073689498c49cca000a1e3119
Thomas Mijieux <thomas.mijieux@aum.bio>
2021-04-13 13:58:32 +0200

    display time measure as fixed size floating point

--- Mapsui/Benchmark/Benchmarking.cs
+++ Mapsui/Benchmark/Benchmarking.cs
@@ -6,6 +6,7 @@ namespace Mapsui.Benchmark
     {
         public string Name { get; set; }
         public double Time { get; set; }
+        public string TimeFixed => Time.ToString("N5");
 
         public int FeatureCount { get; set; }
         public int StyleCount { get; set; }
