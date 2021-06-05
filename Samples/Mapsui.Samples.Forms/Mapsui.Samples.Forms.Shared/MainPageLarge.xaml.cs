37d3b9f9e44ec7f1583004bb766e39c0c01a4c79
Janus Weil <janus@gcc.gnu.org>
2021-06-05 12:19:19 +0200

    Mapsui.Samples.Forms: properly populate the listView on startup (iOS & Android)
    * i.e. show only the samples from the selected category (Forms)
      instead of all samples
    * also simplify the initialization of the category picker

--- Samples/Mapsui.Samples.Forms/Mapsui.Samples.Forms.Shared/MainPageLarge.xaml.cs
+++ Samples/Mapsui.Samples.Forms/Mapsui.Samples.Forms.Shared/MainPageLarge.xaml.cs
@@ -26,10 +26,7 @@ namespace Mapsui.Samples.Forms
             allSamples = AllSamples.GetSamples();
 
             var categories = allSamples.Select(s => s.Category).Distinct().OrderBy(c => c);
-            foreach (var category in categories)
-            {
-                picker.Items?.Add(category);
-            }
+            picker.ItemsSource = categories.ToList<string>();
             picker.SelectedIndexChanged += PickerSelectedIndexChanged;
             picker.SelectedItem = "Forms";
 
