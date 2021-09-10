b25c683b627f670697887b1e66565aa3b3f4a504
pauldendulk <pauldendulk@gmail.com>
2021-09-10 17:16:26 +0200

    Adding Mapsui.Native package to buildpackpush.bat

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -9,5 +9,5 @@ tools\versionupdater /version:%VERSION%  || exit /B 1
 msbuild.exe mapsui.sln /p:Configuration=Release /t:restore  || exit /B 1
 msbuild.exe mapsui.sln /p:Configuration=Release  || exit /B 1
 nuget pack NuSpec\Mapsui.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
-nuget pack NuSpec\Mapsui.Native.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
 nuget pack NuSpec\Mapsui.Forms.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
+nuget pack NuSpec\Mapsui.Native.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
--- Scripts/buildpackpush.bat
+++ Scripts/buildpackpush.bat
@@ -3,6 +3,7 @@ SET VERSION=%1
 CALL Scripts\buildpack %VERSION% || exit /B 1
 nuget push .\Artifacts\Mapsui.%VERSION%.nupkg -source nuget.org || exit /B 1
 nuget push .\Artifacts\Mapsui.Forms.%VERSION%.nupkg -source nuget.org || exit /B 1
+nuget push .\Artifacts\Mapsui.Native.%VERSION%.nupkg -source nuget.org || exit /B 1
 git commit -m %VERSION% -a || exit /B 1
 git tag %VERSION% || exit /B 1
 git push origin %VERSION% || exit /B 1
