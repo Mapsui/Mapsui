9a6c1f34079cb01ac4cae9ebafcc486d3dcd77d4
pauldendulk <pauldendulk@gmail.com>
2021-09-11 13:42:11 +0200

    Revert temporary change to buildpackpush.bat

--- Scripts/buildpackpush.bat
+++ Scripts/buildpackpush.bat
@@ -1,9 +1,9 @@
 SETLOCAL
 SET VERSION=%1
-REM CALL Scripts\buildpack %VERSION% || exit /B 1
-REM nuget push .\Artifacts\Mapsui.%VERSION%.nupkg -source nuget.org || exit /B 1
-REM nuget push .\Artifacts\Mapsui.Forms.%VERSION%.nupkg -source nuget.org || exit /B 1
-REM nuget push .\Artifacts\Mapsui.Native.%VERSION%.nupkg -source nuget.org || exit /B 1
+CALL Scripts\buildpack %VERSION% || exit /B 1
+nuget push .\Artifacts\Mapsui.%VERSION%.nupkg -source nuget.org || exit /B 1
+nuget push .\Artifacts\Mapsui.Forms.%VERSION%.nupkg -source nuget.org || exit /B 1
+nuget push .\Artifacts\Mapsui.Native.%VERSION%.nupkg -source nuget.org || exit /B 1
 git commit -m %VERSION% -a || exit /B 1
 git tag %VERSION% || exit /B 1
 git push origin %VERSION% || exit /B 1
