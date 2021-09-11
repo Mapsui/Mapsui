53a9d27532b116ab4667daaabc4a4c6591535300
pauldendulk <pauldendulk@gmail.com>
2021-09-11 10:09:22 +0200

    Build versionupdater in bin folder
    So that binaries are ignored by git

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -4,5 +4,5 @@ SET VERSION=%1
 
 rmdir obj /s /q
 rmdir Release /s /q
-msbuild tools\versionupdater\versionupdater.csproj /p:Configuration=Release /p:OutputPath=..  || exit /B 1
-tools\versionupdater -v %VERSION%
+msbuild tools\versionupdater\versionupdater.csproj /p:Configuration=Release /p:OutputPath=..\bin || exit /B 1
+tools\bin\versionupdater -v %VERSION% || exit /B 1
