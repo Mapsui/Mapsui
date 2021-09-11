44d065799258498173ccfd94fa2404d1f35a2738
pauldendulk <pauldendulk@gmail.com>
2021-09-11 10:42:57 +0200

    Revert buildpack.bat

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -6,3 +6,8 @@ rmdir obj /s /q
 rmdir Release /s /q
 msbuild tools\versionupdater\versionupdater.csproj /p:Configuration=Release /p:OutputPath=..\bin || exit /B 1
 tools\bin\versionupdater -v %VERSION% || exit /B 1
+msbuild.exe mapsui.sln /p:Configuration=Release /t:restore  || exit /B 1
+msbuild.exe mapsui.sln /p:Configuration=Release  || exit /B 1
+nuget pack NuSpec\Mapsui.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
+nuget pack NuSpec\Mapsui.Forms.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
+nuget pack NuSpec\Mapsui.Native.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
