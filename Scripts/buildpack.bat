f869d25489929c48b1c6d64e1430d70241987f8b
pauldendulk <pauldendulk@gmail.com>
2021-09-17 17:48:15 +0200

    Update build scripts for new nuspecs

--- Scripts/buildpack.bat
+++ Scripts/buildpack.bat
@@ -10,4 +10,7 @@ msbuild.exe mapsui.sln /p:Configuration=Release /t:restore  || exit /B 1
 msbuild.exe mapsui.sln /p:Configuration=Release  || exit /B 1
 nuget pack NuSpec\Mapsui.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
 nuget pack NuSpec\Mapsui.Forms.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
-nuget pack NuSpec\Mapsui.Native.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
+nuget pack NuSpec\Mapsui.Wpf.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
+nuget pack NuSpec\Mapsui.iOS.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
+nuget pack NuSpec\Mapsui.Android.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
+nuget pack NuSpec\Mapsui.Uwp.nuspec -Version %VERSION% -outputdirectory Artifacts  || exit /B 1
