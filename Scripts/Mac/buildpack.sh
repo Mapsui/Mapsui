930849e9c73eacfd1ed55755f04e7557bc321e92
Janus Weil <janus@gcc.gnu.org>
2021-09-18 19:19:06 +0200

    update the Mac build script
    * build the new packages Mapsui.Android and Mapsui.iOS
    * and fix the invocation of VersionUpdater

--- Scripts/Mac/buildpack.sh
+++ Scripts/Mac/buildpack.sh
@@ -6,9 +6,11 @@ find . -type d \( -name 'bin' -o -name 'obj' \) -prune -exec rm -rf {} \;
 nuget restore Mapsui.Mac.sln
 # update version
 msbuild Tools/VersionUpdater/VersionUpdater.csproj /p:Configuration=Release
-mono Tools/VersionUpdater/bin/Release/net48/win-x64/VersionUpdater.exe /version:$VERSION
+dotnet Tools/VersionUpdater/bin/Release/netcoreapp3.1/VersionUpdater.dll -v $VERSION
 # build
 msbuild Mapsui.Mac.sln /p:Configuration=Release
 # package
 nuget pack NuSpec/Mapsui.nuspec -Version $VERSION -outputdirectory Artifacts
 nuget pack NuSpec/Mapsui.Forms.nuspec -Version $VERSION -outputdirectory Artifacts
+nuget pack NuSpec/Mapsui.Android.nuspec -Version $VERSION -outputdirectory Artifacts
+nuget pack NuSpec/Mapsui.iOS.nuspec -Version $VERSION -outputdirectory Artifacts
