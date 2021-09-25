ff1878586ef5632bed49c56892f2eb7efd0ebe2f
Thomas Stocker <thomas.stocker@gmail.com>
2021-09-25 23:34:40 +0200

    added assembly info to Mapsui.WinUI

---
+++ Mapsui.UI.WinUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mapsui.UI.WinUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mapsui.UI.WinUI")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+ 
+
+
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyInformationalVersion("3.0.0-rc.4")]
