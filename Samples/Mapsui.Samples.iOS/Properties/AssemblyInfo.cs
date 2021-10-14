5376b7b7ce4e30adf2a5b0916d7a5b78f878d77c
pauldendulk <pauldendulk@gmail.com>
2021-10-14 21:17:16 +0200

    3.0.1

--- Samples/Mapsui.Samples.iOS/Properties/AssemblyInfo.cs
+++ Samples/Mapsui.Samples.iOS/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
  
 
 
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.1")]
+[assembly: AssemblyFileVersion("3.0.1")]
+[assembly: AssemblyInformationalVersion("3.0.1")]
