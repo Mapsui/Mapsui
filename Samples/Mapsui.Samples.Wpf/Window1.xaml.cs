28bd5c17f7e859c80a20a6090ca64190895f72ad
Thomas Stocker <thomas.stocker@gmail.com>
2021-10-17 17:24:15 +0200

    Fixed Build

--- Samples/Mapsui.Samples.Wpf/Window1.xaml.cs
+++ Samples/Mapsui.Samples.Wpf/Window1.xaml.cs
@@ -13,7 +13,6 @@ using Mapsui.Samples.Wpf.Utilities;
 using Mapsui.UI;
 using Mapsui.Samples.Common;
 using Mapsui.Samples.Common.Desktop;
-using Mapsui.Samples.Common.ExtensionMethods;
 
 namespace Mapsui.Samples.Wpf
 {
