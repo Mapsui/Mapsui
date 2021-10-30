3f7c57aeb30ac8b4f246f1081696968b955253fe
pauldendulk <pauldendulk@gmail.com>
2021-10-30 12:42:32 +0200

    Add ZoomLock sample

---
+++ Samples/Mapsui.Samples.Common/Maps/Navigation/ZoomLockSample.cs
@@ -0,0 +1,23 @@
+﻿using Mapsui.UI;
+using Mapsui.Utilities;
+
+namespace Mapsui.Samples.Common.Maps.Navigation
+{
+    class ZoomLockSample : ISample
+    {
+        public string Name => "ZoomLock";
+        public string Category => "Navigation";
+        public void Setup(IMapControl mapControl)
+        {
+            mapControl.Map = CreateMap();
+            mapControl.Map.ZoomLock = true;
+        }
+
+        public static Map CreateMap()
+        {
+            var map = new Map();
+            map.Layers.Add(OpenStreetMap.CreateTileLayer());
+            return map;
+        }
+    }
+}
