836815d76093365d0f26e332f93ea40111604f1a
pauldendulk <pauldendulk@gmail.com>
2021-11-06 11:03:36 +0100

    Reinstate TMS sample with openbasiskaart.nl data

--- Samples/Mapsui.Samples.Common/Maps/TmsSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/TmsSample.cs
@@ -3,10 +3,13 @@ using Mapsui.UI;
 
 namespace Mapsui.Samples.Common.Maps
 {
-    public class TmsSample // disabled because service is down
-                           //  todo: Replace with another.
+    public class TmsSample : ISample
     {
-        public string Name => "TMS";
+        public string Name => "8 TMS openbasiskaart";
+        public string Category => "Data";
+
+        private static readonly BruTile.Attribution OpenStreetMapAttribution = new(
+            "© OpenStreetMap contributors", "https://www.openstreetmap.org/copyright");
 
         public void Setup(IMapControl mapControl)
         {
@@ -22,13 +25,17 @@ namespace Mapsui.Samples.Common.Maps
 
         public static ILayer CreateLayer()
         {
-            var url = "https://geodata.nationaalgeoregister.nl/tiles/service/tms/1.0.0/opentopo@EPSG%3A28992 ";
+            var url = "https://www.openbasiskaart.nl/mapcache/tms/1.0.0/osm@rd";
             var tileSource = TmsTileSourceBuilder.Build(url, true);
 
-            return new TileLayer(tileSource)
+            var tileLayer =  new TileLayer(tileSource)
             {
-                Name = "Open Topo (PDOK)"
+                Name = "openbasiskaart.nl"
             };
+
+            tileLayer.Attribution.Text = "© OpenStreetMap contributors (via openbasiskaart.nl)";
+            tileLayer.Attribution.Url = "https://www.openstreetmap.org/copyright";
+            return tileLayer;
         }
     }
 }
