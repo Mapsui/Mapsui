b5b0633ebb3130b1e0ce5c685bbe5c2b1f3a0fe4
pauldendulk <pauldendulk@gmail.com>
2021-11-12 15:30:28 +0100

    Some more renaming to projection

--- Tests/Mapsui.Tests/Projection/ProjectionTests.cs
+++ Tests/Mapsui.Tests/Projection/ProjectionTests.cs
@@ -44,10 +44,10 @@ namespace Mapsui.Tests.Projection
             // arrange
             var multiPolygon = (MultiPolygon)GeometryFromWKT.Parse("MULTIPOLYGON (((40 40, 20 45, 45 30, 40 40)), ((20 35, 10 30, 10 10, 30 5, 45 20, 20 35), (30 20, 20 15, 20 25, 30 20)))");
             var copiedMultiPolygon = multiPolygon.Copy();
-            var transformation = new GeometryProjection();
+            var projection = new GeometryProjection();
 
             // act
-            transformation.Project("EPSG:4326", "EPSG:3857", copiedMultiPolygon);
+            projection.Project("EPSG:4326", "EPSG:3857", copiedMultiPolygon);
 
             // assert
             var vertices = multiPolygon.AllVertices().ToList();
