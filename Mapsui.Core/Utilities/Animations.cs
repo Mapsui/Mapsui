43af8134eecacbcddbdbfe085b1273dfa560f97d
Dirk Weltz <web@weltz-online.de>
2021-11-29 16:22:02 +0100

    Added some comments

--- Mapsui.Core/Utilities/Animations.cs
+++ Mapsui.Core/Utilities/Animations.cs
@@ -13,11 +13,22 @@ namespace Mapsui.Utilities
         /// </summary>
         private static readonly List<AnimationEntry> _entries = new();
 
+        /// <summary>
+        /// Start a single AnimationEntry
+        /// </summary>
+        /// <param name="entry">AnimationEntry to start</param>
+        /// <param name="duration">Duration im ms for the given AnimationEntry</param>
         public static void Start(AnimationEntry entry, long duration)
         {
             Start(entry, duration, DateTime.Now.Ticks);
         }
 
+        /// <summary>
+        /// Start a list of AnimationEntrys
+        /// </summary>
+        /// <remarks>All AnimationEntries are started at the same time.</remarks>
+        /// <param name="entries">List of AnimationEntry to start</param>
+        /// <param name="duration">Duration im ms for the given AnimationEntry</param>
         public static void Start(IEnumerable<AnimationEntry> entries, long duration)
         {
             // Start all animations in entries with the same ticks
@@ -27,6 +38,13 @@ namespace Mapsui.Utilities
                 Start(entry, duration, ticks);
         }
 
+        /// <summary>
+        /// Start of a AnimationEntry with given duration at given ticks for StartTicks
+        /// </summary>
+        /// <remarks>When the ticks is given, more than one AnimationEntry with the same StartTicks could be started.</remarks>
+        /// <param name="entry">AnimationEntry to start</param>
+        /// <param name="duration">Duration im ms for the given AnimationEntry</param>
+        /// <param name="ticks">StartTicks for this AnimationEntry</param>
         private static void Start(AnimationEntry entry, long duration, long ticks)
         {
             lock (_syncObject)
@@ -42,6 +60,11 @@ namespace Mapsui.Utilities
             }
         }
 
+        /// <summary>
+        /// Stop a given AnimationEntry
+        /// </summary>
+        /// <param name="entry">AnimationEntry to stop</param>
+        /// <param name="callFinal">Final function is called, if callFinal is true</param>
         public static void Stop(AnimationEntry entry, bool callFinal = true)
         {
             if (entry == null || !_entries.Contains(entry))
@@ -56,12 +79,21 @@ namespace Mapsui.Utilities
             }
         }
 
+        /// <summary>
+        /// Stop all AnimationEntrys in a given list
+        /// </summary>
+        /// <param name="entry">AnimationEntry to stop</param>
+        /// <param name="callFinal">Final function is called, if callFinal is true</param>
         public static void Stop(List<AnimationEntry> entries, bool callFinal = true)
         {
             foreach (var entry in entries)
                 Stop(entry, callFinal);
         }
 
+        /// <summary>
+        /// Update all AnimationEntrys and check, if a redraw is needed
+        /// </summary>
+        /// <returns>True, if a redraw of the screen ist needed</returns>
         public static bool UpdateAnimations()
         {
             AnimationEntry[] entries;
