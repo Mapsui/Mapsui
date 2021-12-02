acbbc796da06006cd801753eaa9c8218ec1c1137
Dirk Weltz <web@weltz-online.de>
2021-12-02 20:03:20 +0100

    Changed animation sample in Special to show the capabilities and usage of the new animation

--- Samples/Mapsui.Samples.Common/Maps/Special/AnimationsSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Special/AnimationsSample.cs
@@ -8,6 +8,57 @@ using Mapsui.Utilities;
 
 namespace Mapsui.Samples.Common.Maps
 {
+    /// <summary>
+    /// This is an example for an animated layer, that has own animations, but respect also
+    /// animated features (features, that implement the IAnimatable interface) and animated
+    /// styles.
+    /// </summary>
+    public class AnimatedLayer : Layer, IAnimatable
+    {
+        private List<AnimationEntry> _animations = new();
+
+        public AnimatedLayer(string name) : base(name)
+        { }
+
+        public bool UpdateAnimations(long ticks)
+        {
+            bool result = false;
+
+            if (Extent != null)
+            {
+                foreach (var feature in GetFeatures(Extent, 0))
+                {
+                    if (feature is IAnimatable animatedFeature)
+                        result |= animatedFeature.UpdateAnimations(ticks);
+
+                    foreach (var style in feature.Styles)
+                    {
+                        if (style is IAnimatable animatedStyle)
+                            result |= animatedStyle.UpdateAnimations(ticks);
+                    }
+                }
+            }
+
+            result |= Animation.UpdateAnimations(_animations, ticks);
+
+            return result;
+        }
+
+        public List<AnimationEntry> Animations => _animations;
+    }
+
+    public class AnimatedSymbolStyle : SymbolStyle, IAnimatable
+    {
+        private List<AnimationEntry> _animations = new();
+
+        public bool UpdateAnimations(long ticks)
+        {
+            return Animation.UpdateAnimations(_animations, ticks);
+        }
+
+        public List<AnimationEntry> Animations => _animations;
+    }
+
     public class AnimationsSample : ISample
     {
         public string Name => "Animated symbols";
@@ -21,22 +72,22 @@ namespace Mapsui.Samples.Common.Maps
         public static Map CreateMap(IMapControl mapControl)
         {
             var style = new SymbolStyle() { Fill = new Brush(Color.Red), SymbolScale = 1, };
+            var layer = CreateLayer(style);
 
             var map = new Map();
             map.Layers.Add(OpenStreetMap.CreateTileLayer());
-            map.Layers.Add(CreateLayer(style));
+            map.Layers.Add(layer);
 
-            var animations = CreateAnimationsForSymbolStyle(style);
+            layer.Animations.AddRange(CreateAnimationsForSymbolStyle(style));
 
-            Animation.Start(animations, 10000);
+            Animation.Start(layer.Animations, 10000);
 
             return map;
         }
 
-        public static ILayer CreateLayer(IStyle style)
+        public static AnimatedLayer CreateLayer(IStyle style)
         {
-
-            return new Layer("Points")
+            return new AnimatedLayer("Points")
             {
                 DataSource = new MemoryProvider<IFeature>(CreatePoints(style)),
             };
@@ -47,9 +98,9 @@ namespace Mapsui.Samples.Common.Maps
             var result = new List<PointFeature>
             {
                 CreatePoint(1000000, 1000000, style),
-                CreatePoint(9000000, 1000000, style),
-                CreatePoint(9000000, 9000000, style),
-                CreatePoint(1000000, 9000000, style)
+                CreatePoint(9000000, 1000000, IncreaseStyle()),
+                CreatePoint(9000000, 9000000, InDecreaseStyle()),
+                CreatePoint(1000000, 9000000, ColorStyle())
             };
 
             return result;
@@ -66,9 +117,9 @@ namespace Mapsui.Samples.Common.Maps
                 animationEnd: .5,
                 easing: Easing.SinInOut,
                 repeat: true,
-                tick: (entry, value) => { style.SymbolScale = (double)((double)entry.Start + ((double)entry.End - (double)entry.Start) * entry.Easing.Ease(value)); },
-                final: (entry) => { style.SymbolScale = (double)entry.End; }
-            );
+                tick: (entry, value) => { style.SymbolScale = (double)((double)entry.Start + ((double)entry.End - (double)entry.Start) * entry.Easing.Ease(value)); return true; },
+                final: (entry) => { style.SymbolScale = (double)entry.End; return true; }
+                );
             animations.Add(entry1);
 
             var entry2 = new AnimationEntry(
@@ -78,9 +129,9 @@ namespace Mapsui.Samples.Common.Maps
                 animationEnd: 1,
                 easing: Easing.SinInOut,
                 repeat: true,
-                tick: (entry, value) => { style.SymbolScale = (double)((double)entry.Start + ((double)entry.End - (double)entry.Start) * entry.Easing.Ease(value)); },
-                final: (entry) => { style.SymbolScale = (double)entry.End; }
-            );
+                tick: (entry, value) => { style.SymbolScale = (double)((double)entry.Start + ((double)entry.End - (double)entry.Start) * entry.Easing.Ease(value)); return true; },
+                final: (entry) => { style.SymbolScale = (double)entry.End; return true; }
+                );
             animations.Add(entry2);
 
             var entry3 = new AnimationEntry(
@@ -93,15 +144,92 @@ namespace Mapsui.Samples.Common.Maps
                     var color = (Color)entry.Start;
                     style.Fill ??= new Brush();
                     style.Fill.Color = new Color(color.R, color.G, (int)(value < 0.5 ? (1.0 - 2.0 * value) * 255 : ((value - 0.5) * 2.0) * 255));
+                    return true;
                 },
                 final: (entry) => {
                     style.Fill ??= new Brush();
                     style.Fill.Color = entry.End as Color;
+                    return true;
                 }
             );
             animations.Add(entry3);
 
             return animations;
+        }
+
+        private static AnimatedSymbolStyle IncreaseStyle()
+        {
+            var style = new AnimatedSymbolStyle() { Fill = new Brush(Color.Red), SymbolScale = 1, };
+
+            var animation = new AnimationEntry(
+                start: style.SymbolScale,
+                end: style.SymbolScale * 2,
+                animationStart: 0,
+                animationEnd: .5,
+                easing: Easing.SinInOut,
+                repeat: true,
+                tick: (entry, value) => { style.SymbolScale = (double)((double)entry.Start + ((double)entry.End - (double)entry.Start) * entry.Easing.Ease(value)); return true; },
+                final: (entry) => { style.SymbolScale = (double)entry.End; return true; }
+                );
+
+            style.Animations.Add(animation);
+
+            Animation.Start(style.Animations, 1000);
+
+            return style;
+        }
+
+        private static AnimatedSymbolStyle InDecreaseStyle()
+        {
+            var style = IncreaseStyle();
+
+            var animation = new AnimationEntry(
+                start: style.SymbolScale * 2,
+                end: style.SymbolScale,
+                animationStart: .5,
+                animationEnd: 1,
+                easing: Easing.SinInOut,
+                repeat: true,
+                tick: (entry, value) => { style.SymbolScale = (double)((double)entry.Start + ((double)entry.End - (double)entry.Start) * entry.Easing.Ease(value)); return true; },
+                final: (entry) => { style.SymbolScale = (double)entry.End; return true; }
+            );
+
+            style.Animations.Add(animation);
+
+            Animation.Start(animation, 1000);
+
+            return style;
+        }
+
+        private static AnimatedSymbolStyle ColorStyle()
+        {
+            var style = new AnimatedSymbolStyle();
+
+            var animation = new AnimationEntry(
+                start: style.Outline?.Color ?? Color.Gray,
+                end: style.Outline?.Color ?? Color.Gray,
+                animationStart: 0,
+                animationEnd: 1,
+                easing: Easing.Linear,
+                repeat: true,
+                tick: (entry, value) => {
+                    var color = (Color)entry.Start;
+                    style.Fill ??= new Brush();
+                    style.Fill.Color = new Color(color.R, color.G, (int)(value < 0.5 ? (1.0 - 2.0 * value) * 255 : ((value - 0.5) * 2.0) * 255));
+                    return true;
+                },
+                final: (entry) => {
+                    style.Fill ??= new Brush();
+                    style.Fill.Color = entry.End as Color;
+                    return true;
+                }
+            );
+
+            style.Animations.Add(animation);
+
+            Animation.Start(style.Animations, 5000);
+
+            return style;
         }
 
         private static PointFeature CreatePoint(double x, double y, IStyle style)
