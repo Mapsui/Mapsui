7581270e26355c98e34008b937cb50decaa23cad
Dirk Weltz <web@weltz-online.de>
2021-12-02 18:06:44 +0100

    Add interface for IAnimatable

---
+++ Mapsui.Core/Utilities/IAnimatable.cs
@@ -0,0 +1,15 @@
+﻿namespace Mapsui.Utilities
+{
+    /// <summary>
+    /// Interface for classes, that could be animated
+    /// </summary>
+    public interface IAnimatable
+    {
+        /// <summary>
+        /// Update possible animations, which belong to the class
+        /// </summary>
+        /// <param name="ticks">Ticks for calculating the update</param>
+        /// <returns>True, if one of the animations need a redraw</returns>
+        bool UpdateAnimations(long ticks);
+    }
+}
