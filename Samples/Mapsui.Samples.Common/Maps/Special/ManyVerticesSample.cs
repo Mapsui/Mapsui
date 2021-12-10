997fbaeadcc10de7cbb10c37d80bda3429c70c1e
pauldendulk <pauldendulk@gmail.com>
2021-12-10 14:20:23 +0100

    Adjust null check in ManyVerticesSample

--- Samples/Mapsui.Samples.Common/Maps/Special/ManyVerticesSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Special/ManyVerticesSample.cs
@@ -29,9 +29,8 @@ namespace Mapsui.Samples.Common.Maps
 
             map.Layers.Add(OpenStreetMap.CreateTileLayer());
             map.Layers.Add(new RasterizingLayer(CreatePointLayer(), pixelDensity: pixelDensity));
-            var extent = map.Layers[1].Extent?.Grow(map.Layers[1].Extent!.Width * 0.25);
-            if (extent != null)
-                map.Home = n => n.NavigateTo(extent);
+            var extent = map.Layers[1].Extent!.Grow(map.Layers[1].Extent!.Width * 0.25);
+            map.Home = n => n.NavigateTo(extent);
             return map;
         }
 
