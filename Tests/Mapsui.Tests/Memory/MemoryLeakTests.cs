7a1bce977246243ca2e29ef36a10375b14eba41d
inforithmics <thomas.stocker@gmail.com>
2021-12-12 21:07:51 +0100

    Added memory tests for mapview and mapcontrol

--- Tests/Mapsui.Tests/Memory/MemoryLeakTests.cs
+++ Tests/Mapsui.Tests/Memory/MemoryLeakTests.cs
@@ -1,9 +1,9 @@
 using System;
 using Mapsui.UI.Forms;
 using NUnit.Framework;
-using Xamarin.Forms.Internals;
 
-#pragma warning disable IDISP001
+#pragma warning disable IDISP001 // Disposable object created
+#pragma warning disable IDISP007 // Don't Dispose injected
 
 namespace Mapsui.Tests.Memory;
 
@@ -27,8 +27,7 @@ public class MemoryLeakTests
     public void MapIsNotAliveAfterUsage()
     {
         var weak = CreateMap();
-        // dispose should not be alive anymore
-        (weak.Target as IDisposable)?.Dispose();
+        Dispose(weak);
 
         GC.Collect();
         GC.WaitForPendingFinalizers();
@@ -37,9 +36,55 @@ public class MemoryLeakTests
         Assert.IsFalse(weak.IsAlive);
     }
 
+    [Test]
+    public void MapViewIsNotAliveAfterUsage()
+    {
+        var weak = CreateMap();
+        Dispose(weak);
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+
+        Assert.IsFalse(weak.IsAlive);
+    }
+
+    [Test]
+    public void MapControlIsNotAliveAfterUsage()
+    {
+        var weak = CreateMapControl();
+        Dispose(weak);
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+
+        Assert.IsFalse(weak.IsAlive);
+    }
+
+    private void Dispose(WeakReference weak)
+    {
+        // the dispose needs to be made in a different method or else the target lives in a local variable.
+        (weak.Target as IDisposable)?.Dispose();
+    }
+
     private static WeakReference CreateMap()
     {
+        var map = new Map();
+        var weak = new WeakReference(map);
+        return weak;
+    }
+
+    private static WeakReference CreateMapControl()
+    {
         var map = new MapControl();
+        var weak = new WeakReference(map);
+        return weak;
+    }
+
+    private static WeakReference CreateMapView()
+    {
+        var map = new MapView();
         var weak = new WeakReference(map);
         return weak;
     }
