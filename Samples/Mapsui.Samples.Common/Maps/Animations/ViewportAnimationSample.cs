84e9afe6ea886f8740f7c6c1272f89f1f91bae2e
pauldendulk <pauldendulk@gmail.com>
2021-12-31 16:50:45 +0100

    Start splitting viewport animations

--- Samples/Mapsui.Samples.Common/Maps/Animations/ViewportAnimationSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Animations/ViewportAnimationSample.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using Mapsui.Extensions;
 using Mapsui.Layers.Tiling;
-using Mapsui.Styles;
 using Mapsui.UI;
-using Mapsui.Utilities;
 using Mapsui.Widgets;
 using Mapsui.Widgets.ScaleBar;
 using Mapsui.Widgets.Zoom;
@@ -13,61 +10,27 @@ namespace Mapsui.Samples.Common.Maps
 {
     public class ViewportAnimationSample : ISample
     {
-        public string Name => "Viewport Animation";
+        public string Name => "FlyTo Viewport Animation";
         public string Category => "Animations";
 
         public static int mode = 1;
         public void Setup(IMapControl mapControl)
         {
             mapControl.Map = CreateMap();
-            var actions = CreateListOfActions(mapControl);
-            var button = CreateButton("Next Mode");
-            button.Touched += (s, e) => Button_Touched(s, e, actions);
-            mapControl.Map.Widgets.Add(button);
 
             mapControl.Map.Info += (s, a) => {
                 if (a.MapInfo?.WorldPosition != null)
                 {
-                    actions[mode](a.MapInfo, mapControl.Viewport);
+                    mapControl.Navigator?.FlyTo(a.MapInfo.WorldPosition, a.MapInfo.Resolution * 1.5, 500);
                 }
             };
         }
 
-        private static List<Action<MapInfo, IReadOnlyViewport>> CreateListOfActions(IMapControl mapControl)
-        {
-            return new List<Action<MapInfo, IReadOnlyViewport>> {
-                (mapInfo, viewport) => mapControl.Navigator?.FlyTo(mapInfo.WorldPosition, viewport.Resolution * 8, 500),
-                (mapInfo, viewport) => mapControl.Navigator?.RotateTo(viewport.Rotation + 56, 500, Easing.CubicIn),
-                (mapInfo, viewport) => mapControl.Navigator?.CenterOn(mapInfo.WorldPosition, 500, Easing.CubicOut),
-                (mapInfo, viewport) => mapControl.Navigator?.NavigateTo(mapInfo.WorldPosition, 4891.9698102512211, 500, Easing.CubicOut),
-                (mapInfo, viewport) => mapControl.Navigator?.ZoomTo(611.49622628140264, mapInfo.ScreenPosition!, 500, Easing.CubicOut),
-                (mapInfo, viewport) => mapControl.Navigator?.ZoomTo(611.49622628140264, 500, Easing.CubicOut)
-            };
-        }
-
-        private void Button_Touched(object _, HyperlinkWidgetArguments e, List<Action<MapInfo, IReadOnlyViewport>> actions)
-        {
-            mode++;
-            if (mode >= actions.Count) mode = 0;
-            e.Handled = true;
-        }
-
-        private static Hyperlink CreateButton(string text)
-        {
-            // Todo: Replace this with a TextButton which diplays the current mode
-            return new Hyperlink()
-            {
-                Text = text,
-                CornerRadius = 2,
-                VerticalAlignment = VerticalAlignment.Top,
-                HorizontalAlignment = HorizontalAlignment.Left,
-                MarginX = 10,
-                MarginY = 10,
-                PaddingX = 4,
-                PaddingY = 4,
-                BackColor = new Color(192, 192, 192),
-            };
-        }
+        //        (mapInfo, viewport) => mapControl.Navigator?.RotateTo(viewport.Rotation + 56, 500, Easing.CubicIn),
+        //        (mapInfo, viewport) => mapControl.Navigator?.CenterOn(mapInfo.WorldPosition, 500, Easing.CubicOut),
+        //        (mapInfo, viewport) => mapControl.Navigator?.NavigateTo(mapInfo.WorldPosition, 4891.9698102512211, 500, Easing.CubicOut),
+        //        (mapInfo, viewport) => mapControl.Navigator?.ZoomTo(611.49622628140264, mapInfo.ScreenPosition!, 500, Easing.CubicOut),
+        //        (mapInfo, viewport) => mapControl.Navigator?.ZoomTo(611.49622628140264, 500, Easing.CubicOut)
 
         public static Map CreateMap()
         {
