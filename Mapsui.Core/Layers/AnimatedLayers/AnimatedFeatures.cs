474c4cf86a9e1badcd77fbec1857bd5c6c0f6d14
pauldendulk <pauldendulk@gmail.com>
2022-01-11 09:19:33 +0100

    Move AnimatedFeature to separate file

--- Mapsui.Core/Layers/AnimatedLayers/AnimatedFeatures.cs
+++ Mapsui.Core/Layers/AnimatedLayers/AnimatedFeatures.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Mapsui.Animations;
+using Mapsui.Layers.AnimationLayers;
 
 namespace Mapsui.Layers
 {
@@ -127,38 +128,6 @@ namespace Mapsui.Layers
             else _animating = false;
 
             return _animating;
-        }
-
-        private class AnimatedFeature
-        {
-            public AnimatedFeature(PointFeature feature, MPoint? previousPoint)
-            {
-                Feature = feature;
-                CurrentPoint = new MPoint(feature.Point);
-                if (previousPoint != null)
-                    PreviousPoint = new MPoint(previousPoint);
-            }
-            public PointFeature Feature { get; }
-            public MPoint? CurrentPoint { get; set; }
-            public MPoint? PreviousPoint { get; }
-
-            public override string ToString()
-            {
-                var stringBuilder = new StringBuilder();
-                stringBuilder.Append("Trackee: " + Feature?["Trackee"]);
-                stringBuilder.Append(", ID: " + Feature?["ID"]);
-                stringBuilder.Append(", speed: " + Feature?["Speed"]);
-                stringBuilder.Append(", Bps: " + Feature?["Bps"]);
-                stringBuilder.Append(", DateGps: " + Feature?["DateGps"]);
-                stringBuilder.Append(", DateReceived: " + Feature?["DateReceived"]);
-                stringBuilder.Append(", Longitude: " + Feature?["Longitude"]);
-                stringBuilder.Append(", Latitude: " + Feature?["Latitude"]);
-                stringBuilder.Append(", X: " + CurrentPoint?.X);
-                stringBuilder.Append(", Y: " + CurrentPoint?.Y);
-                stringBuilder.Append(", Previous X: " + PreviousPoint?.X);
-                stringBuilder.Append(", Previous Y: " + PreviousPoint?.Y);
-                return stringBuilder.ToString();
-            }
         }
     }
 }
