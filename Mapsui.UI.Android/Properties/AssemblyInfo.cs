42e5c9c082bf63c702df2e41a707f31e867503c6
inforithmics <thomas.stocker@gmail.com>
2022-01-28 20:06:27 +0100

    Android net6.0

--- Mapsui.UI.Android/Properties/AssemblyInfo.cs
+++ Mapsui.UI.Android/Properties/AssemblyInfo.cs
@@ -5,12 +5,7 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mapsui.UI.Android")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mapsui.UI.Android")]
-[assembly: AssemblyCopyright("Copyright C  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -23,12 +18,6 @@ using Android.App;
 //      Revision
 //
 
-
-
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.0")]
-
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
-[assembly: AssemblyInformationalVersion("3.0.0")]
