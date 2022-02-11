55bf78c71e0debe143b1b8cdc55135148fd0ec08
pauldendulk <pauldendulk@gmail.com>
2022-02-11 13:52:02 +0100

    Add the to be implemented interface to MRect2

--- Mapsui/MRect2.cs
+++ Mapsui/MRect2.cs
@@ -2,4 +2,34 @@
 
 public class MRect2
 {
+    //double Bottom { get; }
+    //MPoint BottomLeft { get; }
+    //MPoint BottomRight { get; }
+    //MPoint Centroid { get; }
+    //double Height { get; }
+    //double Left { get; }
+    //MPoint Max { get; set; }
+    //double MaxX { get; }
+    //double MaxY { get; }
+    //MPoint Min { get; set; }
+    //double MinX { get; }
+    //double MinY { get; }
+    //double Right { get; }
+    //double Top { get; }
+    //MPoint TopLeft { get; }
+    //MPoint TopRight { get; }
+    //IEnumerable<MPoint> Vertices { get; }
+    //double Width { get; }
+
+    //MRect Clone();
+    //bool Contains(MPoint? p);
+    //bool Contains(MRect r);
+    //bool Equals(MRect? other);
+    //double GetArea();
+    //MRect Grow(double amount);
+    //MRect Grow(double amountInX, double amountInY);
+    //bool Intersects(MRect? box);
+    //MRect Join(MRect? box);
+    //MRect Multiply(double factor);
+    //MQuad Rotate(double degrees);
 }
