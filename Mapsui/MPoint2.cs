c963effd59e099cf9dc5ccc9c6120a6276ed57af
pauldendulk <pauldendulk@gmail.com>
2022-02-11 12:52:47 +0100

    Add Rotate methods contributed by CLA signers

--- Mapsui/MPoint2.cs
+++ Mapsui/MPoint2.cs
@@ -1,9 +1,43 @@
-﻿namespace Mapsui;
+﻿using Mapsui.Utilities;
+
+namespace Mapsui;
 
 public class MPoint2
 {
     public double X { get; set; }
     public double Y { get; set; }
 
+    /// <summary>
+    ///     Calculates a new point by rotating this point clockwise about the specified center point
+    /// </summary>
+    /// <param name="degrees">Angle to rotate clockwise (degrees)</param>
+    /// <param name="centerX">X coordinate of point about which to rotate</param>
+    /// <param name="centerY">Y coordinate of point about which to rotate</param>
+    /// <returns>Returns the rotated point</returns>
+    public MPoint Rotate(double degrees, double centerX, double centerY)
+    {
+        // translate this point back to the center
+        var newX = X - centerX;
+        var newY = Y - centerY;
 
+        // rotate the values
+        var p = Algorithms.RotateClockwiseDegrees(newX, newY, degrees);
+
+        // translate back to original reference frame
+        newX = p.X + centerX;
+        newY = p.Y + centerY;
+
+        return new MPoint(newX, newY);
+    }
+
+    /// <summary>
+    ///     Calculates a new point by rotating this point clockwise about the specified center point
+    /// </summary>
+    /// <param name="degrees">Angle to rotate clockwise (degrees)</param>
+    /// <param name="center">MPoint about which to rotate</param>
+    /// <returns>Returns the rotated point</returns>
+    public MPoint Rotate(double degrees, MPoint center)
+    {
+        return Rotate(degrees, center.X, center.Y);
+    }
 }
