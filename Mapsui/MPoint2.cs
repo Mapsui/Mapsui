e12235885ac0ce0505b2023fb95dc7c7ac7f8f46
pauldendulk <pauldendulk@gmail.com>
2022-02-11 13:17:15 +0100

    Implement the operators in MPoint2

--- Mapsui/MPoint2.cs
+++ Mapsui/MPoint2.cs
@@ -4,34 +4,45 @@ namespace Mapsui;
 
 public class MPoint2
 {
+    public MPoint2(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public MPoint2(MPoint2 point)
+    {
+        X = point.X;
+        Y = point.Y;
+    }
     public double X { get; set; }
     public double Y { get; set; }
     public MRect MRect => new MRect(X, Y, X, Y);
 
-    public MPoint Copy()
+    public MPoint2 Copy()
     {
-        return new MPoint(X, Y);
+        return new MPoint2(X, Y);
     }
 
-    public double Distance(MPoint point)
+    public double Distance(MPoint2 point)
     {
         return Algorithms.Distance(X, Y, point.X, point.Y);
     }
 
-    public bool Equals(MPoint? point)
+    public bool Equals(MPoint2? point)
     {
         if (point == null) return false;
         return X == point.X && Y == point.Y;
     }
 
-    public int GetHashCode()
+    public override int GetHashCode()
     {
         return X.GetHashCode() ^ Y.GetHashCode();
     }
 
-    public MPoint Offset(double offsetX, double offsetY)
+    public MPoint2 Offset(double offsetX, double offsetY)
     {
-        return new MPoint(X + offsetX, Y + offsetY);
+        return new MPoint2(X + offsetX, Y + offsetY);
     }
 
     /// <summary>
@@ -41,7 +52,7 @@ public class MPoint2
     /// <param name="centerX">X coordinate of point about which to rotate</param>
     /// <param name="centerY">Y coordinate of point about which to rotate</param>
     /// <returns>Returns the rotated point</returns>
-    public MPoint Rotate(double degrees, double centerX, double centerY)
+    public MPoint2 Rotate(double degrees, double centerX, double centerY)
     {
         // translate this point back to the center
         var newX = X - centerX;
@@ -54,7 +65,7 @@ public class MPoint2
         newX = p.X + centerX;
         newY = p.Y + centerY;
 
-        return new MPoint(newX, newY);
+        return new MPoint2(newX, newY);
     }
 
     /// <summary>
@@ -63,8 +74,23 @@ public class MPoint2
     /// <param name="degrees">Angle to rotate clockwise (degrees)</param>
     /// <param name="center">MPoint about which to rotate</param>
     /// <returns>Returns the rotated point</returns>
-    public MPoint Rotate(double degrees, MPoint center)
+    public MPoint2 Rotate(double degrees, MPoint2 center)
     {
         return Rotate(degrees, center.X, center.Y);
+    }
+
+    public static MPoint2 operator +(MPoint2 point1, MPoint2 point2)
+    {
+        return new MPoint2(point1.X + point2.X, point1 .Y + point2.Y);
+    }
+
+    public static MPoint2 operator -(MPoint2 point1, MPoint2 point2)
+    {
+        return new MPoint2(point1.X - point2.X, point1.Y - point2.Y);
+    }
+
+    public static MPoint2 operator *(MPoint2 point1, double multiplier)
+    {
+        return new MPoint2(point1.X * multiplier, point1.Y * multiplier);
     }
 }
