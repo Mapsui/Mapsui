1b89f66bda2ecb662d30e49fa27d077ad7a16a2f
inforithmics <thomas.stocker@gmail.com>
2022-02-12 22:54:09 +0100

    Revert Changes in MapRenderer (except Bug Fixes)

--- Mapsui.Layers.Tiling/Provider/RasterizingTileProvider.cs
+++ Mapsui.Layers.Tiling/Provider/RasterizingTileProvider.cs
@@ -59,7 +59,7 @@ public class RasterizingTileProvider : ITileSource
     {
         if (!_rasterizingLayers.TryPop(out var rasterizer))
         {
-            rasterizer = (_rasterizer as IPictureRenderer) ?? (IPictureRenderer)DefaultRendererFactory.Create();
+            rasterizer = _rasterizer ?? DefaultRendererFactory.Create();
         }
 
         return rasterizer;
@@ -68,22 +68,4 @@ public class RasterizingTileProvider : ITileSource
     public ITileSchema Schema => _tileSchema ??= new GlobalSphericalMercator();
     public string Name => _layer.Name;
     public Attribution Attribution => _attribution ??= new Attribution(_layer.Attribution.Text, _layer.Attribution.Url);
-
-    public object? GetPictureTile(TileInfo tileInfo)
-    {
-        var renderer = GetRenderer() as IPictureRenderer;
-
-        if (renderer == null)
-        {
-            return null;
-        }
-
-        Schema.Resolutions.TryGetValue(tileInfo.Index.Level, out var tileResolution);
-
-        var resolution = tileResolution.UnitsPerPixel;
-        var viewPort = RasterizingLayer.CreateViewport(tileInfo.Extent.ToMRect(), resolution, _renderResolutionMultiplier, 1);
-        var result = renderer.RenderToPicture(viewPort, new[] { _layer });
-        _rasterizingLayers.Push(renderer);
-        return result;
-    }
 }
