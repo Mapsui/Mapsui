7b52ec1d178bcf7b7e4676a04150409b5bf8d2b8
pauldendulk <pauldendulk@gmail.com>
2022-02-16 19:25:16 +0100

    Update build bats

--- Scripts/buildpackpush.bat
+++ Scripts/buildpackpush.bat
@@ -11,6 +11,7 @@ nuget push .\Artifacts\Mapsui.Android.%VERSION%.nupkg -source nuget.org || exit 
 nuget push .\Artifacts\Mapsui.Uno.%VERSION%.nupkg -source nuget.org || exit /B 1
 nuget push .\Artifacts\Mapsui.Uwp.%VERSION%.nupkg -source nuget.org || exit /B 1
 nuget push .\Artifacts\Mapsui.WinUI.%VERSION%.nupkg -source nuget.org || exit /B 1
+nuget push .\Artifacts\Mapsui.ArcGIS.%VERSION%.nupkg -source nuget.org || exit /B 1
 git commit -m %VERSION% -a || exit /B 1
 git tag %VERSION% || exit /B 1
 git push origin %VERSION% || exit /B 1
