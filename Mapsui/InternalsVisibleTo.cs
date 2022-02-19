7c1841e5f1222332497a98b38352dd42e10afcce
Thomas Stocker <thomas.stocker@gmail.com>
2022-02-19 17:11:48 +0100

    Fixed project

--- Mapsui/InternalsVisibleTo.cs
+++ Mapsui/InternalsVisibleTo.cs
@@ -1,4 +1,4 @@
 ﻿// In dotnet framework this used to be part of AssemblyInfo.cs.
 // This is how I solved it while porting to dotnet core.
 using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("Mapsui.Tests")] 
+[assembly: InternalsVisibleTo("Mapsui.Tests")]
