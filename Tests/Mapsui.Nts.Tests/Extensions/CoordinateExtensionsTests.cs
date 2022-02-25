0d3b8c1b9d942be555791d7556010b3f7ba83b2c
pauldendulk <pauldendulk@gmail.com>
2022-02-25 09:43:52 +0100

    Add test for throwing ToLineString

---
+++ Tests/Mapsui.Nts.Tests/Extensions/CoordinateExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using NetTopologySuite.Geometries;
+using NUnit.Framework;
+using Mapsui.Nts.Extensions;
+using System;
+
+namespace Mapsui.Nts.Tests;
+
+[TestFixture]
+public class CoordinateExtensionsTests
+{
+    [Test]
+    public void CoordinateExtensionsShouldThrow()
+    {
+        // Arrange
+        IEnumerable<Coordinate> coordinates = new List<Coordinate>();
+
+        // Act & Assert
+        var lineString = Assert.Throws<Exception>(() => coordinates.ToLineString());
+    }
+}
+
