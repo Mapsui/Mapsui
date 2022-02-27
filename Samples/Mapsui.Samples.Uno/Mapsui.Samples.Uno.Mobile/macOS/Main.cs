ab5480f2f6052f9764d7b0a6e5e4a8824e7176b8
inforithmics <thomas.stocker@gmail.com>
2022-02-27 18:53:45 +0100

    Uno .Net 6

---
+++ Samples/Mapsui.Samples.Uno/Mapsui.Samples.Uno.Mobile/macOS/Main.cs
@@ -0,0 +1,16 @@
+using AppKit;
+using Mapsui.Samples.Uwp;
+
+namespace Mapsui.Samples.Uno.macOS
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new App();
+			NSApplication.Main(args);  
+		}
+	}
+}
+
