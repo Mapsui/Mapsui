f058a1203dfe5424984bb601fe5311acc4d572ae
inforithmics <thomas.stocker@gmail.com>
2022-03-01 00:42:55 +0100

    Initial checkin for Sample Regression Tests

---
+++ Tests/Mapsui.Rendering.Skia.Tests/MapRegressionTests.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) The Mapsui authors.
+// The Mapsui authors licensed this file under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using Mapsui.Rendering.Skia.Tests.Extensions;
+using Mapsui.Samples.Common;
+using NUnit.Framework;
+
+namespace Mapsui.Rendering.Skia.Tests;
+
+[TestFixture, Apartment(ApartmentState.STA)]
+public class MapRegressionTests
+{
+    [Test]
+    public void TestAllSamples()
+    {
+        var exceptions = new List<Exception>();
+
+        foreach (var sample in AllSamples.GetSamples())
+        {
+            try
+            {
+                TestSample(sample);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                exceptions.Add(e);
+            }
+        }
+
+        Assert.AreEqual(exceptions.Count, 0, "No exceptions should happen");
+    }
+
+    private void TestSample(ISample sample)
+    {
+        var fileName = sample.GetType().Name + ".Regression.png";
+        var mapControl = new TestMapControl();
+        sample.Setup(mapControl);
+        var map = mapControl.Map;
+        if (map != null)
+        {
+            var viewport = map.Extent!.Multiply(3).ToViewport(200);
+
+            // act
+            using var bitmap = new MapRenderer().RenderToBitmapStream(viewport, map.Layers, map.BackColor, 2);
+
+            // aside
+            File.WriteToGeneratedFolder(fileName, bitmap);
+
+            // assert
+            Assert.IsTrue(MapRendererTests.CompareBitmaps(File.ReadFromOriginalFolder(fileName), bitmap, 1, 0.99));
+        }
+    }
+}
---
+++ Tests/Mapsui.Rendering.Skia.Tests/TestMapControl.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) The Mapsui authors.
+// The Mapsui authors licensed this file under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using Mapsui.Layers;
+using Mapsui.UI;
+using Mapsui.Utilities;
+
+namespace Mapsui.Rendering.Skia.Tests;
+
+public class TestMapControl : IMapControl
+{
+    public event EventHandler<MapInfoEventArgs>? Info;
+    public Map? Map { get; set; }
+    public event EventHandler? ViewportInitialized;
+    public void RefreshGraphics()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RefreshData(ChangeType changeType = ChangeType.Discrete)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Refresh(ChangeType changeType = ChangeType.Discrete)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double UnSnapRotationDegrees { get; set; }
+    public double ReSnapRotationDegrees { get; set; }
+    public void Unsubscribe()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IRenderer Renderer { get; }
+    public void OpenBrowser(string url)
+    {
+        throw new NotImplementedException();
+    }
+
+    public float PixelDensity { get; }
+    public MPoint ToDeviceIndependentUnits(MPoint coordinateInPixels)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MPoint ToPixels(MPoint coordinateInDeviceIndependentUnits)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MapInfo? GetMapInfo(MPoint screenPosition, int margin = 0)
+    {
+        throw new NotImplementedException();
+    }
+
+    public byte[]? GetSnapshot(IEnumerable<ILayer>? layers = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public INavigator? Navigator { get; }
+    public Performance? Performance { get; set; }
+    public IReadOnlyViewport Viewport { get; }
+}
