2cb04110a3b24c827f429653b04a4f830586e296
inforithmics <thomas.stocker@gmail.com>
2022-03-03 14:46:13 +0100

    added geoGif Sample

---
+++ Samples/Mapsui.Samples.Common.Desktop/GeoGifSample.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Reflection;
+using Mapsui.Layers;
+using Mapsui.Providers;
+using Mapsui.UI;
+
+namespace Mapsui.Samples.Common.Desktop
+{
+    public class GeoGifSample : ISample
+    {
+        public string Name => "6 Geo Gif";
+        public string Category => "Desktop";
+
+        public void Setup(IMapControl mapControl)
+        {
+            mapControl.Map = CreateMap();
+        }
+
+        public static Map CreateMap()
+        {
+            var map = new Map();
+            var gif = new GeoTiffProvider(GetAppDir() + "\\Images\\example.tif");
+            map.Layers.Add(CreateGifLayer(gif));
+
+            return map;
+        }
+
+        private static ILayer CreateGifLayer(IProvider<IFeature> gifSource)
+        {
+            return new Layer
+            {
+                Name = "GeoGif",
+                DataSource = gifSource,
+            };
+        }
+
+        private static string GetAppDir()
+        {
+            return Path.GetDirectoryName(Assembly.GetEntryAssembly()!.GetModules()[0].FullyQualifiedName)!;
+        }
+    }
+}
