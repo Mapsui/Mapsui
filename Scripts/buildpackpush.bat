920498ae79585181fc616d2b2cea189b61baa83e
pauldendulk <pauldendulk@gmail.com>
2022-03-20 10:50:02 +0100

    Enable Avalonia and Eto in buildpackpush

--- Scripts/buildpackpush.bat
+++ Scripts/buildpackpush.bat
@@ -7,11 +7,11 @@ nuget push .\Artifacts\Mapsui.Wpf.%VERSION%.nupkg -source nuget.org || exit /B 1
 nuget push .\Artifacts\Mapsui.Android.%VERSION%.nupkg -source nuget.org || exit /B 1
 nuget push .\Artifacts\Mapsui.iOS.%VERSION%.nupkg -source nuget.org || exit /B 1
 nuget push .\Artifacts\Mapsui.Forms.%VERSION%.nupkg -source nuget.org || exit /B 1
-REM nuget push .\Artifacts\Mapsui.Avalonia.%VERSION%.nupkg -source nuget.org || exit /B 1
+nuget push .\Artifacts\Mapsui.Avalonia.%VERSION%.nupkg -source nuget.org || exit /B 1
 REM nuget push .\Artifacts\Mapsui.Maui.%VERSION%.nupkg -source nuget.org || exit /B 1
 REM nuget push .\Artifacts\Mapsui.Uno.%VERSION%.nupkg -source nuget.org || exit /B 1
 REM nuget push .\Artifacts\Mapsui.WinUI.%VERSION%.nupkg -source nuget.org || exit /B 1
-REM nuget push .\Artifacts\Mapsui.Eto.%VERSION%.nupkg -source nuget.org || exit /B 1
+nuget push .\Artifacts\Mapsui.Eto.%VERSION%.nupkg -source nuget.org || exit /B 1
 git commit -m %VERSION% -a || exit /B 1
 git tag %VERSION% || exit /B 1
 git push origin %VERSION% || exit /B 1
