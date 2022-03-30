54a493f87aa8b7772cd826c2170f902b54766bf1
pauldendulk <pauldendulk@gmail.com>
2022-03-30 19:50:23 +0200

    Use svg as symbol in WritableLayerSample

--- Samples/Mapsui.Samples.Common/Maps/Special/WritableLayerSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Special/WritableLayerSample.cs
@@ -1,8 +1,10 @@
 ﻿using Mapsui.Layers;
 using Mapsui.Nts;
 using Mapsui.Samples.Common;
+using Mapsui.Styles;
 using Mapsui.Tiling;
 using Mapsui.UI;
+using Mapsui.Utilities;
 using NetTopologySuite.Geometries;
 
 namespace Mapsui.Tests.Common.Maps
@@ -24,20 +26,22 @@ namespace Mapsui.Tests.Common.Maps
             map.Layers.Add(OpenStreetMap.CreateTileLayer());
 
             var writableLayer = new WritableLayer();
+            var pinId = typeof(Map).LoadSvgId("Resources.Images.Pin.svg");
+            writableLayer.Style = new SymbolStyle { BitmapId = pinId, SymbolOffset = new Offset(0.0, 0.5, true) };
+
             map.Layers.Add(writableLayer);
 
             map.Info += (s, e) =>
+            {
+                if (e.MapInfo?.WorldPosition == null) return;
+                writableLayer?.Add(new GeometryFeature
                 {
-                    if (e.MapInfo?.WorldPosition == null) return;
-
-                    writableLayer?.Add(new GeometryFeature
-                    {
-                        Geometry = new Point(e.MapInfo.WorldPosition.X, e.MapInfo.WorldPosition.Y)
-                    });
-                    // To notify the map that a redraw is needed.
-                    writableLayer?.DataHasChanged();
-                    return;
-                };
+                    Geometry = new Point(e.MapInfo.WorldPosition.X, e.MapInfo.WorldPosition.Y)
+                });
+                // To notify the map that a redraw is needed.
+                writableLayer?.DataHasChanged();
+                return;
+            };
 
             return map;
         }
