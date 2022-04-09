cdb1de0415e903747001cdf053fe543f83d8b466
inforithmics <thomas.stocker@gmail.com>
2022-04-09 09:48:41 +0200

    fixing build

--- Tests/Mapsui.Tests/Wms/WmsProviderTests.cs
+++ Tests/Mapsui.Tests/Wms/WmsProviderTests.cs
@@ -17,13 +17,13 @@ namespace Mapsui.Tests.Wms
             var capabilties = new XmlDocument { XmlResolver = null };
             capabilties.Load($"{AssemblyInfo.AssemblyDirectory}\\Resources\\capabilities_1_3_0.xml");
             var provider = new WmsProvider(capabilties) { CRS = "EPSG:3857" };
-            provider.AddLayer("Maasluis complex - top");
-            provider.AddLayer("Kreftenheye z2 - top");
+            await provider.AddLayerAsync("Maasluis complex - top");
+            await provider.AddLayerAsync("Kreftenheye z2 - top");
             await provider.SetImageFormatAsync((await provider.OutputFormatsAsync())[0]);
             provider.ContinueOnError = true;
 
             // act
-            var legendUrls = provider.GetLegendRequestUrls();
+            var legendUrls = await provider.GetLegendRequestUrlsAsync();
 
             // assert
             Assert.True(legendUrls.Count() == 2);
