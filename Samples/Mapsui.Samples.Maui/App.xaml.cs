16d83a66227dcb8e889ed9de05118b9d3adfdb5f
pauldendulk <pauldendulk@gmail.com>
2022-05-25 16:40:17 +0200

    Add empty Maui sample

---
+++ Samples/Mapsui.Samples.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Mapsui.Samples.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Samples/Mapsui.Samples.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Mapsui.Samples.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
