2b436271311aba4e3dac17aa70031feccad11c92
pauldendulk <pauldendulk@gmail.com>
2022-05-27 09:24:41 +0200

    Add samples

--- Samples/Mapsui.Samples.Maui/MainPage.cs
+++ Samples/Mapsui.Samples.Maui/MainPage.cs
@@ -1,13 +1,76 @@
+using CommunityToolkit.Maui.Markup;
+using Mapsui.Samples.Common;
+using Mapsui.Tiling;
+using Mapsui.UI.Maui;
+
 namespace Mapsui.Samples.Maui;
 
 public class MainPage : ContentPage
 {
-	public MainPage()
+    readonly IEnumerable<ISampleBase> allSamples;
+
+    public MainPage()
 	{
-        var mapControl = new Mapsui.UI.Maui.MapControl();
+        MapControl.Map?.Layers.Add(OpenStreetMap.CreateTileLayer());
 
-        mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+        Content = new Grid
+        {
+            ColumnDefinitions =
+            {
+                new ColumnDefinition { Width = new GridLength(1, GridUnitType.Auto) },
+                new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) }
+            },
+            Children =
+            {
+                new VerticalStackLayout()
+                {
+                    Spacing = 20,
+                    Children =
+                    {
+                        SampleCategoryPicker,
+                        SampleList
+                    }
+                }.Column(0).Padding(20),
+                MapControl.Column(1)
+            }
+        };
 
-        Content = mapControl;
-	}
+        SampleList.ItemSelected += SampleList_ItemSelected;
+
+        allSamples = AllSamples.GetSamples() ?? new List<ISampleBase>();
+        var categories = allSamples.Select(s => s.Category).Distinct().OrderBy(c => c);
+        SampleCategoryPicker!.ItemsSource = categories.ToList<string>();
+        SampleCategoryPicker.SelectedIndexChanged += SampleCategoryPicker_SelectedIndexChanged; ;
+        SampleCategoryPicker.SelectedItem = "Info";
+    }
+
+    private void SampleList_ItemSelected(object? sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem == null)
+        {
+            return; //ItemSelected is called on deselection, which results in SelectedItem being set to null
+        }
+
+        var sampleName = e.SelectedItem.ToString();
+        var baseSample = allSamples.FirstOrDefault(x => x.Name == sampleName);
+
+        if (baseSample is ISample sample)
+            sample.Setup(MapControl);
+    }
+
+    public ListView SampleList { get; set; } = new ListView();
+    public Picker SampleCategoryPicker { get; set; } = new Picker();
+
+    public MapControl MapControl = new MapControl();
+
+    private void SampleCategoryPicker_SelectedIndexChanged(object? sender, EventArgs e)
+    {
+        FillListWithSamples();
+    }
+
+    private void FillListWithSamples()
+    {
+        var selectedCategory = SampleCategoryPicker.SelectedItem?.ToString() ?? "";
+        SampleList.ItemsSource = allSamples.Where(s => s.Category == selectedCategory).Select(x => x.Name);
+    }
 }
