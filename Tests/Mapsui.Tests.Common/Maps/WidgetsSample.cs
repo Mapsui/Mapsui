97ca458e4501b1f9dcb4c19d05dc43f4bc4c838d
pauldendulk <pauldendulk@gmail.com>
2022-06-01 16:47:55 +0200

    Fix scalebar test alignment issue for #1607 and add regression test

---
+++ Tests/Mapsui.Tests.Common/Maps/WidgetsSample.cs
@@ -0,0 +1,60 @@
+﻿using Mapsui.Extensions;
+using Mapsui.Layers;
+using Mapsui.Samples.Common;
+using Mapsui.Styles;
+using Mapsui.UI;
+using Mapsui.Widgets;
+using Mapsui.Widgets.ScaleBar;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mapsui.Tests.Common.Maps
+{
+    public class WidgetsSample : ISample
+    {
+        public string Name => "Widgets";
+
+        public string Category => "Tests";
+
+        public void Setup(IMapControl mapControl)
+        {
+            mapControl.Map = CreateMap();
+        }
+
+        public static Map CreateMap()
+        {
+            var map = new Map
+            {
+                BackColor = Color.FromString("WhiteSmoke"),
+                CRS = "EPSG:3857",
+            };
+
+            map.Widgets.Add(CreateScaleBarWidget(map));
+            map.Widgets.Add(CreateScaleBarWidget(map, HorizontalAlignment.Right, textAligment: Alignment.Right));
+            map.Widgets.Add(CreateScaleBarWidget(map, verticalAlignment: VerticalAlignment.Bottom));
+            map.Widgets.Add(CreateScaleBarWidget(map, HorizontalAlignment.Right, VerticalAlignment.Bottom, textAligment: Alignment.Right));
+
+            return map;
+        }
+
+        private static ScaleBarWidget CreateScaleBarWidget(Map map, 
+            HorizontalAlignment horizontalAlignment = HorizontalAlignment.Left,
+            VerticalAlignment verticalAlignment = VerticalAlignment.Top,
+            Alignment textAligment = Alignment.Left)
+        {
+            return new ScaleBarWidget(map)
+            {
+                ScaleBarMode = ScaleBarMode.Both,
+                SecondaryUnitConverter = ImperialUnitConverter.Instance,
+                MarginX = 10,
+                MarginY = 30,
+                MaxWidth = 200,
+                Halo = Color.White,
+                HorizontalAlignment = horizontalAlignment,
+                VerticalAlignment = verticalAlignment,
+                TextAlignment = textAligment
+            };
+        }
+    }
+}
