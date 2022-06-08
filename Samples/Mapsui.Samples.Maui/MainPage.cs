5e0c165cf538d0c88d9b8489c0ce5712d0ad4c2e
pauldendulk <pauldendulk@gmail.com>
2022-06-08 19:21:20 +0200

    Rewrite initializing collection view

--- Samples/Mapsui.Samples.Maui/MainPage.cs
+++ Samples/Mapsui.Samples.Maui/MainPage.cs
@@ -8,9 +8,51 @@ namespace Mapsui.Samples.Maui;
 public class MainPage : ContentPage
 {
     IEnumerable<ISampleBase> allSamples;
-    CollectionView collectionView = CreateCollectionView();
-    Picker sampleCategoryPicker = new Picker();
+    CollectionView sampleCollectionView = CreateCollectionView();
+    Picker categoryPicker = CreatePicker();
     MapControl mapControl = new MapControl();
+
+    public MainPage()
+	{
+        sampleCollectionView.SelectionChanged += CollectionView_SelectionChanged;
+        mapControl.Map?.Layers.Add(OpenStreetMap.CreateTileLayer());
+
+        Content = new Grid
+        {
+            ColumnDefinitions =
+            {
+                new ColumnDefinition { Width = new GridLength(1, GridUnitType.Auto) },
+                new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) }
+            },
+            Children =
+            {
+                new VerticalStackLayout()
+                {
+                    Spacing = 20,
+                    Children =
+                    {
+                        categoryPicker,
+                        sampleCollectionView
+                    }
+                }.Column(0).Padding(20),
+                mapControl.Column(1)
+            }
+        };
+
+        allSamples = AllSamples.GetSamples() ?? new List<ISampleBase>();
+        var categories = allSamples.Select(s => s.Category).Distinct().OrderBy(c => c);
+        categoryPicker!.ItemsSource = categories.ToList();
+        categoryPicker.SelectedIndexChanged += categoryPicker_SelectedIndexChanged;
+        categoryPicker.SelectedItem = "Info";
+    }
+
+    private static Picker CreatePicker()
+    {
+        return new Picker
+        {
+            WidthRequest = 200,
+        };
+    }
 
     private static CollectionView CreateCollectionView()
     {
@@ -35,76 +77,35 @@ public class MainPage : ContentPage
                 Padding = 0,
                 Children =
                 {
-                    new Label{ Style = CreateLabelStyle() }.Bind(Label.TextProperty, nameof(ISample.Name)),
+                    new Label
+                    {
+                        WidthRequest = 200
+                        
+                    }.Bind(Label.TextProperty, nameof(ISample.Name)),
                 }
             }
         };
-    }
-
-    private static Style CreateLabelStyle()
-    {
-        var style = new Style(typeof(Label));
-        style.Setters.Add(new Setter { Property = Label.VerticalOptionsProperty, Value = LayoutOptions.Center });
-        style.Setters.Add(new Setter { Property = Label.HorizontalOptionsProperty, Value = LayoutOptions.Start });
-        style.Setters.Add(new Setter { Property = Label.HorizontalTextAlignmentProperty, Value = TextAlignment.Start });
-        style.Setters.Add(new Setter { Property = Label.WidthRequestProperty, Value = 200 });
-        return style;
-    }
-
-
-    public MainPage()
-	{
-        collectionView.SelectionChanged += CollectionView_SelectionChanged;
-        mapControl.Map?.Layers.Add(OpenStreetMap.CreateTileLayer());
-
-        Content = new Grid
-        {
-            ColumnDefinitions =
-            {
-                new ColumnDefinition { Width = new GridLength(1, GridUnitType.Auto) },
-                new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) }
-            },
-            Children =
-            {
-                new VerticalStackLayout()
-                {
-                    Spacing = 20,
-                    Children =
-                    {
-                        sampleCategoryPicker,
-                        collectionView
-                    }
-                }.Column(0).Padding(20),
-                mapControl.Column(1)
-            }
-        };
-
-        allSamples = AllSamples.GetSamples() ?? new List<ISampleBase>();
-        var categories = allSamples.Select(s => s.Category).Distinct().OrderBy(c => c);
-        sampleCategoryPicker!.ItemsSource = categories.ToList();
-        sampleCategoryPicker.SelectedIndexChanged += SampleCategoryPicker_SelectedIndexChanged; ;
-        sampleCategoryPicker.SelectedItem = "Info";
     }
 
     private void CollectionView_SelectionChanged(object? sender, SelectionChangedEventArgs e)
     {
         if (e.CurrentSelection == null)
         {
-            return; //ItemSelected is called on deselection, which results in SelectedItem being set to null
+            return;
         }
 
-        var sample = ((ISample)e.CurrentSelection[0]);
+        var sample = (ISample)e.CurrentSelection[0];
         sample.Setup(mapControl);
     }
 
-    private void SampleCategoryPicker_SelectedIndexChanged(object? sender, EventArgs e)
+    private void categoryPicker_SelectedIndexChanged(object? sender, EventArgs e)
     {
         FillListWithSamples();
     }
 
     private void FillListWithSamples()
     {
-        var selectedCategory = sampleCategoryPicker.SelectedItem?.ToString() ?? "";
-        collectionView.ItemsSource = allSamples.Where(s => s.Category == selectedCategory);
+        var selectedCategory = categoryPicker.SelectedItem?.ToString() ?? "";
+        sampleCollectionView.ItemsSource = allSamples.Where(s => s.Category == selectedCategory);
     }
 }
