debe4780525f3b577b3398950a56f127e230eb79
Thomas Stocker <thomas.stocker@gmail.com>
2022-06-22 23:33:46 +0200

    adding Map to MainPage

--- Samples/Mapsui.Samples.Uno.WinUI/Mapsui.Samples.Uno.WinUI.Shared/App.xaml.cs
+++ Samples/Mapsui.Samples.Uno.WinUI/Mapsui.Samples.Uno.WinUI.Shared/App.xaml.cs
@@ -80,7 +80,7 @@ namespace Mapsui.Samples.Uno.WinUI
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), args.Arguments);
+                    rootFrame.Navigate(typeof(MainWindow), args.Arguments);
                 }
                 // Ensure the current window is active
                 _window.Activate();
