b7416c9ae362d3b7932b197151ba943484dfc6e0
Thomas Stocker <thomas.stocker@gmail.com>
2022-07-16 22:17:44 +0200

    Script for disabling central Package management

---
+++ Scripts/DisableCentralPackageManagement.ps1
@@ -0,0 +1,18 @@
+# Disable Central Package Management
+(Get-Content -path $PSScriptRoot\..\Directory.Packages.props -Raw) -replace 'true','false'  | Set-Content -Path $PSScriptRoot\..\Directory.Packages.props
+
+# Set Version in files
+(Get-Content -path $PSScriptRoot\..\Directory.Packages.props -Raw) | Select-Xml -XPath "/Project/ItemGroup/PackageVersion" | foreach {  
+    $include=$_.node.Include
+    $version=$_.node.Version
+
+    # Write-Host $include $version
+    # Write-Host "Include=""$include"""
+    # Write-Host "Include=""$include"" Version=""$version"""
+    # (Get-Content -path $PSScriptRoot\..\Directory.Build.props -Raw) -replace "Include=""$include""" ,"Include=""$include"" Version=""$version""" | Set-Content -Path $PSScriptRoot\..\Directory.Build.props
+
+    $fileNames = Get-ChildItem -Path $PSScriptRoot\.. -Recurse -Include *.csproj,Directory.Build.props
+    foreach ($file in $fileNames) {
+        (Get-Content -path $file -Raw) -replace "Include=""$include""" ,"Include=""$include"" Version=""$version""" | Set-Content -Path $file
+    }
+}
