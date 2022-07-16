74ab6af06feddf095f3caba8dafb932769ca22ba
Thomas Stocker <thomas.stocker@gmail.com>
2022-07-16 22:27:14 +0200

    Script for Disable Central Package Management

--- Scripts/DisableCentralPackageManagement.ps1
+++ Scripts/DisableCentralPackageManagement.ps1
@@ -1,18 +1,22 @@
 # Disable Central Package Management
 (Get-Content -path $PSScriptRoot\..\Directory.Packages.props -Raw) -replace 'true','false'  | Set-Content -Path $PSScriptRoot\..\Directory.Packages.props
 
-# Set Version in files
-(Get-Content -path $PSScriptRoot\..\Directory.Packages.props -Raw) | Select-Xml -XPath "/Project/ItemGroup/PackageVersion" | foreach {  
-    $include=$_.node.Include
-    $version=$_.node.Version
+$fileNames = Get-ChildItem -Path $PSScriptRoot\.. -Recurse -Include *.csproj,Directory.Build.props
 
-    # Write-Host $include $version
-    # Write-Host "Include=""$include"""
-    # Write-Host "Include=""$include"" Version=""$version"""
-    # (Get-Content -path $PSScriptRoot\..\Directory.Build.props -Raw) -replace "Include=""$include""" ,"Include=""$include"" Version=""$version""" | Set-Content -Path $PSScriptRoot\..\Directory.Build.props
+foreach ($file in $fileNames) {
+    $fileContent = (Get-Content -path $file -Raw)
+    # Set Version in files
+    (Get-Content -path $PSScriptRoot\..\Directory.Packages.props -Raw) | Select-Xml -XPath "/Project/ItemGroup/PackageVersion" | foreach {  
+        $include=$_.node.Include
+        $version=$_.node.Version
 
-    $fileNames = Get-ChildItem -Path $PSScriptRoot\.. -Recurse -Include *.csproj,Directory.Build.props
-    foreach ($file in $fileNames) {
-        (Get-Content -path $file -Raw) -replace "Include=""$include""" ,"Include=""$include"" Version=""$version""" | Set-Content -Path $file
+        # Write-Host $include $version
+        # Write-Host "Include=""$include"""
+        # Write-Host "Include=""$include"" Version=""$version"""
+        # (Get-Content -path $PSScriptRoot\..\Directory.Build.props -Raw) -replace "Include=""$include""" ,"Include=""$include"" Version=""$version""" | Set-Content -Path $PSScriptRoot\..\Directory.Build.props
+      
+        $fileContent = $fileContent -replace "Include=""$include""" ,"Include=""$include"" Version=""$version"""
     }
+
+    Set-Content -Path $file $fileContent
 }
