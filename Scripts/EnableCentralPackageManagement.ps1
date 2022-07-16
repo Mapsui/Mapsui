2e2c3a0690805926014aec9bd1135ca318657266
Thomas Stocker <thomas.stocker@gmail.com>
2022-07-16 22:31:30 +0200

    Enable Central Package Maangement

---
+++ Scripts/EnableCentralPackageManagement.ps1
@@ -0,0 +1,22 @@
+# Disable Central Package Management
+(Get-Content -path $PSScriptRoot\..\Directory.Packages.props -Raw) -replace 'false','true'  | Set-Content -Path $PSScriptRoot\..\Directory.Packages.props
+
+$fileNames = Get-ChildItem -Path $PSScriptRoot\.. -Recurse -Include *.csproj,Directory.Build.props
+
+foreach ($file in $fileNames) {
+    $fileContent = (Get-Content -path $file -Raw)
+    # Set Version in files
+    (Get-Content -path $PSScriptRoot\..\Directory.Packages.props -Raw) | Select-Xml -XPath "/Project/ItemGroup/PackageVersion" | foreach {  
+        $include=$_.node.Include
+        $version=$_.node.Version
+
+        # Write-Host $include $version
+        # Write-Host "Include=""$include"""
+        # Write-Host "Include=""$include"" Version=""$version"""
+        # (Get-Content -path $PSScriptRoot\..\Directory.Build.props -Raw) -replace "Include=""$include""" ,"Include=""$include"" Version=""$version""" | Set-Content -Path $PSScriptRoot\..\Directory.Build.props
+      
+        $fileContent = $fileContent -replace "Include=""$include"" Version=""$version""", "Include=""$include"""
+    }
+
+    Set-Content -Path $file $fileContent
+}
