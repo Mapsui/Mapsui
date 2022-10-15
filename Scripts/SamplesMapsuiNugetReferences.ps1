a5d26a73862c6dce446cd7d51621d2ea8a248ef3
Thomas Stocker <thomas.stocker@gmail.com>
2022-10-15 15:33:21 +0200

    Added Samples Mapsui Nuget References Script

---
+++ Scripts/SamplesMapsuiNugetReferences.ps1
@@ -0,0 +1,20 @@
+# Disable Central Package Management
+$Packages = (Get-Content -path $PSScriptRoot\..\Directory.Packages.props -Encoding UTF8)
+$fileNames = Get-ChildItem -Path $PSScriptRoot\..\Samples\.. -Recurse -Include *.csproj
+
+foreach ($file in $fileNames) {
+    $fileContent = (Get-Content -path $file -Encoding UTF8)
+    # Set Version in files
+    $Packages | Select-Xml -XPath "//PackageVersion" | foreach {  
+        $include=$_.node.Include
+        $project = $include + ".csproj"
+        $version=$_.node.Version
+      
+        #$fileContent = $fileContent -replace "Include=""$include"" Version=""$version""", "Include=""$include"""
+        $fileContent = $fileContent -replace "<ProjectReference Include=`"`[\.\\a-zA-Z]*$project`"` />", "<PackageReference Include=""$include"" />"
+    }
+       
+    # Normalize to no Cariage Return at the End
+    $fileContent = $fileContent -replace "</Project>`r`n", "</Project>"
+    Set-Content -Path $file $fileContent -Encoding UTF8
+}
