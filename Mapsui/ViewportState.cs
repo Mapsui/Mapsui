f5bac2b03b0338a993337e300920e3f489b411c0
pauldendulk <pauldendulk@gmail.com>
2022-10-29 20:34:11 +0200

    Add documentation from IViewport

--- Mapsui/ViewportState.cs
+++ Mapsui/ViewportState.cs
@@ -9,13 +9,48 @@ namespace Mapsui
 {
     public record class ViewportState
     {
+        /// <summary>
+        /// The X coordinate of the center of the viewport in world coordinates
+        /// </summary>
         public double CenterX { get; init; }
+        /// <summary>
+        /// The Y coordinate of the center of the viewport in world coordinates
+        /// </summary>
         public double CenterY { get; init; }
+        /// <summary>
+        /// Resolution of the viewport in units per pixel
+        /// </summary>
+        /// <remarks>
+        /// The Resolution in Mapsui is what is often called zoom level. Because Mapsui is projection independent, there 
+        /// aren't any zoom levels as other map libraries have. If your map has EPSG:3857 as projection
+        /// and you want to calculate the zoom, you should use the following equation
+        /// 
+        ///     var zoom = (float)Math.Log(78271.51696401953125 / resolution, 2);
+        /// </remarks>
         public double Resolution { get; init; }
+        /// <summary>
+        /// Viewport rotation from True North (clockwise degrees)
+        /// </summary>
         public double Rotation { get; init; }
+        /// <summary>
+        /// Width of viewport in screen pixels
+        /// </summary>
         public double Width { get; init; }
+        /// <summary>
+        /// Height of viewport in screen pixels
+        /// </summary>
         public double Height { get; init; }
+        /// <summary>
+        /// MRect of viewport in map coordinates respecting Rotation
+        /// </summary>
+        /// <remarks>
+        /// This MRect is horizontally and vertically aligned, even if the viewport
+        /// is rotated. So this MRect perhaps contain parts, that are not visible.
+        /// </remarks>
         public MRect Extent { get; private init; }
+        /// <summary>
+        /// IsRotated is true, when viewport displays map rotated
+        /// </summary>
         public bool IsRotated { get; private init; }
 
         public ViewportState(double centerX, double centerY, double resolution, double rotation, double width, double height)
