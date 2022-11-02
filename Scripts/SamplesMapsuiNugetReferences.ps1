a9f5ddf94cb299cb98c774e0b32da7861790f8e6
Thomas Stocker <thomas.stocker@gmail.com>
2022-11-02 21:38:09 +0100

    fixing ProjectReference to PackageREference script

--- Scripts/SamplesMapsuiNugetReferences.ps1
+++ Scripts/SamplesMapsuiNugetReferences.ps1
@@ -6,19 +6,21 @@ foreach ($file in $fileNames) {
     $fileContent = (Get-Content -raw -path $file -Encoding UTF8)
     # Set Version in files
     $Packages | Select-Xml -XPath "//PackageVersion" | foreach {  
-        $include=$_.node.Include
-        $project = $include + ".csproj"
-        $includeui = $include -replace "Mapsui.", "Mapsui.UI."
-        $projectui = $includeui + ".csproj"
-        $version=$_.node.Version
+        if ($_.node.Include.StartsWith("Mapsui")) {                    
+            $include=$_.node.Include
+            $project = $include + ".csproj"
+            $includeui = $include -replace "Mapsui.", "Mapsui.UI."
+            $projectui = $includeui + ".csproj"
+            $version=$_.node.Version
         
-        # <ProjectReference .... />     
-        $fileContent = $fileContent -replace "<ProjectReference Include=`"`[\.\\a-zA-Z]*$project`"` />", "<PackageReference Include=""$include"" />"
-        $fileContent = $fileContent -replace "<ProjectReference Include=`"`[\.\\a-zA-Z]*$projectui`"` />", "<PackageReference Include=""$include"" />"       
+            # <ProjectReference .... />     
+            $fileContent = $fileContent -replace "<ProjectReference Include=`"`[\.\\a-zA-Z]*$project`"` />", "<PackageReference Include=""$include"" />"
+            $fileContent = $fileContent -replace "<ProjectReference Include=`"`[\.\\a-zA-Z]*$projectui`"` />", "<PackageReference Include=""$include"" />"       
 
-        # <ProjectReference .... >...</ProjectReference'
-        $fileContent = $fileContent -replace "<ProjectReference Include=`"`[\.\\a-zA-Z]*$project`"`>[`r`n <>{}/\.\-a-zA-Z0-9]*</ProjectReference>", "<PackageReference Include=""$include"" />"
-        $fileContent = $fileContent -replace "<ProjectReference Include=`"`[\.\\a-zA-Z]*$projectui`"`>[`r`n <>{}/\.\-a-zA-Z0-9]*</ProjectReference>", "<PackageReference Include=""$include"" />"       
+            # <ProjectReference .... >...</ProjectReference'
+            $fileContent = $fileContent -replace "<ProjectReference Include=`"`[\.\\a-zA-Z]*$project`"`>[`r`n <>{}/\.\-a-zA-Z0-9]*</ProjectReference>", "<PackageReference Include=""$include"" />"
+            $fileContent = $fileContent -replace "<ProjectReference Include=`"`[\.\\a-zA-Z]*$projectui`"`>[`r`n <>{}/\.\-a-zA-Z0-9]*</ProjectReference>", "<PackageReference Include=""$include"" />"       
+        }
     }
        
     # Normalize to no Cariage Return at the End
