6ed7716c78386ae6b2ad22baedb324f105f290f9
Thomas Stocker <thomas.stocker@gmail.com>
2022-11-20 15:16:59 +0100

    fixed more warnings

--- Mapsui.UI.WinUI/MapControl.cs
+++ Mapsui.UI.WinUI/MapControl.cs
@@ -279,12 +279,13 @@ namespace Mapsui.UI.Uwp
         private float GetPixelDensity()
         {
 #if __WINUI__
-            return (float)XamlRoot.RasterizationScale;
+            return (float)(XamlRoot?.RasterizationScale ?? 1f);
 #else
             return (float)DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
 #endif
         }
 
+#pragma warning disable IDISP023 // Don't use reference types in finalizer context
 #if __ANDROID__ 
         protected override void Dispose(bool disposing)
 #elif __IOS__ || __MACOS__
@@ -301,7 +302,6 @@ namespace Mapsui.UI.Uwp
 #endif
                 _map?.Dispose();
             }
-
             CommonDispose(disposing);
 
 #if __ANDROID__ || __IOS__ || __MACOS__
