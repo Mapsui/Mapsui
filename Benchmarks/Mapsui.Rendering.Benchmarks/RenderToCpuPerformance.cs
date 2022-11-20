43d70fef4b2517abd9b5cc06de28f7b84a8408ae
Thomas Stocker <thomas.stocker@gmail.com>
2022-11-20 23:36:52 +0100

    increase benchmark runtime

--- Benchmarks/Mapsui.Rendering.Benchmarks/RenderToCpuPerformance.cs
+++ Benchmarks/Mapsui.Rendering.Benchmarks/RenderToCpuPerformance.cs
@@ -24,7 +24,7 @@ using Mapsui.Utilities;
 
 namespace Mapsui.Rendering.Benchmarks
 {
-    [SimpleJob(RunStrategy.Throughput)]
+    [SimpleJob(RunStrategy.Throughput, targetCount: 1, warmupCount: 0, invocationCount: 333, launchCount: 1)]
     [MemoryDiagnoser]
     [MinColumn, MaxColumn, MeanColumn, MedianColumn]
     public class RenderToCpuPerformance
