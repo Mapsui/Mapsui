5fcb05cc58e33ccd139962c1b744abd3a6c1e266
pauldendulk <pauldendulk@gmail.com>
2022-12-14 18:36:21 +0100

    Add unit test for bug in intialization of StyleCollection

---
+++ Tests/Mapsui.Tests/Rendering/VisibleFeatureIteratorTests.cs
@@ -0,0 +1,31 @@
+﻿using Mapsui.Layers;
+using Mapsui.Rendering;
+using Mapsui.Styles;
+using Mapsui.Styles.Thematics;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Mapsui.Tests.Rendering
+{
+    [TestFixture]
+    internal class VisibleFeatureIteratorTests
+    {
+        [Test]
+        public void TestStyleIfStyleApplies()
+        {
+            // Arrange
+            var viewport = new Viewport {  Width= 100, Height = 100, Resolution = 1 };
+            using var map = new Map();
+            var result = new List<IStyle>();
+            var vectorStyle = new VectorStyle();
+            using var memoryLayer = new MemoryLayer { Style = new ThemeStyle(f => new StyleCollection { vectorStyle }) };
+            memoryLayer.Features = new List<IFeature> { new PointFeature(0, 0) };
+
+            // Act
+            VisibleFeatureIterator.IterateLayers(viewport,new[] { memoryLayer }, 0, (v, l, s, f, o, i) => { result.Add(s); });
+
+            // Assert
+            Assert.IsTrue(result.Contains(vectorStyle));
+        }
+    }
+}
