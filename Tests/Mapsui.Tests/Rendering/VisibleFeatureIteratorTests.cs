390f8fd44c0418d149d83381f57a4ed256d0f81e
pauldendulk <pauldendulk@gmail.com>
2022-12-21 16:58:32 +0100

    Some improvements on the tests themselves

--- Tests/Mapsui.Tests/Rendering/VisibleFeatureIteratorTests.cs
+++ Tests/Mapsui.Tests/Rendering/VisibleFeatureIteratorTests.cs
@@ -11,12 +11,13 @@ namespace Mapsui.Tests.Rendering
     internal class VisibleFeatureIteratorTests
     {
         [Test]
-        public void TestIfStyleInStyleCollectionApplies()
+        public void TestIfStylesInStyleCollectionAreApplied()
         {
             // Arrange
             var viewport = new Viewport { Width = 100, Height = 100, Resolution = 1 };
-            var vectorStyle = new VectorStyle();
-            using var memoryLayer = new MemoryLayer { Style = new ThemeStyle(f => new StyleCollection { Styles = { vectorStyle } }) };
+            var vectorStyle1 = new VectorStyle();
+            var vectorStyle2 = new VectorStyle();
+            using var memoryLayer = new MemoryLayer { Style = new ThemeStyle(f => new StyleCollection { Styles = { vectorStyle1, vectorStyle2 } }) };
             var feature = new PointFeature(0, 0);
             memoryLayer.Features = new List<IFeature> { feature };
             var result = new Dictionary<IFeature, List<IStyle>>();
@@ -31,7 +32,44 @@ namespace Mapsui.Tests.Rendering
             });
 
             // Assert
-            Assert.IsTrue(result[feature].Contains(vectorStyle));
+            Assert.IsTrue(result[feature].Contains(vectorStyle1));
+            Assert.IsTrue(result[feature].Contains(vectorStyle2));
+        }
+
+    
+        [Test, TestCaseSource(nameof(StylesThatShouldBeAppliedOrNot)), TestCaseSource(nameof(LayerStylesThatShouldBeAppliedOrNot))]
+        public void TestIfStylesAreAppliedOrNot(IStyle style, bool isAppliedExpected, string assertMessage)
+        {
+            // Arrange
+            var viewport = new Viewport { Width = 100, Height = 100, Resolution = 1 };
+            using var memoryLayer = new MemoryLayer { Style = style };
+            var feature = new PointFeature(0, 0);
+            memoryLayer.Features = new List<IFeature> { feature };
+            var isApplied = false;
+
+            // Act
+            VisibleFeatureIterator.IterateLayers(viewport, new[] { memoryLayer }, 0, (v, l, s, f, o, i) => isApplied = true);
+
+            // Assert
+            Assert.AreEqual(isAppliedExpected, isApplied, assertMessage);
+        }
+
+        [Test, TestCaseSource(nameof(StylesThatShouldBeAppliedOrNot)), TestCaseSource(nameof(FeatureStylesThatShouldBeAppliedOrNot))]
+        public void TestIfStylesOnFeaturesAreAppliedOrNot(IStyle style, bool isAppliedExpected, string assertMessage)
+        {
+            // Arrange
+            var viewport = new Viewport { Width = 100, Height = 100, Resolution = 1 };
+            using var memoryLayer = new MemoryLayer { Style = null };
+            var feature = new PointFeature(0, 0);
+            feature.Styles.Add(style);
+            memoryLayer.Features = new List<IFeature> { feature };
+            var isApplied = false;
+
+            // Act
+            VisibleFeatureIterator.IterateLayers(viewport, new[] { memoryLayer }, 0, (v, l, s, f, o, i) => isApplied = true);
+
+            // Assert
+            Assert.AreEqual(isAppliedExpected, isApplied, assertMessage);
         }
 
         public static IEnumerable<TestCaseData> StylesThatShouldBeAppliedOrNot
@@ -66,41 +104,6 @@ namespace Mapsui.Tests.Rendering
                 yield return new TestCaseData(new StyleCollection { Styles = { new VectorStyle() } }, false, "A StyleCollection on a feature should not be applied");
                 yield return new TestCaseData(new ThemeStyle(f => new VectorStyle()), false, "A ThemeStyle on a feature should not be applied");
             }
-        }
-
-        [Test, TestCaseSource(nameof(StylesThatShouldBeAppliedOrNot)), TestCaseSource(nameof(LayerStylesThatShouldBeAppliedOrNot))]
-        public void TestIfStylesAreAppliedOrNot(IStyle style, bool isAppliedExpected, string assertMessage)
-        {
-            // Arrange
-            var viewport = new Viewport { Width = 100, Height = 100, Resolution = 1 };
-            using var memoryLayer = new MemoryLayer { Style = style };
-            var feature = new PointFeature(0, 0);
-            memoryLayer.Features = new List<IFeature> { feature };
-            var isApplied = false;
-
-            // Act
-            VisibleFeatureIterator.IterateLayers(viewport, new[] { memoryLayer }, 0, (v, l, s, f, o, i) => isApplied = true);
-
-            // Assert
-            Assert.AreEqual(isAppliedExpected, isApplied, assertMessage);
-        }
-
-        [Test, TestCaseSource(nameof(StylesThatShouldBeAppliedOrNot)), TestCaseSource(nameof(FeatureStylesThatShouldBeAppliedOrNot))]
-        public void TestIfStylesOnFeaturesAreAppliedOrNot(IStyle style, bool isAppliedExpected, string assertMessage)
-        {
-            // Arrange
-            var viewport = new Viewport { Width = 100, Height = 100, Resolution = 1 };
-            using var memoryLayer = new MemoryLayer { Style = null };
-            var feature = new PointFeature(0, 0);
-            feature.Styles.Add(style);
-            memoryLayer.Features = new List<IFeature> { feature };
-            var isApplied = false;
-
-            // Act
-            VisibleFeatureIterator.IterateLayers(viewport, new[] { memoryLayer }, 0, (v, l, s, f, o, i) => isApplied = true);
-
-            // Assert
-            Assert.AreEqual(isAppliedExpected, isApplied, assertMessage);
         }
     }
 }
