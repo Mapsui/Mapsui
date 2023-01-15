460b1bc982e5d296175926a891859eb8089b76b9
Thomas Stocker <thomas.stocker@gmail.com>
2023-01-15 09:17:55 +0100

    centralize handling of caching

--- Mapsui/Extensions/CacheExtensions.cs
+++ Mapsui/Extensions/CacheExtensions.cs
@@ -1,25 +1,59 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Mapsui.Cache;
+using Mapsui.Logging;
 
 namespace Mapsui.Extensions
 {
     public static class CacheExtensions
     {
-        public static async Task<Stream> UrlCachedStreamAsync(this IUrlPersistentCache? persistentCache, string url)
+        public static async Task<Stream> UrlCachedStreamAsync(this IUrlPersistentCache? persistentCache, string url, Func<string, Task<Stream>>? loadUrl = null)
         {
+            var bytes = await UrlCachedArrayAsync(persistentCache, url, loadUrl);
+
+            return new MemoryStream(bytes);
+        }
+
+        public static async Task<byte[]> UrlCachedArrayAsync(this IUrlPersistentCache? persistentCache, string url, Func<string, Task<Stream>>? loadUrl = null)
+        {
+            
             var bytes = persistentCache?.Find(url);
             if (bytes == null)
             {
-                var handler = new HttpClientHandler();
-                using var httpClient = new HttpClient(handler);
-                using var response = await httpClient.GetStreamAsync(url);
-                bytes = response.ToBytes();
+                Logger.Log(LogLevel.Debug, $@"Load Url {url}");
+                Stream response = null;
+                try
+                {
+                    if (loadUrl != null)
+                    {
+                        response = await loadUrl(url);
+
+                    }
+                    else
+                    {
+                        var handler = new HttpClientHandler();
+                        using var httpClient = new HttpClient(handler);
+                        response = await httpClient.GetStreamAsync(url);
+                    }
+
+                    bytes = response.ToBytes();
+                }
+                finally
+                {
+                    response?.Dispose();
+                }
+                
+                Logger.Log(LogLevel.Debug, $@"Caching Url {url}");
                 persistentCache?.Add(url, bytes);
             }
+            else
+            {
+                Logger.Log(LogLevel.Debug, $@"Cached Load Url {url}");
+            }
 
-            return new MemoryStream(bytes);
+            return bytes;
         }
     }
 }
