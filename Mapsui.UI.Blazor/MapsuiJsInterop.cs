a4031e42009dd09028464fb72adecce1412cd36d
Thomas Stocker <thomas.stocker@gmail.com>
2023-01-17 23:19:24 +0100

    Revert "fixing package build"
    This reverts commit d22748a06853a0a8f0e547101bf5fc0c6784ab82.

--- Mapsui.UI.Blazor/MapsuiJsInterop.cs
+++ Mapsui.UI.Blazor/MapsuiJsInterop.cs
@@ -1,7 +1,7 @@
-using DotNext.Threading;
+using Mapsui.UI.Blazor.Extensions;
 using Microsoft.JSInterop;
 
-namespace Mapsui.UI.Blazor.Extensions;
+namespace Mapsui.UI.Blazor;
 
 // This class provides an example of how JavaScript functionality can be wrapped
 // in a .NET class for easy consumption. The associated JavaScript module is
@@ -12,25 +12,25 @@ namespace Mapsui.UI.Blazor.Extensions;
 
 public class MapsuiJsInterop : IAsyncDisposable
 {
-    private readonly AsyncLazy<IJSObjectReference>_moduleTask;
+    private readonly Lazy<Task<IJSObjectReference>> _moduleTask;
 
     public MapsuiJsInterop(IJSRuntime jsRuntime)
     {
-        _moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+        _moduleTask = new (() => jsRuntime.InvokeAsync<IJSObjectReference>(
             "import", "./_content/Mapsui.Blazor/mapsuiJsInterop.js").AsTask());
     }
 
-    public async Task<BoundingClientRect> BoundingClientRectAsync(string elementId)
+    public async Task<BoundingClientRect> BoundingClientRect(string elementId)
     {
-        var module = await _moduleTask;
+        var module = await _moduleTask.Value;
         return await module.InvokeAsync<BoundingClientRect>("getBoundingClientRect", elementId);
-    }
+    }  
 
     public async ValueTask DisposeAsync()
     {
         if (_moduleTask.IsValueCreated)
         {
-            var module = await _moduleTask;
+            var module = await _moduleTask.Value;
             await module.DisposeAsync();
         }
     }
