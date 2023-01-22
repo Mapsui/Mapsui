211b74d09bd3318c3e2cf83628a889f3259fc82f
pauldendulk <pauldendulk@gmail.com>
2023-01-22 10:10:16 +0100

    Mapsui.Samples.Avalonia to file scoped namespace

--- Samples/Mapsui.Samples.Avalonia/Program.cs
+++ Samples/Mapsui.Samples.Avalonia/Program.cs
@@ -1,31 +1,30 @@
 ﻿using Avalonia;
 using Avalonia.ReactiveUI;
 
-namespace Mapsui.Samples.Avalonia
-{
-    internal class Program
-    {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        public static void Main(string[] args)
-        {
-            BuildAvaloniaApp()
-.StartWithClassicDesktopLifetime(args);
-        }
+namespace Mapsui.Samples.Avalonia;
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-        {
-            return AppBuilder.Configure<App>()
-                           .UsePlatformDetect()
-                           .With(new Win32PlatformOptions
-                           {
-                               EnableMultitouch = true,
-                               AllowEglInitialization = true
-                           })
-                           .LogToTrace()
-                           .UseReactiveUI();
-        }
+internal class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp()
+.StartWithClassicDesktopLifetime(args);
+    }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder.Configure<App>()
+                       .UsePlatformDetect()
+                       .With(new Win32PlatformOptions
+                       {
+                           EnableMultitouch = true,
+                           AllowEglInitialization = true
+                       })
+                       .LogToTrace()
+                       .UseReactiveUI();
     }
 }
