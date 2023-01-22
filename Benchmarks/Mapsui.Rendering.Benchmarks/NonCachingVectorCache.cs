e987cdb53fdbe6f832b3d8be211911deb84efbd1
pauldendulk <pauldendulk@gmail.com>
2023-01-22 10:16:18 +0100

    Mapsui.Rendering.Benchmarks to file scoped namespace

--- Benchmarks/Mapsui.Rendering.Benchmarks/NonCachingVectorCache.cs
+++ Benchmarks/Mapsui.Rendering.Benchmarks/NonCachingVectorCache.cs
@@ -1,29 +1,28 @@
 ﻿using Mapsui.Styles;
 
-namespace Mapsui.Rendering.Benchmarks
+namespace Mapsui.Rendering.Benchmarks;
+
+public class NonCachingVectorCache : IVectorCache
 {
-    public class NonCachingVectorCache : IVectorCache
+    private readonly ISymbolCache _symbolCache;
+
+    public NonCachingVectorCache(ISymbolCache symbolCache)
     {
-        private readonly ISymbolCache _symbolCache;
+        _symbolCache = symbolCache;
+    }
 
-        public NonCachingVectorCache(ISymbolCache symbolCache)
-        {
-            _symbolCache = symbolCache;
-        }
+    public T GetOrCreatePaint<T>(Pen? pen, float opacity, Func<Pen?, float, T> toPaint) where T : class
+    {
+        return toPaint(pen, opacity);
+    }
 
-        public T GetOrCreatePaint<T>(Pen? pen, float opacity, Func<Pen?, float, T> toPaint) where T : class
-        {
-            return toPaint(pen, opacity);
-        }
+    public T GetOrCreatePaint<T>(Brush? brush, float opacity, double rotation, Func<Brush?, float, double, ISymbolCache, T> toPaint) where T : class
+    {
+        return toPaint(brush, opacity, rotation, _symbolCache);
+    }
 
-        public T GetOrCreatePaint<T>(Brush? brush, float opacity, double rotation, Func<Brush?, float, double, ISymbolCache, T> toPaint) where T : class
-        {
-            return toPaint(brush, opacity, rotation, _symbolCache);
-        }
-
-        public TPath GetOrCreatePath<TPath, TGeometry>(IReadOnlyViewport viewport, TGeometry geometry, float lineWidth, Func<TGeometry, IReadOnlyViewport, float, TPath> toPath) where TPath : class where TGeometry : class
-        {
-            return toPath(geometry, viewport, lineWidth);
-        }
+    public TPath GetOrCreatePath<TPath, TGeometry>(IReadOnlyViewport viewport, TGeometry geometry, float lineWidth, Func<TGeometry, IReadOnlyViewport, float, TPath> toPath) where TPath : class where TGeometry : class
+    {
+        return toPath(geometry, viewport, lineWidth);
     }
 }
