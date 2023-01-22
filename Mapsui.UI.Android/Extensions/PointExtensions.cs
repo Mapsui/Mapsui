d1c39ea439aba23e9d60fde93f7063aad3e2594d
pauldendulk <pauldendulk@gmail.com>
2023-01-22 11:03:22 +0100

    Mapsui.Android to file scoped namespace

--- Mapsui.UI.Android/Extensions/PointExtensions.cs
+++ Mapsui.UI.Android/Extensions/PointExtensions.cs
@@ -1,17 +1,16 @@
-﻿namespace Mapsui.UI.Android.Extensions
-{
-    public static class PointExtensions
-    {
-        public static MPoint ToDeviceIndependentUnits(this MPoint point, float pixelDensity)
-        {
-            return new MPoint(
-                ToDeviceIndependentUnits(point.X, pixelDensity),
-                ToDeviceIndependentUnits(point.Y, pixelDensity));
-        }
+﻿namespace Mapsui.UI.Android.Extensions;
 
-        private static double ToDeviceIndependentUnits(double devicePixels, float pixelDensity)
-        {
-            return devicePixels / pixelDensity;
-        }
+public static class PointExtensions
+{
+    public static MPoint ToDeviceIndependentUnits(this MPoint point, float pixelDensity)
+    {
+        return new MPoint(
+            ToDeviceIndependentUnits(point.X, pixelDensity),
+            ToDeviceIndependentUnits(point.Y, pixelDensity));
+    }
+
+    private static double ToDeviceIndependentUnits(double devicePixels, float pixelDensity)
+    {
+        return devicePixels / pixelDensity;
     }
 }
