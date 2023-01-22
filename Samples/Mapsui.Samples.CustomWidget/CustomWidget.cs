02f83f296aa5fbee8ab56f81b9915158c40e3cfc
pauldendulk <pauldendulk@gmail.com>
2023-01-22 10:10:42 +0100

    Mapsui.Samples.CustomWidget to file scoped namespace

--- Samples/Mapsui.Samples.CustomWidget/CustomWidget.cs
+++ Samples/Mapsui.Samples.CustomWidget/CustomWidget.cs
@@ -1,24 +1,23 @@
 ﻿using Mapsui.Styles;
 using Mapsui.Widgets;
 
-namespace Mapsui.Samples.CustomWidget
-{
-    public class CustomWidget : IWidget
-    {
-        public HorizontalAlignment HorizontalAlignment { get; set; }
-        public VerticalAlignment VerticalAlignment { get; set; }
-        public float MarginX { get; set; } = 20;
-        public float MarginY { get; set; } = 20;
-        public MRect? Envelope { get; set; }
-        public bool HandleWidgetTouched(INavigator navigator, MPoint position)
-        {
-            navigator.CenterOn(0, 0);
-            return true;
-        }
+namespace Mapsui.Samples.CustomWidget;
 
-        public Color? Color { get; set; }
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public bool Enabled { get; set; } = true;
+public class CustomWidget : IWidget
+{
+    public HorizontalAlignment HorizontalAlignment { get; set; }
+    public VerticalAlignment VerticalAlignment { get; set; }
+    public float MarginX { get; set; } = 20;
+    public float MarginY { get; set; } = 20;
+    public MRect? Envelope { get; set; }
+    public bool HandleWidgetTouched(INavigator navigator, MPoint position)
+    {
+        navigator.CenterOn(0, 0);
+        return true;
     }
+
+    public Color? Color { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public bool Enabled { get; set; } = true;
 }
--- Samples/Mapsui.Samples.CustomWidget/CustomWidgetSkiaRenderer.cs
+++ Samples/Mapsui.Samples.CustomWidget/CustomWidgetSkiaRenderer.cs
@@ -3,29 +3,28 @@ using Mapsui.Rendering.Skia.SkiaWidgets;
 using Mapsui.Widgets;
 using SkiaSharp;
 
-namespace Mapsui.Samples.CustomWidget
+namespace Mapsui.Samples.CustomWidget;
+
+public class CustomWidgetSkiaRenderer : ISkiaWidgetRenderer
 {
-    public class CustomWidgetSkiaRenderer : ISkiaWidgetRenderer
+    public void Draw(SKCanvas canvas, IReadOnlyViewport viewport, IWidget widget, float layerOpacity)
     {
-        public void Draw(SKCanvas canvas, IReadOnlyViewport viewport, IWidget widget, float layerOpacity)
-        {
-            // Cast to custom widget to be able to access the specific CustomWidget fields
-            var customWidget = (CustomWidget)widget;
+        // Cast to custom widget to be able to access the specific CustomWidget fields
+        var customWidget = (CustomWidget)widget;
 
-            // Update the envelope so the MapControl can do hit detection
-            widget.Envelope = ToEnvelope(customWidget);
+        // Update the envelope so the MapControl can do hit detection
+        widget.Envelope = ToEnvelope(customWidget);
 
-            // Use the envelope to draw
-            using var skPaint = new SKPaint { Color = customWidget.Color.ToSkia(0.5f) };
-            canvas.DrawRect(widget.Envelope.ToSkia(), skPaint);
-        }
+        // Use the envelope to draw
+        using var skPaint = new SKPaint { Color = customWidget.Color.ToSkia(0.5f) };
+        canvas.DrawRect(widget.Envelope.ToSkia(), skPaint);
+    }
 
-        private static MRect ToEnvelope(CustomWidget customWidget)
-        {
-            // A better implementation would take into account widget alignment
-            return new MRect(customWidget.MarginX, customWidget.MarginY,
-                customWidget.MarginX + customWidget.Width,
-                customWidget.MarginY + customWidget.Height);
-        }
+    private static MRect ToEnvelope(CustomWidget customWidget)
+    {
+        // A better implementation would take into account widget alignment
+        return new MRect(customWidget.MarginX, customWidget.MarginY,
+            customWidget.MarginX + customWidget.Width,
+            customWidget.MarginY + customWidget.Height);
     }
 }
