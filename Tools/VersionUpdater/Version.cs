aca3fecd595d97842c11c79c8b964fb192204545
pauldendulk <pauldendulk@gmail.com>
2023-01-22 11:07:30 +0100

    VersionUpdater to file scoped namespace

--- Tools/VersionUpdater/Version.cs
+++ Tools/VersionUpdater/Version.cs
@@ -1,39 +1,38 @@
 ﻿using System;
 
-namespace VersionUpdater
+namespace VersionUpdater;
+
+public class Version
 {
-    public class Version
+    public int Major { get; set; }
+    public int Minor { get; set; }
+    public int Patch { get; set; }
+    public int Build { get; set; }
+    public string? PreRelease { get; set; }
+    public string? FullVersion { get; set; }
+
+    public static Version Parse(string version)
     {
-        public int Major { get; set; }
-        public int Minor { get; set; }
-        public int Patch { get; set; }
-        public int Build { get; set; }
-        public string? PreRelease { get; set; }
-        public string? FullVersion { get; set; }
+        var (firstPart, secondPart) = SplitOnDash(version);
 
-        public static Version Parse(string version)
+        var elements = firstPart.Split('.');
+
+        if (elements.Length > 4) throw new Exception("Version can only have 4 or less elements");
+
+        return new Version
         {
-            var (firstPart, secondPart) = SplitOnDash(version);
+            Major = int.Parse(elements[0]),
+            Minor = elements.Length > 1 ? int.Parse(elements[1]) : 0,
+            Patch = elements.Length > 2 ? int.Parse(elements[2]) : 0,
+            Build = elements.Length > 3 ? int.Parse(elements[3]) : 0,
+            PreRelease = secondPart,
+            FullVersion = version
+        };
+    }
 
-            var elements = firstPart.Split('.');
-
-            if (elements.Length > 4) throw new Exception("Version can only have 4 or less elements");
-
-            return new Version
-            {
-                Major = int.Parse(elements[0]),
-                Minor = elements.Length > 1 ? int.Parse(elements[1]) : 0,
-                Patch = elements.Length > 2 ? int.Parse(elements[2]) : 0,
-                Build = elements.Length > 3 ? int.Parse(elements[3]) : 0,
-                PreRelease = secondPart,
-                FullVersion = version
-            };
-        }
-
-        private static (string firstPart, string secondPart) SplitOnDash(string version)
-        {
-            var parts = version.Split('-');
-            return (parts[0], parts.Length > 1 ? "-" + parts[1] : "");
-        }
+    private static (string firstPart, string secondPart) SplitOnDash(string version)
+    {
+        var parts = version.Split('-');
+        return (parts[0], parts.Length > 1 ? "-" + parts[1] : "");
     }
 }
