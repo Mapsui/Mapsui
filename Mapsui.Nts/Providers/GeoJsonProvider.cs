3510fd12779a9a72167f38d496742161ecf71eb8
Thomas Stocker <thomas.stocker@gmail.com>
2023-01-29 01:46:37 +0100

    GeoJsonProvider

---
+++ Mapsui.Nts/Providers/GeoJsonProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Mapsui.Layers;
+using Mapsui.Providers;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO.Converters;
+
+namespace Mapsui.Nts.Providers;
+
+public class GeoJsonProvider : IProvider
+{
+    private string _geoJson;
+    
+    public GeoJsonProvider(string geojson)
+    {
+        _geoJson = geojson;
+    }
+
+    public string? CRS { get; set; }
+    public MRect? GetExtent()
+    {
+    }
+
+    public Task<IEnumerable<IFeature>> GetFeaturesAsync(FetchInfo fetchInfo)
+    {
+        var serializer = GeoJsonSerializer.Create();
+        using (var stringReader = new StringReader(_geoJson))
+        using (var jsonReader = new JsonTextReader(stringReader))
+        {
+            geometry = serializer.Deserialize<Geometry>(jsonReader);
+        }
+    }
+}
---
+++ Samples/Mapsui.Samples.Common/Maps/DataFormats/GeoJsonSample.cs
@@ -0,0 +1,48 @@
+﻿using Mapsui.Extensions.Provider;
+using Mapsui.Layers;
+using Mapsui.Providers;
+using Mapsui.Samples.Common.Utilities;
+using Mapsui.Styles;
+using Mapsui.UI;
+using System.Collections.Generic;
+using System.IO;
+using Mapsui.Nts.Providers;
+
+#pragma warning disable IDISP001 // Dispose created
+
+namespace Mapsui.Samples.Common.Maps.DataFormats;
+
+public class GeoJsonSample : IMapControlSample
+{
+    static GeoJsonSample()
+    {
+        GeoJsonDeployer.CopyEmbeddedResourceToFile("cities.geojson");
+    }
+
+    public string Name => "13 GeoJson";
+    public string Category => "Data Formats";
+
+    public void Setup(IMapControl mapControl)
+    {
+        mapControl.Map = CreateMap();
+    }
+
+    public static Map CreateMap()
+    {
+        var map = new Map();
+        var examplePath = Path.Combine(GeoJsonDeployer.GeoJsonLocation, "cities.geojson");
+        var geoJson = new GeoJsonProvider(examplePath);
+        map.Layers.Add(CreateGeoJsonLayer(geoJson));
+
+        return map;
+    }
+
+    private static ILayer CreateGeoJsonLayer(IProvider geoJsonSource)
+    {
+        return new Layer
+        {
+            Name = "GeoJson",
+            DataSource = geoJsonSource,
+        };
+    }
+}
