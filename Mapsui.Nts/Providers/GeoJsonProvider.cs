32e345516bd7ef3318093bbb4c2360e68368ad23
Thomas Stocker <thomas.stocker@gmail.com>
2023-01-29 10:15:59 +0100

    geojson compiles

--- Mapsui.Nts/Providers/GeoJsonProvider.cs
+++ Mapsui.Nts/Providers/GeoJsonProvider.cs
@@ -1,8 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Mapsui.Extensions;
 using Mapsui.Layers;
+using Mapsui.Nts.Extensions;
 using Mapsui.Providers;
+using NetTopologySuite.Features;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.IO.Converters;
 
@@ -11,24 +17,96 @@ namespace Mapsui.Nts.Providers;
 public class GeoJsonProvider : IProvider
 {
     private string _geoJson;
-    
+    private FeatureCollection _featureCollection;
+
     public GeoJsonProvider(string geojson)
     {
         _geoJson = geojson;
     }
-
-    public string? CRS { get; set; }
-    public MRect? GetExtent()
+    
+    private GeoJsonConverterFactory GeoJsonConverterFactory { get; } = new();
+    
+    private JsonSerializerOptions DefaultOptions
     {
+        get
+        {
+            var res = new JsonSerializerOptions
+                {ReadCommentHandling = JsonCommentHandling.Skip};
+            res.Converters.Add(GeoJsonConverterFactory);
+            return res;
+        }
     }
 
+    private FeatureCollection Deserialize(string geoJson, JsonSerializerOptions options)
+    {
+        using var ms = new MemoryStream(Encoding.UTF8.GetBytes(geoJson));
+        return Deserialize(ms, options);
+    }
+
+    private FeatureCollection Deserialize(Stream stream, JsonSerializerOptions options)
+    {
+        var b = new ReadOnlySpan<byte>(stream.ToBytes());
+        var r = new Utf8JsonReader(b);
+
+        // we are at None
+        r.Read();
+        var res = JsonSerializer.Deserialize<FeatureCollection>(ref r, options);
+
+        return res;
+    }
+
+    /// <summary> Is Geo Json Content </summary>
+    /// <returns>true if it contains geojson {} or []</returns>
+    private bool IsGeoJsonContent()
+    {
+        if (string.IsNullOrWhiteSpace(_geoJson)) 
+            return false;
+        
+        return (_geoJson.IndexOf("{") >= 0 && _geoJson.IndexOf("}") >= 0) || (_geoJson.IndexOf("[") >= 0 && _geoJson.IndexOf("]") >= 0);
+    }
+    
+
+    private FeatureCollection FeatureCollection
+    {
+        get
+        {
+            if (_featureCollection == null)
+            {
+                // maybe it has GeoJson Content.
+                _featureCollection = IsGeoJsonContent() ? Deserialize(_geoJson, DefaultOptions) : Deserialize(File.OpenRead(_geoJson), DefaultOptions);
+            }
+    
+            return _featureCollection;    
+        }
+    }
+    
+    /// <inheritdoc/>
+    public string? CRS { get; set; }
+    
+    /// <inheritdoc/>
+    public MRect? GetExtent()
+    {
+        return FeatureCollection.BoundingBox.ToMRect();
+    }
+
+    /// <inheritdoc/>
     public Task<IEnumerable<IFeature>> GetFeaturesAsync(FetchInfo fetchInfo)
     {
-        var serializer = GeoJsonSerializer.Create();
-        using (var stringReader = new StringReader(_geoJson))
-        using (var jsonReader = new JsonTextReader(stringReader))
+        var fetchExtent = fetchInfo.Extent.ToEnvelope();
+        var list = new List<IFeature>();
+        
+        foreach (NetTopologySuite.Features.IFeature? feature in FeatureCollection)
         {
-            geometry = serializer.Deserialize<Geometry>(jsonReader);
+            if (feature is Geometry geometry)
+            {
+                if (feature.BoundingBox.Intersects(fetchExtent))
+                {
+                    var geometryFeature = new GeometryFeature();
+                    geometryFeature.Geometry = geometry;
+                }
+            }
         }
+
+        return Task.FromResult((IEnumerable<IFeature>)list);
     }
 }
--- Samples/Mapsui.Samples.Common/Maps/DataFormats/GeoJsonSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/DataFormats/GeoJsonSample.cs
@@ -1,12 +1,12 @@
-﻿using Mapsui.Extensions.Provider;
-using Mapsui.Layers;
+﻿using Mapsui.Layers;
 using Mapsui.Providers;
 using Mapsui.Samples.Common.Utilities;
 using Mapsui.Styles;
 using Mapsui.UI;
-using System.Collections.Generic;
 using System.IO;
+using Mapsui.Extensions;
 using Mapsui.Nts.Providers;
+using Mapsui.Styles.Thematics;
 
 #pragma warning disable IDISP001 // Dispose created
 
@@ -32,17 +32,57 @@ public class GeoJsonSample : IMapControlSample
         var map = new Map();
         var examplePath = Path.Combine(GeoJsonDeployer.GeoJsonLocation, "cities.geojson");
         var geoJson = new GeoJsonProvider(examplePath);
-        map.Layers.Add(CreateGeoJsonLayer(geoJson));
+        map.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+        map.Layers.Add(CreateCityLayer(geoJson));
+        map.Layers.Add(CreateCityLabelLayer(geoJson));
 
         return map;
     }
 
-    private static ILayer CreateGeoJsonLayer(IProvider geoJsonSource)
+    private static ILayer CreateCityLayer(IProvider citySource)
     {
         return new Layer
         {
-            Name = "GeoJson",
-            DataSource = geoJsonSource,
+            Name = "Cities",
+            DataSource = citySource,
+            Style = CreateCityTheme()
+        };
+    }
+
+    private static ILayer CreateCityLabelLayer(IProvider citiesProvider)
+    {
+        return new Layer("City labels")
+        {
+            DataSource = citiesProvider,
+            Enabled = true,
+            Style = CreateCityLabelStyle()
+        };
+    }
+
+    private static IThemeStyle CreateCityTheme()
+    {
+        // Scaling city icons based on city population.
+        // Cities below 1.000.000 gets the smallest symbol.
+        // Cities with more than 5.000.000 the largest symbol.
+        var bitmapId = typeof(ShapefileSample).LoadBitmapId(@"Images.icon.png");
+        var cityMin = new SymbolStyle { BitmapId = bitmapId, SymbolScale = 0.5f };
+        var cityMax = new SymbolStyle { BitmapId = bitmapId, SymbolScale = 1f };
+        return new GradientTheme("Population", 1000000, 5000000, cityMin, cityMax);
+    }
+
+    private static LabelStyle CreateCityLabelStyle()
+    {
+        return new LabelStyle
+        {
+            ForeColor = Color.Black,
+            BackColor = new Brush { Color = Color.Orange },
+            Font = new Font { FontFamily = "GenericSerif", Size = 11 },
+            HorizontalAlignment = LabelStyle.HorizontalAlignmentEnum.Center,
+            VerticalAlignment = LabelStyle.VerticalAlignmentEnum.Center,
+            Offset = new Offset { X = 0, Y = 0 },
+            Halo = new Pen { Color = Color.Yellow, Width = 2 },
+            CollisionDetection = true,
+            LabelColumn = "NAME"
         };
     }
 }
