d6b5bf482ec496cdd2ca7078ab1bd56303a688a0
pauldendulk <pauldendulk@gmail.com>
2023-02-04 15:05:08 +0100

    Remove packing from buildpack.sh
    Perhaps the whole file should be removed.

--- Scripts/Mac/buildpack.sh
+++ Scripts/Mac/buildpack.sh
@@ -9,8 +9,3 @@ msbuild Tools/VersionUpdater/VersionUpdater.csproj /p:Configuration=Release
 dotnet Tools/VersionUpdater/bin/Release/netcoreapp3.1/VersionUpdater.dll -v $VERSION
 # build
 msbuild Mapsui.Mac.sln /p:Configuration=Release
-# package
-nuget pack NuSpec/Mapsui.nuspec -Version $VERSION -outputdirectory Artifacts
-nuget pack NuSpec/Mapsui.Forms.nuspec -Version $VERSION -outputdirectory Artifacts
-nuget pack NuSpec/Mapsui.Android.nuspec -Version $VERSION -outputdirectory Artifacts
-nuget pack NuSpec/Mapsui.iOS.nuspec -Version $VERSION -outputdirectory Artifacts
