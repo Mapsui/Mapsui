ca27e90890eef245a31a848a43bee37d349858fc
pauldendulk <pauldendulk@gmail.com>
2023-02-11 11:26:45 +0100

    Remove all version info from AssemblyInfo.cs files

--- Samples/Mapsui.Samples.Forms/Mapsui.Samples.Forms.Android/Properties/AssemblyInfo.cs
+++ Samples/Mapsui.Samples.Forms/Mapsui.Samples.Forms.Android/Properties/AssemblyInfo.cs
@@ -15,19 +15,6 @@ using Android.App;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.0.0")]
-
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
@@ -35,4 +22,3 @@ using Android.App;
 [assembly: UsesFeature("android.hardware.location", Required = false)]
 [assembly: UsesFeature("android.hardware.location.gps", Required = false)]
 [assembly: UsesFeature("android.hardware.location.network", Required = false)]
-
