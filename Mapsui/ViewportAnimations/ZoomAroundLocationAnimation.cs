a85e3b6e5f20cba07f3feadd8d60da178d2a3c3c
pauldendulk <pauldendulk@gmail.com>
2023-02-15 14:59:59 +0100

    Don't inherit but call static create method in ZoomAroundLocationAnimation

--- Mapsui/ViewportAnimations/ZoomAroundLocationAnimation.cs
+++ Mapsui/ViewportAnimations/ZoomAroundLocationAnimation.cs
@@ -3,15 +3,14 @@ using Mapsui.Utilities;
 
 namespace Mapsui.ViewportAnimations;
 
-internal class ZoomAroundLocationAnimation : ZoomOnCenterAnimation
+internal class ZoomAroundLocationAnimation
 {
     public static List<AnimationEntry<Viewport>> Create(IViewport viewport, double centerOfZoomX, double centerOfZoomY, double newResolution,
         double currentCenterOfMapX, double currentCenterOfMapY, double currentResolution, long duration)
     {
-        // todo: Remove the inherited overload somehow.
         var (worldCenterOfMapX, worldCenterOfMapY) = TransformationAlgorithms.CalculateCenterOfMap(
             centerOfZoomX, centerOfZoomY, newResolution, currentCenterOfMapX, currentCenterOfMapY, currentResolution);
 
-        return Create(viewport, worldCenterOfMapX, worldCenterOfMapY, newResolution, duration);
+        return ZoomOnCenterAnimation.Create(viewport, worldCenterOfMapX, worldCenterOfMapY, newResolution, duration);
     }
 }
