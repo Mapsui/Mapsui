85fa71de7ffde5e53f23e4c48b7efbb38e1530ec
pauldendulk <pauldendulk@gmail.com>
2023-02-23 23:47:25 +0100

    Add Multiply for MSection

---
+++ Mapsui/Extensions/SectionExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Mapsui.Extensions;
+public static class SectionExtensions
+{
+    public static MSection Multiply(this MSection section, double extentMultiplier = 1, double resolutionMultiplier = 1)
+    {
+        return new MSection(section.Extent.Multiply(extentMultiplier), section.Resolution / resolutionMultiplier);
+    }
+}
---
+++ Tests/Mapsui.Tests/MSectionTests.cs
@@ -0,0 +1,30 @@
+﻿using Mapsui.Extensions;
+using NUnit.Framework;
+
+namespace Mapsui.Tests;
+
+[TestFixture]
+internal class MSectionTests
+{
+    [TestCase(1d, 1d, 1d, 100d)]
+    [TestCase(2d, 1d, 1d, 200d)]
+    [TestCase(4d, 1d, 1d, 400d)]
+    [TestCase(1d, 2d, 0.5d, 100d)]
+    [TestCase(2d, 2d, 0.5d, 200d)]
+    [TestCase(4d, 2d, 0.5d, 400d)]
+    [TestCase(1d, 4d, 0.25d, 100d)]
+    [TestCase(2d, 4d, 0.25d, 200d)]
+    [TestCase(4d, 4d, 0.25d, 400d)]
+    public void OverscanTest(double extentMultiplier, double resolutionMultiplier, double expectedResolution, double expectedWidth)
+    {
+        // Arrange
+        var section = new MSection(new MRect(-50, -50, 50, 50), 1);
+
+        // Act
+        var grownSection = section.Multiply(extentMultiplier, resolutionMultiplier);
+
+        // Assert
+        Assert.AreEqual(expectedResolution, grownSection.Resolution);
+        Assert.AreEqual(expectedWidth, grownSection.Extent.Width);
+    }
+}
