c34875bae9a245879678c241a7a9d94edc521834
pauldendulk <pauldendulk@gmail.com>
2023-03-03 09:09:41 +0100

    Remove unused MSection Multiply extension and tests

--- Mapsui/Extensions/SectionExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mapsui.Extensions;
-public static class SectionExtensions
-{
-    public static MSection Multiply(this MSection section, double extentMultiplier = 1, double resolutionMultiplier = 1)
-    {
-        return new MSection(section.Extent.Multiply(extentMultiplier), section.Resolution / resolutionMultiplier);
-    }
-}
