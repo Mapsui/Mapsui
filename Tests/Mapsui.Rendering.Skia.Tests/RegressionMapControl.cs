5feefab010d2e687adb09da3ee6cdd00d647b9cf
pauldendulk <pauldendulk@gmail.com>
2023-03-15 18:01:02 +0100

    Add usings back

--- Tests/Mapsui.Rendering.Skia.Tests/RegressionMapControl.cs
+++ Tests/Mapsui.Rendering.Skia.Tests/RegressionMapControl.cs
@@ -2,6 +2,8 @@
 // The Mapsui authors licensed this file under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
+using System;
+using System.Collections.Generic;
 using Mapsui.Extensions;
 using Mapsui.Layers;
 using Mapsui.UI;
