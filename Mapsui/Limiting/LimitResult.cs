5a1f968af519d7ea9920539146a36cdbca5e8693
pauldendulk <pauldendulk@gmail.com>
2023-03-29 07:07:30 +0200

    Simplify LimitResult

--- Mapsui/Limiting/LimitResult.cs
+++ Mapsui/Limiting/LimitResult.cs
@@ -4,14 +4,13 @@ public class LimitResult
     public LimitResult(Viewport input, Viewport output)
     {
         ZoomLimited = input.Resolution != output.Resolution;
-        PanXLimited = input.CenterX != output.CenterX;
-        PanYLimited = input.CenterY != output.CenterY;
-        FullyLimited = ZoomLimited && PanXLimited && PanYLimited;
+        FullyLimited = 
+            input.CenterX != output.CenterX &&
+            input.CenterY != output.CenterY &&
+            ZoomLimited;
     }
 
     public bool ZoomLimited { get; }
-    public bool PanXLimited { get; }
-    public bool PanYLimited { get; }
     public bool FullyLimited { get; }
 
 }
