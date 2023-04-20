b80f3bd376bd65c6c431368fed2abb5f301522da
Thomas Stocker <thomas.stocker@gmail.com>
2023-04-20 21:58:35 +0200

    fixing build

--- Samples/Mapsui.Samples.MapView/Utilities.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mapsui.Samples.MapView;
-
-public static class Utilities
-{
-    // When there is no explicit call to the assembly it is not loaded
-    // Even if there is a project reference. Calling this method is the workaround.
-    public static void LoadAssembly() { }
-}
