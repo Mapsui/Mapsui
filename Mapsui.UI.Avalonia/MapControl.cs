Mapsui/Mapsui

commit ce0f5edee10fffcf424d776318853d9e097c863f

Do some minor cleanup around mouse wheel

Mapsui.UI.Avalonia/MapControl.cs
@@ -14,7 +14,6 @@ using Avalonia.Threading;
 using Mapsui.Extensions;
 using Mapsui.Layers;
 using Mapsui.UI.Avalonia.Extensions;
-using Mapsui.Utilities;
 
 namespace Mapsui.UI.Avalonia;
 
@@ -26,8 +25,6 @@ public partial class MapControl : Grid, IMapControl, IDisposable
     private MPoint? _downMousePosition;
     private bool _mouseDown;
     private MPoint? _previousMousePosition;
-    private double _toResolution = double.NaN;
-    private double _mouseWheelPos = 0.0;
 
     public event EventHandler<FeatureInfoEventArgs>? FeatureInfo;
 
@@ -79,29 +76,8 @@ public partial class MapControl : Grid, IMapControl, IDisposable
         if (!Map.Viewport.State.HasSize()) return;
 
         _currentMousePosition = e.GetPosition(this).ToMapsui();
-        //Needed for both MouseMove and MouseWheel event for mousewheel event
 
-        if (double.IsNaN(_toResolution))
-            _toResolution = Map.Viewport.State.Resolution;
-
-        _mouseWheelPos += e.Delta.Y;
-        int delta = (int)_mouseWheelPos;
-        bool navigate = false;
-        if (_mouseWheelPos >= 1.0)
-        {
-            _toResolution = ZoomHelper.ZoomIn(_map.Resolutions, _toResolution);
-            _mouseWheelPos -= 1.0;
-            navigate = true;
-        }
-        else if (_mouseWheelPos <= -1.0)
-        {
-            _toResolution = ZoomHelper.ZoomOut(_map.Resolutions, _toResolution);
-            _mouseWheelPos += 1.0;
-            navigate = true;
-        }
-        if (!navigate) return;
-
-        ZoomInOrOut(delta, _currentMousePosition);
+        ZoomInOrOut((int)e.Delta.Y, _currentMousePosition);
     }
 
     private void MapControlMouseLeftButtonDown(PointerPressedEventArgs e)

Mapsui.UI.Blazor/MapControl.cs
@@ -141,11 +141,11 @@ public partial class MapControl : ComponentBase, IMapControl
             if (Map.Viewport.Limiter.ZoomLock) return;
             if (!Map.Viewport.State.HasSize()) return;
 
-            var delta = e.DeltaY * -1; // so that it zooms like on windows
+            var mouseWheelDelta = e.DeltaY * -1; // so that it zooms like on windows
 
             var currentMousePosition = e.Location(await BoundingClientRectAsync()).ToMapsui();
 
-            ZoomInOrOut((int)delta, currentMousePosition);
+            ZoomInOrOut((int)mouseWheelDelta, currentMousePosition);
         }
         catch (Exception ex)
         {

Mapsui.UI.Eto/MapControl.cs
@@ -77,9 +77,9 @@ public partial class MapControl : SkiaDrawable, IMapControl
         if (Map.Viewport.Limiter.ZoomLock) return;
         if (!Map.Viewport.State.HasSize()) return;
 
-        var delta = (int)e.Delta.Height;
+        var mouseWheelDelta = (int)e.Delta.Height;
         var currentMousePosition = e.Location.ToMapsui();
-        ZoomInOrOut(delta, currentMousePosition);
+        ZoomInOrOut(mouseWheelDelta, currentMousePosition);
     }
 
     protected override void OnSizeChanged(EventArgs e)

Mapsui.UI.Forms/MapControl.cs
@@ -463,21 +463,21 @@ public partial class MapControl : ContentView, IMapControl, IDisposable
     /// Called, when map should zoom in or out
     /// </summary>
     /// <param name="screenPosition">Center of zoom out event</param>
-    private bool OnZoomInOrOut(int delta, MPoint screenPosition)
+    private bool OnZoomInOrOut(int mouseWheelDelta, MPoint screenPosition)
     {
         if (Map.Viewport.Limiter.ZoomLock)
         {
             return true;
         }
 
-        var args = new ZoomedEventArgs(screenPosition, delta > 0 ? ZoomDirection.ZoomIn : ZoomDirection.ZoomOut);
+        var args = new ZoomedEventArgs(screenPosition, mouseWheelDelta > 0 ? ZoomDirection.ZoomIn : ZoomDirection.ZoomOut);
 
         Zoomed?.Invoke(this, args);
 
         if (args.Handled)
             return true;
 
-        ZoomInOrOut(delta, screenPosition);
+        ZoomInOrOut(mouseWheelDelta, screenPosition);
 
         return true;
     }
@@ -721,7 +721,7 @@ public partial class MapControl : ContentView, IMapControl, IDisposable
             return true;
 
         // Double tap as zoom
-        return OnZoomInOrOut(1, screenPosition); // delta > 0 to zoom in
+        return OnZoomInOrOut(1, screenPosition); // mouseWheelDelta > 0 to zoom in
     }
 
     /// <summary>

Mapsui.UI.Shared/MapControl.cs
@@ -616,14 +616,14 @@ public partial class MapControl : INotifyPropertyChanged, IDisposable
         _invalidateTimer = null;
     }
 
-    private void ZoomInOrOut(int delta, MPoint centerOfZoom)
+    private void ZoomInOrOut(int mouseWheelDelta, MPoint centerOfZoom)
     {
-        var resolution = Map.Navigator.MouseWheelAnimation.GetResolution(delta, Map.Viewport, Map.Resolutions);
-        if (delta > Constants.Epsilon)
+        var resolution = Map.Navigator.MouseWheelAnimation.GetResolution(mouseWheelDelta, Map.Viewport, Map.Resolutions);
+        if (mouseWheelDelta > Constants.Epsilon)
         {
             Map.Navigator.ZoomTo(resolution, centerOfZoom, Map.Navigator.MouseWheelAnimation.Duration, Map.Navigator.MouseWheelAnimation.Easing);
         }
-        else if (delta < Constants.Epsilon)
+        else if (mouseWheelDelta < Constants.Epsilon)
         {
             Map.Navigator.ZoomTo(resolution, centerOfZoom, Map.Navigator.MouseWheelAnimation.Duration, Map.Navigator.MouseWheelAnimation.Easing);
         }

Mapsui.UI.WinUI/MapControl.cs
@@ -163,9 +163,9 @@ public partial class MapControl : Grid, IMapControl, IDisposable
 #else
         var mousePosition = new MPoint(currentPoint.RawPosition.X, currentPoint.RawPosition.Y);
 #endif
-        var delta = currentPoint.Properties.MouseWheelDelta;
+        var mouseWheelDelta = currentPoint.Properties.MouseWheelDelta;
 
-        ZoomInOrOut(delta, mousePosition);
+        ZoomInOrOut(mouseWheelDelta, mousePosition);
 
         e.Handled = true;
     }

Mapsui/Animations/MouseWheelAnimation.cs
@@ -12,7 +12,7 @@ public class MouseWheelAnimation
     public int Duration { get; set; } = 600;
     public Easing Easing { get; set; } = Easing.QuarticOut;
 
-    public double GetResolution(int delta, Viewport viewport, IReadOnlyList<double> resolutions)
+    public double GetResolution(int mouseWheelDelta, Viewport viewport, IReadOnlyList<double> resolutions)
     {
         // If the animation has ended then start from the current resolution.
         // The alternative is to use the previous resolution target and add an extra
@@ -20,14 +20,14 @@ public class MouseWheelAnimation
         if (!IsAnimating())
             _toResolution = viewport.State.Resolution;
 
-        if (delta > Constants.Epsilon)
+        if (mouseWheelDelta > Constants.Epsilon)
         {
             _toResolution = ZoomHelper.ZoomIn(resolutions, _toResolution);
             // Todo: Move this to ZoomIn. Make limiting consistent.
             if (viewport.Limiter.ZoomLimits is not null)
                 _toResolution = Math.Max(_toResolution, viewport.Limiter.ZoomLimits.Min);
         }
-        else if (delta < Constants.Epsilon)
+        else if (mouseWheelDelta < Constants.Epsilon)
         {
             _toResolution = ZoomHelper.ZoomOut(resolutions, _toResolution);
             // Todo: Move this to ZoomOut. Make limiting consistent.

commit dd27fd6e462c4b5c1eb8da3f774da21c06b36eae

Avalonia.MapControl: fix zoom with touchpad on MacOS & Linux
(regression in commit ce0f5edee10fffcf424d776318853d9e097c863f)

Mapsui.UI.Avalonia/MapControl.cs
@@ -25,6 +25,7 @@ public partial class MapControl : Grid, IMapControl, IDisposable
     private MPoint? _downMousePosition;
     private bool _mouseDown;
     private MPoint? _previousMousePosition;
+    private double _mouseWheelPos = 0.0;
 
     public event EventHandler<FeatureInfoEventArgs>? FeatureInfo;
 
@@ -72,9 +73,15 @@ public partial class MapControl : Grid, IMapControl, IDisposable
 
     private void MapControlMouseWheel(object? sender, PointerWheelEventArgs e)
     {
-        var mouseWheelDelta = (int)e.Delta.Y;
+        _mouseWheelPos += e.Delta.Y;
+
+        if (Math.Abs(_mouseWheelPos) < 1.0) return;
+
+        int delta = (int)Math.Sign(_mouseWheelPos);
+        _mouseWheelPos -= delta;
+
         _currentMousePosition = e.GetPosition(this).ToMapsui();
-        Map.Navigator.MouseWheelZoom(mouseWheelDelta, _currentMousePosition);
+        Map.Navigator.MouseWheelZoom(delta, _currentMousePosition);
     }
 
     private void MapControlMouseLeftButtonDown(PointerPressedEventArgs e)