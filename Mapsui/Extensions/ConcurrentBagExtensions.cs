846fc0a6a0f0d1cf087ce693d34867916f75a88e
Thomas Stocker <thomas.stocker@gmail.com>
2023-05-28 22:54:13 +0200

    fixed compilation

---
+++ Mapsui/Extensions/ConcurrentBagExtensions.cs
@@ -0,0 +1,20 @@
+﻿#if NETSTANDARD2_0
+
+namespace System.Collections.Concurrent.Generic;
+
+public static class ConcurrentBagExtensions
+{
+    /// <summary>
+    /// Clear Implementation for .net Standard 2.0
+    /// </summary>
+    /// <typeparam name="T">T</typeparam>
+    /// <param name="bag">bag</param>
+    public static void Clear<T>(ConcurrentBag<T> bag)
+    {
+        while (!bag.IsEmpty)
+        {
+            bag.TryTake(out _);
+        }
+    }
+}
+#endif
