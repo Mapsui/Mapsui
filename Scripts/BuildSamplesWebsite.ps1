b59668f55ad8d9587939272cbc4550a5cf0fd628
Thomas Stocker <thomas.stocker@gmail.com>
2023-05-28 16:40:06 +0200

    improving build samples website

--- Scripts/BuildSamplesWebsite.ps1
+++ Scripts/BuildSamplesWebsite.ps1
@@ -2,5 +2,9 @@
 # build Samples website to release folder
 dotnet publish -c Release Samples\Mapsui.Samples.Blazor\Mapsui.Samples.Blazor.csproj -o release --nologo
 # change base url
-# sed -i 's/<base href="\/" \/>/<base href="\/BlazorGitHubPagesDemo\/" \/>/g' release/wwwroot/index.html
 (Get-Content -Path "release/wwwroot/index.html") -replace '<base href="/" \/>', '<base href="/samples/" />' | Set-Content -Path "release/wwwroot/index.html"
+# no jekill so that everything gets deployed
+New-Item -ItemType File -Path "release/wwwroot/.nojekyll"
+# fix 404 errors
+cp release/wwwroot/index.html release/wwwroot/404.html
+
