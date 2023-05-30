3e210521813cdb8da391775da9d5060efbce64db
Inforithmics <thomas.stocker@gmail.com>
2023-05-30 15:02:11 +0200

    second wms sample

---
+++ Samples/Mapsui.Samples.Common/Maps/DataFormats/WmsSecondSample.cs
@@ -0,0 +1,44 @@
+﻿using Mapsui.Cache;
+using Mapsui.Layers;
+using Mapsui.Providers.Wms;
+using System.Threading.Tasks;
+using Mapsui.Styles;
+
+namespace Mapsui.Samples.Common.Maps.DataFormats;
+
+public class WmsSecondSample : ISample
+{
+    public string Name => " 6 WMS 2";
+    public string Category => "Data Formats";
+    public static IUrlPersistentCache? DefaultCache { get; set; }
+
+    public async Task<Map> CreateMapAsync()
+    {
+        var map = new Map { CRS = "EPSG:28992" };
+        // The WMS request needs a CRS
+        map.Layers.Add(await CreateLayerAsync());
+        map.Home = (n) => n.CenterOnAndZoomTo(new MPoint(155000, 463000), 500);
+        return map;
+    }
+
+    public static async Task<ILayer> CreateLayerAsync()
+    {
+        return new ImageLayer("mainmap")
+        {
+            DataSource = await CreateWmsProviderAsync(),
+            Style = new RasterStyle()
+        };
+    }
+
+    private static async Task<WmsProvider> CreateWmsProviderAsync()
+    {
+        const string wmsUrl = "https://sgi2.isprambiente.it/arcgis/services/raster/igm25k_lazio_wgs/ImageServer/WMSServer?service=wms&request=getCapabilities&version=1.3.0";
+        var provider = await WmsProvider.CreateAsync(wmsUrl, persistentCache: null);
+        provider.ContinueOnError = true;
+        provider.TimeOut = 20000;
+        provider.CRS = "EPSG:3857";
+        provider.AddLayer("igm25k_lazio_wgs");
+        provider.SetImageFormat((provider.OutputFormats)[0]);
+        return provider;
+    }
+}
