113306d172ba9de345a0187fe804df7279bfeaaa
Thomas Stocker <thomas.stocker@gmail.com>
2023-06-04 00:21:58 +0200

    added documentation files that contain the samples

---
+++ Scripts/BuildSamplesDocumentation.ps1
@@ -0,0 +1,14 @@
+$rootFolder = ".\samples"
+$outputFolder = ".\docfx\codesamples"
+$rootPath = Convert-Path .\
+
+Get-ChildItem -Path $rootFolder -Recurse -Filter "*sample.cs" | ForEach-Object {
+    $filePath = $_.FullName
+    $relativePath = $filePath.Substring($rootPath.Length + 1)
+    $fileName = $_.Name -replace '\.cs$'  # Remove the ".cs" extension from the file name
+
+    $outputPath = Join-Path -Path $outputFolder -ChildPath "$fileName.md"
+    $outputContent = "[!code-csharp[Main](../../$relativePath `"$fileName`")]"
+
+    $outputContent | Out-File -FilePath $outputPath -Force
+}
