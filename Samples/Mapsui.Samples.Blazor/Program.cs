c00ffa30182f2f66bf4eca9ccf071ebd3e5ca146
pauldendulk <pauldendulk@gmail.com>
2023-06-23 10:54:19 +0200

    Upgrade to skiasharp preview 82

--- Samples/Mapsui.Samples.Blazor/Program.cs
+++ Samples/Mapsui.Samples.Blazor/Program.cs
@@ -8,4 +8,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+Mapsui.Logging.Logger.LogDelegate += (l, m, e) => Console.WriteLine(m + e?.Message);
+
 await builder.Build().RunAsync();
