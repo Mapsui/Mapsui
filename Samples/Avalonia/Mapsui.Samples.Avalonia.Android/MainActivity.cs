819daf4abf19851d6d39613089d7324c22a2504b
Thomas Stocker <thomas.stocker@gmail.com>
2023-07-08 09:43:15 +0200

    added Android,iOS,Desktop,Browser Avalonia

---
+++ Samples/Avalonia/Mapsui.Samples.Avalonia.Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia;
+using Avalonia.Android;
+using Avalonia.ReactiveUI;
+
+namespace Mapsui.Samples.Avalonia.Android;
+
+[Activity(
+    Label = "Mapsui.Samples.Avalonia.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>
+{
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .UseReactiveUI();
+    }
+}
