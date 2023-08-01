91b24b316f40eac431060c6f52a7c7346081c951
Inforithmics <thomas.stocker@gmail.com>
2023-08-01 11:18:11 +0200

    Unified ExtendedWidgetAndWidgetHandling

--- Mapsui/Widgets/IWidgetExtended.cs
+++ Mapsui/Widgets/IWidgetExtended.cs
@@ -1,5 +1,5 @@
 ﻿namespace Mapsui.Widgets;
-public interface IWidgetExtended : IWidget
+public interface IWidgetExtended : IWidget, IWidgetTouchable
 {
     public bool HandleWidgetMoving(Navigator navigator, MPoint position, WidgetArgs args);
     public bool HandleWidgetTouching(Navigator navigator, MPoint position, WidgetArgs args);
