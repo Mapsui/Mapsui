53b4e48680ba91318f60a8827986331a9a898e0e
Inforithmics <thomas.stocker@gmail.com>
2023-08-05 21:00:43 +0200

    remov unit test because the lru cache shouldn't dispose

--- Tests/Mapsui.Tests/Cache/LruCacheTests.cs
+++ Tests/Mapsui.Tests/Cache/LruCacheTests.cs
@@ -23,34 +23,4 @@ public class LruCacheUnitTest
         Assert.AreEqual("Three", cache.Get(3));
         Assert.AreEqual("Four", cache.Get(4));
     }
-
-    [Test]
-    [SuppressMessage("IDisposableAnalyzers.Correctness", "IDISP001:Dispose created")]
-    public void LruCacheDoesDispose()
-    {
-        var cache = new LruCache<int, TestDisposable>(2);
-        var item1 = new TestDisposable();
-        var item2 = new TestDisposable();
-        var item3 = new TestDisposable();
-
-        // add 3 items
-        cache.Put(1, item1);
-        cache.Put(2, item2);
-        cache.Put(3, item3);
-
-        // Item 1 should be disposed
-        Assert.IsTrue(item1.Disposed);
-    }
-}
-
-public sealed class TestDisposable : IDisposable
-{
-    private bool _disposed;
-
-    public bool Disposed => _disposed;
-
-    public void Dispose()
-    {
-        _disposed = true;
-    }
 }
