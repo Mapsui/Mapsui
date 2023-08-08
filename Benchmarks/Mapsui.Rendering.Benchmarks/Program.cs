69c0bea071d7c49d56e67cbfc88a9031c798ac40
Inforithmics <thomas.stocker@gmail.com>
2023-08-08 16:57:47 +0200

    revert the changes in benchmark

--- Benchmarks/Mapsui.Rendering.Benchmarks/Program.cs
+++ Benchmarks/Mapsui.Rendering.Benchmarks/Program.cs
@@ -5,11 +5,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-#if DEBUG
-        var directTest = new RenderToBitmapPerformance();
-        directTest.RenderDefaultAsync().Wait();
-#else
         var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
-#endif
     }
 }
