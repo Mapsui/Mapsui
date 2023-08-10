c2a1b9345c05e9ef4fb266f53e54c5f28d7ca0d4
Inforithmics <thomas.stocker@gmail.com>
2023-08-10 17:08:08 +0200

    fixing build

--- Samples/Mapsui.Samples.Uno/Mapsui.Samples.Uno.Skia.Wpf/MainWindow.xaml.cs
+++ Samples/Mapsui.Samples.Uno/Mapsui.Samples.Uno.Skia.Wpf/MainWindow.xaml.cs
@@ -24,6 +24,6 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
 
-        root.Content = new global::Uno.UI.Skia.Platform.WpfHost(Dispatcher, () => new App());
+        root.Content = new global::Uno.UI.Skia.Platform.WpfHost(Dispatcher, () => new Uno.App());
     }
 }
