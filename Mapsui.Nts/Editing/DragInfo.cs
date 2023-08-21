716143dc44ba005bd2553800c15276fa6fc40489
Inforithmics <thomas.stocker@gmail.com>
2023-08-21 17:36:20 +0200

    fixing nullability

--- Mapsui.Nts/Editing/DragInfo.cs
+++ Mapsui.Nts/Editing/DragInfo.cs
@@ -7,5 +7,5 @@ public class DragInfo
     public GeometryFeature? Feature { get; set; }
     public Coordinate? Vertex { get; set; }
     public MPoint? StartOffsetToVertex { get; set; }
-    public Coordinate VertexFeature { get; set; }
+    public Coordinate? VertexFeature { get; set; }
 }
