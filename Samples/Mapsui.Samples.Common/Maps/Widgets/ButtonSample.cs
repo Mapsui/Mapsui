531432c998c94a7fe218449b88090448f9bc2da0
pauldendulk <pauldendulk@gmail.com>
2023-10-09 13:58:26 +0200

    Update text again because it had unexpected effects

--- Samples/Mapsui.Samples.Common/Maps/Widgets/ButtonSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Widgets/ButtonSample.cs
@@ -61,7 +61,7 @@ public class ButtonSample : ISample
     {
         return new ButtonWidget()
         {
-            Text = "This text is apparently needed to update the locations",
+            Text = "hi", // This text is apparently needed to update to position of the button
             SvgImage = LoadSomeSvgAsString(),
             VerticalAlignment = verticalAlignment,
             HorizontalAlignment = horizontalAlignment,
