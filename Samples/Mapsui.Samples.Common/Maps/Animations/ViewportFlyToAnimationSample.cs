5b7ee8a08a6606a7ddf76f14beb1f7bd05e58993
pauldendulk <pauldendulk@gmail.com>
2023-10-13 19:22:55 +0200

    Add instructions to the fly to animation sample

--- Samples/Mapsui.Samples.Common/Maps/Animations/ViewportFlyToAnimationSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Animations/ViewportFlyToAnimationSample.cs
@@ -4,6 +4,7 @@ using Mapsui.Widgets;
 using Mapsui.Widgets.ScaleBar;
 using Mapsui.Widgets.Zoom;
 using System.Threading.Tasks;
+using Mapsui.Styles;
 
 namespace Mapsui.Samples.Common.Maps.Animations;
 
@@ -18,19 +19,12 @@ public class ViewportFlyToAnimationSample : ISample
 
     public static Map CreateMap()
     {
-        var map = new Map
-        {
-            CRS = "EPSG:3857"
-        };
+        var map = new Map { CRS = "EPSG:3857" };
         map.Layers.Add(OpenStreetMap.CreateTileLayer());
 
-        map.Widgets.Add(new ScaleBarWidget(map)
-        {
-            TextAlignment = Alignment.Center,
-            HorizontalAlignment = HorizontalAlignment.Center,
-            VerticalAlignment = VerticalAlignment.Top
-        });
+        map.Widgets.Add(CreateScaleBar(map));
         map.Widgets.Add(new ZoomInOutWidget { MarginX = 20, MarginY = 40 });
+        map.Widgets.Add(CreateTextBox("Tap on the map to fly to that location. The fly-to animation zooms out and then in."));
 
         map.Info += (s, a) =>
         {
@@ -42,5 +36,32 @@ public class ViewportFlyToAnimationSample : ISample
         };
 
         return map;
+    }
+
+    private static ScaleBarWidget CreateScaleBar(Map map)
+    {
+        return new ScaleBarWidget(map)
+        {
+            TextAlignment = Alignment.Center,
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Top
+        };
+    }
+
+    private static IWidget CreateTextBox(string text)
+    {
+        return new TextBox()
+        {
+            Text = text,
+            VerticalAlignment = VerticalAlignment.Top,
+            HorizontalAlignment = HorizontalAlignment.Left,
+            MarginX = 10,
+            MarginY = 10,
+            PaddingX = 8,
+            PaddingY = 8,
+            CornerRadius = 4,
+            BackColor = new Color(108, 117, 125, 128),
+            TextColor = Color.White,
+        };
     }
 }
