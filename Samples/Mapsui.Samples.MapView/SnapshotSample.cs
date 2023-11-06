44c4ef46ede712bc40865260b06168965814839f
pauldendulk <pauldendulk@gmail.com>
2023-11-06 12:50:49 +0100

    Remove #ifs in MapView

--- Samples/Mapsui.Samples.MapView/SnapshotSample.cs
+++ Samples/Mapsui.Samples.MapView/SnapshotSample.cs
@@ -1,22 +1,11 @@
 ﻿using System;
 using System.IO;
-using Mapsui.Samples.Common.Maps;
 using Mapsui.Samples.Common.Maps.Demo;
 using Mapsui.UI;
-#if __MAUI__
 using Mapsui.UI.Maui;
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Graphics;
-#else
-using Mapsui.UI.Forms;
-using Xamarin.Forms;
-#endif
 
-#if __MAUI__
 namespace Mapsui.Samples.Maui;
-#else
-namespace Mapsui.Samples.Forms;
-#endif
 
 public class SnapshotSample : IMapViewSample
 {
