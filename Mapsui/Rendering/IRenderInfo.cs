326e77babcf47e0619e827682aa82e0cd75656a7
Inforithmics <thomas.stocker@gmail.com>
2023-11-13 21:23:23 +0100

    Revert GetMapInfo Async

--- Mapsui/Rendering/IRenderInfo.cs
+++ Mapsui/Rendering/IRenderInfo.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Mapsui.Layers;
 
 namespace Mapsui.Rendering;
 
 public interface IRenderInfo
 {
-    [Obsolete("Use GetMapInfoAsync")]
-#if NETSTANDARD2_0
     MapInfo? GetMapInfo(double screenX, double screenY, Viewport viewport, IEnumerable<ILayer> layers, int margin = 0);
-#else
-    MapInfo? GetMapInfo(double screenX, double screenY, Viewport viewport, IEnumerable<ILayer> layers, int margin = 0)
-    {
-#pragma warning disable VSTHRD002 // synchronously waiting
-        return GetMapInfoAsync(screenX, screenY, viewport, layers, margin).Result;
-#pragma warning restore VSTHRD002 // synchronously waiting
-    }
-#endif
-
-
-    Task<MapInfo?> GetMapInfoAsync(double screenX, double screenY, Viewport viewport, IEnumerable<ILayer> layers, int margin = 0);
 }
