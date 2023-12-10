fb4f931763fc76ab032d71b13f85f9a4e69ae02f
Inforithmics <thomas.stocker@gmail.com>
2023-12-11 00:54:36 +0100

    quadruple the minimum Tiles to keep

--- Mapsui.Rendering.Skia/TileCache.cs
+++ Mapsui.Rendering.Skia/TileCache.cs
@@ -9,8 +9,8 @@ namespace Mapsui.Rendering.Skia;
 
 public class TileCache : ITileCache
 {
-    private const int TilesToKeepMultiplier = 6; // in RasterStyle it was 3, I doubled it because now a tile Layers have one Cache
-    private const int MinimumTilesToKeep = 64; // in RasterStyle it was 32, I doubled it because now a tile Layers have one Cache
+    private const int TilesToKeepMultiplier = 3;
+    private const int MinimumTilesToKeep = 128; // in RasterStyle it was 32, I quadrupled it because now a tile Layers have one Cache
     private long _lastIteration;
     
     private readonly IDictionary<object, IBitmapInfo?> _tileCache = 
