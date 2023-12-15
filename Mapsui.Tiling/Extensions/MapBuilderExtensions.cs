41a8014fa1e89abf7778f000d4ca42efbebcfcde
Charlenni <geocaching@weltz-online.de>
2023-12-15 10:08:22 +0100

    First attempt for a MapBuilder

---
+++ Mapsui.Tiling/Extensions/MapBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Mapsui.Tiling.Extensions;
+
+public static class MapBuilderExtension
+{
+    /// <summary>
+    /// Add an OSM layer to a MapBuilder
+    /// </summary>
+    /// <param name="mapBuilder">MapBuilder to use</param>
+    /// <returns>MapBuilder</returns>
+    public static MapBuilder AddOsmMap(this MapBuilder mapBuilder, string? userAgent = null)
+    {
+        var osmLayer = OpenStreetMap.CreateTileLayer(userAgent);
+
+        mapBuilder.Map.Layers.Add(osmLayer);
+
+        return mapBuilder;
+    }
+}
