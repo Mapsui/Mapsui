5668513059287d76bb4fa6640b60c6cd162f419c
pauldendulk <pauldendulk@gmail.com>
2023-12-28 21:27:44 +0100

    Build the mkdocs documentation folder

--- docfx/build-site.cmd
+++ docfx/build-site.cmd
@@ -1,6 +1,7 @@
 
 REM ECHO INSTALL .NET 6 SDK (was necessary on my machine where I develop all kinds of .NET apps)
 REM Perhaps we need: winget install Microsoft.DotNet.SDK.6 --silent
+mkdocs build -f docfx/mkdocs.yml || exit /b
 ECHO INSTALL docfx
 dotnet tool update -g docfx --version 2.60.2  || exit /b 
 ECHO Generate website in docfx\_site folder
