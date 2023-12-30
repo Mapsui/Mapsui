21e99280b49c111be6e5f4caa4201475e53ae2cf
Charlenni <geocaching@weltz-online.de>
2023-12-30 15:14:54 +0100

    Add a type to handle, which area of the viewport should be used for handling events: only the area of the widget or whole viewport.

---
+++ Mapsui/Widgets/TouchableAreaType.cs
@@ -0,0 +1,13 @@
+﻿namespace Mapsui.Widgets;
+
+public enum TouchableAreaType
+{
+    /// <summary>
+    /// Only Envelope gets touch events
+    /// </summary>
+    Widget,
+    /// <summary>
+    /// Whole Viewport is touchable
+    /// </summary>
+    Viewport
+}
