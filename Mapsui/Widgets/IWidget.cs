4ffe3fdb5f76b576f6b590cfb5c0ebb883645765
Charlenni <geocaching@weltz-online.de>
2023-12-30 14:10:51 +0100

    Extend IWidget by MarginX, MarginY, PositionX, PositionY, Width, Height and Enabled, because all Widgets do need this

--- Mapsui/Widgets/IWidget.cs
+++ Mapsui/Widgets/IWidget.cs
@@ -2,15 +2,55 @@
 
 public interface IWidget
 {
+    /// <summary>
+    /// Horizontal alignment of Widget
+    /// </summary>
     HorizontalAlignment HorizontalAlignment { get; set; }
+
+    /// <summary>
+    /// Vertical alignment of Widget
+    /// </summary>
     VerticalAlignment VerticalAlignment { get; set; }
+
+    /// <summary>
+    /// Left, right or both margin depending on HorizontalAlignment
+    /// </summary>
     double MarginX { get; set; }
+
+    /// <summary>
+    /// Top, bottom or both marging depending on VerticalAlignment
+    /// </summary>
     double MarginY { get; set; }
+
+    /// <summary>
+    /// Position in X direction of left side for absolute alignment
+    /// </summary>
+    double PositionX { get; set; }
+
+    /// <summary>
+    /// Position in Y direction of top side for absolute alignment
+    /// </summary>
+    double PositionY { get; set; }
+
+    /// <summary>
+    /// Width of Widget
+    /// </summary>
+    double Width { get; set; }
+
+    /// <summary>
+    /// Height of Widget
+    /// </summary>
+    double Height { get; set; }
 
     /// <summary>
     /// The hit box of the widget. This needs to be updated from the widget renderer.
     /// </summary>
     MRect? Envelope { get; set; }
+
+    /// <summary>
+    /// Is Widget visible on screen
+    /// </summary>
+    bool Enabled { get; set; }
 
     /// <summary>
     /// Function, which is called, when a Widget is hidden
@@ -19,6 +59,4 @@ public interface IWidget
     /// <param name="position">Screen position</param>
     /// <returns>True, if the Widget had handled the touch event</returns>
     bool HandleWidgetTouched(Navigator navigator, MPoint position);
-
-    bool Enabled { get; set; }
 }
