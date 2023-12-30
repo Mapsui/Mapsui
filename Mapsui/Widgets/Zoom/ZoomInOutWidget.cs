2a969e0d07ba9642dba7e2cacb8c3e55ecfe605c
Charlenni <geocaching@weltz-online.de>
2023-12-30 18:31:09 +0100

    Removed a bug, where touchable area isn't set correct in ZoomInOutWidget

--- Mapsui/Widgets/Zoom/ZoomInOutWidget.cs
+++ Mapsui/Widgets/Zoom/ZoomInOutWidget.cs
@@ -136,7 +136,7 @@ public class ZoomInOutWidget : Widget, ITouchableWidget, INotifyPropertyChanged
         }
     }
 
-    public TouchableAreaType TouchableArea => throw new NotImplementedException();
+    public TouchableAreaType TouchableArea => TouchableAreaType.Widget;
 
     public bool HandleWidgetTouched(Navigator navigator, MPoint position, WidgetTouchedEventArgs args)
     {
