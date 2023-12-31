aeecc6b0ab0a654a6c95734bd43cf8d502649696
Charlenni <geocaching@weltz-online.de>
2023-12-31 16:26:53 +0100

    Added new function UpdateEnvelope to Widget for centralized calculation of envelope

--- Mapsui/Widgets/Widget.cs
+++ Mapsui/Widgets/Widget.cs
@@ -181,24 +181,38 @@ public abstract class Widget : IWidget, INotifyPropertyChanged
     /// </summary>
     public event PropertyChangedEventHandler? PropertyChanged;
 
+    public void UpdateEnvelope(double maxWidth, double maxHeight, double screenWidth, double screenHeight)
+    {
+        var minX = CalculatePositionX(0, screenWidth, maxWidth);
+        var minY = CalculatePositionY(0, screenHeight, maxHeight);
+        var maxX = HorizontalAlignment == HorizontalAlignment.Stretch ? screenWidth - MarginX : minX + maxWidth;
+        var maxY = VerticalAlignment == VerticalAlignment.Stretch ? screenHeight - MarginY : minY + maxHeight;
+
+        Envelope = new MRect(minX, minY, maxX, maxY);
+    }
+
     public virtual void OnPropertyChanged([CallerMemberName] string name = "")
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 
-    public double CalculatePositionX(double left, double right, double width) => HorizontalAlignment switch
+    private double CalculatePositionX(double left, double right, double width) => HorizontalAlignment switch
     {
         HorizontalAlignment.Left => MarginX,
         HorizontalAlignment.Center => (right - left - width) / 2f,
         HorizontalAlignment.Right => right - left - width - MarginX,
+        HorizontalAlignment.Stretch => MarginX,
+        HorizontalAlignment.Absolute => PositionX,
         _ => throw new ArgumentException("Unknown horizontal alignment: " + HorizontalAlignment)
     };
 
-    public double CalculatePositionY(double top, double bottom, double height) => VerticalAlignment switch
+    private double CalculatePositionY(double top, double bottom, double height) => VerticalAlignment switch
     {
         VerticalAlignment.Top => MarginY,
         VerticalAlignment.Bottom => bottom - top - height - MarginY,
         VerticalAlignment.Center => (bottom - top - height) / 2f,
+        VerticalAlignment.Stretch => MarginY,
+        VerticalAlignment.Absolute => PositionY,
         _ => throw new ArgumentException("Unknown vertical alignment: " + VerticalAlignment)
     };
 }
