5edaf32e5173b3851243f1020d24050d69859af8
Charlenni <geocaching@weltz-online.de>
2023-12-31 17:08:11 +0100

    Derived MouseCoordinatesWidgetRenderer from TextBoxWidgetRenderer, because it is the same

--- Mapsui.Rendering.Skia/SkiaWidgets/MouseCoordinatesWidgetRenderer.cs
+++ Mapsui.Rendering.Skia/SkiaWidgets/MouseCoordinatesWidgetRenderer.cs
@@ -1,16 +1,13 @@
-﻿
-using Mapsui.Widgets;
+﻿using Mapsui.Widgets;
 using Mapsui.Widgets.MouseCoordinatesWidget;
 using SkiaSharp;
 
 namespace Mapsui.Rendering.Skia.SkiaWidgets;
 
-public class MouseCoordinatesWidgetRenderer : ISkiaWidgetRenderer
+public class MouseCoordinatesWidgetRenderer : TextBoxWidgetRenderer
 {
-    public void Draw(SKCanvas canvas, Viewport viewport, IWidget widget, float layerOpacity)
+    public override void Draw(SKCanvas canvas, Viewport viewport, IWidget widget, float layerOpacity)
     {
-        TextBoxWidgetRenderer.DrawText(canvas, viewport, (MouseCoordinatesWidget)widget, layerOpacity);
-        // use all of the canvas
-        widget.Envelope = new MRect(0, 0, viewport.Width, viewport.Height);
+        base.Draw(canvas, viewport, (MouseCoordinatesWidget)widget, layerOpacity);
     }
 }
