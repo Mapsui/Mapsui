b5c6913cdb03bc6e1fc20557fb6a97495acf474d
Charlenni <geocaching@weltz-online.de>
2023-12-31 17:25:13 +0100

    Changed a wrong using for PerformanceWidget

--- Mapsui.Rendering.Skia/SkiaWidgets/PerformanceWidgetRenderer.cs
+++ Mapsui.Rendering.Skia/SkiaWidgets/PerformanceWidgetRenderer.cs
@@ -1,6 +1,6 @@
 ﻿using Mapsui.Rendering.Skia.Extensions;
 using Mapsui.Widgets;
-using Mapsui.Widgets.InfoWidgets;
+using Mapsui.Widgets.PerformanceWidget;
 using SkiaSharp;
 
 namespace Mapsui.Rendering.Skia.SkiaWidgets;
