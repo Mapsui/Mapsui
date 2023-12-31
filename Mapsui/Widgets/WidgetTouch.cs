49806307c06f333f96e337f52b72302a60903233
Charlenni <geocaching@weltz-online.de>
2023-12-31 11:39:21 +0100

    Change selection of touched widgets from only using envelope to using widgets touchable area

--- Mapsui/Widgets/WidgetTouch.cs
+++ Mapsui/Widgets/WidgetTouch.cs
@@ -12,7 +12,7 @@ public static class WidgetTouch
     /// <param name="startScreenPosition">The start screen position in device independent units (or DIP or DP)</param>
     /// <param name="widgets">The widgets to select from.</param>
     /// <returns>
-    /// Returns the first Widget in the list that contains the screenPosition
+    /// Returns the Widgets in the list that contain the screenPosition
     /// within it's Envelope. Returns null if there are none.
     /// </returns>
     public static IEnumerable<ITouchableWidget> GetTouchedWidget(MPoint screenPosition, MPoint startScreenPosition,
@@ -20,14 +20,21 @@ public static class WidgetTouch
     {
         var touchedWidgets = new List<ITouchableWidget>();
 
-        foreach (var widget in widgets.Reverse())
+        foreach (var widget in widgets.Where(w => w is ITouchableWidget).Where(w => w.Enabled).Reverse())
         {
-            // Also check for start position because it should be click on the widget,
+            // There are two possible TouchableAreaTypes
+
+            if (widget.TouchableArea == TouchableAreaType.Viewport)
+            {
+                // Widget is always touched
+                touchedWidgets.Add(widget);
+                continue;
+            }
+
+            // Also check for start position, because it should be click on the widget,
             // not a drag that ends above the widget.
-            if (widget.Envelope != null &&
-                widget.Enabled &&
-                widget.Envelope.Contains(screenPosition) &&
-                widget.Envelope.Contains(startScreenPosition))
+            if ((widget.Envelope?.Contains(screenPosition) ?? false) &&
+                (widget.Envelope?.Contains(startScreenPosition) ?? false))
                 touchedWidgets.Add(widget);
         }
 
