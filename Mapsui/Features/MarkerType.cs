fb6bd57e4bba1ce4e5b45fc0d3d0c91275061f66
Charlenni <geocaching@weltz-online.de>
2024-01-02 16:03:45 +0100

    Add a MarkerType

---
+++ Mapsui/Features/MarkerType.cs
@@ -0,0 +1,19 @@
+﻿namespace Mapsui.Features;
+
+public enum MarkerType
+{
+    /// <summary>
+    /// Pin with image, which could change color
+    /// </summary>
+    Pin,
+
+    /// <summary>
+    /// Pin as icon image
+    /// </summary>
+    Icon,
+
+    /// <summary>
+    /// Pin as Svg image
+    /// </summary>
+    Svg
+}
