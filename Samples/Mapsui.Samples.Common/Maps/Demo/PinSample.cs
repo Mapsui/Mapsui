e7ae4042323b28d366ba0ff0e8844091d35f6e12
Charlenni <geocaching@weltz-online.de>
2024-01-02 16:04:54 +0100

    Add a sample to show, how Markers work

---
+++ Samples/Mapsui.Samples.Common/Maps/Demo/PinSample.cs
@@ -0,0 +1,42 @@
+﻿using Mapsui.Extensions;
+using Mapsui.Layers;
+using Mapsui.Projections;
+using Mapsui.Tiling;
+using Mapsui.Widgets;
+using Mapsui.Widgets.ScaleBar;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mapsui.Samples.Common.Maps.Demo;
+
+public class PinSample : ISample
+{
+    public string Name => "6 Pin Sample";
+    public string Category => "Demo";
+
+    public Task<Map> CreateMapAsync()
+    {
+        return Task.FromResult(CreateMap());
+    }
+
+    public static Map CreateMap()
+    {
+        var map = new Map
+        {
+            CRS = "EPSG:3857"
+        };
+        map.Layers.Add(OpenStreetMap.CreateTileLayer());
+        map.Layers.Add(new MemoryLayer("Markers") { Style = null, });
+        map.Widgets.Add(new ScaleBarWidget(map) { TextAlignment = Alignment.Center, HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Top });
+
+        var layer = (MemoryLayer)map.Layers.Where(l => l.Name.Equals("Markers")).First();
+
+        if (layer == null)
+            return map;
+
+        layer.AddMarker(SphericalMercator.FromLonLat(9, 48));
+        layer.AddMarker(SphericalMercator.FromLonLat(9.1, 48.1));
+
+        return map;
+    }
+}
