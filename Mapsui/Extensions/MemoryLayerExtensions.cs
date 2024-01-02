2b3035b98a005a04f66b0aedde7aa5bc83bbd183
Charlenni <geocaching@weltz-online.de>
2024-01-02 16:04:22 +0100

    Add extensions to MemoryLayer to add markers

---
+++ Mapsui/Extensions/MemoryLayerExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Mapsui.Features;
+using Mapsui.Layers;
+using System.Collections.Concurrent;
+
+namespace Mapsui.Extensions;
+
+/// <summary>
+/// Extensions for MemoryLayer
+/// </summary>
+public static class MemoryLayerExtensions
+{
+    /// <summary>
+    /// Add a <see cref="Marker"/> to the layer
+    /// </summary>
+    /// <param name="layer">Layer to use</param>
+    /// <param name="x">X position</param>
+    /// <param name="y">Y position</param>
+    public static void AddMarker(this MemoryLayer layer, double x, double y)
+    {
+        ((ConcurrentBag<IFeature>)layer.Features).Add(new Marker(x, y));
+    }
+
+    /// <summary>
+    /// Add a <see cref="Marker"/> to the layer
+    /// </summary>
+    /// <param name="layer">Layer to use</param>
+    /// <param name="x">X position</param>
+    /// <param name="y">Y position</param>
+    public static void AddMarker(this MemoryLayer layer, (double x, double y) position)
+    {
+        ((ConcurrentBag<IFeature>)layer.Features).Add(new Marker(position));
+    }
+
+    /// <summary>
+    /// Add a <see cref="Marker"/> to the layer
+    /// </summary>
+    /// <param name="layer">Layer to use</param>
+    /// <param name="point">Point for position</param>
+    public static void AddMarker(this MemoryLayer layer, MPoint position)
+    {
+        ((ConcurrentBag<IFeature>)layer.Features).Add(new Marker(position));
+    }
+
+}
