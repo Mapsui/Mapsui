5d3af5f811c2a48b7bf9d18cb80211c715781048
Charlenni <geocaching@weltz-online.de>
2024-01-03 17:24:11 +0100

    Add map extension for marker layer handling show/hide of callouts

---
+++ Mapsui/Extensions/MapExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Mapsui.Features;
+using Mapsui.Layers;
+using System.Linq;
+
+namespace Mapsui.Extensions;
+
+public static  class MapExtensions
+{
+    public static MemoryLayer AddMarkerLayer(this Map map, string name)
+    {
+        var layer = new MemoryLayer(name) 
+        { 
+            Style = null, 
+            IsMapInfoLayer = true,
+        };
+
+        map.Info += (s, e) =>
+        {
+            if (e.MapInfo?.Feature == null || e.MapInfo.Feature is not Marker marker) return;
+
+            var hasCallout = marker.HasCallout;
+
+            foreach (var m in layer.Features.Where(f => f is Marker && ((Marker)f).HasCallout))
+                ((Marker)m).HideCallout();
+
+            if (!hasCallout)
+                marker.ShowCallout();
+
+            layer.DataHasChanged();
+        };
+
+        map.Layers.Add(layer);
+
+        return layer;
+    }
+}
