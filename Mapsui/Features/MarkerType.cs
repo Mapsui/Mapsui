4ef4bb4d2e64aec115d56d2508499e106ac851cd
Charlenni <geocaching@weltz-online.de>
2024-01-04 19:06:26 +0100

    Add new marker types

--- Mapsui/Features/MarkerType.cs
+++ Mapsui/Features/MarkerType.cs
@@ -2,10 +2,6 @@
 
 public enum MarkerType
 {
-    /// <summary>
-    /// Pin with image, which could change color
-    /// </summary>
-    Pin,
 
     /// <summary>
     /// Pin as icon image
@@ -15,5 +11,220 @@ public enum MarkerType
     /// <summary>
     /// Pin as Svg image
     /// </summary>
-    Svg
+    Svg,
+
+    /// <summary>
+    /// Pin could change color
+    /// </summary>
+    Pin,
+
+    /// <summary>
+    /// Pin with an 0
+    /// </summary>
+    Pin_0,
+
+    /// <summary>
+    /// Pin with an 1
+    /// </summary>
+    Pin_1,
+
+    /// <summary>
+    /// Pin with an 2
+    /// </summary>
+    Pin_2,
+
+    /// <summary>
+    /// Pin with an 3
+    /// </summary>
+    Pin_3,
+
+    /// <summary>
+    /// Pin with an 4
+    /// </summary>
+    Pin_4,
+
+    /// <summary>
+    /// Pin with an 5
+    /// </summary>
+    Pin_5,
+
+    /// <summary>
+    /// Pin with an 6
+    /// </summary>
+    Pin_6,
+
+    /// <summary>
+    /// Pin with an 7
+    /// </summary>
+    Pin_7,
+
+    /// <summary>
+    /// Pin with an 8
+    /// </summary>
+    Pin_8,
+
+    /// <summary>
+    /// Pin with an 9
+    /// </summary>
+    Pin_9,
+
+    /// <summary>
+    /// Pin with an @
+    /// </summary>
+    Pin_At,
+
+    /// <summary>
+    /// Pin with an A
+    /// </summary>
+    Pin_A,
+
+    /// <summary>
+    /// Pin with an B
+    /// </summary>
+    Pin_B,
+
+    /// <summary>
+    /// Pin with an C
+    /// </summary>
+    Pin_C,
+
+    /// <summary>
+    /// Pin with an D
+    /// </summary>
+    Pin_D,
+
+    /// <summary>
+    /// Pin with an E
+    /// </summary>
+    Pin_E,
+
+    /// <summary>
+    /// Pin with an F
+    /// </summary>
+    Pin_F,
+
+    /// <summary>
+    /// Pin with an G
+    /// </summary>
+    Pin_G,
+
+    /// <summary>
+    /// Pin with an H
+    /// </summary>
+    Pin_H,
+
+    /// <summary>
+    /// Pin with an I
+    /// </summary>
+    Pin_I,
+
+    /// <summary>
+    /// Pin with an J
+    /// </summary>
+    Pin_J,
+
+    /// <summary>
+    /// Pin with an K
+    /// </summary>
+    Pin_K,
+
+    /// <summary>
+    /// Pin with an L
+    /// </summary>
+    Pin_L,
+
+    /// <summary>
+    /// Pin with an M
+    /// </summary>
+    Pin_M,
+
+    /// <summary>
+    /// Pin with an N
+    /// </summary>
+    Pin_N,
+
+    /// <summary>
+    /// Pin with an O
+    /// </summary>
+    Pin_O,
+
+    /// <summary>
+    /// Pin with an P
+    /// </summary>
+    Pin_P,
+
+    /// <summary>
+    /// Pin with an Q
+    /// </summary>
+    Pin_Q,
+
+    /// <summary>
+    /// Pin with an R
+    /// </summary>
+    Pin_R,
+
+    /// <summary>
+    /// Pin with an S
+    /// </summary>
+    Pin_S,
+
+    /// <summary>
+    /// Pin with an T
+    /// </summary>
+    Pin_T,
+
+    /// <summary>
+    /// Pin with an U
+    /// </summary>
+    Pin_U,
+
+    /// <summary>
+    /// Pin with an V
+    /// </summary>
+    Pin_V,
+
+    /// <summary>
+    /// Pin with an W
+    /// </summary>
+    Pin_W,
+
+    /// <summary>
+    /// Pin with an X
+    /// </summary>
+    Pin_X,
+
+    /// <summary>
+    /// Pin with an Y
+    /// </summary>
+    Pin_Y,
+
+    /// <summary>
+    /// Pin with an Z
+    /// </summary>
+    Pin_Z,
+
+    /// <summary>
+    /// Pin with an Questionmark
+    /// </summary>
+    Pin_Questionmark,
+
+    /// <summary>
+    /// Pin with an Exclamationmark
+    /// </summary>
+    Pin_Exclamationmark,
+
+    /// <summary>
+    /// Pin with an Cross
+    /// </summary>
+    Pin_Cross,
+
+    /// <summary>
+    /// Pin with an Euro
+    /// </summary>
+    Pin_Euro,
+
+    /// <summary>
+    /// Pin with an Dollar
+    /// </summary>
+    Pin_Dollar,
 }
