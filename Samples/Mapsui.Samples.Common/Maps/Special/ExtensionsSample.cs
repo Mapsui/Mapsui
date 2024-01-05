7a01d2c6d95e2751c778c1c9a7cec0e24950ac0f
Charlenni <geocaching@weltz-online.de>
2024-01-05 19:36:51 +0100

    Move sample to Special and rename it to ExtensionSample

--- Samples/Mapsui.Samples.Common/Maps/Special/ExtensionsSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Special/ExtensionsSample.cs
@@ -1,6 +1,5 @@
 ﻿using Mapsui.Extensions;
 using Mapsui.Features;
-using Mapsui.Layers;
 using Mapsui.Projections;
 using Mapsui.Styles;
 using Mapsui.Tiling;
@@ -9,15 +8,14 @@ using Mapsui.Widgets;
 using Mapsui.Widgets.ScaleBar;
 using System;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
-namespace Mapsui.Samples.Common.Maps.Demo;
+namespace Mapsui.Samples.Common.Maps.Special;
 
-public class PinSample : ISample
+public class ExtensionsSample : ISample
 {
-    public string Name => "6 Pin Sample";
-    public string Category => "Demo";
+    public string Name => "Extensions";
+    public string Category => "Special";
 
     public Task<Map> CreateMapAsync()
     {
@@ -49,15 +47,15 @@ public class PinSample : ISample
         var icon = GetIconFromResources("Images.icon.png");
 
         // Add markers
-        layer.AddMarker(SphericalMercator.FromLonLat(9.0, 48.025), title: "New York", type: Features.MarkerType.Pin)
+        layer.AddMarker(SphericalMercator.FromLonLat(9.0, 48.025), title: "New York", type: MarkerType.Pin)
             .AddMarker(SphericalMercator.FromLonLat(9.1, 48.0), title: "Ghostscript Tiger", svg: tiger, scale: 0.1, anchor: new Offset(0.3, -0.8, true), calloutAnchor: new Offset(-0.3, -0.3, true)) // Set center point to 30 % in x and -80 % in y direction
             .AddMarker(SphericalMercator.FromLonLat(9.05, 48.0), title: "San Fransisco", icon: icon, anchor: new Offset(0.5, 0.5, true), calloutAnchor: new Offset(-0.5, 1.0, true));
 
         for (var i = 0; i < 10; i++)
-            layer.AddMarker(SphericalMercator.FromLonLat(9.0 + i * 0.015, 48.07), type: (MarkerType.Pin_0 + i), title: (MarkerType.Pin_0 + i).ToString(), color: DemoColor());
+            layer.AddMarker(SphericalMercator.FromLonLat(9.0 + i * 0.015, 48.07), type: MarkerType.Pin_0 + i, title: (MarkerType.Pin_0 + i).ToString(), color: DemoColor());
 
         for (var i = 0; i < 27; i++)
-            layer.AddMarker(SphericalMercator.FromLonLat(9.0 + i * 0.01, 48.05), type: (MarkerType.Pin_At + i), title: (MarkerType.Pin_At + i).ToString(), color: DemoColor());
+            layer.AddMarker(SphericalMercator.FromLonLat(9.0 + i * 0.01, 48.05), type: MarkerType.Pin_At + i, title: (MarkerType.Pin_At + i).ToString(), color: DemoColor());
 
         layer.AddMarker(SphericalMercator.FromLonLat(9.015, 48.025), type: MarkerType.Pin_Questionmark, title: MarkerType.Pin_Questionmark.ToString(), color: DemoColor())
             .AddMarker(SphericalMercator.FromLonLat(9.03, 48.025), type: MarkerType.Pin_Exclamationmark, title: MarkerType.Pin_Exclamationmark.ToString(), color: DemoColor())
@@ -66,7 +64,7 @@ public class PinSample : ISample
             .AddMarker(SphericalMercator.FromLonLat(9.075, 48.025), type: MarkerType.Pin_Euro, title: MarkerType.Pin_Euro.ToString(), color: DemoColor());
 
         for (var i = 0; i < 10; i++)
-            layer.AddMarker(SphericalMercator.FromLonLat(9.12 + i * 0.015, 48.0), type: (MarkerType.Pin_0 + i), title: (MarkerType.Pin_0 + i).ToString(), color: DemoColor(), scale: 0.5 + _rand.NextDouble());
+            layer.AddMarker(SphericalMercator.FromLonLat(9.12 + i * 0.015, 48.0), type: MarkerType.Pin_0 + i, title: (MarkerType.Pin_0 + i).ToString(), color: DemoColor(), scale: 0.5 + _rand.NextDouble());
 
         // Zoom and center map
         var center = layer.Extent?.Centroid ?? new MPoint(SphericalMercator.FromLonLat(9.05, 48.05));
@@ -80,7 +78,7 @@ public class PinSample : ISample
 
     private static string GetSvgFromResources(string name)
     {
-        using var stream = typeof(PinSample).Assembly.GetManifestResourceStream(typeof(PinSample).Assembly.GetFullName(name));
+        using var stream = typeof(ExtensionsSample).Assembly.GetManifestResourceStream(typeof(ExtensionsSample).Assembly.GetFullName(name));
         if (stream == null) return string.Empty;
         using var reader = new StreamReader(stream);
 
@@ -89,7 +87,7 @@ public class PinSample : ISample
 
     private static byte[]? GetIconFromResources(string name)
     {
-        using var stream2 = EmbeddedResourceLoader.Load(name, typeof(PinSample));
+        using var stream2 = EmbeddedResourceLoader.Load(name, typeof(ExtensionsSample));
         if (stream2 == null) return null;
         using var reader2 = new MemoryStream();
         stream2.CopyTo(reader2);
