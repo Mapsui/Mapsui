b9d4da2d833d6c652f868d097f734bde31d35bf2
Charlenni <geocaching@weltz-online.de>
2024-01-05 14:38:41 +0100

    Changed map extension to create a MarkerLayer instead of a MemoryLayer

--- Mapsui/Extensions/MapExtensions.cs
+++ Mapsui/Extensions/MapExtensions.cs
@@ -8,27 +8,11 @@ public static  class MapExtensions
 {
     public static MemoryLayer AddMarkerLayer(this Map map, string name)
     {
-        var layer = new MemoryLayer(name) 
-        { 
-            Style = null, 
-            IsMapInfoLayer = true,
-        };
-
-        map.Info += (s, e) =>
-        {
-            if (e.MapInfo?.Feature == null || e.MapInfo.Feature is not Marker marker) return;
-
-            var hasCallout = marker.HasCallout;
-
-            foreach (var m in layer.Features.Where(f => f is Marker && ((Marker)f).HasCallout))
-                ((Marker)m).HideCallout();
-
-            if (!hasCallout)
-                marker.ShowCallout();
-
-            layer.DataHasChanged();
-        };
-
+        // Create layer
+        var layer = new MarkerLayer(name);
+        // Add handling of touches
+        map.Info += layer.HandleInfo;
+        // Add layer to map
         map.Layers.Add(layer);
 
         return layer;
