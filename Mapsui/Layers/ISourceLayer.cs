f3a063df52a09a0ee122512487ecca099bec5533
Charlenni <geocaching@weltz-online.de>
2024-01-09 22:01:40 +0100

    Add comment for ISourceLayer

--- Mapsui/Layers/ISourceLayer.cs
+++ Mapsui/Layers/ISourceLayer.cs
@@ -1,6 +1,13 @@
 namespace Mapsui.Layers;
 
+/// <summary>
+/// Interface for layers, that contain an underlaying source layer, which 
+/// provides the real data 
+/// </summary>
 public interface ISourceLayer
 {
+    /// <summary>
+    /// Layer with the real source for this layer
+    /// </summary>
     ILayer SourceLayer { get; }
 }
