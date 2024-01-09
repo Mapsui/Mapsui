abc8f3bd99c0e950971ab67b07a0bb307955252d
Charlenni <geocaching@weltz-online.de>
2024-01-09 15:26:40 +0100

    Add implementation of IFeature (ZOrder) Add comments

--- Mapsui/Features/PointFeature.cs
+++ Mapsui/Features/PointFeature.cs
@@ -2,6 +2,9 @@
 
 namespace Mapsui.Layers;
 
+/// <summary>
+/// Feature representing a point on the <cref="Map"/>
+/// </summary>
 public class PointFeature : BaseFeature, IFeature
 {
     public PointFeature(PointFeature pointFeature) : base(pointFeature)
@@ -19,9 +22,25 @@ public class PointFeature : BaseFeature, IFeature
         Point = new MPoint(x, y);
     }
 
+    /// <summary>
+    /// Point of <cref="Map"/>
+    /// </summary>
     public MPoint Point { get; }
+
+    /// <summary>
+    /// Extent of feature
+    /// </summary>
     public MRect Extent => Point.MRect;
 
+    /// <summary>
+    /// Order of feature
+    /// </summary>
+    public int ZOrder { get; set; }
+
+    /// <summary>
+    /// Implementation of visitor pattern for coordinates
+    /// </summary>
+    /// <param name="visit"></param>
     public void CoordinateVisitor(Action<double, double, CoordinateSetter> visit)
     {
         visit(Point.X, Point.Y, (x, y) =>
