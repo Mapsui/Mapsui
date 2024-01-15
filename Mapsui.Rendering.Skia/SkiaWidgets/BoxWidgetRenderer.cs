3ffb03f2637c77177ab9af0240202648a32241ee
Charlenni <geocaching@weltz-online.de>
2024-01-15 11:17:40 +0100

    Remove wrong comment in BoxWidgetRenderer

--- Mapsui.Rendering.Skia/SkiaWidgets/BoxWidgetRenderer.cs
+++ Mapsui.Rendering.Skia/SkiaWidgets/BoxWidgetRenderer.cs
@@ -14,9 +14,6 @@ public class BoxWidgetRenderer : ISkiaWidgetRenderer
         boxWidget.UpdateEnvelope(boxWidget.Width, boxWidget.Height, viewport.Width, viewport.Height);
 
         using var backPaint = new SKPaint { Color = boxWidget.BackColor.ToSkia(layerOpacity), IsAntialias = true };
-        // The textRect has an offset which can be confusing. 
-        // This is because DrawText's origin is the baseline of the text, not the bottom.
-        // Read more here: https://developer.xamarin.com/guides/xamarin-forms/advanced/skiasharp/basics/text/
 
         canvas.DrawRoundRect(boxWidget.Envelope?.ToSkia() ?? new SKRect(0, 0, (float)boxWidget.Width, (float)boxWidget.Height), (float)boxWidget.CornerRadius, (float)boxWidget.CornerRadius, backPaint);
     }
