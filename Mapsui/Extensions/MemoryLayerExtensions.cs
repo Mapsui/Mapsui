d70ff0f578b11a15ec0d591023a59e6b620b0d2b
Charlenni <geocaching@weltz-online.de>
2024-01-20 14:08:26 +0100

    Add touched to extensions

--- Mapsui/Extensions/MemoryLayerExtensions.cs
+++ Mapsui/Extensions/MemoryLayerExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Mapsui.Features;
 using Mapsui.Layers;
 using Mapsui.Styles;
+using System;
 using System.Collections.Concurrent;
+using System.Linq;
 
 namespace Mapsui.Extensions;
 
@@ -16,7 +18,7 @@ public static class MemoryLayerExtensions
     /// <param name="layer">Layer to use</param>
     /// <param name="x">X position</param>
     /// <param name="y">Y position</param>
-    public static MemoryLayer AddMarker(this MemoryLayer layer, double x, double y, MarkerType type = MarkerType.Pin_Circle, string? title = null, string? subtitle = null, Styles.Color? color = null, double scale = 1.0, Offset? anchor = null, Offset? calloutAnchor = null)
+    public static MemoryLayer AddMarker(this MemoryLayer layer, double x, double y, MarkerType type = MarkerType.Pin_Circle, string? title = null, string? subtitle = null, Styles.Color? color = null, double scale = 1.0, Offset? anchor = null, Offset? calloutAnchor = null, Action<ILayer, Marker, MapInfoEventArgs>? touched = null)
     {
         var marker = new Marker(x, y, type);
 
@@ -27,6 +29,7 @@ public static class MemoryLayerExtensions
         if (anchor != null) marker.Anchor = anchor;
         if (calloutAnchor != null) marker.CalloutAnchor = calloutAnchor;
         if (color != null) marker.Color = color;
+        if (touched != null) marker.Touched = touched;
 
         ((ConcurrentBag<IFeature>)layer.Features).Add(marker);
 
@@ -39,9 +42,9 @@ public static class MemoryLayerExtensions
     /// <param name="layer">Layer to use</param>
     /// <param name="x">X position</param>
     /// <param name="y">Y position</param>
-    public static MemoryLayer AddMarker(this MemoryLayer layer, (double x, double y) position, MarkerType type = MarkerType.Pin_Circle, string? title = null, string? subtitle = null, Styles.Color? color = null, double scale = 1.0, Offset? anchor = null, Offset? calloutAnchor = null)
+    public static MemoryLayer AddMarker(this MemoryLayer layer, (double x, double y) position, MarkerType type = MarkerType.Pin_Circle, string? title = null, string? subtitle = null, Styles.Color? color = null, double scale = 1.0, Offset? anchor = null, Offset? calloutAnchor = null, Action<ILayer, Marker, MapInfoEventArgs>? touched = null)
     {
-        return AddMarker(layer, position.x, position.y, type, title, subtitle, color, scale, anchor, calloutAnchor);
+        return AddMarker(layer, position.x, position.y, type, title, subtitle, color, scale, anchor, calloutAnchor, touched);
     }
 
     /// <summary>
@@ -49,8 +52,18 @@ public static class MemoryLayerExtensions
     /// </summary>
     /// <param name="layer">Layer to use</param>
     /// <param name="point">Point for position</param>
-    public static MemoryLayer AddMarker(this MemoryLayer layer, MPoint position, MarkerType type = MarkerType.Pin_Circle, string? title = null, string? subtitle = null, Styles.Color? color = null, double scale = 1.0, Offset? anchor = null, Offset? calloutAnchor = null)
+    public static MemoryLayer AddMarker(this MemoryLayer layer, MPoint position, MarkerType type = MarkerType.Pin_Circle, string? title = null, string? subtitle = null, Styles.Color? color = null, double scale = 1.0, Offset? anchor = null, Offset? calloutAnchor = null, Action<ILayer, Marker, MapInfoEventArgs>? touched = null)
     {
-        return AddMarker(layer, position.X, position.Y, type, title, subtitle, color, scale, anchor, calloutAnchor);
+        return AddMarker(layer, position.X, position.Y, type, title, subtitle, color, scale, anchor, calloutAnchor, touched);
+    }
+
+    /// <summary>
+    /// Hide all callouts of <see cref="Marker"/> on this layer
+    /// </summary>
+    /// <param name="layer"></param>
+    public static void HideAllCallouts(this MemoryLayer layer)
+    {
+        foreach (var m in layer.Features.Where(f => f is Marker && ((Marker)f).HasCallout))
+            ((Marker)m).HideCallout();
     }
 }
