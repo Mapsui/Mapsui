9110373d5bb4caee80a134660b292ad03580b0f9
Inforithmics <thomas.stocker@gmail.com>
2024-01-21 14:09:50 +0100

    fixing build

--- Mapsui/Rendering/NonCachingVectorCache.cs
+++ Mapsui/Rendering/NonCachingVectorCache.cs
@@ -5,16 +5,19 @@ namespace Mapsui.Rendering;
 public sealed class NonCachingVectorCache(ISymbolCache symbolCache) : IVectorCache
 {
     public CacheTracker<T> GetOrCreatePaint<T, TParam>(TParam param, Func<TParam, T> toPaint)
+        where TParam: notnull
     {
         return new CacheTracker<T>(toPaint(param));
     }
 
     public CacheTracker<T> GetOrCreatePaint<T, TParam>(TParam param, Func<TParam, ISymbolCache, T> toPaint)
+        where TParam: notnull
     {
         return new CacheTracker<T>(toPaint(param, symbolCache));
     }
 
     public CacheTracker<T> GetOrCreatePath<T, TParam>(TParam param, Func<TParam, T> toSkRect)
+        where TParam: notnull
     {
         return new CacheTracker<T>(toSkRect(param));
     }
