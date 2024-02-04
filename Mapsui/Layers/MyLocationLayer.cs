b34f1ca059d8c13e4a4320d34c2aab7803703847
pauldendulk <pauldendulk@gmail.com>
2024-02-04 08:06:34 +0100

    Fix MyLocationLayer animation refresh

--- Mapsui/Layers/MyLocationLayer.cs
+++ Mapsui/Layers/MyLocationLayer.cs
@@ -250,6 +250,9 @@ public class MyLocationLayer : BaseLayer, IModifyFeatureLayer, IDisposable
 
                 if (_map.Navigator.Viewport.ToExtent() is not null)
                 {
+                    // Refresh the destination viewport at the start of the animation so it has time to load.
+                    _map.RefreshData(CreateDestinationFetchInfo(_map, _animationMyLocationEnd));
+
                     var fetchInfo = new FetchInfo(_map.Navigator.Viewport.ToSection(), _map?.CRS, ChangeType.Discrete);
                     _animationMyLocation = new AnimationEntry<Map>(
                         MyLocation,
@@ -521,5 +524,11 @@ public class MyLocationLayer : BaseLayer, IModifyFeatureLayer, IDisposable
         {
             Clicked?.Invoke(this, e);
         }
+    }
+
+    private static FetchInfo CreateDestinationFetchInfo(Map map, MPoint destination)
+    {
+        var destinationViewport = map.Navigator.Viewport with { CenterX = destination.X, CenterY = destination.Y };
+        return new FetchInfo(destinationViewport.ToSection(), map.CRS, ChangeType.Discrete);
     }
 }
