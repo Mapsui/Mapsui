2bf5b4dfd02927dcb6d31a64c2482422af026335
pauldendulk <pauldendulk@gmail.com>
2024-02-05 20:38:54 +0100

    Update comments

--- Mapsui/Navigation/Rotator.cs
+++ Mapsui/Navigation/Rotator.cs
@@ -14,15 +14,15 @@ public class Rotator
     /// </summary>
     public double ReSnapRotation { get; set; } = 5;
 
+    /// <summary>
+    /// The virtual rotation that is determined by the users rotating pinch behavior.
+    /// </summary>
     public double VirtualRotation { get; set; }
      
     /// <summary>
     /// Calculates the rotation delta taking into account the snapping parameters. 
     /// </summary>
-    /// <param name="virtualRotation">The virtual rotation that is determined by the users rotating pinch behavior.</param>
     /// <param name="rotation">The rotation of the viewport of the map. This rotation is visible in the map.</param>
-    /// <param name="unSnapRotation">The minimal value of the virtual rotation needed to unsnap (allow rotation other than 0).</param>
-    /// <param name="reSnapRotation">If the virtual rotation is below this value the map will resnap (rotation will be set to 0).</param>
     /// <returns></returns>
     public double CalculateRotationDeltaWithSnapping(double rotation)
     {
