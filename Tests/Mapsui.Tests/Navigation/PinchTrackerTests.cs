e21b7616d42ccdd9470e1fdbf7a1bbf8affe64f0
pauldendulk <pauldendulk@gmail.com>
2024-02-16 20:39:24 +0100

    Simplify unit test

--- Tests/Mapsui.Tests/Navigation/PinchTrackerTests.cs
+++ Tests/Mapsui.Tests/Navigation/PinchTrackerTests.cs
@@ -1,5 +1,4 @@
 ﻿using NUnit.Framework;
-using System.Collections.Generic;
 
 namespace Mapsui.Tests.Navigation;
 
@@ -11,21 +10,13 @@ public class PinchTrackerTests
     {
         // Arrange
         var pinchTracker = new PinchTracker();
-        int[][] pinch1 = [[0, 0], [1, 0]];
-        int[][] pinch2 = [[0, 0], [1, 1]];
+        pinchTracker.Update([new(0, 0), new(1, 0)]);
+        pinchTracker.Update([new(0, 0), new(1, 1)]);
 
         // Act
-        pinchTracker.Update(ToListOfMPoint(pinch1));
-        pinchTracker.Update(ToListOfMPoint(pinch2));
         var pinchManipulation = pinchTracker.GetPinchManipulation();
 
         // Assert
-
         Assert.That(45, Is.EqualTo(pinchManipulation.RotationChange));
-    }
-
-    List<MPoint> ToListOfMPoint(int[][] array)
-    {
-        return new List<MPoint> { new MPoint(array[0][0], array[0][1]), new MPoint(array[1][0], array[1][1]) };
     }
 }
