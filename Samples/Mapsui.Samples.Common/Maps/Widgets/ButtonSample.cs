ad27f930489a25b5b9b3702911a0211f68d0555f
pauldendulk <pauldendulk@gmail.com>
2024-03-01 12:23:40 +0100

    Rewrite ButtonSample a bit

--- Samples/Mapsui.Samples.Common/Maps/Widgets/ButtonSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Widgets/ButtonSample.cs
@@ -15,7 +15,7 @@ public class ButtonSample : ISample
     public string Name => "Button";
     public string Category => "Widgets";
 
-    private int clickCount;
+    private int _clickCount;
 
     public Task<Map> CreateMapAsync()
     {
@@ -23,24 +23,26 @@ public class ButtonSample : ISample
 
         map.Layers.Add(OpenStreetMap.CreateTileLayer());
 
-        var clickMeButton = CreateButton("Click me", VerticalAlignment.Top, HorizontalAlignment.Left);
-        clickMeButton.Tapped = (s, a) =>
-        {
-            s.Text = $"Clicked {++clickCount} times";
-            map.RefreshGraphics();
-            return false;
-        };
-        map.Widgets.Add(clickMeButton);
-
+        map.Widgets.Add(CreateButton("Tap me", VerticalAlignment.Top, HorizontalAlignment.Left, (s, a) =>
+            {
+                s.Text = $"Tapped {++_clickCount} times";
+                map.RefreshGraphics();
+                return false;
+            }));
         map.Widgets.Add(CreateButtonWithImage(VerticalAlignment.Top, HorizontalAlignment.Right));
-        map.Widgets.Add(CreateButton("Button with text", VerticalAlignment.Bottom, HorizontalAlignment.Right));
+        map.Widgets.Add(CreateButton("Hello!", VerticalAlignment.Bottom, HorizontalAlignment.Right, (s, a) =>
+            {
+                s.Text = $"{s.Text}!";
+                map.RefreshGraphics();
+                return false;
+            }));
         map.Widgets.Add(CreateButtonWithImage(VerticalAlignment.Bottom, HorizontalAlignment.Left));
 
         return Task.FromResult(map);
     }
 
-    private static ButtonWidget CreateButton(string text, 
-        VerticalAlignment verticalAlignment, HorizontalAlignment horizontalAlignment) => new()
+    private static ButtonWidget CreateButton(string text, VerticalAlignment verticalAlignment, 
+        HorizontalAlignment horizontalAlignment, Func<ButtonWidget, WidgetEventArgs, bool> tapped) => new()
     {
         Text = text,
         VerticalAlignment = verticalAlignment,
@@ -50,6 +52,7 @@ public class ButtonSample : ISample
         CornerRadius = 8,
         BackColor = new Color(0, 123, 255),
         TextColor = Color.White,
+        Tapped = tapped
     };
 
     private static IconButtonWidget CreateButtonWithImage(
