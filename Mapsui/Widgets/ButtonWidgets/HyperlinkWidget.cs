c06e97c2e93db5ec42e77ada126ea8c68bbacb11
pauldendulk <pauldendulk@gmail.com>
2024-03-01 11:18:01 +0100

    Let HyperLinkWidget.Url be null, there is already a null check in the widget
    This removes a warning. A better solution may be to not add a widget at all. Even better is to not have a widget in the layer at all but just one attribution widget which shows the attributions in the currently selected layer

--- Mapsui/Widgets/ButtonWidgets/HyperlinkWidget.cs
+++ Mapsui/Widgets/ButtonWidgets/HyperlinkWidget.cs
@@ -5,12 +5,12 @@
 /// </summary>
 public class HyperlinkWidget : TextButtonWidget
 {
-    private string _url = string.Empty;
+    private string? _url = string.Empty;
 
     /// <summary>
     /// URL to open when Widget is clicked
     /// </summary>
-    public string Url
+    public string? Url
     {
         get => _url;
         set
