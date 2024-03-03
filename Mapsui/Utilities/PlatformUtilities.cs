b3f3a1adfea07ac81a244865df3029b05c6013cc
pauldendulk <pauldendulk@gmail.com>
2024-03-03 07:36:59 +0100

    Add PlatformUtilities to be called from hyperlink

---
+++ Mapsui/Utilities/PlatformUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Mapsui.Utilities;
+
+public static class PlatformUtilities
+{
+    private static Func<Task> OpenInBrowserFunc { get; set; } = () => throw new Exception(
+        $"The '{nameof(OpenInBrowserFunc)}' method needs to be assigned in the MapControl constructor with the platform" +
+        $" specific implementation before calling it");
+
+    public static void SetOpenInBrowserFunc(Func<Task> openInBrowserFunc)
+    {
+        ArgumentNullException.ThrowIfNull(openInBrowserFunc, nameof(openInBrowserFunc));
+        OpenInBrowserFunc = openInBrowserFunc;
+    }
+
+    public static async Task OpenInBrowserAsync(string url)
+    {
+        ArgumentNullException.ThrowIfNull(url, nameof(url));
+        await OpenInBrowserFunc();
+    }
+}
