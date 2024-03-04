8ff0bf667507f93ed7f8a9480cae9328fc8c64c8
pauldendulk <pauldendulk@gmail.com>
2024-03-04 16:54:15 +0100

    Remove #if DEBUG - it cases an unused using error
    Instead the log level could have been changes, I felt in this case this was not necessary anymore, this kind of thing should be in metrics.

--- Mapsui/Layers/RasterizingLayer.cs
+++ Mapsui/Layers/RasterizingLayer.cs
@@ -3,7 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using Mapsui.Fetcher;
-using Mapsui.Logging;
+//using Mapsui.Logging;
 using Mapsui.Rendering;
 using Mapsui.Styles;
 
@@ -94,9 +94,6 @@ public class RasterizingLayer : BaseLayer, IAsyncDataFetcher, ISourceLayer
                 var features = new RasterFeature[1];
                 features[0] = new RasterFeature(new MRaster(bitmapStream.ToArray(), _currentSection.Extent));
                 _cache.PushRange(features);
-#if DEBUG
-                Logger.Log(LogLevel.Debug, $"Memory after rasterizing layer {GC.GetTotalMemory(true):N0}");
-#endif
                 OnDataChanged(new DataChangedEventArgs());
 
                 if (_modified && _layer is IAsyncDataFetcher asyncDataFetcher)
