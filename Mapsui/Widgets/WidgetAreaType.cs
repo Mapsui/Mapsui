78d72af9735d01657965033639847323c359d9c5
pauldendulk <pauldendulk@gmail.com>
2024-03-10 13:53:08 +0100

    Update comment

--- Mapsui/Widgets/WidgetAreaType.cs
+++ Mapsui/Widgets/WidgetAreaType.cs
@@ -3,11 +3,11 @@
 public enum WidgetAreaType
 {
     /// <summary>
-    /// Will be called for manipulations (or hover) over within the widget area.
+    /// Events will be called for interactions within the widget area.
     /// </summary>
     Widget,
     /// <summary>
-    /// Will be called for manipulations (or hover) over the entire map.
+    /// Events Will be called for interactions over the entire map.
     /// </summary>
     Map
 }
