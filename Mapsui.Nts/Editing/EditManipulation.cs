01127591d5b44b2140cf2a0f6284ccb4dae828ed
pauldendulk <pauldendulk@gmail.com>
2024-03-11 11:16:18 +0100

    Move to methods so that they can be called directly

--- Mapsui.Nts/Editing/EditManipulation.cs
+++ Mapsui.Nts/Editing/EditManipulation.cs
@@ -23,81 +23,94 @@ public class EditManipulation
     public bool Manipulate(PointerState mouseState, ScreenPosition screenPosition,
         EditManager editManager, IMapControl mapControl, WidgetEventArgs e)
     {
-        switch (mouseState)
+        return mouseState switch
         {
-            case PointerState.Tapped:
-                if (editManager.EditMode == EditMode.Modify)
-                    editManager.StopDragging();
-                if (editManager.EditMode == EditMode.Rotate)
-                    editManager.StopRotating();
-                if (editManager.EditMode == EditMode.Scale)
-                    editManager.StopScaling();
+            PointerState.Tapped => OnTapped(screenPosition, editManager, mapControl, e),
+            PointerState.Down => OnPointerPressed(screenPosition, editManager, mapControl),
+            PointerState.Dragging => OnPointerMoved(screenPosition, editManager, mapControl, false),
+            PointerState.Hovering => OnPointerMoved(screenPosition, editManager, mapControl, true),
+            PointerState.Up => OnPointerReleased(editManager),
+            _ => throw new Exception(),
+        };
+    }
 
-                if (editManager.EditMode == EditMode.Modify)
-                {
-                    if (e.Shift || e.TapType == TapType.Double)
-                    {
-                        return editManager.TryDeleteCoordinate(
-                            mapControl.GetMapInfo(screenPosition, editManager.VertexRadius), editManager.VertexRadius);
-                    }
-                    return editManager.TryInsertCoordinate(
-                        mapControl.GetMapInfo(screenPosition, editManager.VertexRadius));
-                }
-                else if (editManager.EditMode == EditMode.DrawingPolygon || editManager.EditMode == EditMode.DrawingLine)
-                {
-                    if (e.Shift || e.TapType == TapType.Double)
-                    {
-                        return editManager.EndEdit();
-                    }
-                }
-                return editManager.AddVertex(mapControl.Map.Navigator.Viewport.ScreenToWorld(screenPosition).ToCoordinate());
-
-            case PointerState.Down:
-                {
-                    // Take into account VertexRadius in feature select, because the objective
-                    // is to select the vertex.
-                    var mapInfo = mapControl.GetMapInfo(screenPosition, editManager.VertexRadius);
-                    if (editManager.EditMode == EditMode.Modify && mapInfo?.Feature != null)
-                    {
-                        return editManager.StartDragging(mapInfo, editManager.VertexRadius);
-                    }
-                    if (editManager.EditMode == EditMode.Rotate && mapInfo?.Feature != null)
-                    {
-                        return editManager.StartRotating(mapInfo);
-                    }
-                    if (editManager.EditMode == EditMode.Scale && mapInfo?.Feature != null)
-                    {
-                        return editManager.StartScaling(mapInfo);
-                    }
-                    return false;
-                }
-            case PointerState.Dragging:
-                {
-                    var args = mapControl.GetMapInfo(screenPosition);
-                    if (editManager.EditMode == EditMode.Modify)
-                        return editManager.Dragging(args?.WorldPosition?.ToPoint());
-                    if (editManager.EditMode == EditMode.Rotate)
-                        return editManager.Rotating(args?.WorldPosition?.ToPoint());
-                    if (editManager.EditMode == EditMode.Scale)
-                        return editManager.Scaling(args?.WorldPosition?.ToPoint());
-
-                    return false;
-                }
-            case PointerState.Hovering:
-                editManager.HoveringVertex(mapControl.GetMapInfo(screenPosition));
-                return false;
-            case PointerState.Up:
-                if (editManager.IsManipulating())
-                {
-                    // The EditingWidget captures the pointer when scaling or rotating.
-                    // It does this in an implicit way, by setting
-                    // the state of info classes. Resetting it releases the capture. 
-                    editManager.ResetManipulations();
-                    return true;
-                }
-                return false;
-            default:
-                throw new Exception();
+    private static bool OnPointerReleased(EditManager editManager)
+    {
+        if (editManager.IsManipulating())
+        {
+            // The EditingWidget captures the pointer when scaling or rotating.
+            // It does this in an implicit way, by setting
+            // the state of info classes. Resetting it releases the capture. 
+            editManager.ResetManipulations();
+            return true;
         }
+        return false;
+    }
+
+    private static bool OnPointerMoved(ScreenPosition screenPosition, EditManager editManager, IMapControl mapControl, bool isHovering)
+    {
+        if (isHovering)
+        {
+            editManager.HoveringVertex(mapControl.GetMapInfo(screenPosition));
+            return false;
+        }
+
+        var args = mapControl.GetMapInfo(screenPosition);
+        if (editManager.EditMode == EditMode.Modify)
+            return editManager.Dragging(args?.WorldPosition?.ToPoint());
+        if (editManager.EditMode == EditMode.Rotate)
+            return editManager.Rotating(args?.WorldPosition?.ToPoint());
+        if (editManager.EditMode == EditMode.Scale)
+            return editManager.Scaling(args?.WorldPosition?.ToPoint());
+        return false;
+    }
+
+    private static bool OnPointerPressed(ScreenPosition screenPosition, EditManager editManager, IMapControl mapControl)
+    {
+        // Take into account VertexRadius in feature select, because the objective
+        // is to select the vertex.
+        var mapInfo = mapControl.GetMapInfo(screenPosition, editManager.VertexRadius);
+        if (editManager.EditMode == EditMode.Modify && mapInfo?.Feature != null)
+        {
+            return editManager.StartDragging(mapInfo, editManager.VertexRadius);
+        }
+        if (editManager.EditMode == EditMode.Rotate && mapInfo?.Feature != null)
+        {
+            return editManager.StartRotating(mapInfo);
+        }
+        if (editManager.EditMode == EditMode.Scale && mapInfo?.Feature != null)
+        {
+            return editManager.StartScaling(mapInfo);
+        }
+        return false;
+    }
+
+    private static bool OnTapped(ScreenPosition screenPosition, EditManager editManager, IMapControl mapControl, WidgetEventArgs e)
+    {
+        if (editManager.EditMode == EditMode.Modify)
+            editManager.StopDragging();
+        if (editManager.EditMode == EditMode.Rotate)
+            editManager.StopRotating();
+        if (editManager.EditMode == EditMode.Scale)
+            editManager.StopScaling();
+
+        if (editManager.EditMode == EditMode.Modify)
+        {
+            if (e.Shift || e.TapType == TapType.Double)
+            {
+                return editManager.TryDeleteCoordinate(
+                    mapControl.GetMapInfo(screenPosition, editManager.VertexRadius), editManager.VertexRadius);
+            }
+            return editManager.TryInsertCoordinate(
+                mapControl.GetMapInfo(screenPosition, editManager.VertexRadius));
+        }
+        else if (editManager.EditMode == EditMode.DrawingPolygon || editManager.EditMode == EditMode.DrawingLine)
+        {
+            if (e.Shift || e.TapType == TapType.Double)
+            {
+                return editManager.EndEdit();
+            }
+        }
+        return editManager.AddVertex(mapControl.Map.Navigator.Viewport.ScreenToWorld(screenPosition).ToCoordinate());
     }
 }
