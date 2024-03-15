42e32a120349e2dddeee09e78ba33dbecd7b7403
pauldendulk <pauldendulk@gmail.com>
2024-03-15 15:07:37 +0100

    Fix samples for deploy to v5 folder

--- Scripts/BuildSamplesWebsite.ps1
+++ Scripts/BuildSamplesWebsite.ps1
@@ -2,7 +2,7 @@
 # build Samples website to release folder
 dotnet publish -c Release Samples\Mapsui.Samples.Blazor\Mapsui.Samples.Blazor.csproj -o release --nologo
 # change base url
-(Get-Content -Path "release/wwwroot/index.html") -replace '<base href="/" \/>', '<base href="/samples/" />' | Set-Content -Path "release/wwwroot/index.html"
+(Get-Content -Path "release/wwwroot/index.html") -replace '<base href="/" \/>', '<base href="/samples/v5/" />' | Set-Content -Path "release/wwwroot/index.html"
 # no jekill so that everything gets deployed
 New-Item -ItemType File -Path "release/wwwroot/.nojekyll" -Force
 # fix 404 errors
