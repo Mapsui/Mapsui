65c14639df73f3e378ec676f0e1d0dc6093cca64
pauldendulk <pauldendulk@gmail.com>
2024-03-15 12:12:40 +0100

    Try to deploy website to v5 folder

--- docfx/build-site.cmd
+++ docfx/build-site.cmd
@@ -10,4 +10,4 @@ mkdir website
 ECHO COPY CNAME file which is used by github to determine the domain name.
 copy docfx\CNAME website /Y || exit /b 
 ECHO COPY _site TO website
-xcopy docfx\_site website /E /Y || exit /b 
+xcopy docfx\_site website\v5\  /E /Y || exit /b 
