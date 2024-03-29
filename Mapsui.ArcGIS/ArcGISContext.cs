5800fa4c1e2c72de7cd8fdd7dfedbe7de4a55a3f
Inforithmics <thomas.stocker@gmail.com>
2024-03-29 08:13:37 +0100

    better jsonoptions

--- Mapsui.ArcGIS/ArcGISContext.cs
+++ Mapsui.ArcGIS/ArcGISContext.cs
@@ -1,9 +1,16 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 using Mapsui.ArcGIS.DynamicProvider;
 using Mapsui.ArcGIS.ImageServiceProvider;
 
 namespace Mapsui.ArcGIS;
 
+[JsonSourceGenerationOptions(
+    PropertyNameCaseInsensitive = true,
+    UnmappedMemberHandling = JsonUnmappedMemberHandling.Skip,
+    NumberHandling = JsonNumberHandling.AllowReadingFromString | JsonNumberHandling.AllowNamedFloatingPointLiterals,
+    ReadCommentHandling = JsonCommentHandling.Skip,
+    AllowTrailingCommas = true)]
 [JsonSerializable(typeof(ArcGISFeatureInfo))]
 [JsonSerializable(typeof(ArcGISLegendResponse))]
 [JsonSerializable(typeof(ArcGISDynamicCapabilities))]
