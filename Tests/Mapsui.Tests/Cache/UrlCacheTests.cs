afb8be4ff3cd0d59fc0d980f5dd2bcf60e0dccb4
Inforithmics <thomas.stocker@gmail.com>
2024-04-08 20:31:22 +0200

    added url cache test without postdata

--- Tests/Mapsui.Tests/Cache/UrlCacheTests.cs
+++ Tests/Mapsui.Tests/Cache/UrlCacheTests.cs
@@ -22,4 +22,21 @@ public class UrlCacheTests
         var notfound = cache.Find("https://test.com", null);
         Assert.That(notfound == null);
     }
+
+    [Test]
+    public void CacheKeyWorks()
+    {
+        var cache = new SqlitePersistentCache("testCache");
+        cache.Add("https://test.com", null, new byte[] { 1, 1, 1 });
+
+        var found = cache.Find("https://test.com", null);
+        Assert.That(found != null);
+        Assert.That(found.Length == 3);
+        Assert.That(found[0] == 1);
+        Assert.That(found[1] == 1);
+        Assert.That(found[2] == 1);
+
+        var notfound = cache.Find("https://test.com", new byte[] { 1, 2, 3 });
+        Assert.That(notfound == null);
+    }
 }
