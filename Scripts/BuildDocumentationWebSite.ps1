c3c8bbb0a571e616b5ac7c3c29129bbd863642f8
pauldendulk <pauldendulk@gmail.com>
2024-04-12 21:03:42 +0200

    Update scripts and sln for changed folder names

--- Scripts/BuildDocumentationWebSite.ps1
+++ Scripts/BuildDocumentationWebSite.ps1
@@ -2,13 +2,13 @@
 # winget install Microsoft.DotNet.SDK.6 --silent
 
 Write-Output "Building documentation using MkDocs"
-mkdocs build -f docfx/mkdocs.yml
+mkdocs build -f docs/mkdocs.yml
 
 Write-Output "Installing DocFX"
 dotnet tool update -g docfx --version 2.75.3
 
-Write-Output "Generating website in docfx\_site folder"
-docfx docfx\docfx.json
+Write-Output "Generating website in docs\api\_site folder"
+docfx docs\api\docfx.json
 
 Write-Output "Deleting existing website folder and contents"
 Remove-Item -Path "website" -Recurse -Force -ErrorAction SilentlyContinue
@@ -17,7 +17,7 @@ Write-Output "Creating a new website folder"
 New-Item -ItemType Directory -Path "website"
 
 Write-Output "Copying CNAME file to website. This is necessary for the mapsui.com domain"
-Copy-Item -Path "docfx\CNAME" -Destination "website" -Force
+Copy-Item -Path "docs\CNAME" -Destination "website" -Force
 
 Write-Output "Copying _site to website"
-Copy-Item -Path "docfx\_site\*" -Destination "website" -Recurse -Force
+Copy-Item -Path "docs\_site\*" -Destination "website" -Recurse -Force
