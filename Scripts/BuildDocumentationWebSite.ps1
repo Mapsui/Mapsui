e19e51d20ccc70a42ffc8855da2f6ec1539ee9ec
pauldendulk <pauldendulk@gmail.com>
2024-04-13 08:37:41 +0200

    Update script for changed locations

--- Scripts/BuildDocumentationWebSite.ps1
+++ Scripts/BuildDocumentationWebSite.ps1
@@ -1,23 +1,28 @@
 # Install .NET 6 SDK using winget (uncomment if needed)
-# winget install Microsoft.DotNet.SDK.6 --silent
+// Currently mkdocs needs to be installed manually. Perhaps it is better to install it in this script.
 
-Write-Output "Building documentation using MkDocs"
-mkdocs build -f docs/mkdocs.yml
+Write-Output "Create general documentation with mkdocs in 'docs/general/_site'"
+mkdocs build -f docs/general/mkdocs.yml
 
 Write-Output "Installing DocFX"
 dotnet tool update -g docfx --version 2.75.3
 
-Write-Output "Generating website in docs\api\_site folder"
-docfx docs\api\docfx.json
+Write-Output "Create api documentation with docfx in 'docs/api/_site'"
+docfx docs/api/docfx.json
 
 Write-Output "Deleting existing 'website'' folder and contents"
 Remove-Item -Path "website" -Recurse -Force -ErrorAction SilentlyContinue
 
-Write-Output "Creating a new 'website' folder"
+Write-Output "Creating a new 'website' and 'website/api' folder"
 New-Item -ItemType Directory -Path "website"
+New-Item -ItemType Directory -Path "website/api"
 
 Write-Output "Copying CNAME file to 'website'. This is necessary for the mapsui.com domain"
-Copy-Item -Path "docs\CNAME" -Destination "website" -Force
+Copy-Item -Path "docs/CNAME" -Destination "website" -Force
 
-Write-Output "Copying '_site' to 'website''"
-Copy-Item -Path "docs\_site\*" -Destination "website" -Recurse -Force
+Write-Output "Copying general '_site' to 'website'"
+Copy-Item -Path "docs/general/_site/*" -Destination "website" -Recurse -Force
+
+Write-Output "Copying api '_site' to 'website'"
+Copy-Item -Path "docs/api/_site/*" -Destination "website/api" -Recurse -Force
+
