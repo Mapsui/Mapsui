30944de15b3b3518acbe1ed541f2004bc8b5f0cf
pauldendulk <pauldendulk@gmail.com>
2024-04-13 08:06:04 +0200

    Moving docs files around

--- Scripts/BuildDocumentationWebSite.ps1
+++ Scripts/BuildDocumentationWebSite.ps1
@@ -10,14 +10,14 @@ dotnet tool update -g docfx --version 2.75.3
 Write-Output "Generating website in docs\api\_site folder"
 docfx docs\api\docfx.json
 
-Write-Output "Deleting existing website folder and contents"
+Write-Output "Deleting existing 'website'' folder and contents"
 Remove-Item -Path "website" -Recurse -Force -ErrorAction SilentlyContinue
 
-Write-Output "Creating a new website folder"
+Write-Output "Creating a new 'website' folder"
 New-Item -ItemType Directory -Path "website"
 
-Write-Output "Copying CNAME file to website. This is necessary for the mapsui.com domain"
+Write-Output "Copying CNAME file to 'website'. This is necessary for the mapsui.com domain"
 Copy-Item -Path "docs\CNAME" -Destination "website" -Force
 
-Write-Output "Copying _site to website"
+Write-Output "Copying '_site' to 'website''"
 Copy-Item -Path "docs\_site\*" -Destination "website" -Recurse -Force
