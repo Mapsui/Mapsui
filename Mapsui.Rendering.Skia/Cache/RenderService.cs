38334b2799c617fd0b7429288ab3ca703bb2adc0
Inforithmics <thomas.stocker@gmail.com>
2024-04-20 14:39:37 +0200

    add bitmap regsitry to render service

--- Mapsui.Rendering.Skia/Cache/RenderService.cs
+++ Mapsui.Rendering.Skia/Cache/RenderService.cs
@@ -1,17 +1,21 @@
-﻿namespace Mapsui.Rendering.Skia.Cache;
+﻿using Mapsui.Styles;
+
+namespace Mapsui.Rendering.Skia.Cache;
 
 public sealed class RenderService : IRenderService
 {
     public RenderService(int capacity = 10000)
     {
+        BitmapRegistry = new BitmapRegistry(Styles.BitmapRegistry.Instance);
         SymbolCache = new SymbolCache();
         VectorCache = new VectorCache(SymbolCache, capacity);
         TileCache = new TileCache();
         LabelCache = new LabelCache();
     }
 
+    public IBitmapRegistry BitmapRegistry { get; }
     public ISymbolCache SymbolCache { get; }
-    public IVectorCache VectorCache { get; private set; }
+    public IVectorCache VectorCache { get; }
     public ITileCache TileCache { get; }
     public ILabelCache LabelCache { get; }
 
@@ -21,5 +25,6 @@ public sealed class RenderService : IRenderService
         SymbolCache.Dispose();
         VectorCache.Dispose();
         TileCache.Dispose();
+        BitmapRegistry.Dispose();
     }
 }
