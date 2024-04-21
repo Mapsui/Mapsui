3c0b0ba126d832ca8ca675780b0eac8b3fa398ac
Inforithmics <thomas.stocker@gmail.com>
2024-04-21 19:28:04 +0200

    Remove unneded SkiaSharp nugets

--- Samples/Mapsui.Samples.Maui.MapView/MauiProgram.cs
+++ Samples/Mapsui.Samples.Maui.MapView/MauiProgram.cs
@@ -10,7 +10,7 @@ public static class MauiProgram
     {
         var builder = MauiApp.CreateBuilder();
         builder
-            .UseSkiaSharp(true)
+            .UseSkiaSharp()
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
             {
