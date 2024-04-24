39f26a4b9ec47e56244e3872e463ee4678529e09
Inforithmics <thomas.stocker@gmail.com>
2024-04-24 22:06:29 +0200

    remove hierarchical unit tests

--- Tests/Mapsui.Tests/Styles/BitmapRegistryTests.cs
+++ Tests/Mapsui.Tests/Styles/BitmapRegistryTests.cs
@@ -28,35 +28,6 @@ public static class BitmapRegistryTests
     }
 
     [Test]
-    public static void HierarchicalGet()
-    {
-        // Arrange
-        using var stream = new MemoryStream();
-        var bitmapId = BitmapRegistry.Instance.Register(stream);
-
-        // Act
-        using var childRegistry = new BitmapRegistry(BitmapRegistry.Instance);
-
-        // Assert
-        Assert.That(() => childRegistry.Get(bitmapId) != null);
-    }
-
-    [Test]
-    public static void HierarchicalAddAndRemoveEntry()
-    {
-        // Arrange
-        using var stream = new MemoryStream();
-        var bitmapId = BitmapRegistry.Instance.Register(stream);
-
-        // Act
-        using var childRegistry = new BitmapRegistry(BitmapRegistry.Instance);
-        childRegistry.Unregister(bitmapId);
-
-        // Assert
-        Assert.Throws<KeyNotFoundException>(() => childRegistry.Get(bitmapId));
-    }
-
-    [Test]
     public static async Task UriResourceEmbeddedRegisterAsyncAddAndRemove()
     {
         // Arrange
