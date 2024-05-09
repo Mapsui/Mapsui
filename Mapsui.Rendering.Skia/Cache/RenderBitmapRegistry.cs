2ce2f2dc59d559dad2794daf1796b2104eeed358
Inforithmics <thomas.stocker@gmail.com>
2024-05-09 18:23:37 +0200

    use disposed for detecting if I'm disposed

--- Mapsui.Rendering.Skia/Cache/RenderBitmapRegistry.cs
+++ Mapsui.Rendering.Skia/Cache/RenderBitmapRegistry.cs
@@ -7,11 +7,12 @@ namespace Mapsui.Rendering.Skia.Cache;
 
 public sealed class RenderBitmapRegistry(BitmapRegistry instance) : IBitmapRegistry
 {
-    private ConcurrentHashSet<int>? _createdImages = new();
+    private bool _disposed;
+    private readonly ConcurrentHashSet<int> _createdImages = new();
 
     public int Register(object bitmapData, string? key = null)
     {
-        if (_createdImages == null) throw new ObjectDisposedException(nameof(RenderBitmapRegistry));
+        if (_disposed) throw new ObjectDisposedException(nameof(RenderBitmapRegistry));
 
         var result = instance.Register(bitmapData, key);
         _createdImages.Add(result);
@@ -20,7 +21,7 @@ public sealed class RenderBitmapRegistry(BitmapRegistry instance) : IBitmapRegis
 
     public async Task<int> RegisterAsync(Uri bitmapPath)
     {
-        if (_createdImages == null) throw new ObjectDisposedException(nameof(RenderBitmapRegistry));
+        if (_disposed) throw new ObjectDisposedException(nameof(RenderBitmapRegistry));
 
         var result = await instance.RegisterAsync(bitmapPath).ConfigureAwait(false);
         _createdImages.Add(result);
@@ -29,7 +30,7 @@ public sealed class RenderBitmapRegistry(BitmapRegistry instance) : IBitmapRegis
 
     public object? Unregister(int id)
     {
-        if (_createdImages == null) throw new ObjectDisposedException(nameof(RenderBitmapRegistry));
+        if (_disposed) throw new ObjectDisposedException(nameof(RenderBitmapRegistry));
 
         instance.Unregister(id);
         return _createdImages.TryRemove(id);
@@ -37,27 +38,29 @@ public sealed class RenderBitmapRegistry(BitmapRegistry instance) : IBitmapRegis
 
     public object Get(int id)
     {
-        if (_createdImages == null) throw new ObjectDisposedException(nameof(RenderBitmapRegistry));
+        if (_disposed) throw new ObjectDisposedException(nameof(RenderBitmapRegistry));
 
         return instance.Get(id);
     }
 
     public bool Update(int id, object bitmapData)
     {
-        if (_createdImages == null) throw new ObjectDisposedException(nameof(RenderBitmapRegistry));
+        if (_disposed) throw new ObjectDisposedException(nameof(RenderBitmapRegistry));
 
         return instance.Update(id, bitmapData);
     }
 
     public bool TryGetBitmapId(string key, out int bitmapId)
     {
-        if (_createdImages == null) throw new ObjectDisposedException(nameof(RenderBitmapRegistry));
+        if (_disposed) throw new ObjectDisposedException(nameof(RenderBitmapRegistry));
 
         return instance.TryGetBitmapId(key, out bitmapId);
     }
 
     public void Dispose()
     {
+        _disposed = true;
+        
         // unregister the created images
         foreach (var id in _createdImages)
         {
@@ -69,6 +72,5 @@ public sealed class RenderBitmapRegistry(BitmapRegistry instance) : IBitmapRegis
         }
 
         _createdImages.Clear();
-        _createdImages = null;
     }
 }
