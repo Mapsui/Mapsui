ebc449fc608a67fef1d5199cf6577b0e07cd13ce
pauldendulk <pauldendulk@gmail.com>
2024-05-25 08:19:21 +0200

    Create BitmapImage from stream

--- Mapsui.Rendering.Skia/Images/BitmapImage.cs
+++ Mapsui.Rendering.Skia/Images/BitmapImage.cs
@@ -1,14 +1,28 @@
-﻿using SkiaSharp;
+﻿using Mapsui.Extensions;
+using SkiaSharp;
+using System.IO;
 
 namespace Mapsui.Rendering.Skia.Images;
 
-internal sealed class BitmapImage(SKImage skImage) : IDrawableImage
+internal sealed class BitmapImage : IDrawableImage
 {
     private bool _disposed;
+    private readonly SKImage _image;
 
-    public SKImage Image => skImage;
-    public float Width => skImage.Width;
-    public float Height => skImage.Height;
+    public BitmapImage(SKImage image)
+    {
+        _image = image;
+    }
+
+    public BitmapImage(Stream stream)
+    {
+        using var skData = SKData.CreateCopy(stream.ToBytes());
+        _image = SKImage.FromEncodedData(skData);
+    }
+
+    public SKImage Image => _image;
+    public float Width => _image.Width;
+    public float Height => _image.Height;
 
     public bool IsDisposed() => _disposed;
 
@@ -19,7 +33,7 @@ internal sealed class BitmapImage(SKImage skImage) : IDrawableImage
             return;
         }
 
-        skImage.Dispose();
+        _image.Dispose();
 
         _disposed = true;
     }
--- Mapsui.Rendering.Skia/Images/ImageHelper.cs
+++ Mapsui.Rendering.Skia/Images/ImageHelper.cs
@@ -1,6 +1,5 @@
 using System.IO;
 using Mapsui.Extensions;
-using SkiaSharp;
 
 namespace Mapsui.Rendering.Skia.Images;
 
@@ -9,12 +8,7 @@ internal static class ImageHelper
     public static IDrawableImage? LoadBitmap(Stream stream)
     {
         if (stream.IsSvg())
-        {
             return new SvgImage(stream);
-        }
-
-        using var skData = SKData.CreateCopy(stream.ToBytes());
-        var image = SKImage.FromEncodedData(skData);
-        return new BitmapImage(image);
+        return new BitmapImage(stream);
     }
 }
