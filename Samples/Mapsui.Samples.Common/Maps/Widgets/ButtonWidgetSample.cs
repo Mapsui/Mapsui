02eea79c3930728698f40a078ed731a48e5087b9
pauldendulk <pauldendulk@gmail.com>
2024-05-27 10:13:02 +0200

    Add tap to ImageButtonWidget

--- Samples/Mapsui.Samples.Common/Maps/Widgets/ButtonWidgetSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Widgets/ButtonWidgetSample.cs
@@ -1,4 +1,5 @@
 ﻿using Mapsui.Extensions;
+using Mapsui.Logging;
 using Mapsui.Manipulations;
 using Mapsui.Styles;
 using Mapsui.Tiling;
@@ -11,11 +12,12 @@ namespace Mapsui.Samples.Common.Maps.Widgets;
 
 public class ButtonWidgetSample : ISample
 {
-    public string Name => "Button";
-    public string Category => "Widgets";
-
     private int _tapCount;
     private int _doubleTapCount;
+    private int _imageTapCount;
+
+    public string Name => "Button";
+    public string Category => "Widgets";
 
     public Task<Map> CreateMapAsync()
     {
@@ -23,22 +25,27 @@ public class ButtonWidgetSample : ISample
 
         map.Layers.Add(OpenStreetMap.CreateTileLayer());
 
-        map.Widgets.Add(CreateButton("Tap me", VerticalAlignment.Top, HorizontalAlignment.Left, (s, a) =>
-            {
-                if (a.TapType == TapType.Double)
-                    return false;
-                s.Text = $"Tapped {++_tapCount} times";
-                map.RefreshGraphics();
+        map.Widgets.Add(CreateButtonWidget("Tap me", VerticalAlignment.Top, HorizontalAlignment.Left, (s, a) =>
+        {
+            if (a.TapType == TapType.Double)
                 return false;
-            }));
-        map.Widgets.Add(CreateImageButtonWidget(VerticalAlignment.Top, HorizontalAlignment.Right));
-        map.Widgets.Add(CreateButton("Hello!", VerticalAlignment.Bottom, HorizontalAlignment.Right, (s, a) =>
-            {
-                s.Text = $"{s.Text}!";
-                map.RefreshGraphics();
-                return false;
-            }));
-        map.Widgets.Add(CreateButton("Double Tap me", VerticalAlignment.Bottom, HorizontalAlignment.Left, (s, a) =>
+            s.Text = $"Tapped {++_tapCount} times";
+            map.RefreshGraphics();
+            return false;
+        }));
+        map.Widgets.Add(CreateImageButtonWidget(VerticalAlignment.Top, HorizontalAlignment.Right, (s, a) =>
+        {
+            Logger.Log(LogLevel.Information, $"Image Tapped {++_imageTapCount} times");
+            map.RefreshGraphics();
+            return false;
+        }));
+        map.Widgets.Add(CreateButtonWidget("Hello!", VerticalAlignment.Bottom, HorizontalAlignment.Right, (s, a) =>
+        {
+            s.Text = $"{s.Text}!";
+            map.RefreshGraphics();
+            return false;
+        }));
+        map.Widgets.Add(CreateButtonWidget("Double Tap me", VerticalAlignment.Bottom, HorizontalAlignment.Left, (s, a) =>
         {
             if (a.TapType == TapType.Single)
                 return false;
@@ -50,7 +57,7 @@ public class ButtonWidgetSample : ISample
         return Task.FromResult(map);
     }
 
-    private static ButtonWidget CreateButton(string text, VerticalAlignment verticalAlignment,
+    private static ButtonWidget CreateButtonWidget(string text, VerticalAlignment verticalAlignment,
         HorizontalAlignment horizontalAlignment, Func<ButtonWidget, WidgetEventArgs, bool> tapped) => new()
         {
             Text = text,
@@ -64,8 +71,8 @@ public class ButtonWidgetSample : ISample
             Tapped = tapped
         };
 
-    private static ImageButtonWidget CreateImageButtonWidget(
-        VerticalAlignment verticalAlignment, HorizontalAlignment horizontalAlignment) => new()
+    private static ImageButtonWidget CreateImageButtonWidget(VerticalAlignment verticalAlignment,
+        HorizontalAlignment horizontalAlignment, Func<ImageButtonWidget, WidgetEventArgs, bool> tapped) => new()
         {
             ImageSource = "embedded://Mapsui.Resources.Images.MyLocationStill.svg",
             VerticalAlignment = verticalAlignment,
@@ -73,6 +80,7 @@ public class ButtonWidgetSample : ISample
             Margin = new MRect(30),
             Padding = new MRect(10, 8),
             CornerRadius = 8,
-            Envelope = new MRect(0, 0, 64, 64)
+            Envelope = new MRect(0, 0, 64, 64),
+            Tapped = tapped
         };
 }
