519f62301815e87b79a2544406fffaeecd68409e
Inforithmics <thomas.stocker@gmail.com>
2024-06-21 21:36:01 +0200

    Revert "_parentExtender should not be global, because then only one GeoTiffProvider works"
    This reverts commit bdecc70b71eed385c0542c2b290575e4219a8026.

--- Mapsui.Extensions/Provider/GeoTiffProvider.cs
+++ Mapsui.Extensions/Provider/GeoTiffProvider.cs
@@ -108,9 +108,9 @@ public class GeoTiffProvider : IProvider, IDisposable
     private const TiffTag TIFFTAG_ModelPixelScaleTag = (TiffTag)33550;
     private const TiffTag TIFFTAG_ModelTiepointTag = (TiffTag)33922;
 
-    private Tiff.TiffExtendProc? _parentExtender;
+    private static Tiff.TiffExtendProc? _parentExtender;
 
-    public void TagExtender(Tiff tif)
+    public static void TagExtender(Tiff tif)
     {
         TiffFieldInfo[] tiffFieldInfo =
         {
@@ -124,7 +124,7 @@ public class GeoTiffProvider : IProvider, IDisposable
             _parentExtender(tif);
     }
 
-    private TiffProperties LoadTiff(string location)
+    private static TiffProperties LoadTiff(string location)
     {
         TiffProperties tiffFileProperties;
 
