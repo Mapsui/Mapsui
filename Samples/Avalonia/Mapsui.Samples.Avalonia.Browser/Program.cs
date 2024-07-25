037124de34f764106cd6011426b75acf6d5126da
Inforithmics <thomas.stocker@gmail.com>
2024-07-25 22:26:16 +0200

    fixed warning in avalonia

--- Samples/Avalonia/Mapsui.Samples.Avalonia.Browser/Program.cs
+++ Samples/Avalonia/Mapsui.Samples.Avalonia.Browser/Program.cs
@@ -1,16 +1,15 @@
-﻿using System.Runtime.Versioning;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Avalonia;
+using Avalonia.Browser;
 using Avalonia.ReactiveUI;
 using Mapsui.Samples.Avalonia;
 
-[assembly: SupportedOSPlatform("browser")]
-
 internal partial class Program
 {
-    private static async Task Main(string[] args) => BuildAvaloniaApp()
+    private static Task Main(string[] args) => BuildAvaloniaApp()
         .WithInterFont()
-        .UseReactiveUI();
+        .UseReactiveUI()
+        .StartBrowserAppAsync("out");
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>();
