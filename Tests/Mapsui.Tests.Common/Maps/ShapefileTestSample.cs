afe987fd49301b0473458d3f043b9afec59c9439
Inforithmics <thomas.stocker@gmail.com>
2024-11-17 21:55:46 +0100

    Added Test Sample

---
+++ Tests/Mapsui.Tests.Common/Maps/ShapefileTestSample.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Mapsui.Nts.Providers.Shapefile;
+using Mapsui.Samples.Common;
+using Mapsui.Samples.Common.Utilities;
+
+namespace Mapsui.Tests.Common.Maps;
+
+public class ShapefileTestSample : ISample
+{
+    static ShapefileTestSample()
+    {
+        ShapeFilesDeployer.CopyEmbeddedResourceToFile("test.shp");
+    }
+
+    public string Name => "Shapefile Zoom";
+    public string Category => "Tests";
+
+    public Task<Map> CreateMapAsync() => Task.FromResult(CreateMap());
+
+    public static Map CreateMap()
+    {
+        var map = new Map();
+        
+        var shapeFilePath = Path.Combine(ShapeFilesDeployer.ShapeFilesLocation, "test.shp");
+        ShapeFile shpSource = new ShapeFile( shapeFilePath );
+        /* Apply basic styles */
+        Mapsui.Layers.Layer layer = new Mapsui.Layers.Layer() {
+            DataSource = shpSource,
+            Style = new Mapsui.Styles.VectorStyle {
+                Fill = new Mapsui.Styles.Brush( Mapsui.Styles.Color.FromArgb( 128, 0 , 255 , 0 ) ) ,
+                Line = new Mapsui.Styles.Pen( Mapsui.Styles.Color.FromString("#0969da") , 4 ){PenStrokeCap = Mapsui.Styles.PenStrokeCap.Round, StrokeJoin = Mapsui.Styles.StrokeJoin.Round}
+            }
+        };
+        /* Add the new layer */
+        map.Layers.Add( layer );
+
+        return map;
+    }
+}
