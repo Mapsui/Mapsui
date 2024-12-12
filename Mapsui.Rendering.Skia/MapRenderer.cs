89bf5952e8c2d048800144e992244f0072402b21
Inforithmics <thomas.stocker@gmail.com>
2024-12-12 23:50:43 +0100

    add not supported exceptoin when i cannod encode pixels

--- Mapsui.Rendering.Skia/MapRenderer.cs
+++ Mapsui.Rendering.Skia/MapRenderer.cs
@@ -126,7 +126,7 @@ public sealed class MapRenderer : IRenderer, IDisposable
                         using var image = surface.Snapshot();
                         var options = new SKPngEncoderOptions(SKPngEncoderFilterFlags.AllFilters, 9); // 9 is the highest compression
                         using var peekPixels = image.PeekPixels();
-                        using var data = peekPixels.Encode(options);
+                        using var data = peekPixels.Encode(options) ?? throw new NotSupportedException();;
                         data.SaveTo(memoryStream);
                         break;
                     }
@@ -141,7 +141,7 @@ public sealed class MapRenderer : IRenderer, IDisposable
                             : SKWebpEncoderCompression.Lossy;
                         var options = new SKWebpEncoderOptions(compression, quality);
                         using var peekPixels = image.PeekPixels();
-                        using var data = peekPixels.Encode(options);
+                        using var data = peekPixels.Encode(options) ?? throw new NotSupportedException();;
                         data.SaveTo(memoryStream);
                         break;
                     }
@@ -153,7 +153,7 @@ public sealed class MapRenderer : IRenderer, IDisposable
                         using var image = surface.Snapshot();
                         var options = new SKJpegEncoderOptions(quality, SKJpegEncoderDownsample.Downsample420, SKJpegEncoderAlphaOption.Ignore);
                         using var peekPixels = image.PeekPixels();
-                        using var data = peekPixels.Encode(options);
+                        using var data = peekPixels.Encode(options) ?? throw new NotSupportedException();;
                         data.SaveTo(memoryStream);
                         break;
                     }
