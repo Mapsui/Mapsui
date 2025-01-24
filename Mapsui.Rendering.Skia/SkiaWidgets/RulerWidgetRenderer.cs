6511df9f08b808f9321145f68151652ec95235ba
pauldendulk <pauldendulk@gmail.com>
2025-01-24 16:37:49 +0100

    Add RulerWidget

---
+++ Mapsui.Rendering.Skia/SkiaWidgets/RulerWidgetRenderer.cs
@@ -0,0 +1,53 @@
+﻿using Mapsui.Extensions;
+using Mapsui.Rendering.Skia.Cache;
+using Mapsui.Rendering.Skia.Extensions;
+using Mapsui.Widgets;
+using Mapsui.Widgets.InfoWidgets;
+using SkiaSharp;
+using System;
+
+namespace Mapsui.Rendering.Skia.SkiaWidgets;
+
+public class RulerWidgetRenderer : ISkiaWidgetRenderer
+{
+    private static readonly TextBoxWidgetRenderer _textBoxRenderer = new();
+
+    public void Draw(SKCanvas canvas, Viewport viewport, IWidget widget, RenderService renderService, float layerOpacity)
+    {
+        if (widget is RulerWidget measureWidget)
+        {
+            // Use all of the canvas
+            measureWidget.Envelope = new MRect(0, 0, viewport.Width, viewport.Height);
+
+            if (!measureWidget.IsActive)
+                return;
+
+            DrawLine(canvas, viewport, measureWidget, renderService, layerOpacity);
+        }
+        else
+            throw new Exception("Widget is not a MeasureWidget");
+    }
+
+    public static void DrawLine(SKCanvas canvas, Viewport viewport, RulerWidget measureWidget, RenderService renderService, float layerOpacity)
+    {
+        if (measureWidget.StartPosition is MPoint start && measureWidget.CurrentPosition is MPoint current && measureWidget.DistanceInKilometers is not null)
+        {
+            var distanceInMeters = measureWidget.DistanceInKilometers;
+            var formattedDistance = $"{distanceInMeters} km.";
+
+            var screenStart = viewport.WorldToScreen(start);
+            var screenCurrent = viewport.WorldToScreen(current);
+
+            measureWidget.TextBox.Text = $"Distance: {formattedDistance}";
+            measureWidget.TextBox.Margin = new MRect((int)screenCurrent.X, (int)(viewport.Height - screenCurrent.Y));
+            _textBoxRenderer.Draw(canvas, viewport, measureWidget.TextBox, renderService, layerOpacity);
+
+            // Use the envelope to draw
+            using var skPaint = new SKPaint { Color = measureWidget.Color.ToSkia(), StrokeWidth = 3, IsAntialias = true };
+            using var skPaintDots = new SKPaint { Color = measureWidget.ColorOfBeginAndEndDots.ToSkia(), StrokeWidth = 3, IsAntialias = true };
+            canvas.DrawCircle((float)screenStart.X, (float)screenStart.Y, 6, skPaintDots);
+            canvas.DrawCircle((float)screenCurrent.X, (float)screenCurrent.Y, 6, skPaintDots);
+            canvas.DrawLine((float)screenStart.X, (float)screenStart.Y, (float)screenCurrent.X, (float)screenCurrent.Y, skPaint);
+        }
+    }
+}
---
+++ Mapsui/Widgets/InfoWidgets/RulerWidget.cs
@@ -0,0 +1,120 @@
+﻿using Mapsui.Extensions;
+using Mapsui.Projections;
+using Mapsui.Styles;
+using System;
+using Mapsui.Widgets.BoxWidgets;
+using System.Collections.Generic;
+
+namespace Mapsui.Widgets.InfoWidgets;
+
+public class RulerWidget(Map map) : BaseWidget
+{
+    public enum TapType
+    {
+        Down,
+        Drag,
+        Hover,
+        Up
+    }
+
+    private readonly Map _map = map;
+
+    public Color Color { get; set; } = new Color(192, 30, 20, 255);
+    public Color ColorOfBeginAndEndDots { get; set; } = new Color(192, 30, 20, 128);
+    public MPoint? StartPosition { get; internal set; }
+    public MPoint? CurrentPosition { get; internal set; }
+    public double? DistanceInKilometers { get; internal set; }
+    public bool IsActive { get; set; }
+
+    public TextBoxWidget TextBox { get; set; } = new TextBoxWidget
+    {
+        VerticalAlignment = VerticalAlignment.Bottom,
+        HorizontalAlignment = HorizontalAlignment.Left,
+        BackColor = new Color(128, 128, 0, 128),
+        Padding = new MRect(6, 4),
+    };
+
+    public event EventHandler<MeasureWidgetUpdatedEventArgs>? DistanceUpdated = null;
+
+    public override bool OnPointerPressed(Navigator navigator, WidgetEventArgs e)
+    {
+        CurrentPosition = null;
+        StartPosition = _map.Navigator.Viewport.ScreenToWorld(e.ScreenPosition);
+        _map.RefreshGraphics();
+        return true;
+    }
+
+    public override bool OnPointerMoved(Navigator navigator, WidgetEventArgs e)
+    {
+        if (!e.LeftButton)
+            return false; // Not dragging.
+
+        CurrentPosition = _map.Navigator.Viewport.ScreenToWorld(e.ScreenPosition);
+        DistanceInKilometers = GetDistance(StartPosition, CurrentPosition);
+        DistanceUpdated?.Invoke(this, new MeasureWidgetUpdatedEventArgs(e.LeftButton ? TapType.Drag : TapType.Hover));
+        _map.RefreshGraphics();
+        return true;
+    }
+
+    public override bool OnPointerReleased(Navigator navigator, WidgetEventArgs e)
+    {
+        DistanceUpdated?.Invoke(this, new MeasureWidgetUpdatedEventArgs(TapType.Up));
+        _map.RefreshGraphics();
+        return true;
+    }
+
+    public override bool OnTapped(Navigator navigator, WidgetEventArgs e)
+    {
+        if (e.TapType == Manipulations.TapType.Single)
+        {
+            StartPosition = _map.Navigator.Viewport.ScreenToWorld(e.ScreenPosition);
+            CurrentPosition = null;
+            DistanceUpdated?.Invoke(this, new MeasureWidgetUpdatedEventArgs(TapType.Down));
+            _map.RefreshGraphics();
+        }
+        return true;
+    }
+
+    public void Reset()
+    {
+        StartPosition = null;
+        CurrentPosition = null;
+    }
+
+    private static double? GetDistance(MPoint? fromPosition, MPoint? toPosition)
+    {
+        if (fromPosition == null || toPosition == null)
+            return null;
+
+        var fromLatLon = SphericalMercator.ToLonLat(fromPosition);
+        var toLatLon = SphericalMercator.ToLonLat(toPosition);
+
+        return Utilities.Haversine.Distance(fromLatLon.X, fromLatLon.Y, toLatLon.X, toLatLon.Y);
+    }
+
+    public (IFeature? startFeature, IFeature? currentFeature) SnapToFeature(Func<MPoint?, IFeature?> getFeaturesToSnapTo)
+    {
+        var result = new List<IFeature>();
+
+        var startFeature = getFeaturesToSnapTo(StartPosition);
+        if (startFeature != null)
+        {
+            result.Add(startFeature);
+            StartPosition = startFeature?.Extent?.Centroid;
+        };
+
+        var currentFeature = getFeaturesToSnapTo(CurrentPosition);
+        if (currentFeature != null)
+        {
+            result.Add(currentFeature);
+            CurrentPosition = currentFeature?.Extent?.Centroid ?? CurrentPosition;
+        }
+
+        return (startFeature, currentFeature);
+    }
+
+    public class MeasureWidgetUpdatedEventArgs(TapType tapType) : EventArgs
+    {
+        public TapType TapType { get; } = tapType;
+    }
+}
