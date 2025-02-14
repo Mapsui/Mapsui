1f25afc910a35a4d536b41397ae42386c04719d6
EmaDozer <emadoeser@gmail.com>
2025-02-14 10:20:13 +0100

    added performance widget to provider

--- Samples/Mapsui.Samples.Common/Maps/Performance/HugeShapeFileWithSimplifierAndClippingSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Performance/HugeShapeFileWithSimplifierAndClippingSample.cs
@@ -1,20 +1,28 @@
-﻿using Mapsui.Layers;
+﻿using Mapsui.Extensions;
+using Mapsui.Layers;
 using Mapsui.Nts.Providers;
+using Mapsui.Rendering.Skia;
+using Mapsui.Rendering.Skia.SkiaWidgets;
 using Mapsui.Samples.Common.Utilities;
 using Mapsui.Styles;
 using Mapsui.Styles.Thematics;
 using Mapsui.Tiling;
 using Mapsui.Tiling.Layers;
+using Mapsui.UI;
+using Mapsui.Widgets;
+using Mapsui.Widgets.InfoWidgets;
 using System.IO;
-using System.Threading.Tasks;
 
 namespace Mapsui.Samples.Common.Maps.Performance;
 
-public class HugeShapeFileWithSimplifierAndClippingSample : ISample
+public class HugeShapeFileWithSimplifierAndClippingSample : IMapControlSample
 {
     private static TileLayer TileLayer = OpenStreetMap.CreateTileLayer();
     private static ILayer ShapeLayer1 = CreateShapeLayer("EZG_KB_LM.shp", "cache1");
     private static ILayer ShapeLayer2 = CreateShapeLayer("modell_ezgs_v02_ohneTalsperren_EPSG3857.shp", "cache2");
+
+    private IMapControl? _mapControl;
+    private readonly Mapsui.Utilities.Performance _performance = new(10);
 
     public HugeShapeFileWithSimplifierAndClippingSample()
     {
@@ -24,6 +32,7 @@ public class HugeShapeFileWithSimplifierAndClippingSample : ISample
 
     public string Name => "Huge Shape File With Simplifier and Clipping";
     public string Category => "Performance";
+
 
     public static Map CreateMap()
     {
@@ -36,7 +45,31 @@ public class HugeShapeFileWithSimplifierAndClippingSample : ISample
         return map;
     }
 
-    public Task<Map> CreateMapAsync() => Task.FromResult(CreateMap());
+    public void Setup(IMapControl mapControl)
+    {
+        _mapControl = mapControl;
+        mapControl.Map = CreateMap();
+        var widget = CreatePerformanceWidget();
+        mapControl.Map.Widgets.Add(widget);
+        mapControl.Performance = _performance;
+        MapRenderer.RegisterWidgetRenderer(typeof(PerformanceWidget), new PerformanceWidgetRenderer());
+    }
+
+    private PerformanceWidget CreatePerformanceWidget() => new(_performance)
+    {
+        HorizontalAlignment = HorizontalAlignment.Left,
+        VerticalAlignment = VerticalAlignment.Top,
+        Margin = new MRect(10),
+        TextSize = 12,
+        TextColor = Color.Black,
+        BackColor = Color.White,
+        Tapped = (s, e) =>
+        {
+            _mapControl?.Performance?.Clear();
+            _mapControl?.RefreshGraphics();
+            return true;
+        }
+    };
 
     private static ILayer CreateShapeLayer(string shapeName, string cacheName)
     {
@@ -89,4 +122,6 @@ public class HugeShapeFileWithSimplifierAndClippingSample : ISample
             return style;
         });
     }
+
+
 }
