da85f6c4de698a1a7401823f2f304df02a8c4a9f
Mikhaylov Mikhail <m.mikhailov@smartdriving.io>
2025-03-05 09:59:16 +0300

    Update Mercator public methods, Add Mercator to Projection

--- Mapsui/Projections/Mercator.cs
+++ Mapsui/Projections/Mercator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Mapsui.Extensions;
 
 namespace Mapsui.Projections;
 
@@ -15,7 +16,12 @@ public class Mercator
     private const double C3 = 0.00000001764564338702;
     private const double C4 = 0.00000000005328478445;
 
-    public static MPoint FromLonLat(double lon, double lat)
+    public static MPoint FromLonLat(MPoint point)
+    {
+        return FromLonLat(point.X, point.Y).ToMPoint();
+    }
+
+    public static (double x, double y) FromLonLat(double lon, double lat)
     {
         var lonRadians = D2R * lon;
         var latRadians = D2R * lat;
@@ -24,10 +30,15 @@ public class Mercator
         //y=a×ln[tan(π/4+φ/2)×((1-e×sinφ)/(1+e×sinφ))^(e/2)]
         var y = Radius * Math.Log(Math.Tan(PiDiv4 + latRadians * 0.5) / Math.Pow(Math.Tan(PiDiv4 + Math.Asin(E * Math.Sin(latRadians)) / 2), E));
 
-        return new MPoint((float)x, (float)y);
+        return new(x, y);
     }
 
-    public static MPoint ToLonLat(double x, double y)
+    public static MPoint ToLonLat(MPoint point)
+    {
+        return ToLonLat(point.X, point.Y).ToMPoint();
+    }
+
+    public static (double lon, double lat) ToLonLat(double x, double y)
     {
         var g = HalfPi - 2 * Math.Atan(1 / Math.Exp(y / Radius));
         var latRadians = g + C1 * Math.Sin(2 * g) + C2 * Math.Sin(4 * g) + C3 * Math.Sin(6 * g) + C4 * Math.Sin(8 * g);
@@ -37,6 +48,6 @@ public class Mercator
         var lon = lonRadians / D2R;
         var lat = latRadians / D2R;
 
-        return new MPoint((float)lon, (float)lat);
+        return new(lon, lat);
     }
 }
