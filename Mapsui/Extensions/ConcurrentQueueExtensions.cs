f6e898b2b620149bbdc78fb491fa84cb51dc508b
pauldendulk <pauldendulk@gmail.com>
2025-03-14 10:41:48 +0100

    Return T to allow fluent syntax

--- Mapsui/Extensions/ConcurrentQueueExtensions.cs
+++ Mapsui/Extensions/ConcurrentQueueExtensions.cs
@@ -7,9 +7,10 @@ public static class ConcurrentQueueExtensions
 {
     // This method was added to make the api more compatible with the previous 
     // List based Widgets library
-    public static void Add<T>(this ConcurrentQueue<T> queue, T item)
+    public static T Add<T>(this ConcurrentQueue<T> queue, T item)
     {
         queue.Enqueue(item);
+        return item;
     }
 
     public static void Clear<T>(this ConcurrentQueue<T> queue)
