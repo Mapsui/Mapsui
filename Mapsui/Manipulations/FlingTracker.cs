ad0cc4a9b71b7f0418f6448cdad1183194d17876
pauldendulk <pauldendulk@gmail.com>
2025-05-24 13:18:42 +0200

    Limit fling velocity

--- Mapsui/Manipulations/FlingTracker.cs
+++ Mapsui/Manipulations/FlingTracker.cs
@@ -1,5 +1,4 @@
-﻿#pragma warning disable IDE0005
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace Mapsui.Manipulations;
@@ -8,6 +7,7 @@ public class FlingTracker
 {
     private const int _maxSize = 50;
     private const long _maxTicks = 200 * 10000;  // Use only events from the last 200 ms
+    private const double _maxVelocity = 4000.0;   // When testing on WPF 4000 felt okay. Not sure if this is a good value in general.
 
     private readonly Queue<(double x, double y, long time)> _events;
 
@@ -57,8 +57,8 @@ public class FlingTracker
             if (now - lastTime < _maxTicks)
             {
                 // Calc velocity in pixel per sec
-                distanceX += (nowX - lastX) * 10000000;// / (nowTime - lastTime);
-                distanceY += (nowY - lastY) * 10000000;// / (nowTime - lastTime);
+                distanceX += (nowX - lastX) * 10000000;
+                distanceY += (nowY - lastY) * 10000000;
             }
 
             finalTime = nowTime;
@@ -73,9 +73,25 @@ public class FlingTracker
     {
         var (velocityX, velocityY) = CalcVelocity(DateTime.Now.Ticks);
 
+        // Only fling if the velocity is above a threshold (e.g., 200)
         if (Math.Abs(velocityX) <= 200 && Math.Abs(velocityY) <= 200)
             return;
 
+        // In some cases overloading of the app can cause incorrect velocity values which 
+        // could result in extreme speeds. Therefor we limit the velocity to a maximum value.
+        (velocityX, velocityY) = LimitVelocity(velocityX, velocityY, _maxVelocity);
+
         onFling(velocityX, velocityY);
+    }
+
+    private static (double vx, double vy) LimitVelocity(double vx, double vy, double maxVelocity)
+    {
+        var magnitude = Math.Sqrt(vx * vx + vy * vy);
+        if (magnitude > maxVelocity && magnitude > 0)
+        {
+            var scale = maxVelocity / magnitude;
+            return (vx * scale, vy * scale);
+        }
+        return (vx, vy);
     }
 }
