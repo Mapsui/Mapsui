9397ef65e62fc332abd4516e4654328dbc18fc3d
pauldendulk <pauldendulk@gmail.com>
2025-06-07 10:05:32 +0200

    Improve the dynamic symbol scale sample

--- Samples/Mapsui.Samples.Common/Maps/Demo/DynamicSymbolScaleStyleSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Demo/DynamicSymbolScaleStyleSample.cs
@@ -1,43 +1,88 @@
 ﻿using Mapsui.Layers;
+using Mapsui.Nts.Extensions;
+using Mapsui.Providers;
 using Mapsui.Samples.Common.DataBuilders;
 using Mapsui.Styles;
 using Mapsui.Styles.Thematics;
-using Mapsui.Tiling;
+using NetTopologySuite.Geometries;
 using System.Threading.Tasks;
 
 namespace Mapsui.Samples.Common.Maps.Demo;
 
 public class DynamicSymbolScaleStyleSample : ISample
 {
-    private const double _resolutionFlippingPoint = 300.0;
-    private const string _imageSource = "embedded://Mapsui.Samples.Common.Images.arrow.svg";
+    private const double _level1 = 32000.0;
+    private const double _level2 = 16000.0;
+    private const double _level3 = 8000.0;
 
     public string Name => "Dynamic Symbol Scale Style";
-    public string Category => "Styles";
+    public string Category => "1";
 
     public Task<Map> CreateMapAsync()
     {
         var map = new Map();
-        map.Layers.Add(OpenStreetMap.CreateTileLayer());
+        map.Layers.Add(CreatePolygonLayer());
         map.Layers.Add(CreateLayerWithDynamicScaleStyle(map));
+        map.Navigator.OverrideResolutions = new[] { _level1, _level2, _level3 };
+        map.Navigator.OverrideZoomBounds = new MMinMax(_level1, _level3);
+        map.Navigator.ZoomToLevel(0);
         return Task.FromResult(map);
     }
 
     private static MemoryLayer CreateLayerWithDynamicScaleStyle(Map map) => new()
     {
         Name = "Dynamic Symbol Scale",
-        Features = RandomPointsBuilder.CreateRandomFeatures(map.Extent, 1000),
+        Features = RandomPointsBuilder.CreateRandomFeatures(map.Extent!.Grow(-map.Extent.Width * 0.94), 50, seed: 245),
         Style = CreateDynamicSymbolScaleStyle()
     };
 
     private static ThemeStyle CreateDynamicSymbolScaleStyle()
     {
-        return new ThemeStyle((f, v) => CreateImageStyle(v));
+        var fill = new Brush(new Color(242, 229, 29, 255));
+        var pen = new Pen(Color.DimGray, 1.6);
+
+        var smallStyle = new SymbolStyle { SymbolScale = 0.75, Fill = fill, Outline = pen };
+        var mediumStyle = new SymbolStyle { SymbolScale = 1, Fill = fill, Outline = pen };
+        var bigStyle = new SymbolStyle { SymbolScale = 1.5, Fill = fill, Outline = pen };
+
+        return new ThemeStyle((f, v) =>
+        {
+            return v.Resolution switch
+            {
+                > _level2 => smallStyle,
+                > _level3 => mediumStyle,
+                _ => bigStyle
+            };
+        });
     }
 
-    private static ImageStyle CreateImageStyle(Viewport v) => new()
+    public static ILayer CreatePolygonLayer()
     {
-        Image = new Image { Source = _imageSource },
-        SymbolScale = v.Resolution > _resolutionFlippingPoint ? 0.5 : 2.0,
-    };
+        return new Layer("Polygons")
+        {
+            DataSource = new MemoryProvider(CreateSquarePolygon(10000000).ToFeature()),
+            Style = new VectorStyle
+            {
+                Fill = new Brush(Color.LightGray),
+                Outline = new Pen
+                {
+                    Color = Color.DimGray,
+                    Width = 3
+                }
+            }
+        };
+    }
+
+    private static Polygon CreateSquarePolygon(int width)
+    {
+        int halfWidth = width / 2;
+        return new Polygon(new LinearRing(new[]
+        {
+            new Coordinate(-halfWidth, -halfWidth),
+            new Coordinate(-halfWidth, halfWidth),
+            new Coordinate(halfWidth, halfWidth),
+            new Coordinate(halfWidth, -halfWidth),
+            new Coordinate(-halfWidth, -halfWidth) // Closing the ring
+        }));
+    }
 }
