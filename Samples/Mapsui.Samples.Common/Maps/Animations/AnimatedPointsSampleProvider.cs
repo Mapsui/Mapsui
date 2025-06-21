a0e474263b863796ee45f398bb9657fdfb709603
pauldendulk <pauldendulk@gmail.com>
2025-06-21 14:04:17 +0200

    Make AnimatedPointSample animate on start

--- Samples/Mapsui.Samples.Common/Maps/Animations/AnimatedPointsSampleProvider.cs
+++ Samples/Mapsui.Samples.Common/Maps/Animations/AnimatedPointsSampleProvider.cs
@@ -17,51 +17,57 @@ namespace Mapsui.Samples.Common.Maps.Animations;
 
 internal class AnimatedPointsSampleProvider : MemoryProvider, IDynamic, IDisposable
 {
-    // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
-    // ReSharper disable once NotAccessedField.Local
     private readonly Timer _timer;
     private readonly Random _random = new(0);
-    private IEnumerable<PointFeature> _previousFeatures = new List<PointFeature>();
+    private List<PointFeature> _previousFeatures = new();
 
     public AnimatedPointsSampleProvider()
     {
-        _timer = new Timer(_ => DataHasChanged(), this, 0, 2000);
+        _timer = new Timer(_ => DataHasChanged(), this, 0, 1600);
     }
 
     public event EventHandler? DataChanged;
 
     public override Task<IEnumerable<IFeature>> GetFeaturesAsync(FetchInfo fetchInfo)
     {
+        var features = CreateNewFeatures(fetchInfo, _random, _previousFeatures);
+        _previousFeatures = MergeWithPreviousFeatures(_previousFeatures, features);
+        return Task.FromResult((IEnumerable<IFeature>)features);
+    }
+
+    private static List<PointFeature> CreateNewFeatures(FetchInfo fetchInfo, Random random, List<PointFeature> previousFeatures)
+    {
         var features = new List<PointFeature>();
-        var points = RandomPointsBuilder.GenerateRandomPoints(fetchInfo.Extent, 10, _random).ToList();
+        var points = RandomPointsBuilder.GenerateRandomPoints(fetchInfo.Extent, 10, random).ToList();
         var count = 0;
-        var random = _random.Next(points.Count);
+        var randomItemToSkip = random.Next(points.Count);
 
         foreach (var point in points)
         {
             count++;
-            if (count == random) continue; // skip a random element to test robustness
+            if (count == randomItemToSkip)
+                continue; // Skip a random element to test robustness.
 
             var countAsString = count.ToString(CultureInfo.InvariantCulture);
             features.Add(new PointFeature(point)
             {
                 ["ID"] = countAsString,
-                ["rotation"] = AngleOf(point, FindPreviousPosition(countAsString)) - 90
+                ["rotation"] = AngleOf(point, FindPreviousPosition(countAsString, previousFeatures), random) - 90
             });
         }
 
-        _previousFeatures = features;
-        return Task.FromResult((IEnumerable<IFeature>)features);
+        return features;
     }
 
-    private MPoint? FindPreviousPosition(string countAsString)
+    private static MPoint? FindPreviousPosition(string countAsString, List<PointFeature> previousFeatures)
     {
-        return _previousFeatures.FirstOrDefault(f => f["ID"]?.ToString() == countAsString)?.Point;
+        return previousFeatures.FirstOrDefault(f => f["ID"]?.ToString() == countAsString)?.Point;
     }
 
-    public static double AngleOf(MPoint point1, MPoint? point2)
+    public static double AngleOf(MPoint point1, MPoint? point2, Random random)
     {
-        if (point2 == null) return 0;
+        if (point2 == null)
+            return random.Next(360);
         double result = Algorithms.RadiansToDegrees(Math.Atan2(point1.Y - point2.Y, point2.X - point1.X));
         return (result < 0) ? (360.0 + result) : result;
     }
@@ -88,5 +94,14 @@ internal class AnimatedPointsSampleProvider : MemoryProvider, IDynamic, IDisposa
         {
             _timer.Dispose();
         }
+    }
+    private static List<PointFeature> MergeWithPreviousFeatures(List<PointFeature> previousFeatures, List<PointFeature> newFeatures)
+    {
+        // Some features are missing in the new list (to test robustness). We want to store the missing ones as well.
+        return newFeatures
+            .Concat(previousFeatures)
+            .GroupBy(f => f["ID"])
+            .Select(g => g.First())
+            .ToList();
     }
 }
