ed33bf5400f0eb3e1abfc026c11a39b751f9d322
pauldendulk <pauldendulk@gmail.com>
2025-07-07 09:46:38 +0200

    Rename TileFetchDispatcher to TileFetchJobPlanner because dispatcher indicates the actual exection, but this is up to the central fetcher

--- Mapsui.Tiling/Layers/TileLayer.cs
+++ Mapsui.Tiling/Layers/TileLayer.cs
@@ -30,7 +30,7 @@ public class TileLayer : BaseLayer, IFetchJobSource, IDisposable
     private readonly int _minExtraTiles;
     private readonly int _maxExtraTiles;
     private int _numberTilesNeeded;
-    private readonly TileFetchDispatcher _tileFetchDispatcher;
+    private readonly TileFetchJobPlanner _fetchJobPlanner;
     private readonly MRect? _extent;
     private readonly HttpClient _httpClient = new();
 
@@ -62,10 +62,10 @@ public class TileLayer : BaseLayer, IFetchJobSource, IDisposable
         _renderFetchStrategy = renderFetchStrategy ?? new RenderFetchStrategy();
         _minExtraTiles = minExtraTiles;
         _maxExtraTiles = maxExtraTiles;
-        _tileFetchDispatcher = new TileFetchDispatcher(MemoryCache, _tileSource.Schema,
+        _fetchJobPlanner = new TileFetchJobPlanner(MemoryCache, _tileSource.Schema,
             fetchTileAsFeature ?? ToFeatureAsync, dataFetchStrategy, this);
-        _tileFetchDispatcher.DataChanged += TileFetchDispatcherOnDataChanged;
-        _tileFetchDispatcher.PropertyChanged += TileFetchDispatcherOnPropertyChanged;
+        _fetchJobPlanner.DataChanged += TileFetchJobPlannerOnDataChanged;
+        _fetchJobPlanner.PropertyChanged += TileFetchJobPlannerOnPropertyChanged;
         // There should be a way to override the application wide default user agent.
         _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", HttpClientTools.GetDefaultApplicationUserAgent());
     }
@@ -101,7 +101,7 @@ public class TileLayer : BaseLayer, IFetchJobSource, IDisposable
 
     public FetchJob[] GetFetchJobs(int activeFetches, int availableFetchSlots)
     {
-        return _tileFetchDispatcher.GetFetchJobs(activeFetches, availableFetchSlots);
+        return _fetchJobPlanner.GetFetchJobs(activeFetches, availableFetchSlots);
     }
 
     protected override void Dispose(bool disposing)
@@ -115,23 +115,23 @@ public class TileLayer : BaseLayer, IFetchJobSource, IDisposable
         base.Dispose(disposing);
     }
 
-    private void TileFetchDispatcherOnPropertyChanged(object? sender, PropertyChangedEventArgs propertyChangedEventArgs)
+    private void TileFetchJobPlannerOnPropertyChanged(object? sender, PropertyChangedEventArgs propertyChangedEventArgs)
     {
         if (propertyChangedEventArgs.PropertyName == nameof(Busy))
-            Busy = _tileFetchDispatcher.Busy;
+            Busy = _fetchJobPlanner.Busy;
     }
 
     private void UpdateMemoryCacheMinAndMax()
     {
         if (_minExtraTiles < 0 || _maxExtraTiles < 0) return;
-        if (_numberTilesNeeded == _tileFetchDispatcher.NumberTilesNeeded) return;
+        if (_numberTilesNeeded == _fetchJobPlanner.NumberTilesNeeded) return;
 
-        _numberTilesNeeded = _tileFetchDispatcher.NumberTilesNeeded;
+        _numberTilesNeeded = _fetchJobPlanner.NumberTilesNeeded;
         MemoryCache.MinTiles = _numberTilesNeeded + _minExtraTiles;
         MemoryCache.MaxTiles = _numberTilesNeeded + _maxExtraTiles;
     }
 
-    private void TileFetchDispatcherOnDataChanged(object? sender, Exception? ex)
+    private void TileFetchJobPlannerOnDataChanged(object? sender, Exception? ex)
     {
         OnDataChanged(new DataChangedEventArgs(ex, Name));
     }
@@ -173,7 +173,7 @@ public class TileLayer : BaseLayer, IFetchJobSource, IDisposable
     public virtual void ViewportChanged(FetchInfo fetchInfo)
     {
         Busy = true;
-        _tileFetchDispatcher.ViewportChanged(fetchInfo);
+        _fetchJobPlanner.ViewportChanged(fetchInfo);
     }
 
     protected virtual void OnRefreshDataRequest()
--- Tests/Mapsui.Tests/Fetcher/FetchMachineTests.cs
+++ Tests/Mapsui.Tests/Fetcher/FetchMachineTests.cs
@@ -29,22 +29,22 @@ public class FetchMachineTests
         // Arrange
         var tileSource = new CountingTileSource();
         using var cache = new MemoryCache<IFeature?>();
-        var fetchDispatcher = new TileFetchDispatcher(cache, tileSource.Schema, async tileInfo => await TileToFeatureAsync(tileSource, tileInfo), new MinimalDataFetchStrategy(), _layer);
+        var fetchJobPlanner = new TileFetchJobPlanner(cache, tileSource.Schema, async tileInfo => await TileToFeatureAsync(tileSource, tileInfo), new MinimalDataFetchStrategy(), _layer);
         var fetchMachine = new FetchMachine();
         var level = 3;
         var expectedTiles = 64;
         var fetchInfo = new FetchInfo(new MSection(tileSource.Schema.Extent.ToMRect(), tileSource.Schema.Resolutions[level].UnitsPerPixel));
-        fetchDispatcher.ViewportChanged(fetchInfo);
+        fetchJobPlanner.ViewportChanged(fetchInfo);
 
         // Act
         do
         {
-            var requests = fetchDispatcher.GetFetchJobs(0, 8);
+            var requests = fetchJobPlanner.GetFetchJobs(0, 8);
             foreach (var request in requests)
             {
                 await request.FetchFunc();
             }
-        } while (fetchDispatcher.Busy);
+        } while (fetchJobPlanner.Busy);
 
         // Assert
         ClassicAssert.AreEqual(expectedTiles, tileSource.CountByTile.Keys.Count);
@@ -58,7 +58,7 @@ public class FetchMachineTests
         // Arrange
         var tileSource = new CountingTileSource();
         using var cache = new MemoryCache<IFeature?>();
-        var fetchDispatcher = new TileFetchDispatcher(
+        var fetchJobPlanner = new TileFetchJobPlanner(
             cache,
             tileSource.Schema,
             async tileInfo => await TileToFeatureAsync(tileSource, tileInfo),
@@ -67,29 +67,29 @@ public class FetchMachineTests
         var level = 3;
         var expectedTiles = 64;
         var fetchInfo = new FetchInfo(new MSection(tileSource.Schema.Extent.ToMRect(), tileSource.Schema.Resolutions[level].UnitsPerPixel));
-        fetchDispatcher.ViewportChanged(fetchInfo);
+        fetchJobPlanner.ViewportChanged(fetchInfo);
 
         // Act (first round)
         do
         {
-            var requests = fetchDispatcher.GetFetchJobs(0, 8);
+            var requests = fetchJobPlanner.GetFetchJobs(0, 8);
             foreach (var request in requests)
             {
                 await request.FetchFunc();
             }
-        } while (fetchDispatcher.Busy);
+        } while (fetchJobPlanner.Busy);
 
         var countAfterFirstTry = tileSource.CountByTile.Keys.Count;
 
         // Act (second round)
         do
         {
-            var requests = fetchDispatcher.GetFetchJobs(0, 8);
+            var requests = fetchJobPlanner.GetFetchJobs(0, 8);
             foreach (var request in requests)
             {
                 await request.FetchFunc();
             }
-        } while (fetchDispatcher.Busy);
+        } while (fetchJobPlanner.Busy);
 
         // Assert
         ClassicAssert.AreEqual(countAfterFirstTry, tileSource.CountByTile.Values.Sum());
@@ -104,7 +104,7 @@ public class FetchMachineTests
         // Arrange
         var tileSource = new NullTileSource();
         using var cache = new MemoryCache<IFeature?>();
-        var fetchDispatcher = new TileFetchDispatcher(
+        var fetchJobPlanner = new TileFetchJobPlanner(
             cache,
             tileSource.Schema,
             async tileInfo => await TileToFeatureAsync(tileSource, tileInfo),
@@ -113,27 +113,27 @@ public class FetchMachineTests
         var level = 3;
         var tilesInLevel = 64;
         var fetchInfo = new FetchInfo(new MSection(tileSource.Schema.Extent.ToMRect(), tileSource.Schema.Resolutions[level].UnitsPerPixel));
-        fetchDispatcher.ViewportChanged(fetchInfo);
+        fetchJobPlanner.ViewportChanged(fetchInfo);
 
         // Act (first round)
         do
         {
-            var requests = fetchDispatcher.GetFetchJobs(0, 8);
+            var requests = fetchJobPlanner.GetFetchJobs(0, 8);
             foreach (var request in requests)
             {
                 await request.FetchFunc();
             }
-        } while (fetchDispatcher.Busy);
+        } while (fetchJobPlanner.Busy);
 
         // Act (second round)
         do
         {
-            var requests = fetchDispatcher.GetFetchJobs(0, 8);
+            var requests = fetchJobPlanner.GetFetchJobs(0, 8);
             foreach (var request in requests)
             {
                 await request.FetchFunc();
             }
-        } while (fetchDispatcher.Busy);
+        } while (fetchJobPlanner.Busy);
 
         // Assert
         ClassicAssert.AreEqual(tilesInLevel, tileSource.TotalCount);
@@ -145,7 +145,7 @@ public class FetchMachineTests
         // Arrange
         var tileSource = new FailingTileSource();
         using var cache = new MemoryCache<IFeature?>();
-        var fetchDispatcher = new TileFetchDispatcher(
+        var fetchJobPlanner = new TileFetchJobPlanner(
             cache,
             tileSource.Schema,
             async tileInfo => await TileToFeatureAsync(tileSource, tileInfo),
@@ -158,27 +158,27 @@ public class FetchMachineTests
 
 
         // Act (first round)
-        fetchDispatcher.ViewportChanged(fetchInfo);
+        fetchJobPlanner.ViewportChanged(fetchInfo);
         do
         {
-            var requests = fetchDispatcher.GetFetchJobs(0, 8);
+            var requests = fetchJobPlanner.GetFetchJobs(0, 8);
             foreach (var request in requests)
             {
                 await request.FetchFunc();
             }
-        } while (fetchDispatcher.Busy);
+        } while (fetchJobPlanner.Busy);
 
 
         // Act (second round)
-        fetchDispatcher.ViewportChanged(fetchInfo);
+        fetchJobPlanner.ViewportChanged(fetchInfo);
         do
         {
-            var requests = fetchDispatcher.GetFetchJobs(0, 8);
+            var requests = fetchJobPlanner.GetFetchJobs(0, 8);
             foreach (var request in requests)
             {
                 await request.FetchFunc();
             }
-        } while (fetchDispatcher.Busy);
+        } while (fetchJobPlanner.Busy);
 
         // Assert
         ClassicAssert.AreEqual(tilesInLevel * 2, tileSource.TotalCount); // tried all tiles twice
@@ -191,7 +191,7 @@ public class FetchMachineTests
         var tileSource = new SometimesFailingTileSource();
         var tileSchema = new GlobalSphericalMercator();
         using var cache = new MemoryCache<IFeature?>();
-        var fetchDispatcher = new TileFetchDispatcher(
+        var fetchJobPlanner = new TileFetchJobPlanner(
             cache,
             tileSource.Schema,
             async tileInfo => await TileToFeatureAsync(tileSource, tileInfo),
@@ -200,29 +200,29 @@ public class FetchMachineTests
         var level = 3;
         var tilesInLevel = 64;
         var fetchInfo = new FetchInfo(new MSection(tileSchema.Extent.ToMRect(), tileSchema.Resolutions[level].UnitsPerPixel));
-        fetchDispatcher.ViewportChanged(fetchInfo);
+        fetchJobPlanner.ViewportChanged(fetchInfo);
 
         // Act (first round)
         do
         {
-            var requests = fetchDispatcher.GetFetchJobs(0, 8);
+            var requests = fetchJobPlanner.GetFetchJobs(0, 8);
             foreach (var request in requests)
             {
                 await request.FetchFunc();
             }
-        } while (fetchDispatcher.Busy);
+        } while (fetchJobPlanner.Busy);
 
         var tileCountAfterFirstBatch = tileSource.TotalCount;
 
         // Act (second round)
         do
         {
-            var requests = fetchDispatcher.GetFetchJobs(0, 8);
+            var requests = fetchJobPlanner.GetFetchJobs(0, 8);
             foreach (var request in requests)
             {
                 await request.FetchFunc();
             }
-        } while (fetchDispatcher.Busy);
+        } while (fetchJobPlanner.Busy);
 
         // Assert
         ClassicAssert.GreaterOrEqual(tileSource.TotalCount, tileCountAfterFirstBatch);
