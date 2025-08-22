3b1a572c69a062f055db6152e2d30c61e73fe85d
pauldendulk <pauldendulk@gmail.com>
2025-08-22 13:47:57 +0200

    Rename IRenderer to IMapRenderer

--- Mapsui/Rendering/IRenderer.cs
+++ Mapsui/Rendering/IRenderer.cs
@@ -8,12 +8,7 @@ using Mapsui.Widgets;
 
 namespace Mapsui.Rendering;
 
-public interface IRenderer
+// We renamed the IRenderer to IMapRenderer but keep the IRenderer for compatibility.
+public interface IRenderer : IMapRenderer
 {
-    void Render(object target, Viewport viewport, IEnumerable<ILayer> layers, IEnumerable<IWidget> widgets, RenderService renderService, Color? background = null);
-    MemoryStream RenderToBitmapStream(Viewport viewport, IEnumerable<ILayer> layers,
-        RenderService renderService, Color? background = null, float pixelDensity = 1, IEnumerable<IWidget>? widgets = null, RenderFormat renderFormat = RenderFormat.Png, int quality = 100);
-    bool TryGetWidgetRenderer(Type widgetType, out IWidgetRenderer? widgetRenderer);
-    bool TryGetStyleRenderer(Type widgetType, out IStyleRenderer? widgetRenderer);
-    MapInfo GetMapInfo(ScreenPosition screenPosition, Viewport viewport, IEnumerable<ILayer> layers, RenderService renderService, int margin = 0);
 }
