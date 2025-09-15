ea1b31999d078647796349aac2db266cb513789e
pauldendulk <pauldendulk@gmail.com>
2025-09-15 17:18:48 +0200

    Make sample IDisposable

--- Samples/Mapsui.Samples.Common/Maps/Tiles/VexTileMvtSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Tiles/VexTileMvtSample.cs
@@ -5,6 +5,7 @@ using Mapsui.Tiling;
 using Mapsui.Tiling.Fetcher;
 using Mapsui.Tiling.Layers;
 using SQLite;
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using VexTile.Common.Enums;
@@ -15,42 +16,58 @@ using VexTile.Renderer.Mvt.AliFlux.Sources;
 
 namespace Mapsui.Samples.Common.Maps.Tiles;
 
-public class RasterizedVectorTilesSample : ISample
+public sealed class RasterizedVectorTilesSample : ISample, IDisposable
 {
+    SqliteDataSource _sqliteDataSource;
+
     static RasterizedVectorTilesSample()
     {
         MbTilesDeployer.CopyEmbeddedResourceToFile("zurich.mbtiles");
     }
-    public string Name => "Rasterized Vector Tiles";
-    public string Category => "Tiles";
-    public Task<Map> CreateMapAsync()
+
+    public RasterizedVectorTilesSample()
     {
-        return Task.FromResult(CreateMap());
+        _sqliteDataSource = CreateSqliteDataSource();
     }
 
-    public static Map CreateMap()
+    public string Name => "Rasterized Vector Tiles";
+    public string Category => "Tiles";
+
+    public Task<Map> CreateMapAsync()
+    {
+        return Task.FromResult(CreateMap(_sqliteDataSource));
+    }
+
+    public static Map CreateMap(SqliteDataSource sqliteDataSource)
     {
         var map = new Map();
 
         map.Layers.Add(OpenStreetMap.CreateTileLayer());
-        map.Layers.Add(CreateLayer());
+        map.Layers.Add(CreateLayer(sqliteDataSource));
 
         return map;
     }
 
-    private static TileLayer CreateLayer()
+    private static TileLayer CreateLayer(SqliteDataSource sqliteDataSource)
     {
-        var path = Path.Combine(MbTilesDeployer.MbTilesLocation, "zurich.mbtiles");
-        SQLiteConnectionString val = new SQLiteConnectionString(path, (SQLiteOpenFlags)1, false);
-#pragma warning disable IDISP001 // Dispose created
-        var sqliteDataSource = new SqliteDataSource(val);
-#pragma warning restore IDISP001 // Dispose created
 
         var tileSource = new VectorTileSourceWrapper(sqliteDataSource);
         return new TileLayer(tileSource, dataFetchStrategy: new DataFetchStrategy()) // DataFetchStrategy prefetches tiles from higher levels
         {
             Name = "VexTile.TileSource.Mvt",
         };
+    }
+
+    private static SqliteDataSource CreateSqliteDataSource()
+    {
+        var path = Path.Combine(MbTilesDeployer.MbTilesLocation, "zurich.mbtiles");
+        SQLiteConnectionString val = new SQLiteConnectionString(path, (SQLiteOpenFlags)1, false);
+        return new SqliteDataSource(val);
+    }
+
+    void IDisposable.Dispose()
+    {
+        _sqliteDataSource.Dispose();
     }
 
     private sealed class VectorTileSourceWrapper : ILocalTileSource
