a56468ca59b65e0936feb82c0aa84470a439dbfb
pauldendulk <pauldendulk@gmail.com>
2025-09-15 08:50:27 +0200

    Revert removing unused field because these fields come from the DOM, it is not something we invented

--- Mapsui.UI.Blazor/Extensions/BoundingClientRect.cs
+++ Mapsui.UI.Blazor/Extensions/BoundingClientRect.cs
@@ -2,6 +2,12 @@
 
 public class BoundingClientRect
 {
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Width { get; set; }
+    public double Height { get; set; }
     public double Top { get; set; }
+    public double Right { get; set; }
+    public double Bottom { get; set; }
     public double Left { get; set; }
 }
