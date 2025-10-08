ec4a3bb11b4c15b29db7f752485c0406834cee31
pauldendulk <pauldendulk@gmail.com>
2025-10-08 21:07:37 +0200

    Remove side bar

--- Samples/Mapsui.Samples.Blazor/Program.cs
+++ Samples/Mapsui.Samples.Blazor/Program.cs
@@ -1,7 +1,10 @@
 #pragma warning disable IDE0005 // Using directive is unnecessary. The dotnet format on the build server is just wrong
 using Mapsui.Samples.Blazor;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using System.Linq;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +12,17 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// Configure response compression
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes
+        .Where(m => m != "text/html"); // Exclude HTML files from compression
+});
+
 Mapsui.Logging.Logger.LogDelegate += (l, m, e) => Console.WriteLine(m + e?.Message);
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+
+// Only keep the code that is valid for Blazor WebAssembly.
+await app.RunAsync();
