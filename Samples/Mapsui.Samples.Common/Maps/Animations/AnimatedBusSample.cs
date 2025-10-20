4739fafcf0a8d310cfebf2f7a969c4e874ee5dd7
pauldendulk <pauldendulk@gmail.com>
2025-10-20 13:11:40 +0200

    Move those classes in the samples that are part of other samples into those samples because we strive to allow copy-pasting our samples (and make it work right away)

--- Samples/Mapsui.Samples.Common/Maps/Animations/AnimatedBusSample.cs
+++ Samples/Mapsui.Samples.Common/Maps/Animations/AnimatedBusSample.cs
@@ -1,9 +1,14 @@
-﻿using Mapsui.Layers.AnimatedLayers;
+﻿using Mapsui.Extensions;
+using Mapsui.Layers;
+using Mapsui.Layers.AnimatedLayers;
+using Mapsui.Projections;
+using Mapsui.Providers;
 using Mapsui.Styles;
-using System.Threading.Tasks;
 using Mapsui.Tiling;
-
-#pragma warning disable IDISP004 // Don't ignore created IDisposable
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Mapsui.Samples.Common.Maps.Animations;
 
@@ -32,5 +37,52 @@ public class AnimatedBusSample : ISample
         map.Navigator.CenterOnAndZoomTo(new MPoint(2776952, 8442653), map.Navigator.Resolutions[18]);
 
         return Task.FromResult(map);
+    }
+
+    internal sealed class BusPointProvider : MemoryProvider, IDynamic, IDisposable
+    {
+        public event EventHandler? DataChanged;
+
+        private readonly PeriodicTimer _timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+
+        public BusPointProvider()
+        {
+            Catch.TaskRun(RunTimerAsync);
+        }
+
+        private (double Lon, double Lat) _previousCoordinates = (24.945831, 60.192059);
+        private async Task RunTimerAsync()
+        {
+            while (true)
+            {
+                await _timer.WaitForNextTickAsync();
+
+                _previousCoordinates = (_previousCoordinates.Lon + 0.00005, _previousCoordinates.Lat + 0.00005);
+
+                OnDataChanged();
+            }
+        }
+
+        void IDynamic.DataHasChanged()
+        {
+            OnDataChanged();
+        }
+
+        private void OnDataChanged()
+        {
+            DataChanged?.Invoke(this, new EventArgs());
+        }
+
+        public override Task<IEnumerable<IFeature>> GetFeaturesAsync(FetchInfo fetchInfo)
+        {
+            var busFeature = new PointFeature(SphericalMercator.FromLonLat(_previousCoordinates.Lon, _previousCoordinates.Lat).ToMPoint());
+            busFeature["ID"] = "bus";
+            return Task.FromResult((IEnumerable<IFeature>)[busFeature]);
+        }
+
+        public void Dispose()
+        {
+            _timer.Dispose();
+        }
     }
 }
