f7cb1ae9df923422c39d5db3f9abea61f71f1b73
pauldendulk <pauldendulk@gmail.com>
2025-12-02 13:31:53 +0100

    Check if the NTS Envelope is in the null state, if it is it can return an incorrect bouding box where max is smaller than min

--- Mapsui.Nts/Extensions/EnvelopeExtensions.cs
+++ Mapsui.Nts/Extensions/EnvelopeExtensions.cs
@@ -4,8 +4,10 @@ namespace Mapsui.Nts.Extensions;
 
 public static class EnvelopeExtensions
 {
-    public static MRect ToMRect(this Envelope envelope)
+    public static MRect? ToMRect(this Envelope envelope)
     {
+        if (envelope.IsNull)
+            return null;
         return new MRect(envelope.MinX, envelope.MinY, envelope.MaxX, envelope.MaxY);
     }
 }
